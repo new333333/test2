11120f4efa72fa5373a828d701b0b00926cf410a
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:06:16 +0000

    add more default fields to the index

--- ssf/main/src/com/sitescape/ef/module/folder/index/IndexUtils.java
+++ ssf/main/src/com/sitescape/ef/module/folder/index/IndexUtils.java
@@ -22,7 +22,11 @@ public class IndexUtils {
     
     public static final String ALL_TEXT_FIELD = "_allText";
     public static final String CREATION_DATE_FIELD = "_creationDate";
+    public static final String MODIFICATION_DATE_FIELD = "_modificationDate";
+    public static final String CREATORID_FIELD = "_creatorId";
+    public static final String MODIFICATIONID_FIELD = "_modificationId";
     public static final String READ_ACL_FIELD = "_readAcl";
+    public static final String DOCID_FIELD = "_docId";
     public static final String COMMAND_DEFINITION_FIELD = "_commandDef";
     public static final String TITLE_FIELD = "_title";
     public static final String TITLE1_FIELD = "_title1";
@@ -46,11 +50,35 @@ public class IndexUtils {
         doc.add(creationDateField);
     }
     
+    public static void addModificationDate(Document doc, Entry entry) {
+    	// Add modification-date field
+    	Field modificationDateField = Field.Keyword(MODIFICATION_DATE_FIELD, entry.getModification().getDate());
+    	doc.add(modificationDateField);
+    }
+    
     public static void addCommandDefinition(Document doc, Entry entry) {
         Field cdefField = Field.Keyword(COMMAND_DEFINITION_FIELD, entry.getEntryDef().getId());
         doc.add(cdefField);
     }
- 
+        
+    public static void addCreationPrincipleId(Document doc, Entry entry) {
+    	//Add the id of the creator (no, not that one...)
+        Field creationIdField = Field.Keyword(CREATORID_FIELD, entry.getCreation().getPrincipal().getStringId());
+        doc.add(creationIdField);
+    }   
+
+    public static void addModificationPrincipleId(Document doc, Entry entry) {
+    	//Add the id of the creator (no, not that one...)
+        Field modificationIdField = Field.Keyword(MODIFICATIONID_FIELD, entry.getModification().getPrincipal().getStringId());
+        doc.add(modificationIdField);
+    }   
+
+    public static void addDocId(Document doc, Entry entry) {
+    	//Add the id of the creator (no, not that one...)
+        Field docIdField = Field.Keyword(DOCID_FIELD, entry.getStringId());
+        doc.add(docIdField);
+    }       
+
     public static void addFolderId(Document doc, Folder folder) {
     	//Add the folder id to the document in the index
         Field folderField = Field.Keyword(FOLDERID_FIELD, folder.getId().toString());
@@ -68,10 +96,13 @@ public class IndexUtils {
 	        }
 	        // I'm not sure if putting together a long string value is more
 	        // efficient than processing multiple short strings... We will see.
-	        racField = new Field(READ_ACL_FIELD, pIds.toString(), false, true, true);
+	        if (pIds.length() != 0)
+	          racField = new Field(READ_ACL_FIELD, pIds.toString(), true, true, true);
+	        else
+	          racField = new Field(READ_ACL_FIELD, READ_ACL_ALL, true, true, true);
         }
         else {
-            racField = new Field(READ_ACL_FIELD, READ_ACL_ALL, false, true, false);
+            racField = new Field(READ_ACL_FIELD, READ_ACL_ALL, true, true, true);
         }
         
         doc.add(racField);
