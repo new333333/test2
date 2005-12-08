f99ad5ccd79b2b72e23f481288381e69e5ffbbb3
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 00:50:04 +0000

    Added more

--- trunk/ssf/main/src/com/sitescape/ef/util/Constants.java
+++ trunk/ssf/main/src/com/sitescape/ef/util/Constants.java
@@ -5,4 +5,14 @@ public interface Constants {
 	public static final String ID = "id";
 	
 	public static final String ANONYMOUS_USER_NAME = "anonymous";
+	
+	public static final String SLASH = "/";
+	
+	public static final String QUESTION = "?";
+	
+	public static final String COLON = ":";
+	
+	public static final String EQUAL = "=";
+	
+	public static final String AMPERSAND = "&";
 }
