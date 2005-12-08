eb1631786c2553a367cbdd30d9e3651d07c029fd
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:03:23 +0000

    dont send actual attachments here

--- ssf/main/src/com/sitescape/ef/module/definition/notify/NotifyBuilderFile.java
+++ ssf/main/src/com/sitescape/ef/module/definition/notify/NotifyBuilderFile.java
@@ -6,16 +6,33 @@ import org.dom4j.Element;
 
 import com.sitescape.ef.domain.CustomAttribute;
 import com.sitescape.ef.domain.FileAttachment;
+import com.sitescape.ef.domain.FolderEntry;
+import com.sitescape.ef.util.WebUrlUtil;
+import com.sitescape.ef.web.WebKeys;
+import com.sitescape.ef.domain.Entry;
 /**
-*
+* Handle file field in mail notification.  This implememtation will
+* send the file name only in a notification 
+* See <code>NotifyBuilderFileSend</code>to send the actual file.
 * @author Janet McCann
 */
 public class NotifyBuilderFile extends AbstractNotifyBuilder {
 
 	   protected boolean build(Element element, Notify notifyDef, CustomAttribute attribute, Map args) {
 	    	FileAttachment att = (FileAttachment)attribute.getValue();
-	    	if (att != null) 
-	    		element.setText(att.toString());
+	    	if (att != null && att.getFileItem() != null) {
+	    		element.setText(att.getFileItem().getName());
+    			Entry owner = att.getOwner().getEntry();
+    			if (owner instanceof FolderEntry) {
+    				FolderEntry fEntry = (FolderEntry)owner;
+	    		
+    				String webUrl = WebUrlUtil.getServletRootURL() + WebKeys.SERVLET_VIEW_FILE + "?" +  
+    				WebKeys.FORUM_URL_FORUM_ID + "+" + fEntry.getParentFolder().getId().toString() +
+    				"&" + WebKeys.FORUM_URL_ENTRY_ID + "+" + fEntry.getId().toString() +
+    				"&" + WebKeys.FORUM_URL_FILE_ID + "+" + att.getId(); 
+    				element.addAttribute("href", webUrl);
+	    		}
+	    	}
 	    	return true;
 	    }
 }
