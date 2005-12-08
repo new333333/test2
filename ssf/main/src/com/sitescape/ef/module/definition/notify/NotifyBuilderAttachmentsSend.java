a59126194ac8b2b7ccffcdf4c0dc0e0fcc8bf799
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:11:13 +0000

    only send unnamed file attachments

--- ssf/main/src/com/sitescape/ef/module/definition/notify/NotifyBuilderAttachmentsSend.java
+++ ssf/main/src/com/sitescape/ef/module/definition/notify/NotifyBuilderAttachmentsSend.java
@@ -12,7 +12,7 @@ import com.sitescape.ef.web.WebKeys;
 import com.sitescape.ef.web.util.WebUrlUtil;
 
 /**
-* Handle attachments in mail notification.  This implememtation will
+* Handle unammed attachments in mail notification.  This implememtation will
 * send the file name on a summary type notification and will send the actual
 * attached file on a full notification.
 * @author Janet McCann
@@ -24,7 +24,7 @@ public class NotifyBuilderAttachmentsSend extends AbstractNotifyBuilder {
     		for (int i=0; i<atts.size(); ++i) {
 		    	Element value = element.addElement("file");		    		
 		    	FileAttachment att = (FileAttachment)atts.get(i);
-		    	if (att != null && att.getFileItem() != null) {
+		    	if (att != null && (att.getName() != null) && att.getFileItem() != null) {
 		    		value.setText(att.getFileItem().getName());
 		    		if (notifyDef.isFull())	
 		    			notifyDef.addAttachment(att);
