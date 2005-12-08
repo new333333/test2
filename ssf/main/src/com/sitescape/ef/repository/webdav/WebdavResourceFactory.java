f25c32b130fa96717f6e0297d682548c6fb301e4
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:02:07 +0000

    More Webdav related changes

--- ssf/main/src/com/sitescape/ef/repository/webdav/WebdavResourceFactory.java
+++ ssf/main/src/com/sitescape/ef/repository/webdav/WebdavResourceFactory.java
@@ -3,18 +3,24 @@ package com.sitescape.ef.repository.webdav;
 import java.io.IOException;
 
 import org.apache.commons.httpclient.URIException;
-import org.apache.webdav.lib.WebdavResource;
 
+/**
+ * Defines interface for obtaining connection to Webdav resource.
+ * 
+ * @author jong
+ *
+ */
 public interface WebdavResourceFactory {
 	
 	public String getHttpUrl();
 
 	public void setHttpUrl(String httpUrl) throws URIException;
+	
+	public void setUsername(String username);
+	
+	public void setPassword(String password);
 
-	public String getDocRootDir();
-
-	public void setDocRootDir(String docRootDir);
-
-	public SWebdavResource openSession(String userName, String password) 
-		throws IOException;
+	public SWebdavResource openResource() throws IOException;
+	
+	public void closeResource(SWebdavResource resource) throws IOException;
 }
