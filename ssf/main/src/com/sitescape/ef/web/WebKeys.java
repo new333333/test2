6f99a2880368a2f84848c07c8e634449ab7ad8ac
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 00:56:23 +0000

    add calendar view stuff

--- ssf/main/src/com/sitescape/ef/web/WebKeys.java
+++ ssf/main/src/com/sitescape/ef/web/WebKeys.java
@@ -12,6 +12,7 @@ public class WebKeys {
 	public static final String CALENDAR_VIEW_DAY = "day";
 	public static final String CALENDAR_VIEW_WEEK = "week";
 	public static final String CALENDAR_VIEW_MONTH = "month";
+	public static final String CALENDAR_CURRENT_DATE = "currentDate";
 
 	public static final String DEFINITION_DEFAULT_FORM_NAME = "entryForm";
     public static final String LOCALE = "ss_locale";
