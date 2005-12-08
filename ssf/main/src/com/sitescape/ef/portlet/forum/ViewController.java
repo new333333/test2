02da095c1692486d52e3176beb652626c3c17e5d
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:09:12 +0000

    Add support for showing unseen counts

--- ssf/main/src/com/sitescape/ef/portlet/forum/ViewController.java
+++ ssf/main/src/com/sitescape/ef/portlet/forum/ViewController.java
@@ -2,7 +2,6 @@ package com.sitescape.ef.portlet.forum;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
@@ -16,16 +15,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Date;
-import java.util.TreeMap;
 
+import com.sitescape.ef.util.NLT;
 import com.sitescape.ef.web.WebKeys;
 import com.sitescape.ef.web.util.PortletRequestUtils;
+import com.sitescape.ef.web.util.Toolbar;
 import com.sitescape.ef.web.util.WebHelper;
 import com.sitescape.ef.web.util.DateHelper;
-import com.sitescape.ef.ObjectKeys;
 import com.sitescape.ef.context.request.RequestContextHolder;
 import com.sitescape.ef.domain.NoFolderByTheIdException;
 import com.sitescape.ef.domain.User;
+import com.sitescape.ef.web.util.WebUrlUtil;
 /**
  * @author Peter Hurley
  *
@@ -39,8 +39,12 @@ public class ViewController  extends SAbstractForumController {
         User user = RequestContextHolder.getRequestContext().getUser();
 		Map formData = request.getParameterMap();
 		Long folderId = null;
+		Map model = new HashMap();
 
 		if (request.getWindowState().equals(WindowState.NORMAL)) {
+			//Build the toolbar and add it to the model
+			buildForumToolbar(model);
+			
 			//This is the portlet view; get the configured list of folders to show
 			String[] preferredFolderIds = request.getPreferences().getValues(WebKeys.FORUM_PREF_FORUM_ID_LIST, new String[0]);
 
@@ -50,15 +54,18 @@ public class ViewController  extends SAbstractForumController {
 				folderIds.add(new Long(preferredFolderIds[i]));
 			}
 			if (folderIds.size() > 0) {
-				return new ModelAndView(WebKeys.VIEW, WebKeys.FOLDER_LIST, getFolderModule().getSortedFolderList(folderIds));
+				model.put(WebKeys.FOLDER_LIST, getFolderModule().getSortedFolderList(folderIds));
+				return new ModelAndView(WebKeys.VIEW, model);
 			}
 			try {
 				folderId = ActionUtil.getForumId(request);
 			} catch (NoFolderByTheIdException nf) {
 				return new ModelAndView(WebKeys.VIEW);
 			}
+			
 			folderIds.add(folderId);
-			return new ModelAndView(WebKeys.VIEW, WebKeys.FOLDER_LIST, getFolderModule().getSortedFolderList(folderIds));
+			model.put(WebKeys.FOLDER_LIST, getFolderModule().getSortedFolderList(folderIds));
+			return new ModelAndView(WebKeys.VIEW, model);
 		}
 
 		try {
@@ -94,4 +101,18 @@ public class ViewController  extends SAbstractForumController {
 
 		return returnToViewForum(request, response, formData, folderId);
 	} 
+	
+	protected void buildForumToolbar(Map model) {
+		//Build the toolbar array
+		Toolbar toolbar = new Toolbar();
+
+		//The "Show unseen" menu
+		String url = "javascript: ;";
+		Map qualifiers = new HashMap();
+		qualifiers.put("onClick", "if (ss_getUnseenCounts) {ss_getUnseenCounts()};return false;");
+		toolbar.addToolbarMenu("1_showunseen", NLT.get("toolbar.showUnseen"), url, qualifiers);
+
+		model.put(WebKeys.FORUM_TOOLBAR, toolbar.getToolbar());
+	}
+
 }
