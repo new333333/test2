9fc29fc112d34578dd7e18dfcf1bed492f012786
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 00:58:41 +0000

    break modules into module and targets.  Targets can be used internally and avoid the module proxy overhead

--- ssf/main/src/com/sitescape/ef/module/workflow/impl/WorkflowModuleImpl.java
+++ ssf/main/src/com/sitescape/ef/module/workflow/impl/WorkflowModuleImpl.java
@@ -3,6 +3,7 @@ package com.sitescape.ef.module.workflow.impl;
 import com.sitescape.ef.dao.CoreDao;
 import com.sitescape.ef.dao.FolderDao;
 import com.sitescape.ef.module.definition.DefinitionModule;
+import com.sitescape.ef.module.impl.AbstractModuleImpl;
 import com.sitescape.ef.module.workflow.WorkflowModule;
 import com.sitescape.ef.security.AccessControlManager;
 import com.sitescape.ef.security.acl.AclManager;
@@ -14,34 +15,18 @@ import org.jbpm.graph.exe.ProcessInstance;
 import org.jbpm.graph.exe.Token;
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
 
-public class WorkflowModuleImpl implements WorkflowModule {
-	protected CoreDao coreDao;
-	protected FolderDao folderDao;
+public class WorkflowModuleImpl extends AbstractModuleImpl implements WorkflowModule {
 	protected DefinitionModule definitionModule;
-	protected AccessControlManager accessControlManager;
-	protected AclManager aclManager;
 	protected WorkflowFactory workflowFactory;
 	
 
 	public void setWorkflowFactory(WorkflowFactory workflowFactory) {
 		this.workflowFactory = workflowFactory;
 	}
-	public void setCoreDao(CoreDao coreDao) {
-	    this.coreDao = coreDao;
-	}   
-    public void setFolderDao(FolderDao folderDao) {
-        this.folderDao = folderDao;
-    }
 	public void setDefinitionModule(DefinitionModule definitionModule) {
 		this.definitionModule=definitionModule;
 	}
-    public void setAccessControlManager(AccessControlManager accessControlManager) {
-        this.accessControlManager=accessControlManager;
-    }
-    public void setAclManager(AclManager aclManager) {
-        this.aclManager = aclManager;
-    }
-
+ 
 
 	public ProcessDefinition getWorkflow(Long id) {
 	    try {
