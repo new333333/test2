e57a9fde8240abeb2b00cccb52a02c83be1a90af
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 00:57:19 +0000

    Switch some order

--- trunk/ssf/main/src/com/sitescape/ef/context/request/RequestContextUtil.java
+++ trunk/ssf/main/src/com/sitescape/ef/context/request/RequestContextUtil.java
@@ -8,10 +8,10 @@ public class RequestContextUtil {
 
 	public static void setThreadContext(String zoneName, String userName) 
 		throws RequestContextException {
-		if(zoneName == null)
-			throw new IllegalArgumentException("Zone name must be specified");
 		if(userName == null)
 			throw new IllegalArgumentException("User name must be specified");
+		if(zoneName == null)
+			throw new IllegalArgumentException("Zone name must be specified");
 		
 		RequestContext rc = new RequestContext(zoneName, userName);
 		RequestContextHolder.setRequestContext(rc);
