e90112914aa8a36fce4230343abb9f7088abf522
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 00:43:26 +0000

    admin

---
+++ ssf/main/src/com/sitescape/ef/module/admin/AdminModule.java
@@ -0,0 +1,26 @@
+/*
+ * Created on Dec 13, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.sitescape.ef.module.admin;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.sitescape.ef.domain.NotificationDef;
+
+/**
+ * @author Janet McCann
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface AdminModule {
+
+    public void setNotification(Long forumId, NotificationDef definition, Set users); 
+    public void disableNotification(Long forumId);
+    public void enableNotification(Long forumId);
+
+}
