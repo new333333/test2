0e62b0f5f9e42439e0889a7d0f89a01ef82f79da
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:04:58 +0000

    File manager

---
+++ ssf/main/src/com/sitescape/ef/file/CheckedOutByOtherException.java
@@ -0,0 +1,14 @@
+package com.sitescape.ef.file;
+
+import com.sitescape.ef.domain.Entry;
+import com.sitescape.ef.domain.User;
+import com.sitescape.ef.exception.CheckedCodedException;
+
+public class CheckedOutByOtherException extends CheckedCodedException {
+	
+	private static final String CheckedOutByOtherException_ErrorCode = "errorcode.checked.out.by.other";
+	 
+		public CheckedOutByOtherException(Entry entry, String fileName, User user) {
+	        super(CheckedOutByOtherException_ErrorCode, new Object[]{entry.getId(), fileName, user.getName()});
+	    }
+}
