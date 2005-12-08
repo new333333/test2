e79ff19192c30d32b7ed4de1c4ae62a6fadb9cc2
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 00:48:54 +0000

    Moved lucene local indexing support over

--- ssf/main/src/com/sitescape/ef/search/local/LocalLuceneSessionFactory.java
+++ ssf/main/src/com/sitescape/ef/search/local/LocalLuceneSessionFactory.java
@@ -1,5 +1,7 @@
 package com.sitescape.ef.search.local;
 
+import java.io.File;
+
 import com.sitescape.ef.search.AbstractLuceneSessionFactory;
 import com.sitescape.ef.search.LuceneSession;
 
@@ -9,15 +11,15 @@ import com.sitescape.ef.search.LuceneSession;
  */
 public class LocalLuceneSessionFactory extends AbstractLuceneSessionFactory {
     
-    public LuceneSession openSession(String indexName) {
-        // TODO needs to be reimplemented.
+	private String rootDirPath;
+	
+    public void setRootDirPath(String rootDirPath) {
+    	if(!rootDirPath.endsWith(File.separator))
+    		rootDirPath += File.separator;
+		this.rootDirPath = rootDirPath;
+	}
 
-        String zoneName = "liferay.com"; // bad!!
-        
-        // Simply ignore index name, and use the hard-coded company name.
-        // This means that the index files are all combined into one, 
-        // ignoring the user-specified index name, under the same ugly
-        // company. 
-        return new LocalLuceneSession(zoneName);
+	public LuceneSession openSession(String indexName) {
+        return new LocalLuceneSession(rootDirPath + indexName + File.separator);
     }
 }
