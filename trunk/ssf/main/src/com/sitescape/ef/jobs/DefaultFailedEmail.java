75cc1da287e0959a1b8afc50ddb5c4f9e404e9cf
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:05:11 +0000

    Renamed SendMail.java to DefaultFolderFailedEmaill.java

--- trunk/ssf/main/src/com/sitescape/ef/jobs/DefaultFailedEmail.java
+++ trunk/ssf/main/src/com/sitescape/ef/jobs/DefaultFailedEmail.java
@@ -1,7 +1,13 @@
 
 package com.sitescape.ef.jobs;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
@@ -13,19 +19,26 @@ import com.sitescape.ef.util.SpringContextUtil;
  *
  */
 public class SendMail extends SSStatefulJob {
+	protected Log logger = LogFactory.getLog(getClass());
 
 	/* (non-Javadoc)
 	 * @see com.sitescape.ef.jobs.SSStatefulJob#doExecute(org.quartz.JobExecutionContext)
 	 */
     public void doExecute(JobExecutionContext context) throws JobExecutionException {
     	MailModule mail = (MailModule)SpringContextUtil.getBean("mailModule");
+		File file = new File((String)jobDataMap.get("mailMessage"));
 		try {
-			MimeMessage msg = (MimeMessage)jobDataMap.get("mailMessage");
-			if (mail.sendMail(msg) == true) {
+			FileInputStream fs = new FileInputStream(file);
+			String name = (String)jobDataMap.get("mailSender");
+			if (mail.sendMail(name, fs) == true) {
 				context.setResult(CleanupJobListener.DeleteJob);
 			}
 		} catch (ConfigurationException cf) {
 			throw new JobExecutionException(cf);
+		} catch (FileNotFoundException fe) {
+			//remove job
+			context.setResult(CleanupJobListener.DeleteJob);
+			logger.error("Mail file missing (" + file.getName() + ") - job cancelled");
 		}
     }
 
