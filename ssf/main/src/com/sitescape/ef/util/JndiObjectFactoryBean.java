92e12a33a8df783a4d082d890f33b66442637594
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:11:48 +0000

    Use PortabilityUtil

--- ssf/main/src/com/sitescape/ef/util/JndiObjectFactoryBean.java
+++ ssf/main/src/com/sitescape/ef/util/JndiObjectFactoryBean.java
@@ -1,7 +1,5 @@
 package com.sitescape.ef.util;
 
-import com.sitescape.util.ServerDetector;
-
 /**
  * 
  * @author jong
@@ -10,12 +8,6 @@ import com.sitescape.util.ServerDetector;
 public class JndiObjectFactoryBean extends org.springframework.jndi.JndiObjectFactoryBean {
 	
 	public void setJndiName(String jndiName) {
-
-		if(ServerDetector.isJBoss()) {
-			jndiName = jndiName.replaceFirst("java:comp/env/jdbc", "java:jdbc");
-			jndiName = jndiName.replaceFirst("java:comp/env/", "");
-		}
-		
-		super.setJndiName(jndiName);
+		super.setJndiName(PortabilityUtil.getJndiName(jndiName));
 	}
 }
