48c9b8cc5c9d9815d1160bc49237fd29e3b5fd25
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:03:22 +0000

    notify modules to send actual attachments

---
+++ trunk/ssf/main/src/com/sitescape/ef/module/definition/notify/NotifyBuilderAttachmentsSend.java
@@ -0,0 +1,44 @@
+package com.sitescape.ef.module.definition.notify;
+
+import java.util.List;
+import java.util.Map;
+
+import org.dom4j.Element;
+
+import com.sitescape.ef.domain.Entry;
+import com.sitescape.ef.domain.FolderEntry;
+import com.sitescape.ef.domain.FileAttachment;
+import com.sitescape.ef.util.WebUrlUtil;
+import com.sitescape.ef.web.WebKeys;
+
+/**
+* Handle attachments in mail notification.  This implememtation will
+* send the file name on a summary type notification and will send the actual
+* attached file on a full notification.
+* @author Janet McCann
+*/
+public class NotifyBuilderAttachmentsSend extends AbstractNotifyBuilder {
+
+	   protected boolean build(Element element, Notify notifyDef, Entry entry, String dataElemName, Map args) {
+	    	List atts = entry.getFileAttachments();
+    		for (int i=0; i<atts.size(); ++i) {
+		    	Element value = element.addElement("file");		    		
+		    	FileAttachment att = (FileAttachment)atts.get(i);
+		    	if (att != null && att.getFileItem() != null) {
+		    		value.setText(att.getFileItem().getName());
+		    		if (notifyDef.isFull())	
+		    			notifyDef.addAttachment(att);
+		    		else if (entry instanceof FolderEntry) {
+		    			FolderEntry fEntry = (FolderEntry)entry;
+		    		
+		    			String webUrl = WebUrlUtil.getServletRootURL() + WebKeys.SERVLET_VIEW_FILE + "?" +
+		    			WebKeys.FORUM_URL_FORUM_ID + "=" + fEntry.getParentFolder().getId().toString() +
+		    			"&" + WebKeys.FORUM_URL_ENTRY_ID + "=" + fEntry.getId().toString() +
+		    			"&" + WebKeys.FORUM_URL_FILE_ID + "=" + att.getId(); 
+		    			value.addAttribute("href", webUrl);
+		    		}
+		    	}
+    		}
+	    	return true;
+	   }
+}
