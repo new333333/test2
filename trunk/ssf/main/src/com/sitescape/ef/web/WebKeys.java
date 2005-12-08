f8aaa8308f402d2542559ba3541bb9679c187b08
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:00:21 +0000

    calendar stuff

--- trunk/ssf/main/src/com/sitescape/ef/web/WebKeys.java
+++ trunk/ssf/main/src/com/sitescape/ef/web/WebKeys.java
@@ -66,6 +66,8 @@ public class WebKeys {
 	public static final String FORUM_OPERATION_VIEW_ENTRY_NEXT = "entry_next";
 	public static final String FORUM_OPERATION_VIEW_ENTRY_PREVIOUS = "entry_previous";
 	public static final String FORUM_OPERATION_VIEW_FILE = "view_file";
+	public static final String FORUM_OPERATION_SET_CALENDAR_DISPLAY_MODE = "set_cal_display_mode";
+	public static final String FORUM_OPERATION_SET_CALENDAR_DISPLAY_DATE = "set_cal_display_date";
 		
     // MODEL TAGS & Attributes
 	public static final String ACTION = "action";
