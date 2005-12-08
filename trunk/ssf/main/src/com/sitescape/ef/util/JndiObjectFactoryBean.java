492cde42e29e4ee9b15ed4a7ece00b7d6804f956
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:07:13 +0000

    Make jndi resource lookup work on JBoss.

--- trunk/ssf/main/src/com/sitescape/ef/util/JndiObjectFactoryBean.java
+++ trunk/ssf/main/src/com/sitescape/ef/util/JndiObjectFactoryBean.java
@@ -12,7 +12,7 @@ public class JndiObjectFactoryBean extends org.springframework.jndi.JndiObjectFa
 	public void setJndiName(String jndiName) {
 
 		if(ServerDetector.isJBoss()) {
-			jndiName = jndiName.replaceFirst("comp/env/", "");
+			jndiName = jndiName.replaceFirst("java:comp/env/jdbc", "java:jdbc");
 			jndiName = jndiName.replaceFirst("java:comp/env/", "");
 		}
 		
