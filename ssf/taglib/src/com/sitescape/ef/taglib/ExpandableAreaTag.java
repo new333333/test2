a0b73812dc8c99b1a863ea2b5596999f75869eb6
nobody <nobody@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-08 01:10:46 +0000

    expandable tag

--- ssf/taglib/src/com/sitescape/ef/taglib/ExpandableAreaTag.java
+++ ssf/taglib/src/com/sitescape/ef/taglib/ExpandableAreaTag.java
@@ -8,7 +8,10 @@ import java.util.Map;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 
@@ -16,6 +19,8 @@ import com.sitescape.ef.ObjectKeys;
 import com.sitescape.ef.portletadapter.AdaptedPortletURL;
 import com.sitescape.ef.web.WebKeys;
 import com.sitescape.ef.web.util.WebUrlUtil;
+import com.sitescape.util.servlet.DynamicServletRequest;
+import com.sitescape.util.servlet.StringServletResponse;
 
 import javax.portlet.PortletURL;
 
@@ -24,188 +29,58 @@ import javax.portlet.PortletURL;
  * @author Peter Hurley
  *
  */
-public class ExpandableAreaTag extends BodyTagSupport implements ParamAncestorTag {
-	private String url = "";
-	private String action = "";
-    private String folderId = "";
-    private String entryId = "";
-    private String operation = "";
-    private String webPath = "";
-    private boolean adapter = false;
-    private String portletName = "";
-    private boolean actionUrl = true;
-    private boolean stayInFrame = false;
-	private Map _params;
+public class ExpandableAreaTag extends BodyTagSupport {
+	private String _bodyContent;
+	private String title = "";
     
+	public int doStartTag() {
+		return EVAL_BODY_BUFFERED;
+	}
+
+	public int doAfterBody() {
+		_bodyContent = getBodyContent().getString();
+
+		return SKIP_BODY;
+	}
+
 	public int doEndTag() throws JspException {
 		try {
-			HttpServletRequest req =
-				(HttpServletRequest)pageContext.getRequest();
+			HttpServletRequest httpReq = (HttpServletRequest) pageContext.getRequest();
+			HttpServletResponse httpRes = (HttpServletResponse) pageContext.getResponse();
 			
-			RenderRequest renderRequest = (RenderRequest) req.getAttribute("javax.portlet.request");
-			RenderResponse renderResponse = (RenderResponse) req.getAttribute("javax.portlet.response");
+			//Output the start of the area
+			RequestDispatcher rd = httpReq.getRequestDispatcher("/WEB-INF/jsp/tags/expandable_area/top.jsp");
 
-			
-			//See if a url was specified
-			String ctxPath = req.getContextPath();
-			if (!this.url.equals("")) {
-				//Yes, a url was explicitly specified. Just add the portal context and return
-				String fullUrl = ctxPath + "/" + this.url;
-				pageContext.getOut().print(fullUrl);
+			Map _params = new HashMap();
+			_params.put("title", new String[] {this.title});
 
-				return SKIP_BODY;				
-			}
+			ServletRequest req = null;
+			req = new DynamicServletRequest(httpReq, _params);
+			StringServletResponse res = new StringServletResponse(httpRes);
+			rd.include(req, res);
+			pageContext.getOut().print(res.getString());
 
-			//There was no explicit url specified, so build the url
-			//Get the SiteScape url parameters
-			Map params = new HashMap();
-			
-			if (!this.action.equals("")) {
-				params.put("action", new String[] {this.action});
-			}
-			
-			if (this.folderId.equals("")) folderId = (String) req.getAttribute(WebKeys.FORUM_URL_FORUM_ID);
-			if (this.folderId != null && !this.folderId.equals("")) {
-				params.put(WebKeys.FORUM_URL_FORUM_ID, new String[] {folderId});
-			} else {
-				this.folderId = "";
-			}
-			if (this.entryId.equals("")) entryId = (String) req.getAttribute(WebKeys.FORUM_URL_ENTRY_ID);
-			if (this.entryId != null && !this.entryId.equals("")) {
-				params.put(WebKeys.FORUM_URL_ENTRY_ID, new String[] {entryId});
-			} else {
-				this.entryId = "";
-			}
-			if (this.operation.equals("")) operation = (String) req.getAttribute(WebKeys.FORUM_URL_OPERATION);
-			if (this.operation != null && !this.operation.equals("")) {
-				params.put(WebKeys.FORUM_URL_OPERATION, new String[] {operation});
-			} else {
-				this.operation = "";
-			}
+			// Body
+			pageContext.getOut().print(_bodyContent);
 
-			if (!this.webPath.equals("")) {
-				String webUrl = WebUrlUtil.getServletRootURL(req) + webPath + "?";
-				Iterator it = params.entrySet().iterator();
-				while (it.hasNext()) {
-					Map.Entry me = (Map.Entry) it.next();
-					webUrl += me.getKey() + "=" + ((String[])me.getValue())[0] + "&";
-				}
-				if (_params != null ) {
-					Iterator _it = _params.entrySet().iterator();
-					while (_it.hasNext()) {
-						Map.Entry me = (Map.Entry) _it.next();
-						webUrl += me.getKey() + "=" + ((String[])me.getValue())[0] + "&";
-					}
-				}
-				pageContext.getOut().print(webUrl);
-			
-			} else if (this.adapter) {
-				AdaptedPortletURL adapterUrl = new AdaptedPortletURL(req, this.portletName, this.actionUrl);
-				Iterator it = params.entrySet().iterator();
-				while (it.hasNext()) {
-					Map.Entry me = (Map.Entry) it.next();
-					adapterUrl.setParameter((String) me.getKey(), ((String[])me.getValue())[0]);
-				}
-				if (_params != null ) {
-					Iterator _it = _params.entrySet().iterator();
-					while (_it.hasNext()) {
-						Map.Entry me = (Map.Entry) _it.next();
-						adapterUrl.setParameter((String) me.getKey(), ((String[])me.getValue())[0]);
-					}
-				}
-				pageContext.getOut().print(adapterUrl.toString());
-				
-			} else {
-				PortletURL portletURL = null;
-				if (this.actionUrl) {
-					portletURL = renderResponse.createActionURL();
-				} else {
-					portletURL = renderResponse.createRenderURL();
-				}
-				portletURL.setWindowState(new WindowState(WindowState.MAXIMIZED.toString()));
-				portletURL.setParameters(params);
-				if (_params != null) {
-					portletURL.setParameters(_params);
-				}
+			//Output the end of the area
+			rd = httpReq.getRequestDispatcher("/WEB-INF/jsp/tags/expandable_area/bottom.jsp");
+			req = new DynamicServletRequest(httpReq, _params);
+			res = new StringServletResponse(httpRes);
+			rd.include(req, res);
+			pageContext.getOut().print(res.getString());
 
-				String portletURLToString = portletURL.toString();
-
-				pageContext.getOut().print(portletURLToString);
-			}
-
-			return SKIP_BODY;
+			return EVAL_PAGE;
 		}
 	    catch(Exception e) {
 	        throw new JspException(e);
 	    }
 		finally {
-			if (_params != null) {
-				_params.clear();
-			}
 		}
 	}
 
-	public void setUrl(String url) {
-	    this.url = url;
-	}
-
-	public void setAction(String action) {
-	    this.action = action;
-	}
-
-	public void setFolderId(String folderId) {
-	    this.folderId = folderId;
-	}
-
-	public void setEntryId(String entryId) {
-	    this.entryId = entryId;
-	}
-
-	public void setWebPath(String webPath) {
-	    this.webPath = webPath;
-	}
-
-	public void setAdapter(boolean adapter) {
-	    this.adapter = adapter;
-	}
-
-	public void setPortletName(String portletName) {
-	    this.portletName = portletName;
-	}
-
-	public void setActionUrl(boolean actionUrl) {
-	    this.actionUrl = actionUrl;
-	}
-
-	public void setStayInFrame(boolean stayInFrame) {
-	    this.stayInFrame = stayInFrame;
-	}
-
-	public void setOperation(String operation) {
-	    this.operation = operation;
-	}
-
-	public void addParam(String name, String value) {
-		if (_params == null) {
-			_params = new LinkedHashMap();
-		}
-
-		String[] values = (String[])_params.get(name);
-
-		if (values == null) {
-			values = new String[] {value};
-		}
-		else {
-			String[] newValues = new String[values.length + 1];
-
-			System.arraycopy(values, 0, newValues, 0, values.length);
-
-			newValues[newValues.length - 1] = value;
-
-			values = newValues;
-		}
-
-		_params.put(name, values);
+	public void setTitle(String title) {
+	    this.title = title;
 	}
 
 }
