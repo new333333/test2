af1eab1a1c856e355b68b3d20406c84e01b6722d
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-13 15:45:29 +0000

    Add getManualTransitions method to workflowState

--- ssf/main/src/com/sitescape/ef/domain/WorkflowState.java
+++ ssf/main/src/com/sitescape/ef/domain/WorkflowState.java
@@ -1,4 +1,14 @@
 package com.sitescape.ef.domain;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import com.sitescape.ef.util.NLT;
+
 /**
  * Use this object as component object.  This would be useful for objects that
  * implement SingletonWorkflowSupport.  Objects that maintain multiple workflows,
@@ -64,5 +74,69 @@ public class WorkflowState {
     	return tokenId.hashCode();
     }
 
+    /**
+     * Method to get the list of allowed manual transitions from this state for this user
+     */
+    public Map getManualTransitions() {
+		Map transitionData = new LinkedHashMap();
+		WorkflowState ws = this;
+		Definition wfDef = ws.getDefinition();
+		Document wfDoc = wfDef.getDefinition();
+		Element wfRoot = wfDoc.getRootElement();
+		//Find the current state in the definition
+		Element statePropertyEle = (Element) wfRoot.selectSingleNode("//item[@name='workflowProcess']/item[@name='state']/properties/property[@name='name' and @value='"+ws.getState()+"']");
+		if (statePropertyEle != null) {
+			Element stateEle = statePropertyEle.getParent().getParent();
+			//Build a list of all manual transitions for this state
+			List transitions = stateEle.selectNodes("./item[@name='transitions']/item[@name='transitionManual']");
+			if (transitions != null) {
+				for (int j = 0; j < transitions.size(); j++) {
+					Element toStateEle = (Element) ((Element) transitions.get(j)).selectSingleNode("./properties/property[@name='toState']");
+					if (toStateEle != null) {
+						String toStateValue = toStateEle.attributeValue("value", "");
+						String toStateCaption = "";
+						if (!toStateValue.equals("")) {
+							//We have a transition. get the caption;
+							Element toStateEle2 = (Element) wfRoot.selectSingleNode("//item[@name='workflowProcess']/item[@name='state']/properties/property[@name='name' and @value='"+toStateValue+"']");
+							if (toStateEle2 != null) {
+								Element toStateCaptionEle = (Element) toStateEle2.selectSingleNode("../property[@name='caption']");
+								if (toStateCaptionEle != null) {
+									toStateCaption = toStateCaptionEle.attributeValue("value", "");
+								}
+							}
+						}
+						if (toStateCaption.equals("")) toStateCaption = toStateValue;
+						if (!toStateValue.equals("") && !toStateCaption.equals("")) {
+							//TODO Check that the user has the right to execute this transition
+							
+							//Ok, add this transition to the map
+							transitionData.put(toStateValue, toStateCaption);
+						}
+					}
+				}
+			}
+		}
+		return transitionData;
+    }
+    
+    public String getStateCaption() {
+    	String stateCaption = "";
+    	//Find the actual caption of the state
+    	Definition wfDef = this.getDefinition();
+    	if (wfDef != null) {
+    		Document wfDefDoc = wfDef.getDefinition();
+        	Element stateProperty = (Element) wfDefDoc.getRootElement().selectSingleNode("//item[@name='state']/properties/property[@name='name' and @value='"+this.getState()+"']");
+        	if (stateProperty != null) {
+        		Element statePropertyCaption = (Element) stateProperty.getParent().selectSingleNode("./property[@name='caption']");
+        		if (statePropertyCaption != null) stateCaption = statePropertyCaption.attributeValue("value", "");
+        	}
+        	if (stateCaption.equals("")) {
+        		stateCaption = this.getState();
+        	} else {
+        		stateCaption = NLT.getDef(stateCaption);
+        	}
+    	}
+    	return stateCaption;
+    }
  	
 }
