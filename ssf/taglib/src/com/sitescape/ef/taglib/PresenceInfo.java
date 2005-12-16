705147f039996a5403a17e5e1832cdf7aaca748b
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-16 16:08:30 +0000

    add ssf:presenceInfo tag to show the presence dudes

---
+++ ssf/taglib/src/com/sitescape/ef/taglib/PresenceInfo.java
@@ -0,0 +1,68 @@
+package com.sitescape.ef.taglib;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import com.sitescape.ef.domain.User;
+import com.sitescape.ef.module.shared.PresenceServiceUtils;
+
+
+/**
+ * @author Roy Klein
+ *
+ */
+public class PresenceInfo extends TagSupport {
+    private User user;
+    private int userStatus=-1;
+    private String contextPath;
+    private String imagePath;
+    
+	public int doStartTag() throws JspException {
+		try {
+			JspWriter jspOut = pageContext.getOut();
+			StringBuffer sb = new StringBuffer();
+			
+			if (this.user != null) {
+				userStatus = PresenceServiceUtils.getPresence(user);
+				//sb.append(NLT.get(this.tag));
+			}
+			HttpServletRequest req = (HttpServletRequest) pageContext.getRequest();
+
+			this.contextPath = req.getContextPath();
+			if (contextPath.endsWith("/")) contextPath = contextPath.substring(0,contextPath.length()-1);
+		    imagePath = contextPath + "/images";
+		    
+		    switch (userStatus) {
+				case -1: 
+					sb.append("<img border=\"0\" src=\"" + imagePath + "/pics/sym_m_white_dude.gif\">");
+					break;
+				case 1:
+					sb.append("<img border=\"0\" src=\"" + imagePath + "/pics/sym_m_green_dude.gif\">");
+					break;
+				case -99:
+					break;
+				default:
+					sb.append("<img border=\"0\" src=\"" + imagePath + "/pics/sym_m_yellow_dude.gif\">");
+            }
+            jspOut.print(sb.toString());
+		}
+	    catch(Exception e) {
+	        throw new JspException(e);
+	    }
+	    
+		return SKIP_BODY;
+	}
+
+	public int doEndTag() throws JspException {
+		return EVAL_PAGE;
+	}
+	
+	public void setUser(User user) {
+	    this.user = user;
+	}
+
+}
+
+
