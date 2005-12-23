67034303fd6b8bf493056e3101381805dfb273dd
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2005-12-23 13:48:28 +0000

    Interceptors for remote service

---
+++ trunk/ssf/main/src/com/sitescape/ef/remoting/interceptor/LoggingInterceptor.java
@@ -0,0 +1,22 @@
+package com.sitescape.ef.remoting.interceptor;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class LoggingInterceptor implements MethodInterceptor {
+
+	protected Log logger = LogFactory.getLog(getClass());
+
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		try {
+			return invocation.proceed();
+		}
+		catch(Throwable t) {
+			logger.error("", t);
+			throw t;
+		}
+	}
+
+}
