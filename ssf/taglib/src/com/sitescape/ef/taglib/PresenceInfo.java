d696f26c00f9d82746282a2e264aebecab611fa1
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-01-16 22:25:31 +0000

    Don't show dudes if presence not enabled

--- ssf/taglib/src/com/sitescape/ef/taglib/PresenceInfo.java
+++ ssf/taglib/src/com/sitescape/ef/taglib/PresenceInfo.java
@@ -38,36 +38,37 @@ public class PresenceInfo extends BodyTagSupport {
 			if (this.user != null) {
 				userStatus = PresenceServiceUtils.getPresence(user);
 			}
-			String dudeGif = "sym_s_white_dude.gif";
-			String altText = NLT.get("presence.none");
-			if (userStatus > 0) {
-				if ((userStatus & 16) == 16) {
-					dudeGif = "sym_s_yellow_dude.gif";
-					altText = NLT.get("presence.away");
-				} else {
-					dudeGif = "sym_s_green_dude.gif";
-					altText = NLT.get("presence.online");
+			if (userStatus != -99) {
+				String dudeGif = "sym_s_white_dude.gif"; 
+				String altText = NLT.get("presence.none");
+				if (userStatus > 0) {
+					if ((userStatus & 16) == 16) {
+						dudeGif = "sym_s_yellow_dude.gif";
+						altText = NLT.get("presence.away");
+					} else {
+						dudeGif = "sym_s_green_dude.gif";
+						altText = NLT.get("presence.online");
+					}
+				} else if (userStatus == 0) {
+					dudeGif = "sym_s_gray_dude.gif";
+					altText = NLT.get("presence.offline");
 				}
-			} else if (userStatus == 0) {
-				dudeGif = "sym_s_gray_dude.gif";
-				altText = NLT.get("presence.offline");
-			}
-			
-			//Pass the user status to the jsp
-			httpReq.setAttribute(WebKeys.PRESENCE_USER, user);
-			httpReq.setAttribute(WebKeys.PRESENCE_STATUS, new Integer(userStatus));
-			httpReq.setAttribute(WebKeys.PRESENCE_DUDE, dudeGif);
-			httpReq.setAttribute(WebKeys.PRESENCE_TEXT, altText);
-			httpReq.setAttribute(WebKeys.PRESENCE_ZON_BRIDGE, "enabled");
-
-			// Output the presence info
-			String jsp = "/WEB-INF/jsp/tag_jsps/presence/show_dude.jsp";
-			RequestDispatcher rd = httpReq.getRequestDispatcher(jsp);
-			ServletRequest req = pageContext.getRequest();
-			StringServletResponse res = new StringServletResponse(httpRes);
-			rd.include(req, res);
-			pageContext.getOut().print(res.getString());
 				
+				//Pass the user status to the jsp
+				httpReq.setAttribute(WebKeys.PRESENCE_USER, user);
+				httpReq.setAttribute(WebKeys.PRESENCE_STATUS, new Integer(userStatus));
+				httpReq.setAttribute(WebKeys.PRESENCE_DUDE, dudeGif);
+				httpReq.setAttribute(WebKeys.PRESENCE_TEXT, altText);
+				httpReq.setAttribute(WebKeys.PRESENCE_ZON_BRIDGE, "enabled");
+	
+				// Output the presence info
+				String jsp = "/WEB-INF/jsp/tag_jsps/presence/show_dude.jsp";
+				RequestDispatcher rd = httpReq.getRequestDispatcher(jsp);
+				ServletRequest req = pageContext.getRequest();
+				StringServletResponse res = new StringServletResponse(httpRes);
+				rd.include(req, res);
+				pageContext.getOut().print(res.getString());
+			}
 		}
 	    catch(Exception e) {
 			throw new JspTagException(e.getMessage());
