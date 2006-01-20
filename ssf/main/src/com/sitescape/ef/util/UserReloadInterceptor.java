cebacf8e9275a8e6db6173f439a52a24fb3d36d6
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-01-20 17:43:41 +0000

    Reload user object upon transaction failure

---
+++ ssf/main/src/com/sitescape/ef/util/UserReloadInterceptor.java
@@ -0,0 +1,75 @@
+package com.sitescape.ef.util;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.sitescape.ef.context.request.RequestContext;
+import com.sitescape.ef.context.request.RequestContextHolder;
+import com.sitescape.ef.dao.CoreDao;
+import com.sitescape.ef.domain.User;
+
+public class UserReloadInterceptor implements MethodInterceptor {
+	
+	protected final Log logger = LogFactory.getLog(getClass());
+	
+	private CoreDao coreDao;
+	
+	protected CoreDao getCoreDao() {
+		return coreDao;
+	}
+
+	public void setCoreDao(CoreDao coreDao) {
+		this.coreDao = coreDao;
+	}
+
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		try {
+			return invocation.proceed();
+		}
+		catch(Throwable t) {
+			if(betterReload(t)) // Determine if we need to reload user object
+				reloadUser();
+			
+			throw t; // Rethrow the original exception
+		}
+	}
+
+	/**
+	 * Default behavior is as with EJB which Springframework also follows as
+	 * default: rollback on unchecked exception. Additionally attempt to 
+	 * rollback on Error. Consistent with Spring's TransactionTemplate's behavior.
+	 */
+	protected boolean betterReload(Throwable ex) {
+		// If we use customized (i.e., non-default) behavior for exception
+		// handling in conjunction with Spring's transaction proxy, then
+		// this implementation may need to be changed. 
+		
+		return (ex instanceof RuntimeException || ex instanceof Error);
+	}
+	
+	private void reloadUser() {
+		RequestContext requestContext = RequestContextHolder.getRequestContext();
+		String userName = requestContext.getUserName();
+		String zoneName = requestContext.getZoneName();
+		
+		try {
+			User user = getCoreDao().findUserByNameOnlyIfEnabled
+			(userName, zoneName);
+			
+			requestContext.setUser(user);
+		}
+		catch(Throwable t) {
+			// If any error during reloading, at least clear the old user 
+			// object in the request context.
+			requestContext.setUser(null);
+			// Do not rethrow this exception, because it will cause the 
+			// interceptor to throw this exception rather than the original
+			// exception that came from the invocation target. Out of the two,
+			// this is less important an exception. We will log it though. 
+			logger.error("Failed to reload user " + userName 
+					+ " for zone " + zoneName, t);
+		}
+	}
+}
