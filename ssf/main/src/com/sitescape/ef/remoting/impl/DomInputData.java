fa24e4421ce2cafcba267a04a81bb1fe314cbf86
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-01-24 13:25:20 +0000

    new file

---
+++ ssf/main/src/com/sitescape/ef/remoting/impl/DomInputData.java
@@ -0,0 +1,65 @@
+package com.sitescape.ef.remoting.impl;
+
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import com.sitescape.ef.module.folder.InputDataAccessor;
+
+/**
+ * An implementation of <code>InputDataAccessor</code> based on element-only
+ * flat dom tree.
+ * 
+ * @author jong
+ *
+ */
+public class DomInputData implements InputDataAccessor {
+
+	private Document doc;
+	private Element root;
+	
+	public DomInputData(Document doc) {
+		this.doc = doc;
+		this.root = doc.getRootElement();
+	}
+	
+	public DomInputData(Element root) {
+		this.root = root;
+	}
+	
+	public String getSingleValue(String key) {
+		Element elem = (Element) root.selectSingleNode("attribute[@name='" + key + "']");
+		
+		if(elem != null) {
+			return elem.getText();
+		}
+		else {
+			return null;
+		}
+	}
+
+	public String[] getValues(String key) {
+		List nodes = root.selectNodes("attribute[@name='" + key + "']");
+		
+		int size = nodes.size();
+		
+		if(size > 0) {
+			String[] values = new String[size];
+			for(int i = 0; i < size; i++) {
+				values[i] = ((Element) nodes.get(i)).getText();
+			}
+			return values;
+		}
+		else {
+			return null;
+		}
+	}
+
+	public boolean exists(String key) {
+		if(root.selectSingleNode("attribute[@name='" + key + "']") != null)
+			return true;
+		else
+			return false;
+	}
+}
