350fe20d3935ed033759401526195b5ab31eafd6
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-02-03 15:40:46 +0000

    Variable name changes

--- trunk/ssf/main/src/com/sitescape/ef/security/impl/NullAccessControlManager.java
+++ trunk/ssf/main/src/com/sitescape/ef/security/impl/NullAccessControlManager.java
@@ -71,37 +71,37 @@ public class NullAccessControlManager implements AccessControlManager {
 	public void checkOperation(User user, Long additionalPrincipalId, WorkArea workArea, WorkAreaOperation workAreaOperation) throws AccessControlException {
 	}
 
-	public void checkAcl(AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) throws AccessControlException {
+	public void checkAcl(AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeCreator, boolean includeParentAcl) throws AccessControlException {
 
 	}
 
-	public boolean testAcl(AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) {
+	public boolean testAcl(AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeCreator, boolean includeParentAcl) {
         return true; // Permission granted with no checking. 
 
 	}
 
-	public void checkAcl(User user, AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) throws AccessControlException {
+	public void checkAcl(User user, AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeCreator, boolean includeParentAcl) throws AccessControlException {
 	}
 
-	public boolean testAcl(User user, AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) {
+	public boolean testAcl(User user, AclContainer parent, AclControlled aclControlledObj, AccessType accessType, boolean includeCreator, boolean includeParentAcl) {
         return true; // Permission granted with no checking. 
 
 	}
 
-	public void checkAcl(AclContainer aclContainer, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) throws AccessControlException {		// TODO Auto-generated method stub
+	public void checkAcl(AclContainer aclContainer, AccessType accessType, boolean includeCreator, boolean includeParentAcl) throws AccessControlException {		// TODO Auto-generated method stub
 		
 	}
 
-	public boolean testAcl(AclContainer aclContainer, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) {
+	public boolean testAcl(AclContainer aclContainer, AccessType accessType, boolean includeCreator, boolean includeParentAcl) {
         return true; // Permission granted with no checking. 
 
 	}
 
-	public void checkAcl(User user, AclContainer aclContainer, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) throws AccessControlException {
+	public void checkAcl(User user, AclContainer aclContainer, AccessType accessType, boolean includeCreator, boolean includeParentAcl) throws AccessControlException {
 
 	}
 
-	public boolean testAcl(User user, AclContainer aclContainer, AccessType accessType, boolean includeEntryCreator, boolean includeForumDefault) {
+	public boolean testAcl(User user, AclContainer aclContainer, AccessType accessType, boolean includeCreator, boolean includeParentAcl) {
         return true; // Permission granted with no checking. 
 
 	}
