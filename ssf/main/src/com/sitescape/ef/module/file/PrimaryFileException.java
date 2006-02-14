958fbcec5d128c016b2dc4716844e7ce665a0e81
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-02-14 19:17:18 +0000

    File related exceptions

---
+++ ssf/main/src/com/sitescape/ef/module/file/PrimaryFileException.java
@@ -0,0 +1,10 @@
+package com.sitescape.ef.module.file;
+
+import com.sitescape.ef.exception.UncheckedException;
+
+public class PrimaryFileException extends UncheckedException {
+
+	public PrimaryFileException(Throwable cause) {
+		super(cause);
+	}
+}
