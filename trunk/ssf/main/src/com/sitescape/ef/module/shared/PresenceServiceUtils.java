0aaa319fbd831fc0c5ca5bcb933616d45fd80fba
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-02-28 20:11:27 +0000

    add screenNameExists() call

--- trunk/ssf/main/src/com/sitescape/ef/module/shared/PresenceServiceUtils.java
+++ trunk/ssf/main/src/com/sitescape/ef/module/shared/PresenceServiceUtils.java
@@ -4,6 +4,7 @@ package com.sitescape.ef.module.shared;
 
 import com.sitescape.ef.domain.User;
 import com.sitescape.ef.util.SpringContextUtil;
+import com.sitescape.ef.presence.PresenceBroker;
 import com.sitescape.ef.presence.PresenceService;
 
 /**
@@ -17,5 +18,8 @@ public class PresenceServiceUtils {
     	PresenceService ps = (PresenceService)SpringContextUtil.getBean("presenceService");
     	return ps.getPresenceInfo(user);
     }
-    
+    public static boolean getScreenNameExists(String zonname) {
+    	PresenceBroker pb = (PresenceBroker)SpringContextUtil.getBean("presenceBroker");
+    	return pb.getScreenNameExists(zonname);   	
+    }
 }
