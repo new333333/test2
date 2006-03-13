f6fa62feaf9228fce17aecc20e8738d17e04ec29
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-03-13 19:50:14 +0000

    More interim checkin for pipeline

---
+++ ssf/main/src/com/sitescape/ef/pipeline/util/TempFileUtil.java
@@ -0,0 +1,22 @@
+package com.sitescape.ef.pipeline.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.sitescape.ef.UncheckedIOException;
+import com.sitescape.ef.util.FileHelper;
+
+public class TempFileUtil {
+
+	public static File createTempFile(String producerName, File fileDir) 
+		throws UncheckedIOException {
+		try {
+			if(!fileDir.exists())
+				FileHelper.mkdirs(fileDir);
+			return File.createTempFile(producerName + "_" + System.currentTimeMillis() + "_", null, fileDir);
+		}
+		catch(IOException e) {
+			throw new UncheckedIOException(e);
+		}	
+	}
+}
