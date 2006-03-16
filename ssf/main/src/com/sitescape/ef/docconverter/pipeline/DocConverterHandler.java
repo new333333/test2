64ddba09afddf107a035e12ab358ff3ed6f08789
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-03-16 15:17:22 +0000

    Added comment

--- ssf/main/src/com/sitescape/ef/docconverter/pipeline/DocConverterHandler.java
+++ ssf/main/src/com/sitescape/ef/docconverter/pipeline/DocConverterHandler.java
@@ -23,7 +23,6 @@ import org.dom4j.io.SAXReader;
 import org.springframework.util.FileCopyUtils;
 
 import com.sitescape.ef.docconverter.DocConverter;
-import com.sitescape.ef.docconverter.DocConverterException;
 import com.sitescape.ef.pipeline.impl.AbstractDocHandler;
 import com.sitescape.ef.pipeline.util.TempFileUtil;
 import com.sitescape.ef.pipeline.DocSink;
@@ -32,15 +31,21 @@ import com.sitescape.ef.pipeline.PipelineInvocation;
 import com.sitescape.ef.util.FileHelper;
 import com.sitescape.ef.util.SPropsUtil;
 
+/**
+ * This is a DocHandler wrapper around DocConverter and allows document
+ * conversion functionality to be plugged into the doc processing pipeline. 
+ * 
+ * @author jong
+ *
+ */
 public class DocConverterHandler extends AbstractDocHandler {
 
-    private static final String NULLXSL = "<?xml version='1.0' ?> \n    <xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform' version='1.0' />"; 
-
-    private static TransformerFactory transFactory = TransformerFactory.newInstance();
+    private TransformerFactory transFactory = TransformerFactory.newInstance();
     
 	protected final Log logger = LogFactory.getLog(getClass());
 
 	private DocConverter docConverter;
+	
 	// Export process timeout in milliseconds.
 	private long timeout = 10000; // default value
 
@@ -61,27 +66,39 @@ public class DocConverterHandler extends AbstractDocHandler {
 	}
 
 	public void doHandle(DocSource source, DocSink sink, PipelineInvocation invocation) throws Throwable {
-		// Get source file handed over by the pipeline. 
-		File inputFile = source.getFile();
 		File inputTempFile = null;
+		// Get source file, if any, handed over by the pipeline. 
+		File inputFile = source.getFile();
+		// Input data doesn't come in the form of a file. Since our doc converter
+		// can only work with a file, we must create a temporary file and store
+		// the input data in it before invoking the actual doc converter function. 
 		if(inputFile == null) {
 			inputFile = inputTempFile = TempFileUtil.createTempFile("dcinput", SPropsUtil.getFile("temp.dir"));
 			FileCopyUtils.copy(source.getDataAsInputStream(), new BufferedOutputStream(new FileOutputStream(inputFile)));
 		}
 		
 		try {
+			// Create an empty file to be used as output file for the converter.
+			// The output file will contain text data in xml format. 
 			File outputTextFile = TempFileUtil.createTempFile("docconverter", SPropsUtil.getFile("temp.dir"));
 			
 			try {
+				// Invoke the actual converter function giving it timeout value.
 				docConverter.convert(inputFile, outputTextFile, timeout);
 				
 				if(outputTextFile.length() > 0) {
+					// Create a dom object from the output file containing xml text.
 					org.dom4j.Document document = getDomDocument(outputTextFile);
 					
 					if(document != null) {
+						// Run the stylesheet to extract text from the xml. 
 						String text = getTextFromXML(document, docConverter.getNullTransformFile());
+						// Note: Roy, for some reason, the text coming out of the transformer
+						// always contain <?xml version="1.0" encoding="UTF-8"?> prefix??
 						
 						if(text != null) {
+							// Put the text data (result of conversion) into the sink 
+							// so that it can be consumed by the next guy in the chain.
 							sink.setString(text, "UTF-8");
 						}
 					}
@@ -89,6 +106,8 @@ public class DocConverterHandler extends AbstractDocHandler {
 			}
 			finally {
 				try {
+					// It is important to delete the output text file, since
+					// it is owned by this handler not by the framework. 
 					FileHelper.delete(outputTextFile);
 				}
 				catch(IOException e) {
@@ -98,6 +117,7 @@ public class DocConverterHandler extends AbstractDocHandler {
 		}
 		finally {
 			if(inputTempFile != null) {
+				// If we generated temporary input file, we must delete it. 
 				try {
 					FileHelper.delete(inputTempFile);
 				}
@@ -119,12 +139,11 @@ public class DocConverterHandler extends AbstractDocHandler {
 		}	
 	}
 	
-    private static String getTextFromXML(org.dom4j.Document tempfile, File transformFile) {
+    private String getTextFromXML(org.dom4j.Document tempfile, File transformFile) {
     	
     	Locale l = Locale.getDefault();
 		Templates trans;
 		Transformer tranny = null;
-		Source xsltSource = new StreamSource(NULLXSL);
         
         try {
 			Source s = new StreamSource(transformFile);
