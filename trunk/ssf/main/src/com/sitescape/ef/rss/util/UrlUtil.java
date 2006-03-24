7e2ecad818336e44059bbeb46be17d02794bb1ce
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-03-24 21:13:24 +0000

    generate URL for rss aggregators

--- trunk/ssf/main/src/com/sitescape/ef/rss/util/UrlUtil.java
+++ trunk/ssf/main/src/com/sitescape/ef/rss/util/UrlUtil.java
@@ -6,11 +6,30 @@ import com.sitescape.ef.web.util.WebUrlUtil;
 
 public class UrlUtil {
 
+	public static String getFeedURL(String binderId) {
+		RequestContext rc = RequestContextHolder.getRequestContext();
+		
+		StringBuffer url = new StringBuffer();
+		
+		url.append(WebUrlUtil.getRssRootURL()).
+			append("list").
+			append("?zn=").
+			append(rc.getZoneName()).
+			append("&bi=").
+			append(binderId).
+			append("&ui=").
+			append(rc.getUserId()).
+			append("&pd=").
+			append(rc.getUser().getPasswordDigest());
+		
+		return url.toString();
+	}
+	// NOTE: This is just here until we're done testing, then it'll be ripped out.
 	public static String getFeedURL() {
 		RequestContext rc = RequestContextHolder.getRequestContext();
 		
 		StringBuffer url = new StringBuffer();
-
+		
 		url.append(WebUrlUtil.getRssRootURL()).
 			append("list").
 			append("?zn=").
