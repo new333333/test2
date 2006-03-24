551438baeb2116805092a53c7f26d3d7c125ec09
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-03-24 21:13:24 +0000

    generate URL for rss aggregators

--- ssf/main/src/com/sitescape/ef/rss/servlet/ListController.java
+++ ssf/main/src/com/sitescape/ef/rss/servlet/ListController.java
@@ -1,25 +1,39 @@
 package com.sitescape.ef.rss.servlet;
 
+import java.io.OutputStream;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.RequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.sitescape.ef.context.request.RequestContextHolder;
+import com.sitescape.ef.domain.Binder;
+import com.sitescape.ef.domain.User;
 import com.sitescape.ef.web.servlet.SAbstractController;
 
 public class ListController extends SAbstractController {
 
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO To be done by Roy 
-		// Get the list of RSS items and write it to servlet response as XML.
 
-		// Just for testing
-		String data = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<abc>\nHello\n</abc>";
+		// Get the list of RSS items and write it to servlet response as XML.
+		Long binderId = new Long(RequestUtils.getRequiredStringParameter(request, "bi"));
+		Binder binder = getBinderModule().getBinder(binderId);
+		User user = RequestContextHolder.getRequestContext().getUser();
+		
+		
+		response.resetBuffer();
 		response.setContentType("text/xml");
 		response.setHeader("Cache-Control", "private");
 		response.setHeader("Pragma", "no-cache");
-		response.getOutputStream().write(data.getBytes());
+
+		OutputStream out = response.getOutputStream();
+		byte[] buffer = getRssGenerator().filterRss(binder,user).getBytes();
+		out.write(buffer, 0, buffer.length);
+
+		out.flush();
 
 		response.getOutputStream().flush();
 
