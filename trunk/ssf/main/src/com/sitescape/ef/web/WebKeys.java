b220ff014d950982b0c3080963a3e4ae7db21d2d
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-04-04 18:55:45 +0000

    Add support for adding workspaces

--- trunk/ssf/main/src/com/sitescape/ef/web/WebKeys.java
+++ trunk/ssf/main/src/com/sitescape/ef/web/WebKeys.java
@@ -163,6 +163,7 @@ public class WebKeys {
     public static final String PRESENCE_USER="ss_presence_user";
     public static final String PRESENCE_ZON_BRIDGE="ss_presence_zonBridge";
     public static final String PUBLIC_DEFINITIONS="ssPublicDefinitions";
+    public static final String PUBLIC_BINDER_DEFINITIONS="ssPublicBinderDefinitions";
     public static final String PUBLIC_ENTRY_DEFINITIONS="ssPublicEntryDefinitions";
     public static final String PUBLIC_FOLDER_DEFINITIONS="ssPublicFolderDefinitions";
     public static final String PUBLIC_PROFILE_DEFINITIONS="ssPublicProfileDefinitions";
