9b8978b2e3c04dad76095026a6b133a377eca33d
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-04-23 16:53:49 +0000

    To bypass Slide's ACL checking

---
+++ ssf/ssfs/src/com/sitescape/ef/ssfs/slide/SiteScapeSecurity.java
@@ -0,0 +1,20 @@
+package com.sitescape.ef.ssfs.slide;
+
+import org.apache.slide.common.ServiceAccessException;
+import org.apache.slide.common.SlideToken;
+import org.apache.slide.security.AccessDeniedException;
+import org.apache.slide.security.SecurityImpl;
+import org.apache.slide.structure.ActionNode;
+import org.apache.slide.structure.ObjectNode;
+
+public class SiteScapeSecurity extends SecurityImpl {
+
+    public void checkCredentials(SlideToken token, ObjectNode object,
+            ActionNode action) 
+    throws ServiceAccessException, AccessDeniedException {
+    	// Do not let Slide be responsible for ACL checking.
+    	// It will be performed entirely by SSF. 
+    	
+    	// Noop
+    }
+}
