eac350877e3888d3b2b4ecab09b927c120971a50
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-05-02 17:05:29 +0000

    interim checkin for work on locking

---
+++ ssf/main/src/com/sitescape/ef/ssfs/LockException.java
@@ -0,0 +1,11 @@
+package com.sitescape.ef.ssfs;
+
+public class LockException extends RuntimeException {
+
+	public LockException() {
+	}
+	
+	public LockException(String message) {
+		super(message);
+	}
+}
