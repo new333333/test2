91169cfa78826e38ac048be774136d614b43c33f
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-05-16 14:23:11 +0000

    JSP tags for exposing some of the SSFS-related functionality

---
+++ ssf/taglib/src/com/sitescape/ef/taglib/SsfsAttachmentUrlTag.java
@@ -0,0 +1,56 @@
+package com.sitescape.ef.taglib;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import com.sitescape.ef.domain.Binder;
+import com.sitescape.ef.domain.DefinableEntity;
+import com.sitescape.ef.domain.FileAttachment;
+import com.sitescape.ef.ssfs.util.SsfsUtil;
+
+public class SsfsAttachmentUrlTag extends TagSupport {
+	private Binder binder;
+	private DefinableEntity entity;
+	private FileAttachment fa;
+    
+	public int doStartTag() throws JspException {
+		if(binder == null)
+			throw new JspException("Binder must be specified");
+		
+		if(entity == null)
+			throw new JspException("Entity must be specified");
+		
+		if(fa == null)
+			throw new JspException("File attachment must be specified");
+		
+		String url = SsfsUtil.getAttachmentUrl(binder, entity, fa);
+		
+		try {
+			pageContext.getOut().print(url);
+		} catch (IOException e) {
+			throw new JspException(e);
+		}
+	    
+		return SKIP_BODY;
+	}
+
+	public int doEndTag() throws JspException {
+		return EVAL_PAGE;
+	}
+	
+	public void setBinder(Binder binder) {
+		this.binder = binder;
+	}
+	
+	public void setEntity(DefinableEntity entity) {
+		this.entity = entity;
+	}
+	
+	public void setFileAttachment(FileAttachment fa) {
+		this.fa = fa;
+	}
+}
+
+
---
+++ ssf/taglib/src/com/sitescape/ef/taglib/SsfsFileUrlTag.java
@@ -0,0 +1,64 @@
+package com.sitescape.ef.taglib;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import com.sitescape.ef.domain.Binder;
+import com.sitescape.ef.domain.DefinableEntity;
+import com.sitescape.ef.domain.FileAttachment;
+import com.sitescape.ef.ssfs.util.SsfsUtil;
+
+public class SsfsFileUrlTag extends TagSupport {
+	private Binder binder;
+	private DefinableEntity entity;
+	private String elemName;
+	private FileAttachment fa;
+    
+	public int doStartTag() throws JspException {
+		if(binder == null)
+			throw new JspException("Binder must be specified");
+		
+		if(entity == null)
+			throw new JspException("Entity must be specified");
+		
+		if(elemName == null)
+			throw new JspException("Definition element name must be specified");
+		
+		if(fa == null)
+			throw new JspException("File attachment must be specified");
+		
+		String url = SsfsUtil.getFileUrl(binder, entity, elemName, fa);
+		
+		try {
+			pageContext.getOut().print(url);
+		} catch (IOException e) {
+			throw new JspException(e);
+		}
+	    
+		return SKIP_BODY;
+	}
+
+	public int doEndTag() throws JspException {
+		return EVAL_PAGE;
+	}
+	
+	public void setBinder(Binder binder) {
+		this.binder = binder;
+	}
+	
+	public void setEntity(DefinableEntity entity) {
+		this.entity = entity;
+	}
+	
+	public void setElemName(String elemName) {
+		this.elemName = elemName;
+	}
+	
+	public void setFileAttachment(FileAttachment fa) {
+		this.fa = fa;
+	}
+}
+
+
