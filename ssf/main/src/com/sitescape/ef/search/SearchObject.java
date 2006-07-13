f9952746f6e673d36d219e54a6624242671f77dd
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-07-13 14:18:00 +0000

    keep a static queryparser object around to reduce parsing time

--- ssf/main/src/com/sitescape/ef/search/SearchObject.java
+++ ssf/main/src/com/sitescape/ef/search/SearchObject.java
@@ -1,27 +1,36 @@
 package com.sitescape.ef.search;
 
 import java.io.Serializable;
-import org.apache.lucene.search.SortField;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.search.Sort;
-import org.apache.lucene.analysis.WhitespaceAnalyzer;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
 
 public class SearchObject implements Serializable {
 
-	
+	protected Log logger = LogFactory.getLog(getClass());
 	private SortField[] sortBy = null;
 	private String queryString = null;
 	private Query query = null;
+	private static QueryParser qp = null;
 	
 	/**
 	 * 
 	 */
 	public SearchObject() {
 		super();
+		//synchronized(SearchObject.class){
+		if (qp == null) {
+			logger.info("QueryParser instantiating new QP");
+			qp = new QueryParser("content",new WhitespaceAnalyzer());
+		}
+		//}
 	}
 	
 	/**
@@ -56,7 +65,11 @@ public class SearchObject implements Serializable {
 	 */
 	public Query getQuery() {
 		try {
-			return QueryParser.parse(queryString,"content",new WhitespaceAnalyzer());
+			long startTime = System.currentTimeMillis();
+			Query retQ = qp.parse(queryString);
+			long endTime = System.currentTimeMillis();
+			logger.info("QueryParser took " + (endTime - startTime) + " milliseconds");
+			return retQ;
 		} catch (ParseException pe){ return new BooleanQuery();}
 	}
 
