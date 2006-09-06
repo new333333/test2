af44e14df25d000248c2ce9760fc270e98d8cd57
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-09-06 20:11:38 +0000

    clean up lucene 1.9 compiler warnings (deprecations)

--- ssf/main/src/com/sitescape/ef/module/shared/EntityIndexUtils.java
+++ ssf/main/src/com/sitescape/ef/module/shared/EntityIndexUtils.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.lucene.document.DateTools;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.dom4j.Element;
@@ -84,8 +85,8 @@ public class EntityIndexUtils {
             
             if(title.length() > 0) {
     	        Field allTextField = BasicIndexUtils.allTextField(title);
-    	        Field titleField = new Field(EntityIndexUtils.TITLE_FIELD, title, true, true, true); 
-    	        Field title1Field = Field.Keyword(EntityIndexUtils.TITLE1_FIELD, title.substring(0, 1));
+    	        Field titleField = new Field(EntityIndexUtils.TITLE_FIELD, title, Field.Store.YES, Field.Index.TOKENIZED); 
+    	        Field title1Field = new Field(EntityIndexUtils.TITLE1_FIELD, title.substring(0, 1), Field.Store.YES, Field.Index.UN_TOKENIZED);
                 doc.add(titleField);
                 doc.add(title1Field);
                 doc.add(allTextField);
@@ -95,30 +96,30 @@ public class EntityIndexUtils {
     public static void addRating(Document doc, DefinableEntity entry) {
     	//rating may not exist or not be supported
     	try {
-        	Field rateField = Field.Keyword(RATING_FIELD, entry.getAverageRating().getAverage().toString());
+        	Field rateField = new Field(RATING_FIELD, entry.getAverageRating().getAverage().toString(), Field.Store.YES, Field.Index.UN_TOKENIZED);
         	doc.add(rateField);
         } catch (Exception ex) {};
    	
     }
     public static void addEntityType(Document doc, DefinableEntity entry) {
-      	Field eField = Field.Keyword(ENTITY_FIELD, entry.getEntityIdentifier().getEntityType().name());
+      	Field eField = new Field(ENTITY_FIELD, entry.getEntityIdentifier().getEntityType().name(), Field.Store.YES, Field.Index.UN_TOKENIZED);
        	doc.add(eField);
     }
     public static void addEntryType(Document doc, DefinableEntity entry) {
         // Add the entry type (entry or reply)
     	if (entry instanceof FolderEntry) {
 	        if (((FolderEntry)entry).getTopEntry() == null || ((FolderEntry)entry).getTopEntry() == entry) {
-	        	Field entryTypeField = Field.Keyword(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_ENTRY);
+	        	Field entryTypeField = new Field(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_ENTRY, Field.Store.YES, Field.Index.UN_TOKENIZED);
 	        	doc.add(entryTypeField);
 	        } else {
-	        	Field entryTypeField = Field.Keyword(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_REPLY);
+	        	Field entryTypeField = new Field(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_REPLY, Field.Store.YES, Field.Index.UN_TOKENIZED);
 	        	doc.add(entryTypeField);
 	        }
     	} else if (entry instanceof User) {
-        	Field entryTypeField = Field.Keyword(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_USER);
+        	Field entryTypeField = new Field(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_USER, Field.Store.YES, Field.Index.UN_TOKENIZED);
         	doc.add(entryTypeField);
     	} else if (entry instanceof Group) {
-    		Field entryTypeField = Field.Keyword(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_GROUP);
+    		Field entryTypeField = new Field(EntityIndexUtils.ENTRY_TYPE_FIELD, EntityIndexUtils.ENTRY_TYPE_GROUP, Field.Store.YES, Field.Index.UN_TOKENIZED);
     		doc.add(entryTypeField);
     	} 
    }
@@ -127,9 +128,9 @@ public class EntityIndexUtils {
         // Add creation-date field
     	if (entry.getCreation() != null) {
     		Date creationDate = entry.getCreation().getDate();
-            Field creationDateField = Field.Keyword(CREATION_DATE_FIELD, creationDate);
+            Field creationDateField = new Field(CREATION_DATE_FIELD, DateTools.dateToString(creationDate,DateTools.Resolution.SECOND), Field.Store.YES, Field.Index.UN_TOKENIZED);
             doc.add(creationDateField);
-            Field creationDayField = Field.Keyword(CREATION_DAY_FIELD, formatDayString(creationDate));
+            Field creationDayField = new Field(CREATION_DAY_FIELD, formatDayString(creationDate), Field.Store.YES, Field.Index.UN_TOKENIZED);
             doc.add(creationDayField);
     	}
         
@@ -139,9 +140,9 @@ public class EntityIndexUtils {
     	// Add modification-date field
     	if (entry.getModification() != null ) {
     		Date modDate = entry.getModification().getDate();
-        	Field modificationDateField = Field.Keyword(MODIFICATION_DATE_FIELD, modDate);
+        	Field modificationDateField = new Field(MODIFICATION_DATE_FIELD, DateTools.dateToString(modDate,DateTools.Resolution.SECOND), Field.Store.YES, Field.Index.UN_TOKENIZED);
         	doc.add(modificationDateField);        
-        	Field modificationDayField = Field.Keyword(MODIFICATION_DAY_FIELD, formatDayString(modDate));
+        	Field modificationDayField = new Field(MODIFICATION_DAY_FIELD, formatDayString(modDate), Field.Store.YES, Field.Index.UN_TOKENIZED);
             doc.add(modificationDayField);
     	}
     }
@@ -154,18 +155,18 @@ public class EntityIndexUtils {
     		if (workflowStates != null) {
     			for (Iterator iter=workflowStates.iterator(); iter.hasNext();) {
     				WorkflowState ws = (WorkflowState)iter.next();
-    				Field workflowStateField = Field.Keyword(WORKFLOW_STATE_FIELD, 
-   						ws.getState());
-    				Field workflowStateCaptionField = Field.Keyword(WORKFLOW_STATE_CAPTION_FIELD, 
-   						WorkflowUtils.getStateCaption(ws.getDefinition(), ws.getState()));
+    				Field workflowStateField = new Field(WORKFLOW_STATE_FIELD, 
+   						ws.getState(), Field.Store.YES, Field.Index.UN_TOKENIZED);
+    				Field workflowStateCaptionField = new Field(WORKFLOW_STATE_CAPTION_FIELD, 
+   						WorkflowUtils.getStateCaption(ws.getDefinition(), ws.getState()), Field.Store.YES, Field.Index.UN_TOKENIZED);
     				//Index the workflow state
     				doc.add(workflowStateField);
     				doc.add(workflowStateCaptionField);
    				
     				Definition def = ws.getDefinition();
     				if (def != null) {
-    					Field workflowProcessField = Field.Keyword(WORKFLOW_PROCESS_FIELD, 
-    							def.getId());
+    					Field workflowProcessField = new Field(WORKFLOW_PROCESS_FIELD, 
+    							def.getId(), Field.Store.YES, Field.Index.UN_TOKENIZED);
     					//	Index the workflow title (which is always the id of the workflow definition)
     					doc.add(workflowProcessField);
     				}
@@ -195,21 +196,21 @@ public class EntityIndexUtils {
 			if (att.getValueType() == CustomAttribute.EVENT) {
 				// set the event name to event + count
 				if (att.getValue() != null) {
-					eventName = Field.Keyword(EVENT_FIELD + count, att.getName());
+					eventName = new Field(EVENT_FIELD + count, att.getName(), Field.Store.YES, Field.Index.UN_TOKENIZED);
 					doc.add(eventName);
 					count++;
 				}
 			}
 		}    	
 		// Add event count field
-    	Field eventCountField = Field.Keyword(EVENT_COUNT_FIELD, Integer.toString(count));
+    	Field eventCountField = new Field(EVENT_COUNT_FIELD, Integer.toString(count), Field.Store.YES, Field.Index.UN_TOKENIZED);
     	doc.add(eventCountField);
   
     }
     
     public static void addCommandDefinition(Document doc, DefinableEntity entry) {
         if (entry.getEntryDef() != null) {
-        	Field cdefField = Field.Keyword(COMMAND_DEFINITION_FIELD, entry.getEntryDef().getId());
+        	Field cdefField = new Field(COMMAND_DEFINITION_FIELD, entry.getEntryDef().getId(), Field.Store.YES, Field.Index.UN_TOKENIZED);
             doc.add(cdefField);
         }
     }
@@ -217,7 +218,7 @@ public class EntityIndexUtils {
     public static void addCreationPrincipalId(Document doc, DefinableEntity entry) {
     	//Add the id of the creator (no, not that one...)
         if (entry.getCreation() != null && entry.getCreation().getPrincipal() != null) {
-        	Field creationIdField = Field.Keyword(CREATORID_FIELD, entry.getCreation().getPrincipal().getId().toString());
+        	Field creationIdField = new Field(CREATORID_FIELD, entry.getCreation().getPrincipal().getId().toString(), Field.Store.YES, Field.Index.UN_TOKENIZED);
             doc.add(creationIdField);
         }
     }   
@@ -225,23 +226,23 @@ public class EntityIndexUtils {
     public static void addModificationPrincipalId(Document doc, DefinableEntity entry) {
     	//Add the id of the creator (no, not that one...)
         if (entry.getModification() != null && entry.getModification().getPrincipal() != null) {
-        	Field modificationIdField = Field.Keyword(MODIFICATIONID_FIELD, entry.getModification().getPrincipal().getId().toString());
+        	Field modificationIdField = new Field(MODIFICATIONID_FIELD, entry.getModification().getPrincipal().getId().toString(), Field.Store.YES, Field.Index.UN_TOKENIZED);
         	doc.add(modificationIdField);
         }
     }   
 
      public static void addDocId(Document doc, DefinableEntity entry) {
     	//Add the id of the creator (no, not that one...)
-        Field docIdField = Field.Keyword(DOCID_FIELD, entry.getId().toString());
+        Field docIdField = new Field(DOCID_FIELD, entry.getId().toString(), Field.Store.YES, Field.Index.UN_TOKENIZED);
         doc.add(docIdField);
     }
 
     public static void addBinder(Document doc, Binder binder) {
     	Field binderIdField;
     	if (binder != null)
-    		binderIdField = Field.Keyword(BINDER_ID_FIELD, binder.getId().toString());
+    		binderIdField = new Field(BINDER_ID_FIELD, binder.getId().toString(), Field.Store.YES, Field.Index.UN_TOKENIZED);
     	else
-    		binderIdField = Field.Keyword(BINDER_ID_FIELD, "");
+    		binderIdField = new Field(BINDER_ID_FIELD, "", Field.Store.YES, Field.Index.UN_TOKENIZED);
     		
        	doc.add(binderIdField);
     }   
@@ -262,7 +263,7 @@ public class EntityIndexUtils {
    		for (Iterator i = ids.iterator(); i.hasNext();) {
     		pIds.append(i.next()).append(" ");
     	}
-        racField = new Field(BasicIndexUtils.READ_ACL_FIELD, pIds.toString(), true, true, true);      
+        racField = new Field(BasicIndexUtils.READ_ACL_FIELD, pIds.toString(), Field.Store.YES, Field.Index.TOKENIZED);      
         doc.add(racField);
     }
 
@@ -299,23 +300,23 @@ public class EntityIndexUtils {
 			aclTags += " " + BasicIndexUtils.buildAclTag(thisTag.getName(), thisTag.getOwnerIdentifier().getEntityId().toString());
 		}
     
-    	Field tagField = new Field(BasicIndexUtils.TAG_FIELD, indexableTags, true, true, true);
+    	Field tagField = new Field(BasicIndexUtils.TAG_FIELD, indexableTags, Field.Store.YES, Field.Index.TOKENIZED);
     	doc.add(tagField);
     	
     	tagField = BasicIndexUtils.allTextField(indexableTags);
     	doc.add(tagField);
     	
-    	tagField = new Field(BasicIndexUtils.ACL_TAG_FIELD, aclTags, true, true, true);
+    	tagField = new Field(BasicIndexUtils.ACL_TAG_FIELD, aclTags, Field.Store.YES, Field.Index.TOKENIZED);
     	doc.add(tagField);
     }
 	
     public static void addFileAttachmentName(Document doc,String filename) {
-      	Field fileNameField = Field.Keyword(FILENAME_FIELD, filename);
+      	Field fileNameField = new Field(FILENAME_FIELD, filename, Field.Store.YES, Field.Index.UN_TOKENIZED);
        	doc.add(fileNameField);
     }
     
     public static void addFileExtension(Document doc,String fileName) {
-      	Field fileExtField = Field.Keyword(FILE_EXT_FIELD, getFileExtension(fileName));
+      	Field fileExtField = new Field(FILE_EXT_FIELD, getFileExtension(fileName), Field.Store.YES, Field.Index.UN_TOKENIZED);
        	doc.add(fileExtField);   	
     }
 
@@ -336,7 +337,7 @@ public class EntityIndexUtils {
 		List nodes = document.selectNodes("/searchml");
 		
       	
-		Field fileTypeField = Field.Keyword(FILE_TYPE_FIELD, x.getText());
+		Field fileTypeField = new Field(FILE_TYPE_FIELD, x.getText(), Field.Store.YES, Field.Index.UN_TOKENIZED);
        	doc.add(fileTypeField);   	
 	
 		
@@ -355,7 +356,7 @@ public class EntityIndexUtils {
     }
     
     public static void addFileAttachmentUid(Document doc, FileAttachment fa) {
-       	Field fileIDField = Field.Keyword(FILE_ID_FIELD, fa.getId());
+       	Field fileIDField = new Field(FILE_ID_FIELD, fa.getId(), Field.Store.YES, Field.Index.UN_TOKENIZED);
     	doc.add(fileIDField); 
     }
 }
