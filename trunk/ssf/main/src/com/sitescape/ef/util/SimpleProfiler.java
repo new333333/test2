1b1b4c7bc9ec739d309449ce45b665a242ee0f84
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2006-09-21 18:56:47 +0000

    Use simplified profiler object for gathering very basic timing information; Doesn't affect production.

--- trunk/ssf/main/src/com/sitescape/ef/util/SimpleProfiler.java
+++ trunk/ssf/main/src/com/sitescape/ef/util/SimpleProfiler.java
@@ -13,40 +13,89 @@ import org.apache.commons.logging.LogFactory;
 public class SimpleProfiler {
 	private static final Log logger = LogFactory.getLog(SimpleProfiler.class);
 
+	private boolean active = true; // defaults to true 
 	private String title;
-    private long t1;
-    private long t2;
+    private long t1; // nano time
+    private long t2; // nano time
     private long total = 0;
     private int count = 0;
 
+    public SimpleProfiler() {}
+    public SimpleProfiler(boolean active) {
+    	this.active = active;
+    }
     public SimpleProfiler(String title) {
-        this.title = title;
+    	this.title = title;
+    }
+    public SimpleProfiler reset(String title) {
+    	if(active) {
+	    	this.title = title;
+	    	this.t1 = 0;
+	    	this.t2 = 0;
+	    	this.total = 0;
+	    	this.count = 0;
+    	}
+	    return this;
     }
     public void begin() {
-        t1 = System.currentTimeMillis();
+    	if(active)
+    		t1 = System.nanoTime();
     }
-    public void end() {
-        t2 = System.currentTimeMillis();
-        total += (t2-t1);
-        count++;
+    public SimpleProfiler end() {
+    	if(active) {
+	        t2 = System.nanoTime();
+	        total += (t2-t1);
+	        count++;
+    	}
+        return this;
     }
-    public long totalTime() {
-        return total;
+    public long totalNSTime() {
+    	if(active)
+    		return total;
+    	else
+    		return 0;
     }
-    public double averageTime() {
-        return ((double)total) / count;
+    public double averageNSTime() {
+    	if(active) {
+	    	if(count > 0)
+	    		return ((double)total) / count;
+	    	else
+	    		return 0;
+    	}
+    	else {
+    		return 0;
+    	}
+    }
+    public double totalMSTime() {
+    	if(active)
+    		return totalNSTime() / 1000000.0;
+    	else
+    		return 0.0;
+    }
+    public double averageMSTime() {
+    	if(active)
+    		return averageNSTime() / 1000000.0;
+    	else
+    		return 0.0;
     }
     public String toString() {
-        return new StringBuffer(title).append(": count = ").append(count).append(" total = ").append(total).append(" (ms) average = ").append(averageTime()).append(" (ms)").toString(); 
+    	if(active)
+    		return new StringBuffer(title).append(": count = ").append(count).append(" total = ").append(totalMSTime()).append(" (ms) average = ").append(averageMSTime()).append(" (ms)").toString();
+    	else
+    		return "";
     }
     public void logDebug() {
-        if(logger.isDebugEnabled())
-            logger.debug(toString());
+    	if(active) {
+    		if(logger.isDebugEnabled())
+    			logger.debug(toString());
+    	}
     }
     public void logInfo() {
-        logger.info(toString());
+    	if(active)
+    		logger.info(toString());
     }
     public void print() {
-        System.out.println(toString());
+    	if(active)
+    		System.out.println("*** " + toString());
     }
 }
