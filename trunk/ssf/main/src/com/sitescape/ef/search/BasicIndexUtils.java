825dfa38d71e9907d551ec5b0233d8e239eea44f
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-01-02 21:31:58 +0000

    Add methods to sort thru tags. (orig developed by Hemanth, fixed and modified for more generic use by Roy)

--- trunk/ssf/main/src/com/sitescape/ef/search/BasicIndexUtils.java
+++ trunk/ssf/main/src/com/sitescape/ef/search/BasicIndexUtils.java
@@ -1,15 +1,24 @@
 package com.sitescape.ef.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 
+import com.sitescape.ef.context.request.RequestContextHolder;
+import com.sitescape.ef.domain.User;
 import com.sitescape.ef.security.acl.AclContainer;
 import com.sitescape.ef.security.acl.AccessType;
 import com.sitescape.ef.security.acl.AclManager;
 import com.sitescape.ef.security.acl.AclControlled;
+import com.sitescape.ef.web.WebKeys;
 /**
  *
  * @author Jong Kim
@@ -161,4 +170,120 @@ public class BasicIndexUtils {
     	String aclTag = BasicIndexUtils.TAG_ACL_PRE + aclId + BasicIndexUtils.TAG + tag;
     	return aclTag;
     }
+    
+    
+	// This method reads thru the results from a search, finds the unique community tags, 
+	// and places them into an array of hashmaps(each hashmap contains the tag 
+    // and it's occurence count), in alphabetic order.    
+	public static List sortCommunityTags(List entries) {
+		HashMap tagMap = new HashMap();
+		ArrayList tagList = new ArrayList();
+		// first go thru the original search results and 
+		// find all the unique tagfields.  Keep a count to see
+		// if any are more active than others.
+		for (int i = 0; i < entries.size(); i++) {
+			Map entry = (Map)entries.get(i);
+			String strTags = (String)entry.get(WebKeys.SEARCH_TAG_ID);
+			if (strTags == null || "".equals(strTags)) continue;
+			
+		    String [] strTagArray = strTags.split("\\s");
+		    for (int j = 0; j < strTagArray.length; j++) {
+		    	String strTag = strTagArray[j];
+
+		    	if (strTag.equals("")) continue;
+		    	
+		    	Integer tagCount = (Integer) tagMap.get(strTag);
+		    	if (tagCount == null) {
+		    		tagMap.put(strTag, new Integer(1));
+		    	}
+		    	else {
+		    		int intTagCount = tagCount.intValue();
+		    		tagMap.put(strTag, new Integer(intTagCount+1));
+		    	}
+		    }
+		}
+		
+		//sort the tags string
+		Collection collection = tagMap.keySet();
+		Object[] array = collection.toArray();
+		Arrays.sort(array);
+		
+		for (int j = 0; j < array.length; j++) {
+			HashMap tags = new HashMap();
+			String strTag = (String) array[j];
+			tags.put(WebKeys.TAG_NAME, strTag);
+			tags.put(WebKeys.SEARCH_RESULTS_COUNT, (Integer) tagMap.get(strTag));
+			tagList.add(tags);
+		}
+		return tagList;
+	}
+
+
+
+
+
+	// This method reads thru the results from a search, finds the unique personal tags, 
+	// and places them into an array of hashmaps(each hashmap contains the tag 
+    // and it's occurence count), in alphabetic order.
+	//
+	public static List sortPersonalTags(List entries) {
+		HashMap tagMap = new HashMap();
+		ArrayList tagList = new ArrayList();
+
+		User user = RequestContextHolder.getRequestContext().getUser();
+    	long userId = user.getId();
+    	
+    	for (int i = 0; i < entries.size(); i++) {
+			Map entry = (Map) entries.get(i);
+			String strTags = (String) entry.get(WebKeys.SEARCH_ACL_TAG_ID);
+			if (strTags == null || "".equals(strTags))
+				continue;
+
+			String[] strTagArray = strTags.split(TAG_ACL_PRE);
+			for (int j = 0; j < strTagArray.length; j++) {
+				String strTag = strTagArray[j].trim();
+				if (strTag.equals(""))
+					continue;
+
+				// Ignore these entries as they refer to community entries.
+				if (strTag.startsWith(READ_ACL_ALL + TAG))
+					continue;
+
+				String strUserIdTag = userId + TAG;
+
+				// We are going to get only the personal tags relating to the
+				// user
+				if (strTag.startsWith(strUserIdTag)) {
+					String thisTag = strTag.substring(strUserIdTag.length()).trim();
+
+					if (thisTag.equals(""))
+						continue;
+
+					Integer tagCount = (Integer) tagMap.get(thisTag);
+					if (tagCount == null) {
+						tagMap.put(thisTag, new Integer(1));
+					} else {
+						int intTagCount = tagCount.intValue();
+						tagMap.put(thisTag, new Integer(intTagCount + 1));
+					}
+				} else
+					continue;
+			}
+		}
+
+		// sort the tags string
+		Collection collection = tagMap.keySet();
+		Object[] array = collection.toArray();
+		Arrays.sort(array);
+		
+		for (int j = 0; j < array.length; j++) {
+			HashMap tags = new HashMap();
+			String strTag = (String) array[j];
+			tags.put(WebKeys.TAG_NAME, strTag);
+			tags.put(WebKeys.SEARCH_RESULTS_COUNT, (Integer) tagMap.get(strTag));
+			tagList.add(tags);
+		}
+		return tagList;
+	}
+	
 }
