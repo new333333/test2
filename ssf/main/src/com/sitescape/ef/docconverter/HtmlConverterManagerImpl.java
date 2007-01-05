e8c7c9b9f086f338b6b98b82fc9fde788450b83a
rsordillo <rsordillo@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-01-05 14:44:55 +0000

    View as HTML functionality

---
+++ ssf/main/src/com/sitescape/ef/docconverter/HtmlConverterManagerImpl.java
@@ -0,0 +1,63 @@
+package com.sitescape.ef.docconverter;
+
+public class HtmlConverterManagerImpl
+	implements IHtmlConverterManager
+{
+	int _active = IHtmlConverterManager.OPENOFFICE;
+	HtmlStellentConverter _stellentConverter = null;
+	HtmlOpenOfficeConverter _openOfficeConverter = null;
+	
+	public HtmlConverterManagerImpl() {}
+	
+	public HtmlConverter getConverter()
+	{
+		if (_active == IHtmlConverterManager.STELLANT)
+			return _stellentConverter;
+		else
+		if (_active == IHtmlConverterManager.OPENOFFICE)
+			return _openOfficeConverter;
+		
+		return null;
+	}
+	
+	public HtmlConverter getConverter(int type)
+	{
+		if (type == IHtmlConverterManager.STELLANT)
+			return _stellentConverter;
+		else
+		if (type == IHtmlConverterManager.OPENOFFICE)
+			return _openOfficeConverter;
+		
+		return null;
+	}
+	
+	public void setActive(int active)
+	{
+		_active = active;
+	}
+	
+	public int getActive()
+	{
+		return _active;
+	}
+	
+	public void setStellentConverter(HtmlStellentConverter converter_in)
+	{
+		_stellentConverter = converter_in;
+	}
+	
+	public HtmlStellentConverter getStellentConverter()
+	{
+		return _stellentConverter;
+	}
+	
+	public void setOpenOfficeConverter(HtmlOpenOfficeConverter converter_in)
+	{
+		_openOfficeConverter = converter_in;
+	}
+	
+	public HtmlOpenOfficeConverter getOpenOfficeConverter()
+	{
+		return _openOfficeConverter;
+	}
+}
