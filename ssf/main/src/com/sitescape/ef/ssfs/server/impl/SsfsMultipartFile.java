ca7ac8ed50af5c78023931f31668f985760c1eaf
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-01-22 18:29:08 +0000

    Add support for storing and addressing image files uploaded through html editor

--- ssf/main/src/com/sitescape/ef/ssfs/server/impl/SsfsMultipartFile.java
+++ ssf/main/src/com/sitescape/ef/ssfs/server/impl/SsfsMultipartFile.java
@@ -1,84 +1,23 @@
 package com.sitescape.ef.ssfs.server.impl;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.sitescape.ef.util.FileModDateSupport;
+import com.sitescape.ef.util.SimpleMultipartFile;
 
-public class SsfsMultipartFile implements MultipartFile, FileModDateSupport {
+public class SsfsMultipartFile extends SimpleMultipartFile implements FileModDateSupport {
 
-	private String fileName;
-	private InputStream content;
 	private Date modDate;
 
 	public SsfsMultipartFile(String fileName, InputStream content) {
-		this.fileName = fileName;
-		this.content = content;
-	}
-
-	public SsfsMultipartFile(String fileName, InputStream content, 
-			Date modificationDate) {
-		this.fileName = fileName;
-		this.content = content;
-		this.modDate = modificationDate;
+		super(fileName, content);
 	}
 	
-	public String getName() {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean isEmpty() {
-		throw new UnsupportedOperationException();
-	}
-
-	public String getOriginalFilename() {
-		return fileName;
-	}
-
-	public String getContentType() {
-		return null;
-	}
-
-	/**
-	 * Return the size of the file in bytes.
-	 * <p>
-	 * Note: This method overrides/extends the semantics of the interface 
-	 * it is implementing. The interface does not specify what to return
-	 * when the size is unknown. In other word, it assumes that the size
-	 * is always computable. This particular implementation extends the
-	 * semantics by allowing for return value of -1 (or any negative 
-	 * long value) to indicate to the caller that the size is unknown.
-	 * In purely OO sense, this is a violation of the contract defined
-	 * by the interface, hence very bad programming practice that should be
-	 * avoided whenever possible. However, the cost of avoiding this
-	 * hack is too high for our project. So I think it justifies this
-	 * small wrinkle (we are not living in an ideal world). 
-	 */
-	public long getSize() {
-		return -1;
-	}
-
-	public byte[] getBytes() throws IOException {
-		return FileCopyUtils.copyToByteArray(content);
-	}
-
-	public InputStream getInputStream() throws IOException {
-		return content;
-	}
-
-	public void transferTo(File dest) throws IOException, IllegalStateException {
-		if (dest.exists() && !dest.delete()) {
-			throw new IOException(
-					"Destination file [" + dest.getAbsolutePath() + "] already exists and could not be deleted");
-		}
-		FileCopyUtils.copy(content, new BufferedOutputStream(new FileOutputStream(dest)));
+	public SsfsMultipartFile(String fileName, InputStream content, 
+			Date modificationDate) {
+		super(fileName, content);
+		this.modDate = modificationDate;
 	}
 
 	public Date getModDate() {
