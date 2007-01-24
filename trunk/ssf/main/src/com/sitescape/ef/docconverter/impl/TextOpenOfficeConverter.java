12cc29e7b9fe0a598943205990c42bea9bbe1232
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-01-24 15:26:13 +0000

    Fake implementation of text converter based on OpenOffice; To be replaced with real implementation

---
+++ trunk/ssf/main/src/com/sitescape/ef/docconverter/impl/TextOpenOfficeConverter.java
@@ -0,0 +1,13 @@
+package com.sitescape.ef.docconverter.impl;
+
+import java.io.File;
+
+import com.sitescape.ef.docconverter.TextConverter;
+
+public class TextOpenOfficeConverter implements TextConverter {
+
+	public String convertToText(File inputFile, long timeout) throws Exception {
+		return ""; // No converion for now
+	}
+
+}
