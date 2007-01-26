6c83040cd06d44c4e24ef2f5357e004bbc5f04e0
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-01-26 18:43:50 +0000

    Added comment

--- trunk/ssf/main/src/com/sitescape/ef/module/file/ArchiveStore.java
+++ trunk/ssf/main/src/com/sitescape/ef/module/file/ArchiveStore.java
@@ -6,13 +6,16 @@ import com.sitescape.ef.domain.VersionAttachment;
 import com.sitescape.ef.domain.DefinableEntity;
 
 public interface ArchiveStore {
-
-	/*
-	public String write(String zoneName, Long binderId, String entityId, 
-			String repositoryName, int versionNo, String fileName,
-			InputStream content) throws UncheckedIOException;
-			*/
 	
+	/**
+	 * Archive the file version and return its unique URI or <code>null</code>
+	 * if archiving is not supported.
+	 * 
+	 * @param binder
+	 * @param entity
+	 * @param v
+	 * @return	archive URI or <code>null</code>
+	 */
 	public String write(Binder binder, DefinableEntity entity, 
 			VersionAttachment v) throws UncheckedIOException; 
 }
