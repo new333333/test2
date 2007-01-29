892c78311bb29c8109eabf709e4535e2cf08ab59
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-01-29 13:55:34 +0000

    backout inheritAcl change.  Breaks add_entry

--- ssf/main/src/com/sitescape/ef/domain/WorkflowControlledEntry.java
+++ ssf/main/src/com/sitescape/ef/domain/WorkflowControlledEntry.java
@@ -100,7 +100,7 @@ public abstract class WorkflowControlledEntry extends Entry
      * 
      */
     public boolean getInheritAclFromParent() {
-    	return false;
+    	return !hasAclSet();
     }
 
     /**
