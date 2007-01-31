0567b15ed1e2c91b8b0fd04e8dc83fa13d83f4b4
rsordillo <rsordillo@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-01-31 19:43:26 +0000

    Converter functionality

---
+++ ssf/main/src/com/sitescape/ef/docconverter/ImageConverterManagerImpl.java
@@ -0,0 +1,63 @@
+package com.sitescape.ef.docconverter;
+
+public class ImageConverterManagerImpl
+	implements IImageConverterManager
+{
+	int _active = IImageConverterManager.OPENOFFICE;
+	ImageStellentConverter _stellentConverter = null;
+	ImageOpenOfficeConverter _openOfficeConverter = null;
+	
+	public ImageConverterManagerImpl() {}
+	
+	public ImageConverter getConverter()
+	{
+		if (_active == IImageConverterManager.STELLANT)
+			return _stellentConverter;
+		else
+		if (_active == IImageConverterManager.OPENOFFICE)
+			return _openOfficeConverter;
+		
+		return null;
+	}
+	
+	public ImageConverter getConverter(int type)
+	{
+		if (type == IImageConverterManager.STELLANT)
+			return _stellentConverter;
+		else
+		if (type == IImageConverterManager.OPENOFFICE)
+			return _openOfficeConverter;
+		
+		return null;
+	}
+	
+	public void setActive(int active)
+	{
+		_active = active;
+	}
+	
+	public int getActive()
+	{
+		return _active;
+	}
+	
+	public void setStellentConverter(ImageStellentConverter converter_in)
+	{
+		_stellentConverter = converter_in;
+	}
+	
+	public ImageStellentConverter getStellentConverter()
+	{
+		return _stellentConverter;
+	}
+
+	public void setOpenOfficeConverter(ImageOpenOfficeConverter converter_in)
+	{
+		_openOfficeConverter = converter_in;
+	}
+	
+	public ImageOpenOfficeConverter getOpenOfficeConverter()
+	{
+		return _openOfficeConverter;
+	}
+}
---
+++ ssf/main/src/com/sitescape/ef/docconverter/TextConverterManagerImpl.java
@@ -0,0 +1,63 @@
+package com.sitescape.ef.docconverter;
+
+public class TextConverterManagerImpl
+	implements ITextConverterManager
+{
+	int _active = ITextConverterManager.OPENOFFICE;
+	TextStellentConverter _stellentConverter = null;
+	TextOpenOfficeConverter _openOfficeConverter = null;
+	
+	public TextConverterManagerImpl() {}
+	
+	public TextConverter getConverter()
+	{
+		if (_active == ITextConverterManager.STELLANT)
+			return _stellentConverter;
+		else
+		if (_active == ITextConverterManager.OPENOFFICE)
+			return _openOfficeConverter;
+		
+		return null;
+	}
+	
+	public TextConverter getConverter(int type)
+	{
+		if (type == ITextConverterManager.STELLANT)
+			return _stellentConverter;
+		else
+		if (type == ITextConverterManager.OPENOFFICE)
+			return _openOfficeConverter;
+		
+		return null;
+	}
+	
+	public void setActive(int active)
+	{
+		_active = active;
+	}
+	
+	public int getActive()
+	{
+		return _active;
+	}
+	
+	public void setStellentConverter(TextStellentConverter converter_in)
+	{
+		_stellentConverter = converter_in;
+	}
+	
+	public TextStellentConverter getStellentConverter()
+	{
+		return _stellentConverter;
+	}
+
+	public void setOpenOfficeConverter(TextOpenOfficeConverter converter_in)
+	{
+		_openOfficeConverter = converter_in;
+	}
+	
+	public TextOpenOfficeConverter getOpenOfficeConverter()
+	{
+		return _openOfficeConverter;
+	}
+}
