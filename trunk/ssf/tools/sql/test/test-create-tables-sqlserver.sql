36db14e6ae39a1402a2cde0afc229b09da4af2ad
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-02-07 16:26:18 +0000

    More work on the source tree and build, etc, to support 3X splits

--- trunk/ssf/tools/sql/test/test-create-tables-sqlserver.sql
+++
@@ -1,7 +0,0 @@
-drop table Employees;
-create table Employees (
-    id numeric(19,0) identity not null,
-    firstName varchar(32) not null,
-    lastName varchar(32) not null,
-    age tinyint null
-);
