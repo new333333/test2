12bd727d7fd889e22cd0ae93ad1fea7232869525
griffin <griffin@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-02-07 14:42:12 +0000

    Refactor code to remove Java code from the JSP.  (No Scriptlets!) Also adding some time zone information whilst we straighten out (finish) that work.

--- ssf/taglib/src/com/sitescape/team/taglib/Eventtext.java
+++ ssf/taglib/src/com/sitescape/team/taglib/Eventtext.java
@@ -3,6 +3,20 @@
  *
  */
 package com.sitescape.team.taglib;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.tagext.TagSupport;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+
+import com.sitescape.team.context.request.RequestContextHolder;
+import com.sitescape.team.domain.User;
+import com.sitescape.team.util.NLT;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.GregorianCalendar;
+import java.util.regex.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -61,8 +75,6 @@ public class Eventtext extends TagSupport {
       String jsp = "/WEB-INF/jsp/tag_jsps/eventtext/eventtext.jsp";
       RequestDispatcher rd = ctx.getRequestDispatcher(jsp); 
 
-      // any attributes we might want to pass into the jsp go here
-      req.setAttribute("event", event);
       // in addition to the raw event, we disintangle some of the recurrence stuff
       // to make the jsp page less complex
       DayAndPosition dpa[] = event.getByDay();
@@ -80,14 +92,102 @@ public class Eventtext extends TagSupport {
           bynum = nn;
       }
       
+      User user = RequestContextHolder.getRequestContext().getUser();
+
       req.setAttribute("bydays", bydays);
-      req.setAttribute("bynum", bynum);
-      
+      req.setAttribute("bynum", bynum);      
+
       Calendar st = event.getDtStart();
       Calendar en = event.getDtEnd();
-      SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy, h a");
+      Calendar un = event.getUntil();
+      
+      // array of text strings for days of the week
+      // these should be removed from this file (mhu)
+      String days[] = new String[10];
+      days[Calendar.SUNDAY] = NLT.get("calendar.day.abbrevs.su");
+      days[Calendar.MONDAY] = NLT.get("calendar.day.abbrevs.mo");
+      days[Calendar.TUESDAY] = NLT.get("calendar.day.abbrevs.tu");
+      days[Calendar.WEDNESDAY] = NLT.get("calendar.day.abbrevs.we");
+      days[Calendar.THURSDAY] = NLT.get("calendar.day.abbrevs.th");
+      days[Calendar.FRIDAY] = NLT.get("calendar.day.abbrevs.fr");
+      days[Calendar.SATURDAY] = NLT.get("calendar.day.abbrevs.sa");
+
+      String nums[] = new String[6];
+      nums[1] = NLT.get("calendar.first");
+      nums[2] = NLT.get("calendar.second");
+      nums[3] = NLT.get("calendar.third");
+      nums[4] = NLT.get("calendar.fourth");
+      nums[5] = NLT.get("calendar.last");
+
+      SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy, hh:mm a z");
+      sdf.setTimeZone(user.getTimeZone());
       String startString = sdf.format(st.getTime());
       String endString = sdf.format(en.getTime());
+
+      long interval = event.getDuration().getInterval();
+      String freqString = event.getFrequencyString();
+      String onString = "";
+      String untilString = "";
+      String onStringSeparator = "";
+      if (freqString == null) {
+    	  freqString = "does not repeat";
+      } else {
+    	  freqString = freqString.toLowerCase();
+    	  if (event.getInterval() > 1) {
+    		  freqString = "every " + event.getInterval();
+    		  if (event.getFrequency() == Event.DAILY) {
+    			  freqString += " days";
+    		  }
+    		  if (event.getFrequency() == Event.WEEKLY) {
+    			  freqString += " weeks";
+    		  }
+    		  if (event.getFrequency() == Event.MONTHLY) {
+    			  freqString += " months";
+    		  }
+    	  }
+    	  Iterator it = bydays.listIterator();
+    	  
+    	  // format weekly events as comma-separated list of ondays
+    	  if (event.getFrequency() == Event.WEEKLY && it.hasNext()) {
+    		  onString += "on ";
+    		  while (it.hasNext()) {
+    			  Integer ii = (Integer) it.next();
+    			  onString += onStringSeparator + days[ii.intValue()];
+    			  onStringSeparator = ", ";
+    		  }
+    	  }
+    	  // monthly events include the ondaycard stuff
+    	  // note that bydays will now only have one entry (it may be "weekday")
+    	  // and bynum will be meaningful here (again, it is a singleton, not a list)
+    	  if (event.getFrequency() == Event.MONTHLY && it.hasNext()) {
+    		  Integer ii = (Integer) it.next();
+    		  onString += "on the " + nums[bynum.intValue()] + " ";
+    		  onString += days[ii.intValue()];
+    	  }
+      }
+      if (event.getFrequencyString() != null) {
+    	  untilString += "<br>Repeats: ";
+    	  if (event.getCount() == 0) {
+    		  untilString += "indefinitely";
+    	  } else if (event.getCount() == -1) {
+    		  untilString += "until " + sdf.format(un.getTime());
+    	  } else {
+    		  untilString += event.getCount() + " times";
+    	  }
+      }
+  	
+      
+      req.setAttribute("startString", startString);
+      req.setAttribute("endString", endString);
+      req.setAttribute("freqString", freqString);
+      req.setAttribute("onString", onString);
+      req.setAttribute("untilString", untilString);
+      if (interval > 0) {
+    	  req.setAttribute("hasDuration", "yes");
+      } else {
+    	  req.setAttribute("hasDuration", "no");
+      }
+      
       
       StringServletResponse res =
           new StringServletResponse((HttpServletResponse)pageContext.getResponse());
