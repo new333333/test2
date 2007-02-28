2e5864d619ebc645e3fa38cf1f9897cd5b81dea3
rsordillo <rsordillo@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-02-28 13:36:53 +0000

    Converter functionality

--- ssf/main/src/com/sitescape/team/docconverter/IImageConverterManager.java
+++ ssf/main/src/com/sitescape/team/docconverter/IImageConverterManager.java
@@ -4,7 +4,9 @@ public interface IImageConverterManager
 {
 	static final String IMG_EXTENSION = ".jpg";
 	static final int OPENOFFICE = 1,
-	 				 STELLANT = 2;
+	 				 STELLANT = 2,
+	 				 IMAGEWIDTH = 200,
+	 				 IMAGEHEIGHT = 200;
 
 	public ImageConverter getConverter();
 	public ImageConverter getConverter(int type);
