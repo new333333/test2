5169b973cd367bdce7f3bfb7a19ed1d142cfb8d0
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-03-24 03:39:56 +0000

    Expose very basic statistics info for folder module

---
+++ ssf/main/src/com/sitescape/team/module/folder/impl/FolderModuleImplMBean.java
@@ -0,0 +1,9 @@
+package com.sitescape.team.module.folder.impl;
+
+public interface FolderModuleImplMBean {
+
+	public void clearStatistics();
+	public int getAddEntryCount();
+	public int getModifyEntryCount();
+	public int getDeleteEntryCount();
+}
