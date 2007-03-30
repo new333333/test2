cd080ba2ae978699f4fccac1d3bc289f8ae8a53c
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-03-30 16:33:09 +0000

    Manage groups rewritten (still doesn't work in IE)

--- trunk/ssf/main/src/com/sitescape/team/portlet/administration/ManageGroupsController.java
+++ trunk/ssf/main/src/com/sitescape/team/portlet/administration/ManageGroupsController.java
@@ -1,8 +1,11 @@
 package com.sitescape.team.portlet.administration;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -12,18 +15,23 @@ import javax.portlet.RenderResponse;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sitescape.team.ObjectKeys;
+import com.sitescape.team.context.request.RequestContextHolder;
 import com.sitescape.team.domain.Binder;
+import com.sitescape.team.domain.Group;
+import com.sitescape.team.domain.Principal;
 import com.sitescape.team.module.shared.EntityIndexUtils;
 import com.sitescape.team.module.shared.MapInputData;
 import com.sitescape.team.portletadapter.MultipartFileSupport;
 import com.sitescape.team.web.WebKeys;
 import com.sitescape.team.web.portlet.SAbstractController;
+import com.sitescape.team.web.util.FindIdsHelper;
 import com.sitescape.team.web.util.PortletRequestUtils;
 import com.sitescape.util.Validator;
 public class ManageGroupsController extends  SAbstractController {
 	
 	public void handleActionRequestAfterValidation(ActionRequest request, ActionResponse response) throws Exception {
 		Map formData = request.getParameterMap();
+		response.setRenderParameters(formData);
 		if (formData.containsKey("addBtn")) {
 			Long binderId = PortletRequestUtils.getRequiredLongParameter(request, WebKeys.URL_BINDER_ID);
 			//make sure it is present
@@ -36,10 +44,24 @@ public class ManageGroupsController extends  SAbstractController {
 				fileMap = new HashMap();
 			}
 			getProfileModule().addGroup(binderId, null, inputData, fileMap);
+			
+		} else if (formData.containsKey("applyBtn") || formData.containsKey("okBtn")) {
+			Long binderId = PortletRequestUtils.getRequiredLongParameter(request, WebKeys.URL_BINDER_ID);
+			Long groupId = PortletRequestUtils.getRequiredLongParameter(request, WebKeys.URL_ENTRY_ID);
+			String title = PortletRequestUtils.getStringParameter(request, "title", "");
+			String description = PortletRequestUtils.getStringParameter(request, "description", "");
+			Set ids = FindIdsHelper.getIdsAsLongSet(request.getParameterValues("users"));
+			ids.addAll(FindIdsHelper.getIdsAsLongSet(request.getParameterValues("groups")));
+			List principals = getProfileModule().getPrincipals(ids, RequestContextHolder.getRequestContext().getZoneId());
+			Map updates = new HashMap();
+			updates.put(ObjectKeys.FIELD_ENTITY_TITLE, title);
+			updates.put(ObjectKeys.FIELD_ENTITY_DESCRIPTION, description);
+			updates.put(ObjectKeys.FIELD_GROUP_MEMBERS, principals);
+			getProfileModule().modifyEntry(binderId, groupId, new MapInputData(updates));
+		
 		} else if (formData.containsKey("closeBtn") || formData.containsKey("cancelBtn")) {
 			response.setRenderParameter("redirect", "true");
-		} else
-			response.setRenderParameters(formData);
+		}
 	}
 
 	public ModelAndView handleRenderRequestInternal(RenderRequest request, 
@@ -59,6 +81,24 @@ public class ManageGroupsController extends  SAbstractController {
 		model.put(WebKeys.BINDER, binder);
 		model.put(WebKeys.GROUP_LIST, groups);
 		
+		Long groupId = PortletRequestUtils.getLongParameter(request, WebKeys.URL_ENTRY_ID);
+		if (groupId != null) {
+			String namespace = PortletRequestUtils.getStringParameter(request, "namespace", "");
+			model.put(WebKeys.NAMESPACE, namespace);
+			model.put(WebKeys.BINDER_ID, binder.getId());
+			Group group = (Group)getProfileModule().getEntry(binder.getId(), groupId);		
+			model.put(WebKeys.GROUP, group);
+			List memberList = group.getMembers();
+			Set ids = new HashSet();
+			Iterator itUsers = memberList.iterator();
+			while (itUsers.hasNext()) {
+				Principal member = (Principal) itUsers.next();
+				ids.add(member.getId());
+			}
+			model.put(WebKeys.USERS, getProfileModule().getUsers(ids));
+			model.put(WebKeys.GROUPS, getProfileModule().getGroups(ids));
+		}
+
 		return new ModelAndView(WebKeys.VIEW_ADMIN_MANAGE_GROUPS, model);
 	}
 
