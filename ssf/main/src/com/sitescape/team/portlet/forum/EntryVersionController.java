92493f121cafad9b30e72bca777f7446407fcf8e
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-04-02 21:39:57 +0000

    Entry history (not done)

--- ssf/main/src/com/sitescape/team/portlet/forum/EntryVersionController.java
+++ ssf/main/src/com/sitescape/team/portlet/forum/EntryVersionController.java
@@ -1,5 +1,6 @@
 package com.sitescape.team.portlet.forum;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,9 +14,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.sitescape.team.context.request.RequestContextHolder;
 import com.sitescape.team.domain.Binder;
+import com.sitescape.team.domain.ChangeLog;
 import com.sitescape.team.domain.User;
 import com.sitescape.team.web.WebKeys;
 import com.sitescape.team.web.portlet.SAbstractController;
+import com.sitescape.team.web.util.BinderHelper;
 import com.sitescape.team.web.util.PortletRequestUtils;
 
 
@@ -27,19 +30,23 @@ public class EntryVersionController extends  SAbstractController {
 			RenderResponse response) throws Exception {
 		Long entityId = PortletRequestUtils.getRequiredLongParameter(request, WebKeys.URL_ENTITY_ID);
 		String operation = PortletRequestUtils.getStringParameter(request, WebKeys.URL_OPERATION, null);
-		Map formData = request.getParameterMap();
-		
+		String viewPath = "forum/view_description_history";
+		Map model = new HashMap();
+				
+		List changeList = new ArrayList();
 		List changes = null;
 		String entityType = PortletRequestUtils.getStringParameter(request,  "entityType", "folderEntry");
 		if (entityId != null) {
+			if (operation.equals(ChangeLog.MODIFYENTRY)) {
+				//Start the list with the "addEntry" entry
+				changes = getAdminModule().getChanges(entityId, entityType, ChangeLog.ADDENTRY);
+				changeList.addAll(BinderHelper.BuildChangeLogBeans(changes));
+			}
 			changes = getAdminModule().getChanges(entityId, entityType, operation);
+			changeList.addAll(BinderHelper.BuildChangeLogBeans(changes));
 		}
 		
-		String viewPath = "forum/view_description_history";
-		Map model = new HashMap();
-		
-		User user = RequestContextHolder.getRequestContext().getUser();
-		model.put("changeLogs", changes);
+		model.put(WebKeys.CHANGE_LOG_LIST, changeList);
 		
 		return new ModelAndView(viewPath, model);
 	} 
