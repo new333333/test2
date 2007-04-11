5ce4da162fe4eaa606586d001774e2704bdccb10
pawel <pawel@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-04-11 15:48:09 +0000

    duplicated copyright removed

--- trunk/ssf/web/docroot/js/common/ss_calendar.js
+++ trunk/ssf/web/docroot/js/common/ss_calendar.js
@@ -8,21 +8,6 @@
  * Copyright (c) 2007 SiteScape, Inc.
  *
  */
-//
-//   Copyright (c) 2007 / SiteScape, Inc.  All Rights Reserved.
-//
-//  This information in this document is subject to change without notice 
-//  and should not be construed as a commitment by SiteScape, Inc.  
-//  SiteScape, Inc. assumes no responsibility for any errors that may appear 
-//  in this document.
-//
-//  Restricted Rights:  Use, duplication, or disclosure by the U.S. Government 
-//  is subject to restrictions as set forth in subparagraph (c)(1)(ii) of the
-//  Rights in Technical Data and Computer Software clause at DFARS 252.227-7013.
-//
-//  SiteScape and SiteScape Forum are trademarks of SiteScape, Inc.
-//
-//
 
 dojo.require("dojo.html");
 dojo.require("dojo.html.util");
