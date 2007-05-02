affae44784a16dbc879ba48baf375d74e49f58dc
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-05-02 15:41:14 +0000

    Add reporting related module

---
+++ ssf/main/src/com/sitescape/team/module/report/impl/ReportModuleImpl.java
@@ -0,0 +1,14 @@
+package com.sitescape.team.module.report.impl;
+
+import com.sitescape.team.domain.LoginInfo;
+import com.sitescape.team.module.impl.CommonDependencyInjection;
+import com.sitescape.team.module.report.ReportModule;
+
+public class ReportModuleImpl extends CommonDependencyInjection implements ReportModule {
+
+	public void addLoginInfo(LoginInfo loginInfo) {
+		// TODO To be written by Janet
+		
+	}
+
+}
