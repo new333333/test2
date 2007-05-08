f16455f770a26ae666271cffdcbaee79c78c7115
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-05-08 18:21:56 +0000

    JBoss portal upgrade and more integration work

--- trunk/ssf/jboss-portal/src/com/sitescape/team/jbossportal/jaas/SiteScapeLoginModule.java
+++ trunk/ssf/jboss-portal/src/com/sitescape/team/jbossportal/jaas/SiteScapeLoginModule.java
@@ -11,28 +11,35 @@
 package com.sitescape.team.jbossportal.jaas;
 
 import java.lang.reflect.Method;
+import java.security.acl.Group;
+import java.util.Map;
 
 import com.sitescape.team.asmodule.bridge.SiteScapeBridgeUtil;
 
-import org.jboss.portal.identity.auth.IdentityLoginModule;
+import org.jboss.security.SimpleGroup;
+import org.jboss.security.auth.spi.UsernamePasswordLoginModule;
 
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.login.LoginException;
 
 /**
- * A login module that wraps JBoss Portal's default login module 
- * <code>org.jboss.portal.identity.auth.IdentityLoginModule</code>.
- * <p>
- * The side effect of using this login module is the possible automatic
- * user synchronization from the Portal user database to Aspen user database.
- *
+ * A SiteScape-supplied login module used to synchronize authenticated user
+ * into SiteScape user database.
  */
-public class SiteScapeLoginModule extends IdentityLoginModule {
+public class SiteScapeLoginModule extends UsernamePasswordLoginModule {
 	
+	private static final org.jboss.logging.Logger log = 
+		org.jboss.logging.Logger.getLogger(SiteScapeLoginModule.class);
+
 	private static final String CLASS_NAME =
 		"com.sitescape.team.util.SynchUser";
 	
 	private Object synchUser;
 	private Method synchMethod;
+
+	private String additionalRole;
+	private String synchronizeIdentity;
 
 	public SiteScapeLoginModule() {
 		try { 
@@ -46,40 +53,88 @@ public class SiteScapeLoginModule extends IdentityLoginModule {
 			(CLASS_NAME, "synch", String.class, String.class, String.class);
 		}
 		catch (Exception e) {
-			// Perhaps we should have log facility available here...
-			e.printStackTrace();
+			log.error("Error instantiating SiteScapeLoginModule: ", e);
 		}	
 	}
 	
+	public void initialize(Subject subject, CallbackHandler callbackHandler, Map sharedState, Map options) {
+	      super.initialize(subject, callbackHandler, sharedState, options);
+	      
+	      additionalRole = (String)options.get("additionalRole");
+	      synchronizeIdentity = (String)options.get("synchronizeIdentity");
+	}
+
 	public boolean commit() throws LoginException {
-		boolean result = super.commit();
-		if(result) {
+		if(isSynchronizeIdentity()) {
 			String username = getUsername();
-			String password = new String((char[]) getCredentials());
-			
-			//System.out.println("*** [" + username + "], [" + password + "]");
-			
-			/*
-			try {
-				AuthenticationManager.authenticate(null, username, password, null);
-			} 
-			catch (Exception e) {
-				// It's unclear whether we should abort the user login or let
-				// the user continue in this case. For now, we will abort it.
-				throw new LoginException(e.toString());
-			}
-			*/
-			
+			String password = getPassword();
+						
 			try {
 				SiteScapeBridgeUtil.invoke(synchMethod, synchUser, null, username, password);
 			}
 			catch(Exception e) {
-				e.printStackTrace();
+				log.warn("Failed to synchronize identity of user: " + getUsername(), e);
 				// It's unclear whether we should abort the user login or let
 				// the user continue in this case. For now, we will abort it.
 				throw new LoginException(e.toString());				
 			}
 		}
-		return result;
+		
+		return super.commit();
+	}
+
+	/**
+	 * Simply return empty string. 
+	 * It is ok because we override <code>validatePassword</code>.
+	 */
+	protected String getUsersPassword() throws LoginException {
+		return "";
+	}
+
+	@Override
+	protected Group[] getRoleSets() throws LoginException {
+	      Group group = new SimpleGroup("Roles");
+	      if (additionalRole != null)
+	      {
+	         try
+	         {
+	            group.addMember(createIdentity(additionalRole));
+	         }
+	         catch (Exception e)
+	         {
+	            //just a try
+	            log.error("Error when adding additional role: ", e);
+	         }
+	      }
+	      return new Group[]{group};
+	}
+
+   /**
+	 * This always returns true so that this login module always passes
+	 * regardless of the password returned from <code>getUsersPassword</code>.
+	 * 
+	 * @param inputPassword
+	 * @param expectedPassword
+	 * @return
+	 */
+	protected boolean validatePassword(String inputPassword,
+			String expectedPassword) {
+		return true;
+	}
+	
+	protected boolean isSynchronizeIdentity() {
+		// default is true
+		if (synchronizeIdentity != null
+				&& synchronizeIdentity.equalsIgnoreCase("false")) {
+			return Boolean.FALSE.booleanValue();
+		}
+		return Boolean.TRUE.booleanValue();
+	}
+
+	protected String getPassword() {
+		// Since this login module is invoked if and only if the regular portal
+		// login has been successful, we do know that the user-supplied password
+		// is the right one. 
+		return new String((char[]) getCredentials());
 	}
 }
