eb4d279414e56786f456540c47265a5d43a5483d
klein <klein@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-05-17 12:54:47 +0000

    add getValueArray helper method

--- ssf/main/src/com/sitescape/team/search/SearchFieldResult.java
+++ ssf/main/src/com/sitescape/team/search/SearchFieldResult.java
@@ -10,6 +10,7 @@
  */
 package com.sitescape.team.search;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,18 +18,25 @@ import com.sitescape.util.StringPool;
 import com.sitescape.util.StringUtil;
 
 public class SearchFieldResult {
-	private Set values=new HashSet();
+	private Set<String> values=new HashSet<String>();
     private static String [] sample = new String[0];
 
     public void addValue(String value) {
     	values.add(value);	 
 	}
+    
  	public Set getValueSet() {
 		return values;
 	}
+ 	
+ 	public ArrayList getValueArray() {
+ 		return new ArrayList<String>(values);
+ 	}
+ 	
 	public String getValueString() {
 		return toString();
 	}
+	
     public String toString() {
         if ((values == null) || values.isEmpty()) return null;
         return StringUtil.merge((String[])values.toArray(sample), 
