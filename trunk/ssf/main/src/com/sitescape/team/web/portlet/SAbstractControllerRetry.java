dade969dabbcd0fa03ecbc72e25a4631a550f143
mccann <mccann@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-06-05 18:33:59 +0000

    Turn off error logging from hibernate where it fills up the log unnecessarily. Change workflow logging to debug, or will fill log with junk

--- trunk/ssf/main/src/com/sitescape/team/web/portlet/SAbstractControllerRetry.java
+++ trunk/ssf/main/src/com/sitescape/team/web/portlet/SAbstractControllerRetry.java
@@ -2,6 +2,7 @@ package com.sitescape.team.web.portlet;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import java.util.Random;
 
 import org.hibernate.StaleObjectStateException;
 
@@ -13,16 +14,22 @@ public abstract class SAbstractControllerRetry extends SAbstractController {
 		//wrap a return loop around the request. Retry on Optimistic lock exceptions
 		//since the user can hit browser buttons faster than we can process them,
 		//we get optimistic lock exceptions.  Try to reduce this by retrying.
+
 		int retryCount =0;
+		Random random=null;
 		while (true) {
 			try {
 				handleActionRequestWithRetry(request, response);
 				return;
 			} catch (org.springframework.orm.hibernate3.HibernateOptimisticLockingFailureException ol) {
 				++retryCount;
+				if (random == null) random = new Random();
+				Thread.sleep(random.nextInt(20));
 				if (retryCount >= 5) throw ol; //give up
 			} catch (StaleObjectStateException os) {
 				++retryCount;
+				if (random == null) random = new Random();
+				Thread.sleep(random.nextInt(20));
 				if (retryCount >= 5) throw os; //give up
 			}
 		}
