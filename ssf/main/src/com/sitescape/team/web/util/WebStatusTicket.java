f42c4cf4fcda0a298ee130b65de85b9ad329d029
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-06-06 12:14:40 +0000

    id of the status ticket must be passed in by the client

--- ssf/main/src/com/sitescape/team/web/util/WebStatusTicket.java
+++ ssf/main/src/com/sitescape/team/web/util/WebStatusTicket.java
@@ -1,15 +1,11 @@
 package com.sitescape.team.web.util;
 
-import java.util.UUID;
-
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletSession;
 
 import com.sitescape.team.util.StatusTicket;
 
 public class WebStatusTicket implements StatusTicket {
-
-	private static final String ID_PREFIX = "wst.";
 
 	private PortletSession session;
 	private String id;
@@ -20,16 +16,16 @@ public class WebStatusTicket implements StatusTicket {
 	 * If there is no user session associated with the portlet request, this
 	 * returns <code>null</code>.
 	 * 
+	 * @param id unique id for the ticket
 	 * @param request portlet request
 	 * @return a status ticket, or <code>null</code> if no user session exists.
 	 */
-	public static StatusTicket newStatusTicket(PortletRequest request) {
+	public static StatusTicket newStatusTicket(String id, PortletRequest request) {
 		PortletSession session = request.getPortletSession(false);
 		
 		if(session == null)
 			return null; // unable to allocate a new ticket in this case
 	
-		String id = ID_PREFIX + UUID.randomUUID().toString();
 		WebStatusTicket ticket = new WebStatusTicket(session, id);
 		session.setAttribute(id, ticket, PortletSession.APPLICATION_SCOPE);
 		return ticket;
