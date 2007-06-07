c57e98c6d2b73c730cdb8ada888c4b38cded3a00
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-06-07 01:44:20 +0000

    issue #688

--- ssf/taglib/src/com/sitescape/team/taglib/IfNotAdapterTag.java
+++ ssf/taglib/src/com/sitescape/team/taglib/IfNotAdapterTag.java
@@ -10,14 +10,35 @@
  */
 package com.sitescape.team.taglib;
 
+import javax.portlet.PortletConfig;
+import javax.servlet.ServletRequest;
 import javax.servlet.jsp.JspException;
+
+import com.sitescape.team.web.WebKeys;
 
 public class IfNotAdapterTag extends IfAdapterTag {
 
+	/**
+	 * Return <code>EVAL_BODY_INCLUDE</code> only if the request is being 
+	 * made through the portlet container. Otherwise (that is, the request 
+	 * came through our own portlet adapter or as a servlet), it returns 
+	 * <code>SKIP_BODY</code>.
+	 */
 	public int doStartTag() throws JspException {
-		if(super.doStartTag() == EVAL_BODY_INCLUDE)
+		if(super.doStartTag() == EVAL_BODY_INCLUDE) { // adapter
 			return SKIP_BODY;
-		else
-			return EVAL_BODY_INCLUDE;
+		}
+		else { // not adapter
+			ServletRequest req = pageContext.getRequest();
+			
+			PortletConfig portletConfig =
+				(PortletConfig)req.getAttribute(WebKeys.JAVAX_PORTLET_CONFIG);
+
+			if (portletConfig != null) // container
+				return EVAL_BODY_INCLUDE;
+			else // not container
+				return SKIP_BODY;
+		}
 	}
+
 }
