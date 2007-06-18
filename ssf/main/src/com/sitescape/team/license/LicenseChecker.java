7cd02a4a0cb5eaf8e214076606205cdd444bb2ec
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-06-18 01:36:06 +0000

    Add method for checking whether the license allows a certain feature or not. For now, the implementation is stubbed.

--- ssf/main/src/com/sitescape/team/license/LicenseChecker.java
+++ ssf/main/src/com/sitescape/team/license/LicenseChecker.java
@@ -12,7 +12,19 @@ package com.sitescape.team.license;
 
 public class LicenseChecker {
 
-	public static boolean isAuthorized(String featureName) {
-		return true; // TODO 
+	/**
+	 * Check if the license permits the specified feature to be executed within
+	 * the current runtime environment.
+	 *  
+	 * @param featureName name of the feature to check the licensing againt;
+	 * typically feature name begins with the package name of the interface
+	 * through which the particular feature is exposed followed by a short
+	 * name representing the feature. For example,
+	 * com.sitescape.team.module.workflow.AdvancedWorkflow
+	 * @return
+	 */
+	public static boolean isAuthorizedByLicense(String featureName) {
+		// TODO To be rewritten to use License Manager.
+		return false; // brain-dead for now
 	}
 }
