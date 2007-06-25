e96a127c87f12bd2d25dc97e81744f3d3e421265
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-06-25 21:38:15 +0000

    Credits page

---
+++ trunk/ssf/main/src/com/sitescape/team/portlet/administration/CreditsController.java
@@ -0,0 +1,59 @@
+/**
+ * The contents of this file are governed by the terms of your license
+ * with SiteScape, Inc., which includes disclaimers of warranties and
+ * limitations on liability. You may not use this file except in accordance
+ * with the terms of that license. See the license for the specific language
+ * governing your rights and limitations under the license.
+ *
+ * Copyright (c) 2007 SiteScape, Inc.
+ *
+ */
+package com.sitescape.team.portlet.administration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.dom4j.Document;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sitescape.team.NotSupportedException;
+import com.sitescape.team.context.request.RequestContextHolder;
+import com.sitescape.team.domain.Binder;
+import com.sitescape.team.domain.Folder;
+import com.sitescape.team.module.binder.BinderModule.BinderOperation;
+import com.sitescape.team.security.function.WorkAreaOperation;
+import com.sitescape.team.web.WebKeys;
+import com.sitescape.team.web.portlet.SAbstractController;
+import com.sitescape.team.web.tree.SearchTreeHelper;
+import com.sitescape.team.web.tree.WsDomTreeBuilder;
+import com.sitescape.team.web.util.BinderHelper;
+import com.sitescape.team.web.util.PortletRequestUtils;
+import com.sitescape.util.Validator;
+
+public class CreditsController extends  SAbstractController {
+	
+	public void handleActionRequestAfterValidation(ActionRequest request, ActionResponse response) throws Exception {
+		response.setRenderParameters(request.getParameterMap());
+		Map formData = request.getParameterMap();
+		if (formData.containsKey("cancelBtn") || formData.containsKey("closeBtn")) {
+			response.setRenderParameter("redirect", "true");
+		}
+	}
+
+	public ModelAndView handleRenderRequestInternal(RenderRequest request, 
+			RenderResponse response) throws Exception {
+		if (!Validator.isNull(request.getParameter("redirect"))) {
+			return new ModelAndView(WebKeys.VIEW_ADMIN_REDIRECT);
+		}
+		Map model = new HashMap();
+		return new ModelAndView("administration/credits", model);
+		
+	}
+}
