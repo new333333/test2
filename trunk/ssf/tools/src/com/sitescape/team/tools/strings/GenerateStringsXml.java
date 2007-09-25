276909e4fbc5b66a5ca0364f061454671d15608f
griffin <griffin@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-09-25 14:23:42 +0000

    Add Danish

--- trunk/ssf/tools/src/com/sitescape/team/tools/strings/GenerateStringsXml.java
+++ trunk/ssf/tools/src/com/sitescape/team/tools/strings/GenerateStringsXml.java
@@ -71,7 +71,7 @@ public class GenerateStringsXml {
            	Element strRoot = document.getRootElement();
             
            	
-           	String []localeCodes = {"en", "de", "es", "fr", "it", "ja",	
+           	String []localeCodes = {"en", "da", "de", "es", "fr", "it", "ja",	
            			"nl", "pl", "pt_BR", "sv", "zh_CN", "zh_TW"};
            	
            	generateTags(strRoot, localeCodes, pathname);
