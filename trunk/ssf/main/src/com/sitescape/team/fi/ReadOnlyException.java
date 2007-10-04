f6e4599c529281aab3b95a9ba9beca88d6b5d7f4
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-10-04 19:49:00 +0000

    issue #1404

--- trunk/ssf/main/src/com/sitescape/team/fi/ReadOnlyException.java
+++ trunk/ssf/main/src/com/sitescape/team/fi/ReadOnlyException.java
@@ -32,7 +32,7 @@ public class ReadOnlyException extends FIException {
 
 	private static final long serialVersionUID = 1L;
 
-	public ReadOnlyException(String driverName, String operationName) {
-		super("Cannot execute " + operationName + ": Driver " + driverName + " is read-only");
+	public ReadOnlyException(String driverTitle, String operationName) {
+		super("Cannot execute " + operationName + ": Driver " + driverTitle + " is read-only");
 	}
 }
