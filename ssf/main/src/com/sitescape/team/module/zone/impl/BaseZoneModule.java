e77778950f497d61b1bdf30c0cf6af5f933e02a9
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-11-27 02:18:35 +0000

    Add license support to multi zone feature

--- ssf/main/src/com/sitescape/team/module/zone/impl/BaseZoneModule.java
+++ ssf/main/src/com/sitescape/team/module/zone/impl/BaseZoneModule.java
@@ -4,10 +4,12 @@ import com.sitescape.team.domain.Workspace;
 import com.sitescape.team.util.SZoneConfig;
 
 public class BaseZoneModule extends AbstractZoneModule {
-	public void writeZone(String zoneName, String virtualHost) {}
+	public void writeZone(String zoneName, String virtualHost) {
+		logger.info("Cannot write zone " + zoneName + " - Open source edition does not support multi zone");
+	}
 
 	public Long getZoneIdByVirtualHost(String virtualHost) {
-		String zoneName = getZoneNameByVirtualHost(virtualHost);
+		String zoneName = SZoneConfig.getDefaultZoneName();
 		Workspace top = getCoreDao().findTopWorkspace(zoneName);
 		return top.getId();
 	}
