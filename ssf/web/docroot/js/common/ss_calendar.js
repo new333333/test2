be96a3690134fd00ee1240ae1cc3625faf4cd201
pawel <pawel@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2007-11-30 11:34:36 +0000

    AI#1528 - fix display events with end time 12:00 AM

--- ssf/web/docroot/js/common/ss_calendar.js
+++ ssf/web/docroot/js/common/ss_calendar.js
@@ -1180,7 +1180,11 @@ function ss_calendarEngine(
 				hours =  nei.endDate.substr(9, 2) * 1;
 				minutes =  nei.endDate.substr(11, 2) * 1;
 	            nei.endDate = new Date(year, month - 1, day, hours, minutes);
-				if (hours === 0 && minutes === 0) {
+				if (hours === 0 && minutes === 0 && 
+					!(nei.startDate.getHours() == 0 && nei.startDate.getMinutes() == 0 &&
+						nei.startDate.getFullYear() == nei.endDate.getFullYear() && 
+						nei.startDate.getMonth() == nei.endDate.getMonth() && 
+						nei.startDate.getDate() == nei.endDate.getDate())) {
 					nei.endDate = new Date(nei.endDate - 1);
 				}
 	
