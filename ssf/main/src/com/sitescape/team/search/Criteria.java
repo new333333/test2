8ae454d9751980f83d5ae7deb030d6c78a0bde6d
destefano <destefano@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-03-10 14:32:36 +0000

    Added Order to search criteria

--- ssf/main/src/com/sitescape/team/search/Criteria.java
+++ ssf/main/src/com/sitescape/team/search/Criteria.java
@@ -1,5 +1,6 @@
 package com.sitescape.team.search;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.dom4j.Document;
@@ -9,10 +10,12 @@ import org.dom4j.Element;
 public class Criteria
 {
 	Junction root;
+	List<Order> orders;
 	
 	public Criteria()
 	{
 		root = new Junction.Conjunction();
+		orders = new LinkedList<Order>();
 	}
 	
 	public Criteria add(Criterion crit)
@@ -21,13 +24,23 @@ public class Criteria
 		return this;
 	}
 	
+	public Criteria addOrder(Order order)
+	{
+		orders.add(order);
+		return this;
+	}
 	
 	public Document toQuery()
 	{
 		Document doc = DocumentHelper.createDocument();
 		Element rootElement = doc.addElement(QueryBuilder.QUERY_ELEMENT);
 		root.toQuery(rootElement);
-		
+		if(orders.size() > 0) {
+			Element sortBy = rootElement.addElement(QueryBuilder.SORTBY_ELEMENT);
+			for(Order o : orders) {
+				o.toQuery(sortBy);
+			}
+		}
 		return doc;
 	}
 }
