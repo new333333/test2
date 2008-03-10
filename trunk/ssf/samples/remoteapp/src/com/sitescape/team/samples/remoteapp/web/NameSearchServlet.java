944876eb9fb5a1210b0227e7b41b70f5b5a9e690
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-03-10 00:18:30 +0000

    sample remote application (interim checkin)

---
+++ trunk/ssf/samples/remoteapp/src/com/sitescape/team/samples/remoteapp/web/NameSearchServlet.java
@@ -0,0 +1,55 @@
+/**
+ * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the "CPAL");
+ * you may not use this file except in compliance with the CPAL. You may obtain a copy of the CPAL at
+ * http://www.opensource.org/licenses/cpal_1.0. The CPAL is based on the Mozilla Public License Version 1.1
+ * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
+ * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ * 
+ * Software distributed under the CPAL is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+ * either express or implied. See the CPAL for the specific language governing rights and limitations
+ * under the CPAL.
+ * 
+ * The Original Code is ICEcore. The Original Developer is SiteScape, Inc. All portions of the code
+ * written by SiteScape, Inc. are Copyright (c) 1998-2007 SiteScape, Inc. All Rights Reserved.
+ * 
+ * 
+ * Attribution Information
+ * Attribution Copyright Notice: Copyright (c) 1998-2007 SiteScape, Inc. All Rights Reserved.
+ * Attribution Phrase (not exceeding 10 words): [Powered by ICEcore]
+ * Attribution URL: [www.icecore.com]
+ * Graphic Image as provided in the Covered Code [web/docroot/images/pics/powered_by_icecore.png].
+ * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a
+ * work which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
+ * 
+ * 
+ * SITESCAPE and the SiteScape logo are registered trademarks and ICEcore and the ICEcore logos
+ * are trademarks of SiteScape, Inc.
+ */
+package com.sitescape.team.samples.remoteapp.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NameSearchServlet extends HttpServlet {
+
+	private static final String PARAMETER_NAME_ACTION = "ss_action_name";
+	private static final String PARAMETER_NAME_APPLICATION_ID = "ss_application_id";
+	private static final String PARAMETER_NAME_USER_ID = "ss_user_id";
+	private static final String PARAMETER_NAME_ACCESS_TOKEN = "ss_access_token";
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+	throws ServletException, IOException {
+		String action = req.getParameter(PARAMETER_NAME_ACTION);
+		String applicationId = req.getParameter(PARAMETER_NAME_APPLICATION_ID);
+		String userId = req.getParameter(PARAMETER_NAME_USER_ID);
+		String accessToken = req.getParameter(PARAMETER_NAME_ACCESS_TOKEN);
+		
+		resp.getWriter().print("Hey! How are you?");
+	}
+
+}
