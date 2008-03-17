e3b3e3b2ae5345d95b1dd242f1dbee63ab410228
mccann <mccann@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-03-17 20:54:56 +0000

    simplify

--- ssf/main/src/com/sitescape/team/portlet/forum/CloseWindowController.java
+++ ssf/main/src/com/sitescape/team/portlet/forum/CloseWindowController.java
@@ -28,9 +28,6 @@
  */
 package com.sitescape.team.portlet.forum;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.RenderRequest;
@@ -38,11 +35,7 @@ import javax.portlet.RenderResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 
-import com.sitescape.team.domain.EntityIdentifier;
-import com.sitescape.team.web.WebKeys;
 import com.sitescape.team.web.portlet.SAbstractController;
-import com.sitescape.team.web.util.BinderHelper;
-import com.sitescape.team.web.util.PortletRequestUtils;
 
 
 /**
@@ -51,13 +44,11 @@ import com.sitescape.team.web.util.PortletRequestUtils;
  */
 public class CloseWindowController  extends SAbstractController {
 	public void handleActionRequestAfterValidation(ActionRequest request, ActionResponse response) throws Exception {
-		response.setRenderParameters(request.getParameterMap());
 	}
 	public ModelAndView handleRenderRequestInternal(RenderRequest request, 
 			RenderResponse response) throws Exception {
- 		Map<String,Object> model = new HashMap<String,Object>();
  		
-	    return new ModelAndView("forum/close_window", model);
+	    return new ModelAndView("forum/close_window");
 	}
 
 }
