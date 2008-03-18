0fbda6abc1c9272c7f0a2d2ead3c3f3f9d3d5b7b
hurley <hurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-03-18 16:22:09 +0000

    Defect 1768: change download url to one that ends in the file name so gifs and text files can be saved

--- main/src/com/sitescape/team/servlet/forum/ReadFileController.java
+++ main/src/com/sitescape/team/servlet/forum/ReadFileController.java
@@ -28,13 +28,26 @@
  */
 package com.sitescape.team.servlet.forum;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.activation.FileTypeMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.RequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.sitescape.team.domain.Binder;
+import com.sitescape.team.domain.DefinableEntity;
+import com.sitescape.team.domain.EntityIdentifier;
+import com.sitescape.team.domain.FileAttachment;
+import com.sitescape.team.util.FileHelper;
+import com.sitescape.team.util.NLT;
+import com.sitescape.team.web.WebKeys;
 import com.sitescape.team.web.servlet.SAbstractController;
+import com.sitescape.util.FileUtil;
 
 public class ReadFileController extends SAbstractController {
 	
@@ -49,14 +62,98 @@ public class ReadFileController extends SAbstractController {
 	
 	protected ModelAndView handleRequestAfterValidation(HttpServletRequest request,
             HttpServletResponse response) throws Exception {
-		// TODO To be completed by Peter
-
-		// Assuming that the full request URL was http://localhost:8080/ssf/s/readFile/123/456/789/junk.doc,
-		// the following call returns "/readFile/123/456/789/junk.doc" portion of the URL.
+		// Assuming that the full request URL was http://localhost:8080/ssf/s/readFile/xxx/123/456/789/junk.doc,
+		// the following call returns "/readFile/xxx/123/456/789/junk.doc" portion of the URL.
 		String pathInfo = request.getPathInfo();
 		
-		// Do whatever you want with the path info obtained.
-		
+		String[] args = pathInfo.split("/");
+		//We expect the url to be formatted as /readFile/entityType/binderId/entryId/fileId/fileTime/filename.ext
+		//  If there is no entryId (in the case where the file is in the binder itself), the entryId is set to "-"
+		if (args.length >= 6) {
+			try {
+				String strEntityType = args[2];
+				Long binderId = Long.valueOf(args[3]);
+				Long entryId = null;
+				if (!args[4].equals("-")) {
+					//There is an entryId specified
+					entryId = Long.valueOf(args[4]);
+				}
+				String fileId = args[5];
+				String fileTime = args[6];
+
+				DefinableEntity entity = null;
+				Binder parent;
+				EntityIdentifier.EntityType entityType = EntityIdentifier.EntityType.valueOf(strEntityType);
+				if (entityType.equals(EntityIdentifier.EntityType.folder) || entityType.equals(EntityIdentifier.EntityType.workspace) ||
+						entityType.equals(EntityIdentifier.EntityType.profiles)) {
+					//the entry is the binder
+					if (entryId == null) entryId = new Long(RequestUtils.getRequiredLongParameter(request, WebKeys.URL_BINDER_ID));
+					entity = getBinderModule().getBinder(entryId);
+					parent = (Binder) entity;
+				} else if (entryId != null) {
+					if (entityType.equals(EntityIdentifier.EntityType.folderEntry)) {
+						entity = getFolderModule().getEntry(binderId, entryId);
+					} else if (entityType.equals(EntityIdentifier.EntityType.none)) {
+						//Try to figure out what type of entity this is
+						try {
+							entity = getFolderModule().getEntry(binderId, entryId);
+						} catch (Exception e) {}
+						if (entity == null) {
+							try {
+								entity = getProfileModule().getEntry(binderId, entryId);
+							} catch (Exception e) {}
+						}
+						
+					} else {
+						entity = getProfileModule().getEntry(binderId, entryId);
+					}
+					parent = entity.getParentBinder();
+				} else {
+					parent = getBinderModule().getBinder(binderId);
+					entity = parent;
+				}
+				//Set up the beans needed by the jsps
+				FileAttachment fa = null;
+				if (!fileId.equals("")) fa = (FileAttachment)entity.getAttachment(fileId);
+
+				if (fa != null) {
+					String shortFileName = FileUtil.getShortFileName(fa.getFileItem().getName());	
+					String contentType = mimeTypes.getContentType(shortFileName);
+					response.setContentType(contentType);
+					if (fileTime.equals("") || 
+							!fileTime.equals(String.valueOf(fa.getModification().getDate().getTime()))) {
+						response.setHeader("Cache-Control", "private");
+						response.setHeader("Pragma", "no-cache");
+					}
+					String attachment = "";
+					if (FileHelper.checkIfAttachment(contentType)) attachment = "attachment; ";
+					response.setHeader("Content-Disposition",
+							attachment + "filename=\"" + FileHelper.encodeFileName(request, shortFileName) + "\"");
+					
+					SimpleDateFormat df = (SimpleDateFormat)DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.FULL);
+					Date d = fa.getModification().getDate();
+					df.applyPattern("EEE, dd MMM yyyy kk:mm:ss zzz");
+					response.setHeader("Last-Modified", df.format(d));
+					try {
+						response.setHeader("Content-Length", 
+								String.valueOf(FileHelper.getLength(parent, entity, fa)));
+						getFileModule().readFile(parent, entity, fa, response.getOutputStream());
+						//getReportModule().addFileInfo(AuditType.download, fa);
+					}
+					catch(Exception e) {
+						response.getOutputStream().print(NLT.get("file.error") + ": " + e.getLocalizedMessage());
+					}
+				}
+				try {
+					response.getOutputStream().flush();
+				}
+				catch(Exception ignore) {}
+
+			} catch(Exception e) {
+				//Bad format of url; just return null
+				response.getOutputStream().print(NLT.get("file.error.unknownFile"));
+			}
+		}
 		return null;
 	}
 }
