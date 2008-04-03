ff4cfac8d910f739f88cd89bd7c18fa22b763d0d
mccann <mccann@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-04-03 22:54:30 +0000

    tests

--- branches/ssf-v1/samples/remotingclient/src/com/sitescape/team/samples/remoting/client/ws/axis/WSClient.java
+++ branches/ssf-v1/samples/remotingclient/src/com/sitescape/team/samples/remoting/client/ws/axis/WSClient.java
@@ -33,6 +33,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
@@ -105,12 +107,37 @@ public class WSClient
 					attachFile = args[3];
 				}
 				justDoIt("uploadCalendarEntries", new Object[] {Long.parseLong(args[1]), s}, attachFile);
+			} else if(args[0].equals("addWorkflow")) {
+				justDoIt("addEntryWorkflow", new Object[] {Long.parseLong(args[1]), Long.parseLong(args[2]), args[3], args[4]});
 			} else if(args[0].equals("search")) {
 				String s = FacadeClientHelper.readText(args[1]);
 				System.out.println("XML: " + s);
 				fetchAndPrintXML("search", new Object[] {s, Integer.parseInt(args[2]), Integer.parseInt(args[3])});
 			} else if(args[0].equals("getBinderTitle")) {
 				fetchAndPrintString("getBinderTitle", new Object[] {Long.parseLong(args[1])});
+			} else if(args[0].equals("listDefinitions")) {
+				fetchAndPrintXML("getDefinitionListAsXML", new Object[] {});
+			} else if(args[0].equals("setTeamMembers")) {
+				String ids[] = args[2].split(",");
+				List idsList = new ArrayList();
+				for (int i=0; i<ids.length; ++i) {
+					idsList.add(Long.parseLong((String)ids[i]));
+				}
+				justDoIt("setTeamMembers", new Object[] {Long.parseLong(args[1]), idsList});
+			} else if(args[0].equals("setDefinitions")) {
+				String ids[] = args[2].split(",");
+				List idsList = new ArrayList();
+				for (int i=0; i<ids.length; ++i) {
+					idsList.add(ids[i]);
+				}
+				List wfs = new ArrayList();
+				if (args.length > 3) {
+					ids = args[3].split(","); //entryDef,workflowDef
+					for (int i=0; i<ids.length; ++i) {
+						wfs.add(ids[i]);
+					}
+				}
+				justDoIt("setDefinitions", new Object[] {Long.parseLong(args[1]), idsList, wfs});
 			} else {
 				System.out.println("Invalid arguments");
 				printUsage();
@@ -210,22 +237,26 @@ public class WSClient
 
 	private static void printUsage() {
 		System.out.println("Usage:");
+		System.out.println("addFolder <parent binder id> <binder config id> <title>");
 		System.out.println("printWorkspaceTree <workspace id> <depth> [<page>]");
 		System.out.println("printPrincipal <binder id> <principal id>");
 		System.out.println("printAllPrincipals <first> <max>");
-		System.out.println("addFolder <parent binder id> <binder config id> <title>");
 		System.out.println("printFolderEntries <folder id>");
 		System.out.println("search <xmlFilename> <offset> <maxResults>");
 		System.out.println("printTeamMembers <binder id>");
 		System.out.println("printTeams");
+		System.out.println("setTeamMembers <team id> <command separated list of memberId>");
 		System.out.println("printFolderEntry <folder id> <entry id> <includeAttachments>");
 		System.out.println("printDefinition <definition id>");
 		System.out.println("printDefinitionConfig");
+		System.out.println("listDefinitions");
 		System.out.println("addEntry <folder id> <definition id> <entryDataXMLString> [<attachmentFileName>]");
 		System.out.println("modifyEntry <folder id> <entry id> <entryDataXMLString>");
+		System.out.println("addWorkflow <folder id> <entry id> <definition id> <startState>");
 		System.out.println("uploadFile <folder id> <entry id> <fileDataFieldName> <filename>");
 		System.out.println("uploadCalendarEntries <folder id> <xmlFilename> [<iCalFilename>]");
 		System.out.println("-- The following is to be used only in conjunction with extendedws sample --");
 		System.out.println("getBinderTitle <binder id>");
+		System.out.println("setDefinitions <folder id> <comma separated definitionIds> comma separated definitionId,workflowId>");
 	}
 }
