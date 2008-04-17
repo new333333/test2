8907c7d876e36c816a781f229359b7449e73bedf
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-04-17 18:30:52 +0000

    Aspen Teaming Issue #1856

--- ssf/main/src/com/sitescape/team/portlet/forum/AjaxController.java
+++ ssf/main/src/com/sitescape/team/portlet/forum/AjaxController.java
@@ -1347,14 +1347,6 @@ public class AjaxController  extends SAbstractControllerRetry {
 		//This replace has been done AJAX does not allow "&"
 		String strURL = adapterUrl.toString();
 		strURL = strURL.replaceAll("&", "&amp;");
-
-		// This is a hack for iChain which doesn't always rewrite the
-		// applet URL properly.  We force https if we're leaning that way.
-		String protocol;
-		protocol = SPropsUtil.getString("adapter.web.protocol", "context");
-		if (protocol.equalsIgnoreCase("https") || protocol.equalsIgnoreCase("context-https")) {
-			strURL = strURL.replaceFirst("http://","https://");
-		}
 		
 		Map model = new HashMap();
 		model.put(WebKeys.NAMESPACE, namespace);
@@ -1457,23 +1449,10 @@ public class AjaxController  extends SAbstractControllerRetry {
 		//This replace has been done AJAX does not allow "&"
 		String strURL = adapterUrl.toString();
 		strURL = strURL.replaceAll("&", "&amp;");
-
-		// This is a hack for iChain which doesn't always rewrite the
-		// applet URL properly.  We force https if we're leaning that way.
-		String protocol;
-		protocol = SPropsUtil.getString("adapter.web.protocol", "context");
-		if (protocol.equalsIgnoreCase("https") || protocol.equalsIgnoreCase("context-https")) {
-			strURL = strURL.replaceFirst("http://","https://");
-		}
 		
 		//This replace has been done AJAX does not allow "&"
 		String strRefreshURL = adapterFolderRefreshUrl.toString();
 		//strRefreshURL = strRefreshURL.replaceAll("&", "&amp;");
-		
-		// Same iChain hack as above...
-		if (protocol.equalsIgnoreCase("https") || protocol.equalsIgnoreCase("context-https")) {
-			strRefreshURL = strRefreshURL.replaceFirst("http://","https://");
-		}
 		
 		Map model = new HashMap();
 		model.put(WebKeys.NAMESPACE, namespace);
