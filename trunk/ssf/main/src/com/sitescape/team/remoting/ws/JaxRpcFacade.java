943437595083a59aa28c753ce11fd17d938b8b50
kim <kim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-04-28 20:18:46 +0000

    Undo my last checkin [Remove getDefinitionConfigAsXML from the new WS interfaces; still remains in the old (and deprecated) interfaces]

--- trunk/ssf/main/src/com/sitescape/team/remoting/ws/JaxRpcFacade.java
+++ trunk/ssf/main/src/com/sitescape/team/remoting/ws/JaxRpcFacade.java
@@ -35,7 +35,6 @@ import org.springframework.remoting.jaxrpc.ServletEndpointSupport;
 import com.sitescape.team.remoting.Facade;
 import com.sitescape.team.remoting.ws.service.binder.BinderService;
 import com.sitescape.team.remoting.ws.service.definition.DefinitionService;
-import com.sitescape.team.remoting.ws.service.definition.DefinitionServiceInternal;
 import com.sitescape.team.remoting.ws.service.folder.FolderService;
 import com.sitescape.team.remoting.ws.service.folder.MigrationService;
 import com.sitescape.team.remoting.ws.service.ical.IcalService;
@@ -90,7 +89,7 @@ public class JaxRpcFacade extends ServletEndpointSupport implements Facade {
 	 * replaced by {@link com.sitescape.team.remoting.ws.service.definition#getDefinitionConfigAsXML}.
 	 */
 	public String getDefinitionConfigAsXML() {
-		return ((DefinitionServiceInternal) this.definitionService).getDefinitionConfigAsXML(null);
+		return this.definitionService.getDefinitionConfigAsXML(null);
 	}
 	
 	/**
