67f591a1603e1b06604e2d4d52c3ceaad5847d4c
peterhurley <peterhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-07-27 03:43:22 +0000

    Put a slider onto the folder page navigation widget

--- ssf/web/docroot/js/common/ss_common.js
+++ ssf/web/docroot/js/common/ss_common.js
@@ -859,18 +859,17 @@ function ss_executeJavascript(xmlNode) {
     }
 }
 
-function ss_showFolderPage(obj, binderId, currentPageIndex, divId, cTag, pTag, yearMonth, endDate) {
+function ss_showFolderPageIndex(hrefUrl, binderId, currentPageIndex, divId, cTag, pTag, yearMonth, endDate) {
 	if (currentPageIndex == "") currentPageIndex = "0";
 	var page = parseInt(currentPageIndex);
 	
 	var divObj = self.document.getElementById(divId);
 	if (divObj == null || ss_userDisplayStyle == "accessible") {
 		//In accessible mode, redraw the whole page
-		var url = obj.href;
-		self.location.href = url;
+		self.location.href = hrefUrl;
 	} else {
 		ss_setupStatusMessageDiv();
-		var url = ss_buildAdapterUrl(ss_AjaxBaseUrl, {binderId:binderId, operation:"show_folder_page", ssPageStartIndex:page, cTag:cTag, pTag:pTag, yearMonth:yearMonth, endDate:endDate});
+		var url = ss_buildAdapterUrl(ss_AjaxBaseUrl, {binderId:binderId, operation:"show_folder_page", ssPageStartIndex:page, cTag:cTag, pTag:pTag, yearMonth:yearMonth, endDate:endDate, random:ss_random++});
 		ss_fetch_url(url, ss_showFolderPageDiv, divId)
 	}
 }
