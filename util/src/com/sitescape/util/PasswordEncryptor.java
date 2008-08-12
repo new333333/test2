df7e5855b83085b21f50e41c0d5220a2618633d1
jongkim32 <jongkim32@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-08-12 14:08:42 +0000

    Add support for SHA and SHA-256 encryption for user passwords;

--- util/src/com/sitescape/util/PasswordEncryptor.java
+++ util/src/com/sitescape/util/PasswordEncryptor.java
@@ -32,6 +32,8 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import com.sitescape.team.util.SPropsUtil;
+
 /**
  * Miscellaneous utilities.
  */
@@ -39,23 +41,66 @@ public class PasswordEncryptor {
 	
     private static Long PASSWORD_DIGEST=new Long(32958);
 
+    private static final String ALGORITHM = SPropsUtil.getString("user.password.encryption.algorithm", "MD5");
+    
     public static String encrypt(String password) {
-    	return encrypt(password, PASSWORD_DIGEST);
+    	// If algorithm is MD5, do NOT ever call encrypt(). Instead, call encryptMD5
+    	// which uses slightly different (and non-standard) encoding when converting
+    	// computed bytes into a hex string. This is to maintain backward compatibility
+    	// with existing installations that use MD5.
+    	if(ALGORITHM.equals("MD5"))
+    		return encryptMD5(password, PASSWORD_DIGEST);
+    	else
+    		return encrypt(ALGORITHM, password, PASSWORD_DIGEST);
     }
     
-	public static String encrypt(String password, Long digestSeed) {
+	private static String encryptMD5(String password, Long digestSeed) {
+		// Use this method only for MD5 encryption.
 		try {
-			MessageDigest algorithm = MessageDigest.getInstance("MD5");
-			algorithm.reset();
-			algorithm.update(password.getBytes("UTF-8"));
-			algorithm.update(digestSeed.toString().getBytes("UTF-8"));
-			byte[] messageDigest = algorithm.digest();
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			digest.reset();
+			digest.update(password.getBytes("UTF-8"));
+			digest.update(digestSeed.toString().getBytes("UTF-8"));
+			byte[] messageDigest = digest.digest();
 			
 			StringBuffer hexString = new StringBuffer();
 			for(int i = 0; i < messageDigest.length; i++) {
 				// Convert each digest byte value to hex string (which is either
 				// one or two characters long). 
+				// Note: It appears this is a non-standard encoding. Each byte
+				// should have been converted into two hex digits. Due to backward
+				// compatibility with existing installations, we won't (can't) 
+				// correct this.
 				hexString.append(Integer.toHexString(0xff & messageDigest[i]));
+			}
+			return hexString.toString();
+		}
+		catch(NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+		catch(UnsupportedEncodingException e) {
+			throw new RuntimeException(e);			
+		}
+		catch (NullPointerException e) {
+			// this will occur when the password.getBytes("UTF-8") returns a null.
+			return new StringBuffer().toString();
+		}
+	}
+    
+	private static String encrypt(String algorithm, String password, Long digestSeed) {
+		// Use this method for all encryptions but MD5.
+		try {
+			MessageDigest digest = MessageDigest.getInstance(algorithm);
+			digest.reset();
+			digest.update(password.getBytes("UTF-8"));
+			digest.update(digestSeed.toString().getBytes("UTF-8"));
+			byte[] messageDigest = digest.digest();
+			
+			StringBuffer hexString = new StringBuffer();
+			for(int i = 0; i < messageDigest.length; i++) {
+				// Convert each byte into two digit hex characters.
+				hexString.append(Integer.toHexString((0xf0 & messageDigest[i])>>4));
+				hexString.append(Integer.toHexString(0x0f & messageDigest[i]));
 			}
 			return hexString.toString();
 		}
