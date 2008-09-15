ec84cfe51434e4128ed38973f1eecabf4458b12a
janetmmccann <janetmmccann@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-09-15 17:47:50 +0000

    Add search utils to build queries

---
+++ util/src/com/sitescape/util/search/Junction.java
@@ -0,0 +1,75 @@
+package com.sitescape.util.search;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.dom4j.Branch;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+
+abstract public class Junction implements Criterion
+{
+	List<Criterion> criteria;
+	
+	public Junction()
+	{
+		this.criteria = new LinkedList<Criterion>();
+	}
+	
+	public Junction add(Criterion crit)
+	{
+		criteria.add(crit);
+		return this;
+	}
+	
+	public Element toQuery(Branch parent)
+	{
+		Element root = parent.addElement(this.getOp());
+		for(Criterion crit : criteria)
+		{
+			crit.toQuery(root);
+		}
+		return root;
+	}
+	
+	abstract String getOp();
+	
+	public static class Disjunction extends Junction
+	{
+		public Disjunction()
+		{
+			super();
+		}
+		
+		public String getOp()
+		{
+			return Constants.OR_ELEMENT;
+		}
+	}
+
+	public static class Conjunction extends Junction
+	{
+		public Conjunction()
+		{
+			super();
+		}
+
+		public String getOp()
+		{
+			return Constants.AND_ELEMENT;
+		}
+	}
+	public static class Not extends Junction
+	{
+		public Not()
+		{
+			super();
+		}
+
+		public String getOp()
+		{
+			return Constants.NOT_ELEMENT;
+		}
+	}
+}
