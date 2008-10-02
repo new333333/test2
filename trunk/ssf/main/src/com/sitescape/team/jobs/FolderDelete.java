0b97a1f0f7ffcc3fc4758f0f13c52346b5d5f602
janetmmccann <janetmmccann@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-10-02 14:23:18 +0000

    Reduce time folderDelete job runs

--- trunk/ssf/main/src/com/sitescape/team/jobs/FolderDelete.java
+++ trunk/ssf/main/src/com/sitescape/team/jobs/FolderDelete.java
@@ -32,7 +32,7 @@ public interface FolderDelete {
 	public final static String FOLDER_DELETE_GROUP="folder-delete";
 	public final static String FOLDER_DELETE_DESCRIPTION="complete folder delete";
 	public final static String DELETE_JOB="delete.job";
-	public final static String DELETE_HOURS="timeout.hours";
+	public final static String DELETE_HOURS="delete.timeout.hours";
 	public void schedule(Long zoneId, int hours);
 	public void remove(Long zoneId);
 }
