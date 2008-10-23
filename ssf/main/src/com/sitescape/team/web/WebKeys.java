a4ffe8cac1bfa39ec978ab2181632f7671080e42
peterhurley <peterhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-10-23 15:18:20 +0000

    Mobile, allow stepping to next or previous entries.

--- ssf/main/src/com/sitescape/team/web/WebKeys.java
+++ ssf/main/src/com/sitescape/team/web/WebKeys.java
@@ -369,6 +369,8 @@ public class WebKeys {
     public static final String OPERATION_MOBILE_MODIFY_ENTRY="mobile_modify_entry"; 
     public static final String OPERATION_MOBILE_SHOW_FOLDER="mobile_show_folder"; 
     public static final String OPERATION_MOBILE_SHOW_ENTRY="mobile_show_entry"; 
+    public static final String OPERATION_MOBILE_SHOW_NEXT_ENTRY="mobile_show_next_entry"; 
+    public static final String OPERATION_MOBILE_SHOW_PREV_ENTRY="mobile_show_prev_entry"; 
     public static final String OPERATION_MOBILE_SHOW_FRONT_PAGE="mobile_show_front_page"; 
     public static final String OPERATION_MOBILE_SHOW_SEARCH_RESULTS="mobile_show_search_results"; 
     public static final String OPERATION_MOBILE_SHOW_WORKSPACE="mobile_show_workspace"; 
