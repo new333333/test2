d6076a8ec4dd33ee3ee80b4feb5e079c62692868
peterhurley <peterhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-12-04 14:45:25 +0000

    Bug:0 Comment: Remove TinyMCE; then add new version

---
+++ trunk/ssf/web/docroot/js/tiny_mce/plugins/ss_wikilink/editor_plugin_src.js
@@ -0,0 +1,85 @@
+/**
+ * $Id: editor_plugin_src.js 163 2007-01-03 16:31:00Z spocke $
+ *
+ * @author Moxiecode
+ * @copyright Copyright © 2004-2007, Moxiecode Systems AB, All rights reserved.
+ */
+
+/* Import plugin specific language pack */
+tinymce.PluginManager.requireLangPack('ss_wikilink');
+
+
+var TinyMCE_ssWikiLinkPlugin = {
+	// Load plugin specific language pack
+	tinymce.PluginManager.requireLangPack('ss_wikilink');
+
+	tinymce.create('tinymce.plugins.ssWikiLinkPlugin', {
+		/**
+		 * Initializes the plugin, this will be executed after the plugin has been created.
+		 * This call is done before the editor instance has finished it's initialization so use the onInit event
+		 * of the editor instance to intercept that event.
+		 *
+		 * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
+		 * @param {string} url Absolute URL to where the plugin is located.
+		 */
+		init : function(ed, url) {
+			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mce_ssWikiLink');
+			ed.addCommand('mce_ssWikiLink', function() {
+				ed.windowManager.open({
+					file : ss_wikiLinkUrl,
+					width : 250 + ed.getLang('ss_wikilink.delta_width', 0),
+					height : 160 + ed.getLang('ss_wikilink.delta_height', 0),
+					inline : 1
+				}, {
+					plugin_url : ss_wikiLinkUrl, // Plugin absolute URL
+				});
+			});
+
+			// Register ss_wikilink button
+			ed.addButton('ss_wikilink', {
+				title : 'ss_wikilink.desc',
+				cmd : 'mce_ssWikiLink',
+				image : url + '/images/wikilink.gif'
+			});
+
+			// Add a node change handler, selects the button in the UI when a image is selected
+			ed.onNodeChange.add(function(ed, cm, n) {
+				cm.setActive('ss_wikilink', n.nodeName == 'IMG');
+			});
+		},
+
+		/**
+		 * Creates control instances based in the incomming name. This method is normally not
+		 * needed since the addButton method of the tinymce.Editor class is a more easy way of adding buttons
+		 * but you sometimes need to create more complex controls like listboxes, split buttons etc then this
+		 * method can be used to create those.
+		 *
+		 * @param {String} n Name of the control to create.
+		 * @param {tinymce.ControlManager} cm Control manager to use inorder to create new control.
+		 * @return {tinymce.ui.Control} New control instance or null if no control was created.
+		 */
+		createControl : function(n, cm) {
+			return null;
+		},
+
+		/**
+		 * Returns information about the plugin as a name/value array.
+		 * The current keys are longname, author, authorurl, infourl and version.
+		 *
+		 * @return {Object} Name/value array containing information about the plugin.
+		 */
+		getInfo : function() {
+			return {
+				longname : 'ss_wikilink plugin',
+				author : 'Some author',
+				authorurl : 'http://tinymce.moxiecode.com',
+				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/ss_wikilink',
+				version : "1.0"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('ss_wikilink', tinymce.plugins.ssWikiLinkPlugin);
+})();
+
