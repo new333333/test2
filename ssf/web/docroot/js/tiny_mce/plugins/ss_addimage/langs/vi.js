c861f89411f659f5405374e79947242b86443a48
peterhurley <peterhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-12-04 14:45:25 +0000

    Bug:0 Comment: Remove TinyMCE; then add new version

---
+++ ssf/web/docroot/js/tiny_mce/plugins/ss_addimage/langs/vi.js
@@ -0,0 +1,27 @@
+// Vietnamese lang variables - �?ỗ Xuân Tiến - tiendx2002@yahoo.com Việt hóa
+
+tinyMCE.addToLang('ss_addimage',{
+tab_general : 'Chung',
+tab_appearance : 'Xuất hiện',
+tab_advanced : 'Nâng cao',
+general : 'Chung',
+title : 'Tiêu đ�?',
+preview : 'Xem trước',
+constrain_proportions : 'Ràng buộc kích thước',
+langdir : 'Hướng ngôn ngữ',
+langcode : 'Bộ mã ngôn ngữ',
+long_desc : 'Mô tả đầy đủ',
+style : 'Kiểu',
+classes : 'Lớp',
+ltr : 'Trái sang phải',
+rtl : 'Phải sang trái',
+id : 'Id',
+image_map : 'Bản đồ ảnh',
+swap_image : 'Thay đổi ảnh',
+alt_image : 'Ảnh thay thế',
+mouseover : 'khi di chuột vào',
+mouseout : 'khi di chuột ra ngoài',
+misc : 'Linh tinh',
+example_img : 'Ảnh&nbsp;xem&nbsp;trước',
+missing_alt : 'Bạn có chắc bạn muốn tiếp tục mà không cần nhập vào mô tả ảnh? Không có mô tả sẽ hạn chế khả năng truy cập của những ngư�?i sử dụng khuyết tật, hoặc những ngư�?i sử dụng trình duyệt văn bản, hoặc xem trang này khi đã tắt chế độ hiển thị ảnh.'
+});
