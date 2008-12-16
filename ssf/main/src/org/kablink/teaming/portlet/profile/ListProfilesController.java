4ea53be4e5fef34839476d7482287af148fe2956
peterhurley <peterhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-12-16 03:48:03 +0000

    Bug: 456962 Comment: Make sure the workaround works. If the user account doesn't get all the folders created, delete it and then have the user log in again. The workspace will get created properly.

--- ssf/main/src/org/kablink/teaming/portlet/profile/ListProfilesController.java
+++ ssf/main/src/org/kablink/teaming/portlet/profile/ListProfilesController.java
@@ -28,9 +28,7 @@
  */
 package org.kablink.teaming.portlet.profile;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.portlet.ActionRequest;
@@ -40,33 +38,19 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
 
-import org.dom4j.Document;
 import org.kablink.teaming.ObjectKeys;
 import org.kablink.teaming.context.request.RequestContextHolder;
 import org.kablink.teaming.domain.Binder;
-import org.kablink.teaming.domain.Definition;
-import org.kablink.teaming.domain.ProfileBinder;
 import org.kablink.teaming.domain.User;
-import org.kablink.teaming.domain.UserProperties;
 import org.kablink.teaming.domain.AuditTrail.AuditType;
-import org.kablink.teaming.module.admin.AdminModule.AdminOperation;
-import org.kablink.teaming.module.binder.BinderModule.BinderOperation;
-import org.kablink.teaming.module.profile.ProfileModule.ProfileOperation;
 import org.kablink.teaming.module.shared.MapInputData;
 import org.kablink.teaming.portletadapter.AdaptedPortletURL;
-import org.kablink.teaming.util.NLT;
-import org.kablink.teaming.util.SPropsUtil;
-import org.kablink.teaming.util.TagUtil;
 import org.kablink.teaming.web.WebKeys;
 import org.kablink.teaming.web.portlet.SAbstractController;
 import org.kablink.teaming.web.util.BinderHelper;
-import org.kablink.teaming.web.util.DashboardHelper;
-import org.kablink.teaming.web.util.DefinitionHelper;
 import org.kablink.teaming.web.util.PortletRequestUtils;
+import org.kablink.teaming.web.util.ProfilesBinderHelper;
 import org.kablink.teaming.web.util.Tabs;
-import org.kablink.teaming.web.util.Toolbar;
-import org.kablink.util.Validator;
-import org.kablink.util.search.Constants;
 import org.springframework.web.portlet.ModelAndView;
 
 
@@ -148,298 +132,9 @@ public class ListProfilesController extends   SAbstractController {
 		} else {
 			getReportModule().addAuditTrail(AuditType.view, binderObj);
 		}
+		return ProfilesBinderHelper.setupProfilesBinderBeans(this, binderId, request, response);
 
-	   	User user = RequestContextHolder.getRequestContext().getUser();
-		
-		//Set up the standard beans
-		BinderHelper.setupStandardBeans(this, request, response, model, binderId);
-		UserProperties userProperties = (UserProperties)model.get(WebKeys.USER_PROPERTIES_OBJ);
-		UserProperties userFolderProperties = (UserProperties)model.get(WebKeys.USER_FOLDER_PROPERTIES_OBJ);
-
-		model.put(WebKeys.ACTION, WebKeys.ACTION_VIEW_PROFILE_LISTING);
-		//Build a reload url
-		PortletURL reloadUrl = response.createRenderURL();
-		reloadUrl.setParameter(WebKeys.URL_BINDER_ID, binderId.toString());
-		reloadUrl.setParameter(WebKeys.ACTION, WebKeys.ACTION_VIEW_PROFILE_LISTING);
-		reloadUrl.setParameter(WebKeys.URL_ENTRY_ID, WebKeys.URL_ENTRY_ID_PLACE_HOLDER);
-		reloadUrl.setParameter(WebKeys.URL_RANDOM, WebKeys.URL_RANDOM_PLACEHOLDER);
-		model.put(WebKeys.RELOAD_URL, reloadUrl.toString());
-		
-		Map users = null;
-		Map options = new HashMap();
-		
-		options.put(ObjectKeys.SEARCH_MAX_HITS, new Integer(ObjectKeys.LISTING_MAX_PAGE_SIZE));
-		options.put(ObjectKeys.SEARCH_SORT_BY, Constants.TITLE1_FIELD);
-		options.put(ObjectKeys.SEARCH_SORT_DESCEND, Boolean.FALSE);
-
-		//initializing tabs
-		Tabs.TabEntry tab = BinderHelper.initTabs(request, binderObj);
-		model.put(WebKeys.TABS, tab.getTabs());
-		
-		//determine page starts/ counts
-		initPageCounts(request, userProperties.getProperties(), tab, options);
-		options.put(ObjectKeys.SEARCH_SEARCH_FILTER, BinderHelper.getSearchFilter(this, userFolderProperties));
-		users = getProfileModule().getUsers(options);
-		ProfileBinder binder = (ProfileBinder)users.get(ObjectKeys.BINDER);
-		model.put(WebKeys.BINDER, binder);
-		model.put(WebKeys.FOLDER, binder);
-		model.put(WebKeys.DEFINITION_ENTRY, binder);
-		
-		model.put(WebKeys.ENTRIES, users.get(ObjectKeys.SEARCH_ENTRIES));
-		model.put(WebKeys.SEEN_MAP,getProfileModule().getUserSeenMap(user.getId()));
-		
-		model.putAll(getSearchAndPagingModels(users, options));		
-
-		model.put(WebKeys.URL_TAB_ID, String.valueOf(tab.getTabId()));
-		model.put(WebKeys.PAGE_ENTRIES_PER_PAGE, (Integer) options.get(ObjectKeys.SEARCH_MAX_HITS));
-		model.put(WebKeys.PAGE_MENU_CONTROL_TITLE, NLT.get("folder.Page", new Object[]{options.get(ObjectKeys.SEARCH_MAX_HITS)}));
-		
-		DashboardHelper.getDashboardMap(binder, userProperties.getProperties(), model);
-		DefinitionHelper.getDefinitions(binder, model);
-		Object obj = model.get(WebKeys.CONFIG_ELEMENT);
-		if ((obj == null) || (obj.equals(""))) 
-			return new ModelAndView(WebKeys.VIEW_NO_DEFINITION, model);
-		obj = model.get(WebKeys.CONFIG_DEFINITION);
-		if ((obj == null) || (obj.equals(""))) 
-			return new ModelAndView(WebKeys.VIEW_NO_DEFINITION, model);
-		model.put(WebKeys.FOLDER_TOOLBAR, buildViewFolderToolbar(request, response, binder, model).getToolbar());
-		model.put(WebKeys.ENTRY_TOOLBAR, buildViewEntryToolbar(request, response, binder).getToolbar());
-
-		//Build the navigation beans
-		BinderHelper.buildNavigationLinkBeans(this, binder, model);
-		Map tagResults = TagUtil.uniqueTags(getBinderModule().getTags(binder));
-		model.put(WebKeys.COMMUNITY_TAGS, tagResults.get(ObjectKeys.COMMUNITY_ENTITY_TAGS));
-		model.put(WebKeys.PERSONAL_TAGS, tagResults.get(ObjectKeys.PERSONAL_ENTITY_TAGS));
-		
-		return new ModelAndView(BinderHelper.getViewListingJsp(this, BinderHelper.getViewType(this, binder)), model);
 	}
 
-	protected void initPageCounts(RenderRequest request, Map userProperties, Tabs.TabEntry tab, Map options) {
-		Map tabOptions = tab.getData();
-		//Determine the Records Per Page
-		//Getting the entries per page from the user properties
-		//String entriesPerPage = (String) userFolderProperties.getProperty(ObjectKeys.PAGE_ENTRIES_PER_PAGE);
-		//Moving the entries per information from the user/folder level to the user level.
-		String entriesPerPage = (String) userProperties.get(ObjectKeys.PAGE_ENTRIES_PER_PAGE);
-		//Getting the number of records per page entry in the tab
-		Integer recordsInPage = (Integer) tabOptions.get(Tabs.RECORDS_IN_PAGE);
-		Integer pageRecordIndex = (Integer) tabOptions.get(Tabs.PAGE);
-
-		//If the entries per page is not present in the user properties, then it means the
-		//number of records per page is obtained from the ssf properties file, so we do not have 
-		//to worry about checking the old and new number or records per page.
-		if (Validator.isNull(entriesPerPage)) {
-			//This means that the tab does not have the information about the number of records to display in a page
-			//So we need to add this information into the tab
-			if (recordsInPage == null) {
-				Integer searchMaxHits = Integer.valueOf(SPropsUtil.getString("folder.records.listed"));
-				options.put(ObjectKeys.SEARCH_MAX_HITS, searchMaxHits);
-				tabOptions.put(Tabs.RECORDS_IN_PAGE, searchMaxHits);
-			} else {
-				options.put(ObjectKeys.SEARCH_MAX_HITS, recordsInPage);
-			}
-		} else {
-			Integer perPage = Integer.valueOf(entriesPerPage);
-			options.put(ObjectKeys.SEARCH_MAX_HITS, perPage);
-			tabOptions.put(Tabs.RECORDS_IN_PAGE, perPage);
-			if (recordsInPage != null) {
-				int intEntriesPerPage = perPage.intValue();
-				int intEntriesPerPageInTab = recordsInPage.intValue();
-				
-				if (intEntriesPerPage != intEntriesPerPageInTab) {
-					//We need to check and see if the page number is set in the tabs. If so, reset it
-					if (pageRecordIndex != null) {
-						int intPageRecordIndex = pageRecordIndex.intValue();
-						int intNewPageNumber = (intPageRecordIndex + 1)/(intEntriesPerPage);
-						int intNewPageStartIndex = (intNewPageNumber) * intEntriesPerPage;
-						tabOptions.put(Tabs.PAGE, Integer.valueOf(intNewPageStartIndex));
-					}
-				}
-			} else {
-				tabOptions.put(Tabs.PAGE, new Integer(0));
-			}
-		}
-		
-		//Determine the Folder Start Index
-		Integer tabPageNumber = (Integer) tabOptions.get(Tabs.PAGE);
-		if (tabPageNumber == null) tabPageNumber = Integer.valueOf(0);
-		options.put(ObjectKeys.SEARCH_OFFSET, tabPageNumber);
-		tab.setData(tabOptions); //use synchronzied method
-	}		
-
-	protected Map getSearchAndPagingModels(Map userEntries, Map options) {
-		return BinderHelper.getSearchAndPagingModels(userEntries, options);
-	}
-	
-	//This method returns a HashMap with Keys referring to the Previous Page Keys,
-	//Paging Number related Page Keys and the Next Page Keys.
-	public HashMap getPagingLinks(int intTotalRecordsFound, int intSearchOffset, 
-			int intSearchPageIncrement, int intGoBackSoManyPages, int intGoFrontSoManyPages) {
-		
-		HashMap<String, Object> hmRet = new HashMap<String, Object>();
-		ArrayList<HashMap> pagingInfo = new ArrayList<HashMap>(); 
-		int currentDisplayValue = ( intSearchOffset + intSearchPageIncrement) / intSearchPageIncrement;		
-
-		//Adding Prev Page Link
-		int prevInternalValue = intSearchOffset - intSearchPageIncrement;
-		HashMap<String, Object> hmRetPrev = new HashMap<String, Object>();
-		hmRetPrev.put(WebKeys.PAGE_DISPLAY_VALUE, "<<");
-		hmRetPrev.put(WebKeys.PAGE_INTERNAL_VALUE, "" + prevInternalValue);
-		if (intSearchOffset == 0) {
-			hmRetPrev.put(WebKeys.PAGE_NO_LINK, "" + new Boolean(true));
-		}
-		hmRet.put(WebKeys.PAGE_PREVIOUS, hmRetPrev);
-
-		//Adding Links before Current Display
-		if (intSearchOffset != 0) {
-			//Code for generating the Numeric Paging Information previous to offset			
-			int startPrevDisplayFrom = currentDisplayValue - intGoBackSoManyPages;
-			
-			int wentBackSoManyPages = intGoBackSoManyPages + 1;
-			for (int i = startPrevDisplayFrom; i < currentDisplayValue; i++) {
-				wentBackSoManyPages--;
-				if (i < 1) continue;
-				prevInternalValue = (intSearchOffset - (intSearchPageIncrement * wentBackSoManyPages));
-				HashMap<String, Object> hmPrev = new HashMap<String, Object>();
-				hmPrev.put(WebKeys.PAGE_DISPLAY_VALUE, "" + i);
-				hmPrev.put(WebKeys.PAGE_INTERNAL_VALUE, "" + prevInternalValue);
-				pagingInfo.add(hmPrev);
-			}
-		}
-		
-		//Adding Links after Current Display
-		for (int i = 0; i < intGoFrontSoManyPages; i++) {
-			int nextInternalValue = intSearchOffset + (intSearchPageIncrement * i);
-			int nextDisplayValue = (nextInternalValue + intSearchPageIncrement) / intSearchPageIncrement;  
-			if ( !(nextInternalValue >= intTotalRecordsFound) ) {
-				HashMap<String, Object> hmNext = new HashMap<String, Object>();
-				hmNext.put(WebKeys.PAGE_DISPLAY_VALUE, "" + nextDisplayValue);
-				hmNext.put(WebKeys.PAGE_INTERNAL_VALUE, "" + nextInternalValue);
-				if (nextDisplayValue == currentDisplayValue) hmNext.put(WebKeys.PAGE_IS_CURRENT, new Boolean(true));
-				pagingInfo.add(hmNext);
-			}
-			else break;
-		}
-		hmRet.put(WebKeys.PAGE_NUMBERS, pagingInfo);
-		
-		//Adding Next Page Link
-		int nextInternalValue = intSearchOffset + intSearchPageIncrement;
-		HashMap<String, Object> hmRetNext = new HashMap<String, Object>();
-		hmRetNext.put(WebKeys.PAGE_DISPLAY_VALUE, ">>");
-		hmRetNext.put(WebKeys.PAGE_INTERNAL_VALUE, "" + nextInternalValue);
-		
-		if ( (nextInternalValue >= intTotalRecordsFound) ) {
-			hmRetNext.put(WebKeys.PAGE_NO_LINK, "" + new Boolean(true));
-		}
-		hmRet.put(WebKeys.PAGE_NEXT, hmRetNext);
-		hmRet.put(WebKeys.PAGE_START_INDEX, "" + (intSearchOffset + 1));
-		
-		if (nextInternalValue >= intTotalRecordsFound) hmRet.put(WebKeys.PAGE_END_INDEX, "" + intTotalRecordsFound);
-		else hmRet.put(WebKeys.PAGE_END_INDEX, "" + nextInternalValue);
-		
-		return hmRet;
-	}
-	
-	protected Toolbar buildViewFolderToolbar(RenderRequest request, RenderResponse response, 
-			ProfileBinder binder, Map model) {
-        User user = RequestContextHolder.getRequestContext().getUser();
-        String userDisplayStyle = user.getDisplayStyle();
-        if (userDisplayStyle == null) userDisplayStyle = ObjectKeys.USER_DISPLAY_STYLE_IFRAME;
-        String binderId = binder.getId().toString();
-		Toolbar dashboardToolbar = new Toolbar();
-        
-		//Build the toolbar array
-		Toolbar toolbar = new Toolbar();
-		AdaptedPortletURL adapterUrl;
-		//The "Administration" menu
-		Map qualifiers = new HashMap();
-		qualifiers.put("popup", new Boolean(true));
-		boolean adminMenuCreated=false;
-		toolbar.addToolbarMenu("1_administration", NLT.get("toolbar.manageThisWorkspace"));
-		if (getBinderModule().testAccess(binder, BinderOperation.modifyBinder)) {
-			adminMenuCreated=true;
-			adapterUrl = new AdaptedPortletURL(request, "ss_forum", true);
-			adapterUrl.setParameter(WebKeys.ACTION, WebKeys.ACTION_CONFIGURE_DEFINITIONS);
-			adapterUrl.setParameter(WebKeys.URL_BINDER_ID, binderId);
-			adapterUrl.setParameter(WebKeys.URL_BINDER_TYPE, binder.getEntityType().name());
-			toolbar.addToolbarMenuItem("1_administration", "", NLT.get("toolbar.menu.configuration"), adapterUrl.toString(), qualifiers);
-			//Modify
-			adapterUrl = new AdaptedPortletURL(request, "ss_forum", true);
-			adapterUrl.setParameter(WebKeys.ACTION, WebKeys.ACTION_MODIFY_BINDER);
-			adapterUrl.setParameter(WebKeys.URL_OPERATION, WebKeys.OPERATION_MODIFY);
-			adapterUrl.setParameter(WebKeys.URL_BINDER_ID, binderId);
-			adapterUrl.setParameter(WebKeys.URL_BINDER_TYPE, binder.getEntityType().name());
-			toolbar.addToolbarMenuItem("1_administration", "", NLT.get("toolbar.menu.modify_workspace"), adapterUrl.toString(), qualifiers);
-		}
-		//if no menu items were added, remove the empty menu
-		if (!adminMenuCreated) toolbar.deleteToolbarMenu("1_administration");
-
-		//Access control
-		if (getAdminModule().testAccess(binder, AdminOperation.manageFunctionMembership)) {
-			adminMenuCreated = true;
-			qualifiers = new HashMap();
-			qualifiers.put(WebKeys.HELP_SPOT, "helpSpot.accessControlMenu");
-			qualifiers.put("popup", new Boolean(true));
-			adapterUrl = new AdaptedPortletURL(request, "ss_forum", true);
-			adapterUrl.setParameter(WebKeys.ACTION, WebKeys.ACTION_ACCESS_CONTROL);
-			adapterUrl.setParameter(WebKeys.URL_BINDER_ID, binderId);
-			adapterUrl.setParameter(WebKeys.URL_BINDER_TYPE, binder.getEntityType().name());
-			toolbar.addToolbarMenuItem("1_administration", "", NLT.get("toolbar.menu.accessControl"), adapterUrl.toString(), qualifiers);
-		}
-
-		//Site administration
-		if (getAdminModule().testAccess(AdminOperation.manageFunction)) {
-			adminMenuCreated=true;
-			PortletURL url = response.createRenderURL();
-			url.setParameter(WebKeys.ACTION, WebKeys.ACTION_SITE_ADMINISTRATION);
-			url.setParameter(WebKeys.URL_BINDER_ID, binderId);
-			toolbar.addToolbarMenuItem("1_administration", "", 
-					NLT.get("toolbar.menu.siteAdministration"), url);
-		}
-
-		//	The "Manage dashboard" menu
-		BinderHelper.buildDashboardToolbar(request, response, this, binder, dashboardToolbar, model);
-		model.put(WebKeys.DASHBOARD_TOOLBAR, dashboardToolbar.getToolbar());
-
-		//Color themes
-		if (!ObjectKeys.GUEST_USER_INTERNALID.equals(user.getInternalId())) {
-			qualifiers = new HashMap();
-			qualifiers.put("onClick", "javascript: ss_changeUITheme('" +
-					NLT.get("ui.availableThemeIds") + "', '" +
-					NLT.get("ui.availableThemeNames") + "', '" +
-					NLT.get("sidebar.themeChange") + "'); return false;");
-			//footerToolbar.addToolbarMenu("themeChanger", NLT.get("toolbar.menu.changeUiTheme"), "javascript: ;", qualifiers);
-			model.put(WebKeys.TOOLBAR_THEME_IDS, NLT.get("ui.availableThemeIds"));
-			model.put(WebKeys.TOOLBAR_THEME_NAMES, NLT.get("ui.availableThemeNames"));
-		}
-		
-		return toolbar;
-	}
-	
-	protected Toolbar buildViewEntryToolbar (RenderRequest request, RenderResponse response, ProfileBinder binder) {
-        User user = RequestContextHolder.getRequestContext().getUser();
-        String userDisplayStyle = user.getDisplayStyle();
-        if (userDisplayStyle == null) userDisplayStyle = ObjectKeys.USER_DISPLAY_STYLE_IFRAME;
-		//Build the toolbar array
-		Toolbar toolbar = new Toolbar();
-		if (getProfileModule().testAccess(binder, ProfileOperation.addEntry)) {
-			List defaultEntryDefinitions = binder.getEntryDefinitions();
-			if (!defaultEntryDefinitions.isEmpty()) {
-				// Only one option
-				Definition def = (Definition) defaultEntryDefinitions.get(0);
-				AdaptedPortletURL adapterUrl = new AdaptedPortletURL(request, "ss_forum", true);
-				adapterUrl.setParameter(WebKeys.ACTION, WebKeys.ACTION_ADD_PROFILE_ENTRY);
-				adapterUrl.setParameter(WebKeys.URL_BINDER_ID, binder.getId().toString());
-				adapterUrl.setParameter(WebKeys.URL_ENTRY_TYPE, def.getId());
-				String[] nltArgs = new String[] {NLT.getDef(def.getTitle())};
-				String title = NLT.get("toolbar.new_with_arg", nltArgs);
-				Map qualifiers = new HashMap();
-				qualifiers.put("popup", new Boolean(true));
-				qualifiers.put("highlight", new Boolean(true));
-				toolbar.addToolbarMenu("1_add", title, adapterUrl.toString(), qualifiers);
-			}
-		}
-		return toolbar;
-	}
 
 }
