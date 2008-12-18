d8ec1e6d18a59211b172373527f8289db0c73fc2
janetmmccann <janetmmccann@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2008-12-18 17:29:17 +0000

    Support clusters with deployer

--- trunk/ssf/main/src/org/kablink/teaming/extension/ExtensionDeployer.java
+++ trunk/ssf/main/src/org/kablink/teaming/extension/ExtensionDeployer.java
@@ -7,17 +7,18 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * @author dml
  * 
  * Interface for a service which deploys and registers application extensions.
  * 
  */
 public interface ExtensionDeployer {
 	
+	public void check();
 	/**
 	 * Deploys a specified {@link File}-based extension
 	 * @param extension - the extension to be deployed
+	 * @param full - deploy into database and disk or just disk
 	 */
-	public void deploy(File extension) throws IOException;
+	public void deploy(File extension, boolean full) throws IOException;
 
 }
