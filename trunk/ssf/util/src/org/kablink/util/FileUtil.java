546a6f94927052691f974756731f341ad93bed4b
jongkim32 <jongkim32@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-01-21 18:52:31 +0000

    Bug: 402962 Comment: When viewing a file as HTML, surround the HTML with NOVELL_REWRITER_OFF/ON tags to prevent rewritting of file content. Use file.html.view.inject.novell.tag property in ssf.properties file to control whether these tags should be injected or not.

--- trunk/ssf/util/src/org/kablink/util/FileUtil.java
+++ trunk/ssf/util/src/org/kablink/util/FileUtil.java
@@ -32,6 +32,8 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Reader;
 
 import java.nio.channels.FileChannel;
@@ -49,6 +51,8 @@ import java.util.Properties;
  *
  */
 public class FileUtil {
+
+	private static final int BUFFER_SIZE = 4096;
 
 	public static void copyDirectory(
 		String sourceDirName, String destinationDirName) {
@@ -387,4 +391,15 @@ public class FileUtil {
 		write(new File(pathName, fileName), s);
 	}
 
+	public static int copy(InputStream in, OutputStream out) throws IOException {
+		int byteCount = 0;
+		byte[] buffer = new byte[BUFFER_SIZE];
+		int bytesRead = -1;
+		while ((bytesRead = in.read(buffer)) != -1) {
+			out.write(buffer, 0, bytesRead);
+			byteCount += bytesRead;
+		}
+		out.flush();
+		return byteCount;
+	}
 }
