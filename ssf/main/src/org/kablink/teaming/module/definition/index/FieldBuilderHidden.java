d503ecfa1dbb7ec928497b06f5b8131a736b85fc
peterhurley <peterhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-01-26 22:09:59 +0000

    Bug: 0 Comment: Add sortable field for text box elements

--- ssf/main/src/org/kablink/teaming/module/definition/index/FieldBuilderHidden.java
+++ ssf/main/src/org/kablink/teaming/module/definition/index/FieldBuilderHidden.java
@@ -33,6 +33,7 @@ import java.util.Set;
 
 import org.apache.lucene.document.Field;
 import org.kablink.teaming.search.BasicIndexUtils;
+import org.kablink.util.search.Constants;
 
 /**
  *
@@ -52,12 +53,13 @@ public class FieldBuilderHidden extends AbstractFieldBuilder {
         if (val == null) {
             return new Field[0];
         }
-         Field field = new Field(makeFieldName(dataElemName), val.toString(), Field.Store.YES, Field.Index.TOKENIZED);
+        Field sortField = new Field(makeFieldName(Constants.SORT_FIELD_PREFIX + dataElemName), val.toString().toLowerCase(), Field.Store.YES, Field.Index.UN_TOKENIZED);
+        Field field = new Field(makeFieldName(dataElemName), val.toString(), Field.Store.YES, Field.Index.TOKENIZED);
         if (!fieldsOnly) {
             Field allTextField = BasicIndexUtils.allTextField(val);
-        	return new Field[] {allTextField, field};
+        	return new Field[] {allTextField, field, sortField};
         } else {
-        	return new Field[] {field};
+        	return new Field[] {field, sortField};
         }
     }
 
