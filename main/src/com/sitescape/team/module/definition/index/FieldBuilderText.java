eed64a31d0fa03ce3afa193afadf7ef20f32d503
bramsey <bramsey@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-01-28 23:14:49 +0000

    Modified FieldBuilderText.java to add sort fields for text "custom command" fields that are placed in the Search Index.  This is for creating reports returned by service lookups. The changes add a new field _sort_fieldName with the value lowercased (as requested by peter)

--- main/src/com/sitescape/team/module/definition/index/FieldBuilderText.java
+++ main/src/com/sitescape/team/module/definition/index/FieldBuilderText.java
@@ -1,30 +1,12 @@
 /**
- * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the "CPAL");
- * you may not use this file except in compliance with the CPAL. You may obtain a copy of the CPAL at
- * http://www.opensource.org/licenses/cpal_1.0. The CPAL is based on the Mozilla Public License Version 1.1
- * but Sections 14 and 15 have been added to cover use of software over a computer network and provide for
- * limited attribution for the Original Developer. In addition, Exhibit A has been modified to be
- * consistent with Exhibit B.
- * 
- * Software distributed under the CPAL is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
- * either express or implied. See the CPAL for the specific language governing rights and limitations
- * under the CPAL.
- * 
- * The Original Code is ICEcore. The Original Developer is SiteScape, Inc. All portions of the code
- * written by SiteScape, Inc. are Copyright (c) 1998-2007 SiteScape, Inc. All Rights Reserved.
- * 
- * 
- * Attribution Information
- * Attribution Copyright Notice: Copyright (c) 1998-2007 SiteScape, Inc. All Rights Reserved.
- * Attribution Phrase (not exceeding 10 words): [Powered by ICEcore]
- * Attribution URL: [www.icecore.com]
- * Graphic Image as provided in the Covered Code [web/docroot/images/pics/powered_by_icecore.png].
- * Display of Attribution Information is required in Larger Works which are defined in the CPAL as a
- * work which combines Covered Code or portions thereof with code not governed by the terms of the CPAL.
- * 
- * 
- * SITESCAPE and the SiteScape logo are registered trademarks and ICEcore and the ICEcore logos
- * are trademarks of SiteScape, Inc.
+ * The contents of this file are governed by the terms of your license
+ * with SiteScape, Inc., which includes disclaimers of warranties and
+ * limitations on liability. You may not use this file except in accordance
+ * with the terms of that license. See the license for the specific language
+ * governing your rights and limitations under the license.
+ *
+ * Copyright (c) 2007 SiteScape, Inc.
+ *
  */
 package com.sitescape.team.module.definition.index;
 
@@ -45,7 +27,13 @@ public class FieldBuilderText extends AbstractFieldBuilder {
         //Just use the data name. It is guaranteed to be unique within its definition
     	return dataElemName;
     }
-    
+    public String makeSortName(String dataElemName) {
+    	//Return _sort_dataElemName
+    	return "_sort_" + dataElemName;
+    }
+    public String fixUpValue(String val) {
+    	return val.toLowerCase();
+    }
     protected Field[] build(String dataElemName, Set dataElemValue, Map args) {
         // This default text implementation ignores args.
               
@@ -73,11 +61,12 @@ public class FieldBuilderText extends AbstractFieldBuilder {
         }
         else {
            	Field textField = new Field(makeFieldName(dataElemName), val, Field.Store.YES, Field.Index.TOKENIZED); 
+           	Field sortField = new Field(makeSortName(dataElemName), fixUpValue(val), Field.Store.YES, Field.Index.UN_TOKENIZED); 
            	if (!fieldsOnly) {
                	Field allTextField = BasicIndexUtils.allTextField(val);
-               	return new Field[] {allTextField, textField};
+               	return new Field[] {allTextField, textField,sortField};
            	} else {
-               	return new Field[] {textField};
+               	return new Field[] {textField,sortField};
            	}
         }
     }
