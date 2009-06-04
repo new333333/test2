2eed893796ec3355562a5e6bfb72bedc5b8ead18
jboogaard <jboogaard@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-06-04 19:35:47 +0000

    Bug: BUILD Reviewer: jboogaard

--- ssf/web/docroot/help_doc/en/css/expandable.js
+++ ssf/web/docroot/help_doc/en/css/expandable.js
@@ -1,35 +1,3 @@
-/**
- * Copyright (c) 1998-2009 Novell, Inc. and its licensors. All rights reserved.
- * 
- * This work is governed by the Common Public Attribution License Version 1.0 (the
- * "CPAL"); you may not use this file except in compliance with the CPAL. You may
- * obtain a copy of the CPAL at http://www.opensource.org/licenses/cpal_1.0. The
- * CPAL is based on the Mozilla Public License Version 1.1 but Sections 14 and 15
- * have been added to cover use of software over a computer network and provide
- * for limited attribution for the Original Developer. In addition, Exhibit A has
- * been modified to be consistent with Exhibit B.
- * 
- * Software distributed under the CPAL is distributed on an "AS IS" basis, WITHOUT
- * WARRANTY OF ANY KIND, either express or implied. See the CPAL for the specific
- * language governing rights and limitations under the CPAL.
- * 
- * The Original Code is ICEcore, now called Kablink. The Original Developer is
- * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2009 Novell, Inc. All Rights Reserved.
- * 
- * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2009 Novell, Inc. All Rights Reserved.
- * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
- * Attribution URL: [www.kablink.org]
- * Graphic Image as provided in the Covered Code
- * [ssf/images/pics/powered_by_icecore.png].
- * Display of Attribution Information is required in Larger Works which are
- * defined in the CPAL as a work which combines Covered Code or portions thereof
- * with code not governed by the terms of the CPAL.
- * 
- * NOVELL and the Novell logo are registered trademarks and Kablink and the
- * Kablink logos are trademarks of Novell, Inc.
- */
 function addFirstChildElement(_1,_2){
 var _3=_1.firstChild;
 var _4=document.createElement(_2);
@@ -144,22 +112,21 @@ processExpandableSection(_21[i]);
 }
 }
 }
-_LOADERS=Array();
-function callAllLoaders(){
-var i,_24;
-for(i=0;i<_LOADERS.length;i++){
-_24=_LOADERS[i];
-if(_24!=callAllLoaders){
+_E_LDRS=Array();
+function callExpandableLoaders(){
+for(var i=0;i<_E_LDRS.length;i++){
+var _24=_E_LDRS[i];
+if(_24!=callExpandableLoaders){
 _24();
 }
 }
 }
-function appendLoader(_25){
-if(window.onload&&window.onload!=callAllLoaders){
-_LOADERS[_LOADERS.length]=window.onload;
+function addExpandableLoader(_25){
+if(window.onload&&window.onload!=callExpandableLoaders){
+_E_LDRS[_E_LDRS.length]=window.onload;
 }
-window.onload=callAllLoaders;
-_LOADERS[_LOADERS.length]=_25;
+window.onload=callExpandableLoaders;
+_E_LDRS[_E_LDRS.length]=_25;
 }
-appendLoader(autoInit_collapsed_sections);
+addExpandableLoader(autoInit_collapsed_sections);
 
