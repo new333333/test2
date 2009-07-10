3be0f5467672671779beb1d4fc1d9d1e5a44472e
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-07-10 16:43:08 +0000

    Bug:  PRD Reviewer:  Self Comment:  More GWT connections.
    Files:
    A    tools/gwt
    AM   tools/gwt/gwt-dev-windows.jar
    AM   tools/gwt/gwt-api-checker.jar
    AM   tools/gwt/gwt-dev-linux.jar
    AM   tools/gwt/gwt-dev-mac.jar
    AM   tools/gwt/gwt-user.jar
    M    lib/gwt-servlet.jar
    M    lib/versions.txt
    MM   gwt/gwt-compile.sh

--- trunk/ssf/gwt/gwt-compile.sh
+++ trunk/ssf/gwt/gwt-compile.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+
+
+# Hack to recognize the Mac vs. Linux.
+GWT_TOOLS="../tools/gwt"
+if [ -z "$Apple_PubSub_Socket_Render" ] ; then
+	GWT_DEVJAR="gwt-dev-linux.jar"
+else
+	GWT_DEVJAR="gwt-dev-mac.jar"
+fi
+
+
 APPDIR=`dirname $0`;
-java  -Xmx256M -cp "$APPDIR/src:$APPDIR/bin:/usr/local/gwt-linux-1.5.3/gwt-user.jar:/usr/local/gwt-linux-1.5.3/gwt-dev-linux.jar" com.google.gwt.dev.GWTCompiler -out "$APPDIR/www" "$@" org.kablink.teaming.gwt.GwtTeaming
+$JAVA_HOME/bin/java  -Xmx256M -cp "$APPDIR/src:$APPDIR/bin:$GWT_TOOLS/gwt-user.jar:$GWT_TOOLS/$GWT_DEVJAR" com.google.gwt.dev.Compiler -war "$APPDIR/www" "$@" org.kablink.teaming.gwt.GwtTeaming
+
