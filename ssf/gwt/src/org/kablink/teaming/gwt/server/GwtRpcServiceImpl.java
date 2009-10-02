fe3e7291b7bb931d327e1be0b198e4c19665f15f
jwootton <jwootton@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-10-02 23:31:02 +0000

    Bug: PRD Reviewer: Jay Wootton Comment: In the landing page editor, modified the GwtRpcServiceImpl class to extend AbstractAllModulesInjected instead of implementing the AllModulesInjected interface.

--- ssf/gwt/src/org/kablink/teaming/gwt/server/GwtRpcServiceImpl.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/server/GwtRpcServiceImpl.java
@@ -33,7 +33,6 @@
 package org.kablink.teaming.gwt.server;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,7 +41,6 @@ import org.kablink.teaming.gwt.client.GwtFolder;
 import org.kablink.teaming.gwt.client.GwtFolderEntry;
 import org.kablink.teaming.gwt.client.GwtSearchCriteria;
 import org.kablink.teaming.gwt.client.GwtSearchResults;
-import org.kablink.teaming.gwt.client.GwtSearchCriteria.SearchType;
 import org.kablink.teaming.gwt.client.service.GwtRpcService;
 
 import org.kablink.teaming.ObjectKeys;
@@ -50,36 +48,16 @@ import org.kablink.teaming.domain.Binder;
 import org.kablink.teaming.domain.EntityIdentifier;
 import org.kablink.teaming.domain.FolderEntry;
 import org.kablink.teaming.domain.UserProperties;
-import org.kablink.teaming.module.admin.AdminModule;
-import org.kablink.teaming.module.authentication.AuthenticationModule;
 import org.kablink.teaming.module.binder.BinderModule;
-import org.kablink.teaming.module.dashboard.DashboardModule;
-import org.kablink.teaming.module.definition.DefinitionModule;
-import org.kablink.teaming.module.file.ConvertedFileModule;
-import org.kablink.teaming.module.file.FileModule;
 import org.kablink.teaming.module.folder.FolderModule;
-import org.kablink.teaming.module.ic.ICBrokerModule;
-import org.kablink.teaming.module.ical.IcalModule;
-import org.kablink.teaming.module.ldap.LdapModule;
-import org.kablink.teaming.module.license.LicenseModule;
 import org.kablink.teaming.module.profile.ProfileModule;
-import org.kablink.teaming.module.report.ReportModule;
-import org.kablink.teaming.module.rss.RssModule;
-import org.kablink.teaming.module.template.TemplateModule;
-import org.kablink.teaming.module.workflow.WorkflowModule;
-import org.kablink.teaming.module.workspace.WorkspaceModule;
-import org.kablink.teaming.module.zone.ZoneModule;
 import org.kablink.teaming.search.filter.SearchFilter;
 import org.kablink.teaming.search.filter.SearchFilterKeys;
 import org.kablink.teaming.security.AccessControlException;
-import org.kablink.teaming.util.AllModulesInjected;
+import org.kablink.teaming.util.AbstractAllModulesInjected;
 import org.kablink.teaming.util.NLT;
-import org.kablink.teaming.util.SPropsUtil;
-import org.kablink.teaming.web.WebKeys;
 import org.kablink.teaming.web.util.BinderHelper;
-import org.kablink.teaming.web.util.PortletRequestUtils;
 import org.kablink.util.search.Constants;
-import org.springframework.web.portlet.ModelAndView;
 
 
 
@@ -88,29 +66,9 @@ import org.springframework.web.portlet.ModelAndView;
  * @author jwootton
  *
  */
-public class GwtRpcServiceImpl  implements GwtRpcService, AllModulesInjected
+public class GwtRpcServiceImpl  extends AbstractAllModulesInjected
+	implements GwtRpcService
 {
-	private AdminModule			m_adminModule;
-	private AuthenticationModule	m_authenticationModule;
-	private BinderModule			m_binderModule;
-	private ConvertedFileModule	m_convertedFileModule;
-	private DashboardModule		m_dashboardModule;
-	private DefinitionModule		m_definitionModule;
-	private FileModule				m_fileModule;
-	private FolderModule			m_folderModule;
-	private IcalModule				m_icalModule;
-	private ICBrokerModule			m_icBrokerModule;
-	private LdapModule				m_ldapModule;
-	private LicenseModule			m_licenseModule;
-	private ProfileModule			m_profileModule;
-	private ReportModule			m_reportModule;
-	private RssModule				m_rssModule;
-	private TemplateModule			m_templateModule;
-	private WorkflowModule			m_workflowModule;
-	private WorkspaceModule		m_workspaceModule;
-	private ZoneModule				m_zoneModule;
-	
-
 	/**
 	 * This method is meant to search for applications or entries or groups or places or tags or teams or users.
 	 */
@@ -432,344 +390,4 @@ public class GwtRpcServiceImpl  implements GwtRpcService, AllModulesInjected
     	return tutorialPanelState;
     }// end getTutorialPanelState()
     
-
-    /**
-     * 
-     */
-	public AdminModule getAdminModule()
-	{
-		return m_adminModule;
-	}// end getAdminModule()
-	
-
-	/**
-	 * 
-	 */
-	public AuthenticationModule getAuthenticationModule()
-	{
-		return m_authenticationModule;
-	}// end getAuthenticationModule()
-	
-
-    /**
-     * 
-     */
-	public BinderModule getBinderModule()
-	{
-		return m_binderModule;
-	}// end getBinderModule()
-	
-
-	/**
-	 * 
-	 */
-	public ConvertedFileModule getConvertedFileModule()
-	{
-		return m_convertedFileModule;
-	}// end getConvertedFileModule()
-	
-
-	/**
-	 * 
-	 */
-	public DashboardModule getDashboardModule()
-	{
-		return m_dashboardModule;
-	}// end getDashboardModule()
-	
-
-	/**
-	 * 
-	 */
-	public DefinitionModule getDefinitionModule()
-	{
-		return m_definitionModule;
-	}// end getDefinitionModule()
-	
-	
-	/**
-	 * 
-	 */
-	public FileModule getFileModule()
-	{
-		return m_fileModule;
-	}// end getFileModule()
-	
-	
-	/**
-	 * 
-	 */
-	public FolderModule getFolderModule()
-	{
-		return m_folderModule;
-	}// end getFolderModule()
-	
-
-	/**
-	 * 
-	 */
-	public IcalModule getIcalModule()
-	{
-		return m_icalModule;
-	}// end getIcalModule()
-	
-	
-	/**
-	 * 
-	 */
-	public ICBrokerModule getIcBrokerModule()
-	{
-		return m_icBrokerModule;
-	}// end getIcBrokerModule()
-	
-
-	/**
-	 * 
-	 */
-	public LdapModule getLdapModule()
-	{
-		return m_ldapModule;
-	}// end getLdapModule()
-	
-	
-	/**
-	 * 
-	 */
-	public LicenseModule getLicenseModule()
-	{
-		return m_licenseModule;
-	}// end getLicenseModule()
-	
-	
-	/**
-	 * 
-	 */
-	public ProfileModule getProfileModule()
-	{
-		return m_profileModule;
-	}// end getProfileModule()
-	
-	
-	/**
-	 * 
-	 */
-	public ReportModule getReportModule()
-	{
-		return m_reportModule;
-	}// end getReportModule()
-	
-	
-	/**
-	 * 
-	 */
-	public RssModule getRssModule()
-	{
-		return m_rssModule;
-	}// end getRssModule()
-	
-	
-	/**
-	 * 
-	 */
-    public TemplateModule getTemplateModule()
-    {
-    	return m_templateModule;
-    }// end getTemplateModule()
-    
-    
-    /**
-     * 
-     */
-	public WorkflowModule getWorkflowModule()
-	{
-		return m_workflowModule;
-	}// end getWorkflowModule()
-
-	
-	/**
-	 * 
-	 */
-	public WorkspaceModule getWorkspaceModule()
-	{
-		return m_workspaceModule;
-	}// end getWorkspaceModule()
-	
-
-	/**
-	 * 
-	 */
-	public ZoneModule getZoneModule()
-	{
-		return m_zoneModule;
-	}// end getZoneModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setAdminModule( AdminModule adminModule )
-	{
-		m_adminModule = adminModule;
-	}// end setAdminModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setAuthenticationModule( AuthenticationModule authenticationModule )
-	{
-		m_authenticationModule = authenticationModule;
-	}// end setAuthenticationModule()
-
-	
-    /**
-     * 
-     */
-	public void setBinderModule( BinderModule binderModule )
-	{
-		m_binderModule = binderModule;
-	}// end setBinderModule()
-	
-
-	/**
-	 * 
-	 */
-	public void setConvertedFileModule( ConvertedFileModule convertedFileModule )
-	{
-		m_convertedFileModule = convertedFileModule;
-	}// end setConvertedFileModule()
-	
-
-	/**
-	 * 
-	 */
-	public void setDashboardModule( DashboardModule dashboardModule )
-	{
-		m_dashboardModule = dashboardModule;
-	}// end setDashboardModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setDefinitionModule( DefinitionModule definitionModule )
-	{
-		m_definitionModule = definitionModule;
-	}// end setDefinitionModule()
-	
-
-	/**
-	 * 
-	 */
-	public void setFileModule( FileModule fileModule )
-	{
-		m_fileModule = fileModule;
-	}// end setFileModule()
-	
-
-	/**
-	 * 
-	 */
-	public void setFolderModule( FolderModule folderModule )
-	{
-		m_folderModule = folderModule;
-	}// end setFolderModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setIcalModule( IcalModule icalModule )
-	{
-		m_icalModule = icalModule;
-	}// end setIcalModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setIcBrokerModule( ICBrokerModule icBrokerModule )
-	{
-		m_icBrokerModule = icBrokerModule;
-	}// end setIcBrokerModule()
-	
-	/**
-	 * 
-	 */
-	public void setLdapModule( LdapModule ldapModule )
-	{
-		m_ldapModule = ldapModule;
-	}// end setLdapModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setLicenseModule( LicenseModule licenseModule )
-	{
-		m_licenseModule = licenseModule;
-	}// end setLicenseModule()
-	
-
-	/**
-	 * 
-	 */
-	public void setProfileModule( ProfileModule profileModule )
-	{
-		m_profileModule = profileModule;
-	}// end setProfileModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setReportModule( ReportModule reportModule )
-	{
-		m_reportModule = reportModule;
-	}// end setReportModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setRssModule( RssModule rssModule )
-	{
-		m_rssModule = rssModule;
-	}// end setRssModule()
-	
-	
-    /**
-     * 
-     */
-    public void setTemplateModule( TemplateModule templateModule )
-    {
-    	m_templateModule = templateModule;
-    }// end setTemplateModule()
-    
-
-	/**
-	 * 
-	 */
-	public void setWorkflowModule( WorkflowModule workflowModule )
-	{
-		m_workflowModule = workflowModule;
-	}// end setWorkflowModule()
-	
-	
-	/**
-	 * 
-	 */
-	public void setWorkspaceModule( WorkspaceModule workspaceModule )
-	{
-		m_workspaceModule = workspaceModule;
-	}// end setWorkspaceModule()
-	
-	/**
-	 * 
-	 */
-	public void setZoneModule( ZoneModule zoneModule )
-	{
-		m_zoneModule = zoneModule;
-	}// end setZoneModule()
-
 }// end GwtRpcServiceImpl
