97e4e1c9c7d898757ef73ace2c77ee55c1716911
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-10-08 15:57:55 +0000

    Bug:  PRD Reviewer:  Self Comment:  Added the relevance UUID to the SS_Attachments table.
    Files:
    M    tools/sql/update-2.0.0-2.1.0/update-mysql.sql
    M    tools/sql/update-2.0.0-2.1.0/update-oracle.sql
    M    tools/sql/update-2.0.0-2.1.0/update-sqlserver.sql
    M    main/hibernate/org/kablink/teaming/domain/Attachment.hbm.xml
    M    main/src/org/kablink/teaming/domain/Attachment.java

--- ssf/tools/sql/update-2.0.0-2.1.0/update-mysql.sql
+++ ssf/tools/sql/update-2.0.0-2.1.0/update-mysql.sql
@@ -3,3 +3,4 @@ alter table SS_ZoneConfig add column mobileAccessEnabled bit;
 alter table SS_AuditTrail add column deletedFolderEntryFamily varchar(32);
 alter table SS_FolderEntries add preDeleted bit;
 alter table SS_Forums add preDeleted bit;
+alter table SS_Attachments add column relevanceUUID varchar(256);
