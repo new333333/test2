fb84a52b3176986f46e829d1e034f3bcd9308cc6
nbjensen <nbjensen@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-10-22 21:40:15 +0000

    Bug: PRD Reviewer: Nathan Jensen Comment: Extensions

--- trunk/ssf/tools/sql/update-2.0.0-2.1.0/update-sqlserver.sql
+++ trunk/ssf/tools/sql/update-2.0.0-2.1.0/update-sqlserver.sql
@@ -14,3 +14,5 @@ alter table SS_Principals add maxGroupsQuota numeric(19,0);
 alter table SS_ZoneConfig add diskQuotasEnabled tinyint;
 alter table SS_ZoneConfig add diskQuotaUserDefault numeric(19,0);
 alter table SS_ZoneConfig add diskQuotasHighwaterPercentage numeric(19,0);
+create table SS_Extensions (id char(32) not null, lockVersion numeric(19,0) not null, name varchar(64) null, description varchar(256) null, type int null, author varchar(255) null, authorEmail varchar(255) null, authorSite varchar(255) null, dateCreated varchar(255) null, dateDeployed varchar(255) null, zoneId numeric(19,0) null, primary key (id), unique (zoneId, name));
+
