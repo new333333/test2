b86764903d4bf1b98e127b88ca4f16b6199d6cc7
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-11-11 14:47:41 +0000

    Bug: 554441 Reviewer: Peter Comment: fix quota display on profile page if user does not have a quota

--- trunk/ssf/main/src/org/kablink/teaming/ObjectKeys.java
+++ trunk/ssf/main/src/org/kablink/teaming/ObjectKeys.java
@@ -624,6 +624,7 @@ public interface ObjectKeys {
     public static final	int DISKQUOTA_EXCEEDED = 3;
     public static final String PRINCIPAL_TYPE_GROUP = "group";
     public static final String PRINCIPAL_TYPE_USER = "user";
+    public static final	int DISKQUOTA_MAX = 1000000;
     
     //Mobile related constants
 	public static final String USER_PROPERTY_MOBILE_WHATS_NEW_TYPE = "mobile_whats_new_type";
--- trunk/ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
@@ -456,6 +456,7 @@ public class BinderHelper {
 		model.put(WebKeys.QUOTAS_ENABLED, bs.getAdminModule().isQuotaEnabled());
 		model.put(WebKeys.DISK_QUOTA_EXCEEDED, bs.getProfileModule().isDiskQuotaExceeded());
 		model.put(WebKeys.DISK_QUOTA_HIGH_WATER_MARK_EXCEEDED, bs.getProfileModule().isDiskQuotaHighWaterMarkExceeded());
+		model.put(WebKeys.DISK_QUOTA_USER_MAXIMUM, bs.getProfileModule().getMaxUserQuota());
 		
 		model.put(WebKeys.PRODUCT_NAME, SPropsUtil.getString("product.name", ObjectKeys.PRODUCT_NAME_DEFAULT));
 		model.put(WebKeys.PRODUCT_TITLE, SPropsUtil.getString("product.title", ObjectKeys.PRODUCT_TITLE_DEFAULT));
