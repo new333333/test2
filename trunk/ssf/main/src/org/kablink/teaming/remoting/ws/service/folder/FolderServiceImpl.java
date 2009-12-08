818ef9339bc3e9f530b092473127b77c7ebde742
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2009-12-08 19:24:17 +0000

    Bug: 561683 Reviewer: Jong Comment: Remove an import statement added by accident.

--- trunk/ssf/main/src/org/kablink/teaming/remoting/ws/service/folder/FolderServiceImpl.java
+++ trunk/ssf/main/src/org/kablink/teaming/remoting/ws/service/folder/FolderServiceImpl.java
@@ -88,9 +88,6 @@ import org.kablink.util.search.Constants;
 import org.kablink.util.search.Criteria;
 import org.kablink.util.search.Order;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
-
-
 public class FolderServiceImpl extends BaseService implements FolderService, FolderServiceInternal {
 
 	protected ProcessorManager processorManager;
