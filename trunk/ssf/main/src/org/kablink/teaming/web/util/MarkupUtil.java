6b498ff62afbea8869860eac291347917f396222
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-01-06 22:54:54 +0000

    Bug: 537929 Reviewer: Peter Comment: fix markup to correctly find wiki links if there is html in the title

--- trunk/ssf/main/src/org/kablink/teaming/web/util/MarkupUtil.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/util/MarkupUtil.java
@@ -96,8 +96,9 @@ public class MarkupUtil {
 	protected final static Pattern readFilePathPattern = Pattern.compile("/readFile/[^\"]*");
 	protected final static Pattern attachedImagePattern = Pattern.compile("(<img [^>]*class=\"\\s*ss_addimage_att\\s*\"[^>]*>)");
 	
-	protected final static Pattern iceCoreLinkPattern = Pattern.compile("(<a [^>]*class=\"\\s*ss_icecore_link\\s*\"[^>]*>)([^<]*)</a>");
+	protected final static Pattern iceCoreLinkPattern = Pattern.compile("(<a [^>]*class=\"\\s*ss_icecore_link\\s*\"[^>]*>)(.*)");
 	protected final static Pattern iceCoreLinkRelPattern = Pattern.compile("rel=\"([^\"]*)");
+	protected final static Pattern iceCoreLinkAPattern = Pattern.compile("</a>");
 	
 	protected final static Pattern youtubeLinkPattern = Pattern.compile("(<a [^>]*class=\"\\s*ss_youtube_link\\s*\"[^>]*>)([^<]*)</a>");
 	protected final static Pattern youtubeLinkRelPattern = Pattern.compile("rel=\"([^\"]*)");
@@ -290,9 +291,13 @@ public class MarkupUtil {
     		
         	String linkText = "" ;
         	if (m.groupCount() >= 2) { linkText = m.group(2).trim().replace("$", "\\$"); }
+        	int i = linkText.indexOf("</a>");
+        	if (i < 0) break;
+        	String titleText = linkText.substring(0, i);
+        	String remainderText = linkText.substring(i+4, linkText.length());
 
         	if (Validator.isNotNull(linkArgs)) {
-        		description.setText(m.replaceFirst("{{titleUrl: " + linkArgs.replaceAll("%2B", "+") + " text=" + Html.stripHtml(linkText) + "}}"));
+        		description.setText(m.replaceFirst("{{titleUrl: " + linkArgs.replaceAll("%2B", "+") + " text=" + Html.stripHtml(titleText) + "}}" + remainderText));
         		m = iceCoreLinkPattern.matcher(description.getText());
 	    	}
     	}
