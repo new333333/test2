eaa156f4e7d821a4120ea159bfae02881bdf403d
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-01-11 21:37:04 +0000

    Bug:  PRD Reviewer:  Self Comment:  Moved Durango to GWT 2.0.0 from GWT 1.7.1.
    Files:
    D    tools/gwt/gwt-dev-windows.jar
    M    tools/gwt/gwt-api-checker.jar
    D    tools/gwt/gwt-dev-linux.jar
    D    tools/gwt/gwt-dev-mac.jar
    M    tools/gwt/gwt-user.jar
    AM   tools/gwt/gwt-dev.jar
    M    lib/gwt-servlet.jar
    M    lib/versions.txt
    M    gwt/gwt-compile.sh
    M    gwt/gwt-compile.cmd
    M    gwt/build.xml

--- ssf/gwt/gwt-compile.cmd
+++ ssf/gwt/gwt-compile.cmd
@@ -2,6 +2,6 @@
 
 setlocal
 	set GWT_TOOLS="../tools/gwt"
-	%JAVA_HOME%/bin/java -Xmx256M -cp "%~dp0\src;%~dp0\bin;%GWT_TOOLS%/gwt-user.jar;%GWT_TOOLS%/gwt-dev-windows.jar;%GWT_TOOLS%/smartgwt.jar" com.google.gwt.dev.Compiler -war "%~dp0\www" %* org.kablink.teaming.gwt.GwtTeaming
+	%JAVA_HOME%/bin/java -Xmx256M -cp "%~dp0\src;%~dp0\bin;%GWT_TOOLS%/gwt-user.jar;%GWT_TOOLS%/gwt-dev.jar;%GWT_TOOLS%/smartgwt.jar" com.google.gwt.dev.Compiler -war "%~dp0\www" %* org.kablink.teaming.gwt.GwtTeaming
 endlocal
 
