6edfe4f1ad8163de1cdea8f06d281ab4b9799f68
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-01-14 20:40:29 +0000

    Bug: PRD Reviewer: Peter Comment: Upgrade to TinyMCE 3.2.7

--- ssf/web/docroot/js/tiny_mce/plugins/ss_addimage/editor_plugin.js
+++ ssf/web/docroot/js/tiny_mce/plugins/ss_addimage/editor_plugin.js
@@ -55,7 +55,7 @@
 
 			// Add a node change handler, selects the button in the UI when a image is selected
 			ed.onNodeChange.add(function(ed, cm, n) {
-				cm.setActive('ss_addimage', n.nodeName == 'IMG' && n.className == 'ss_addimage');
+				cm.setActive('ss_addimage', n.nodeName == 'IMG' && tinymce.trim(n.className) == 'ss_addimage');
 			});
 		},
 
