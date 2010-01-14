364cfb79d4160fd4f18800cf65f1c40924d4b198
jwootton <jwootton@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-01-14 23:37:40 +0000

    Bug: PRD Reviewer: Jay Wootton Comment: Durango UI investigation.

--- ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeaming.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeaming.java
@@ -40,8 +40,17 @@ import org.kablink.teaming.gwt.client.widgets.MastHead;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Frame;
+import com.google.gwt.user.client.ui.Hidden;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -51,6 +60,9 @@ public class GwtTeaming implements EntryPoint
 	private static final GwtTeamingMessages		m_stringMessages = GWT.create( GwtTeamingMessages.class );
 	private static final GwtTeamingImageBundle	m_imageBundle = GWT.create( GwtTeamingImageBundle.class );
 	private static final GwtRpcServiceAsync		m_gwtRpcService = (GwtRpcServiceAsync) GWT.create( GwtRpcService.class );
+	
+	private static TextBox m_txtBox;
+	private static Frame m_frame = null;
 	
 	/**
 	 * Return the object that is used to retrieve images.
@@ -144,13 +156,106 @@ public class GwtTeaming implements EntryPoint
 		rootPanel = RootPanel.get( "gwtMainPageDiv" );
 		if ( rootPanel != null )
 		{
+			FlowPanel flowPanel;
+			FlowPanel flowPanel2;
 			MastHead mastHead;
+			Button btn;
+			ClickHandler clkHandler;
+			Hidden hidden;
+			
+			// Has the main GWT page already been loaded.
+			if ( isGwtMainPageLoaded() )
+			{
+				String contentPanelUrl;
+				
+				contentPanelUrl = getContentPanelUrl();
+				
+				Window.alert( "GwtMainPage.jsp is being loaded inside GwtMainPage.jsp " );
+				passUrlToMainPage( contentPanelUrl );
+				return;
+			}
+			
+			flowPanel = new FlowPanel();
+			
+			// Add a hidden element that indicates that the main GWT page has already been loaded.
+			hidden = new Hidden();
+			hidden.setID( "gwtMainPageLoaded" );
+			flowPanel.add( hidden );
 			
 			mastHead = new MastHead();
-			rootPanel.add( mastHead );
+			flowPanel.add( mastHead );
 			
+			m_txtBox = new TextBox();
+			m_txtBox.setVisibleLength( 125 );
+			flowPanel.add( m_txtBox );
+			
+			clkHandler = new ClickHandler()
+			{
+				public void onClick( ClickEvent event )
+				{
+					String url;
+					
+					url = m_txtBox.getText();
+					setContentPanelUrl( url );
+					m_frame.setUrl( url );
+				}
+			};
+			btn = new Button( "Test", clkHandler );
+			flowPanel.add( btn );
+			
+			flowPanel2 = new FlowPanel();
+			flowPanel.add( flowPanel2 );
+			
+			m_frame = new Frame();
+			m_frame.setPixelSize( 700, 500 );
+			m_frame.getElement().setId( "gwtContentPanel" );
+			flowPanel2.add( m_frame );
+			
+			rootPanel.add( flowPanel );
 			return;
 		}
 		
 	}// end onModuleLoad()
+
+
+	/**
+	 * 
+	 */
+	public static native String getContentPanelUrl() /*-{
+		return window.top.m_contentPanelUrl;
+	}-*/;
+	
+
+	/**
+	 * 
+	 */
+	public static native String setContentPanelUrl( String url ) /*-{
+		window.top.m_contentPanelUrl = url;
+	}-*/;
+	
+
+	/**
+	 * This method will return true if the GWT Main page has already been loaded.  We want
+	 * to prevent the GWT main page from being loaded into the Content Panel.  When the
+	 * GWT main page loads it will create a hidden input with the id, 'gwtMainPageLoaded'.
+	 */
+	public static native boolean isGwtMainPageLoaded() /*-{
+		var input;
+		
+		// Does the hidden input, "gwtMainPageLoaded", exist?
+		input = window.top.document.getElementById( 'gwtMainPageLoaded' );
+		if ( input != null )
+			return true;
+			
+		return false;
+	}-*/;
+	
+
+	/**
+	 * 
+	 */
+	public static native void passUrlToMainPage( String url ) /*-{
+		window.top.location.href = url;
+	}-*/;
+	
 }// end GwtTeaming
