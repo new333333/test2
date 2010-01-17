7a6f1185fc2c018d039190ee6793b2f7761d6240
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-01-17 16:32:10 +0000

    Bug: 569836 Reviewer: Peter Comment: Make export store graphic titles if they don't already exist in the mashup config.

--- trunk/ssf/main/src/org/kablink/teaming/ObjectKeys.java
+++ trunk/ssf/main/src/org/kablink/teaming/ObjectKeys.java
@@ -242,10 +242,13 @@ public interface ObjectKeys {
     public static final String MASHUP_ATTR_ENTRIES_TO_SHOW = "entriesToShow";
     public static final String MASHUP_ATTR_FOLDER_ID = "folderId";
     public static final String MASHUP_ATTR_BINDER_ID = "binderId";
+    public static final String MASHUP_ATTR_GRAPHIC = "graphic";
+    public static final String MASHUP_ATTR_TITLE = "title";
     public static final String MASHUP_ATTR_ZONE_UUID = "zoneUUID";
     public static final String MASHUP_TYPE_CUSTOM_JSP = "customJsp";
     public static final String MASHUP_TYPE_ENTRY = "entry";
     public static final String MASHUP_TYPE_FOLDER = "folder";
+    public static final String MASHUP_TYPE_GRAPHIC = "graphic";
     public static final String MASHUP_TYPE_BINDER_URL = "binderUrl";
     public static final String MASHUP_TYPE_ENTRY_URL = "entryUrl";
     public static final String MASHUP_TYPE_TABLE = "table";
