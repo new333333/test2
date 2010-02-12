bbcebd2431c09d6831189f172b491adc2c9343da
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-02-12 14:01:14 +0000

    Bug: PRD Reviewer: Peter Comment: Remove old unused TinyMCE version from the kit

--- trunk/ssf/web/docroot/js/tiny_mce/plugins/paste/langs/es_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('es.paste_dlg',{
-text_title:"Use CTRL+V en su teclado para pegar el texto en la ventana.",
-text_linebreaks:"Conservar saltos de línea",
-word_title:"Use CTRL+V en su teclado para pegar el texto en la ventana."
-});
