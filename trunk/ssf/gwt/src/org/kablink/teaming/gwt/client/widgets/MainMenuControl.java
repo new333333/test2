4fae976be3b568560a287179401533b0c7ab568d
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-03-09 16:38:59 +0000

    Bug:  PRD Reviewer:  Self Comments:  Continued implementation of the MainMainControl.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/MainMenuControl.java
    AM   gwt/src/org/kablink/teaming/gwt/public/images/blue_slice.png
    M    gwt/src/org/kablink/teaming/gwt/public/GwtTeaming.css

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/MainMenuControl.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/MainMenuControl.java
@@ -33,18 +33,24 @@
 
 package org.kablink.teaming.gwt.client.widgets;
 
-
 import org.kablink.teaming.gwt.client.GwtTeaming;
 import org.kablink.teaming.gwt.client.service.GwtRpcServiceAsync;
 
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Widget;
 
 
 /**
@@ -53,126 +59,236 @@ import com.google.gwt.user.client.ui.Image;
  * @author drfoster@novell.com
  */
 public class MainMenuControl extends Composite {
-	private Image m_slideLeftImg;
-	private Image m_slideRightImg;
-	private Image m_slideUpImg;
-	private Image m_slideDownImg;
-	private Image m_browseHierarchyImg;
+	/*
+	 * Inner classed used to track clicks on the GWT UI button.
+	 */
+	private static class GwtUISelector implements ClickHandler {
+		/**
+		 * Called when the button is clicked.
+		 * 
+		 * @param event
+		 */
+		public void onClick(ClickEvent event) {
+			GwtRpcServiceAsync rpcService = GwtTeaming.getRpcService();
+			rpcService.getUserWorkspacePermalink(new AsyncCallback<String>() {
+				public void onFailure(Throwable t) {}
+				public void onSuccess(String userWorkspaceURL)  {
+					jsToggleGwtUI();
+					jsLoadUserWorkspaceURL(userWorkspaceURL + "&captive=false");
+				}
+				
+				private native void jsToggleGwtUI() /*-{
+					// Toggle the GWT UI state.
+					window.top.ss_toggleGwtUI(false);
+				}-*/;
+
+				private native void jsLoadUserWorkspaceURL(String userWorkspaceURL) /*-{
+					// Give the GWT UI state toggling 1/2
+					// second to complete and reload the user
+					// workspace.
+					window.setTimeout(
+						function() {
+							window.top.location.href = userWorkspaceURL;
+						},
+						500);
+				}-*/;
+			});
+		}
+	}
 	
+	/*
+	 * Inner class used to handle mouse events for menu buttons.  
+	 */
+	private static class MenuButtonHover implements MouseOverHandler, MouseOutHandler {
+		private Widget m_buttonWidget;
+		
+		/**
+		 * Class constructor.
+		 * 
+		 * @param buttonAnchor
+		 */
+		MenuButtonHover(Widget buttonAnchor) {
+			// Simply store the parameters.
+			m_buttonWidget = buttonAnchor;
+		}
+		
+		/**
+		 * Called when the mouse leaves a menu button.
+		 * 
+		 * @param me
+		 */
+		public void onMouseOut(MouseOutEvent me) {
+			// Simply remove the hover style.
+			m_buttonWidget.removeStyleName("subhead-control-bg2");
+		}
+		
+		/**
+		 * Called when the mouse enters a menu button.
+		 * 
+		 * @param me
+		 */
+		public void onMouseOver(MouseOverEvent me) {
+			// Simply add the hover style.
+			m_buttonWidget.addStyleName("subhead-control-bg2");
+		}
+	}
+	
+	/*
+	 * Inner class that implements clicking on button on the menu.
+	 */
+	private static class MenuButtonSelector implements ClickHandler {
+		private String m_action;
+
+		/**
+		 * Class constructor.
+		 * 
+		 * @param action
+		 */
+		MenuButtonSelector(String action) {
+			// Simply store the parameters.
+			m_action = action;
+		}
+		
+		/**
+		 * Called when the menu button is clicked.
+		 * 
+		 * @param event
+		 */
+		public void onClick(ClickEvent event) {
+			// Fire the action.
+			Window.alert("MainMenuControl.MenuButtonSelector.onClick( 'NOT IMPLEMENTED' ):  " + m_action);
+		}
+	}
+
+
+	/*
+	 * Inner class that implements clicking on toggle on the menu.
+	 */
+	private static class MenuToggleSelector implements ClickHandler {
+		private boolean m_isBase;
+		private Anchor m_menuAnchor;
+		private Image m_altImg;
+		private Image m_baseImg;
+		private String m_altAction;
+		private String m_baseAction;
+
+		/**
+		 * Class constructor.
+		 * 
+		 * @param menuAnchor
+		 * @param baseImg
+		 * @param altImg
+		 * @param baseAction
+		 * @param altAction
+		 */
+		MenuToggleSelector(Anchor menuAnchor, Image baseImg, Image altImg, String baseAction, String altAction) {
+			// Store the parameters...
+			m_menuAnchor = menuAnchor;
+			m_altImg = altImg;
+			m_baseImg = baseImg;
+			m_altAction = altAction;
+			m_baseAction = baseAction;
+
+			// ...and initialize everything else.
+			m_isBase = true;
+			m_menuAnchor.getElement().appendChild(m_baseImg.getElement());
+		}
+		
+		/**
+		 * Called when the menu button is clicked.
+		 * 
+		 * @param event
+		 */
+		public void onClick(ClickEvent event) {
+			// Fire the action...
+			String action = (m_isBase ? m_baseAction : m_altAction);
+			Window.alert("MainMenuControl.MenuButtonToggle.onClick( 'NOT IMPLEMENTED' ):  " + action);
+			
+			// ...and toggle the state of the image.
+			Image addImg;
+			Image removeImg;
+			if (m_isBase) {
+				addImg    = m_altImg;
+				removeImg = m_baseImg;
+			}
+			else {
+				addImg    = m_baseImg;
+				removeImg = m_altImg;
+			}
+			m_isBase = (!m_isBase);
+			m_menuAnchor.getElement().replaceChild(addImg.getElement(), removeImg.getElement());
+		}
+	}
+
 	/**
 	 * Constructor method.
 	 */
 	public MainMenuControl() {
-		FlowPanel mainPanel;
-		FlowPanel panel;
-		ImageResource imageResource;
-
-		mainPanel = new FlowPanel();
+		// Create the menu's main FlowPanel...
+		FlowPanel mainPanel = new FlowPanel();
 		mainPanel.addStyleName("mainMenuControl");
 		
-		// Add the "slide left/right" actions.
-		panel = new FlowPanel();
-		panel.addStyleName("mainMenuNavControl");
-		panel.addStyleName("mainMenuLeftRightNavControl");
-		
-		//!!! Put these images in an anchor.
-		
-		// Add the slide-left image to the menu.
-		imageResource = GwtTeaming.getImageBundle().slideLeft();
-		m_slideLeftImg = new Image(imageResource);
-		m_slideLeftImg.addStyleName("mainMenuButton");
-		panel.add(m_slideLeftImg);
-		
-		// Add the slide-right image to the menu and hide it.
-		imageResource = GwtTeaming.getImageBundle().slideRight();
-		m_slideRightImg = new Image(imageResource);
-		m_slideRightImg.addStyleName("mainMenuButton");
-		m_slideRightImg.setVisible(false);
-		panel.add(m_slideRightImg);
-		
+		// ...add the slide-left/right toggle...
+		FlowPanel panel = new FlowPanel();
+		panel.addStyleName("mainMenuButton mainMenuButton_LeftRight subhead-control-bg1 roundcornerSM");
+		createMenuToggle(panel, GwtTeaming.getImageBundle().slideLeft(),  GwtTeaming.getImageBundle().slideRight(), "slideLeft", "slideRight");
 		mainPanel.add(panel);
 
-		// Add the "slide up/down" actions.
+		// ...add the slide-up/down toggle...
 		panel = new FlowPanel();
-		panel.addStyleName("mainMenuNavControl");
-		panel.addStyleName("mainMenuUpDownNavControl");
-		
-		//!!! Put these images in an anchor.
-		
-		// Add the slide-up image to the menu.
-		imageResource = GwtTeaming.getImageBundle().slideUp();
-		m_slideUpImg = new Image(imageResource);
-		m_slideUpImg.addStyleName("mainMenuButton");
-		panel.add(m_slideUpImg);
-		
-		// Add the slide-down image to the menu and hide it.
-		imageResource = GwtTeaming.getImageBundle().slideDown();
-		m_slideDownImg = new Image(imageResource);
-		m_slideDownImg.addStyleName("mainMenuButton");
-		m_slideDownImg.setVisible(false);
-		panel.add(m_slideDownImg);
-		
+		panel.addStyleName("mainMenuButton mainMenuButton_UpDown subhead-control-bg1 roundcornerSM");
+		createMenuToggle(panel, GwtTeaming.getImageBundle().slideUp(),   GwtTeaming.getImageBundle().slideDown(), "slideUp", "slideDown");
 		mainPanel.add(panel);
 
-		// Add the "browse hierarchy" action.
+		// ...add the browse hierarchy button...
 		panel = new FlowPanel();
-		panel.addStyleName("mainMenuNavControl");
-		panel.addStyleName("mainMenuUpBrowseHierarchyControl");
-		
-		//!!! Put this image in an anchor.
-		
-		// Add the browse hierarchy image to the menu.
-		imageResource = GwtTeaming.getImageBundle().browseHierarchy();
-		m_browseHierarchyImg = new Image(imageResource);
-		m_browseHierarchyImg.addStyleName("mainMenuButton");
-		panel.add(m_browseHierarchyImg);
-		
+		panel.addStyleName("mainMenuButton mainMenuButton_BrowseHierarchy subhead-control-bg1 roundcornerSM");
+		createMenuButton(panel, GwtTeaming.getImageBundle().browseHierarchy(), new MenuButtonSelector("browseHierarchy"));
 		mainPanel.add(panel);
 		
-		// Add the "GWT UI" action.
-		Anchor gwtUIAnchor = new Anchor();
-		gwtUIAnchor.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				GwtRpcServiceAsync rpcService = GwtTeaming.getRpcService();
-				rpcService.getUserWorkspacePermalink(new AsyncCallback<String>() {
-					public void onFailure(Throwable t) {}
-					public void onSuccess(String userWorkspaceURL)  {
-						jsToggleGwtUI();
-						jsLoadUserWorkspaceURL(userWorkspaceURL + "&captive=false");
-					}
-					
-					private native void jsToggleGwtUI() /*-{
-						// Toggle the GWT UI state.
-						window.top.ss_toggleGwtUI(false);
-					}-*/;
-
-					private native void jsLoadUserWorkspaceURL(String userWorkspaceURL) /*-{
-						// Give the GWT UI state toggling 1/2
-						// second to complete and reload the user
-						// workspace.
-						window.setTimeout(
-							function() {
-								window.top.location.href = userWorkspaceURL;
-							},
-							500);
-					}-*/;
-				});
-			}
-			
-		});
-		
-		imageResource = GwtTeaming.getImageBundle().gwtUI();
-		Image gwtUIImg = new Image(imageResource);
-		gwtUIImg.addStyleName("mainMenuButton");
-		gwtUIAnchor.getElement().appendChild(gwtUIImg.getElement());
-		
+		// ...and add the GWT UI button.
 		panel = new FlowPanel();
-		panel.addStyleName("mainMenuNavControl");
-		panel.addStyleName("mainMenuGwtUIControl");
-		panel.add(gwtUIAnchor);
+		panel.addStyleName("mainMenuButton mainMenuButton_GwtUI subhead-control-bg1 roundcornerSM");
+		createMenuButton(panel, GwtTeaming.getImageBundle().gwtUI(), new GwtUISelector());
 		mainPanel.add(panel);
 		
-		// All composites must call initWidget() in their constructors.
+		// Finally, all composites must call initWidget() in their
+		// constructors.
 		initWidget(mainPanel);
+	}
+
+	/*
+	 * Creates an image based button on the menu bar.
+	 */
+	private static Anchor createMenuButton(FlowPanel panel, ImageResource imgRes, ClickHandler ch) {
+		Image img = new Image(imgRes);
+		img.addStyleName("mainMenuButton_WidgetImage");
+		Anchor a = new Anchor();
+		a.addStyleName("mainMenuButton_WidgetAnchor");
+		a.getElement().appendChild(img.getElement());
+		a.addClickHandler(ch);
+		MenuButtonHover hover = new MenuButtonHover(panel);
+		a.addMouseOverHandler(hover);
+		a.addMouseOutHandler( hover);
+		panel.add(a);
+		return a;
+	}
+	
+	/*
+	 * Creates an image based toggle on the menu bar.
+	 */
+	private static Anchor createMenuToggle(FlowPanel panel, ImageResource baseImgRes, ImageResource altImgRes, String baseAction, String altAction) {
+		Image altImg = new Image(altImgRes);
+		altImg.addStyleName("mainMenuButton_WidgetImage");
+		Image baseImg = new Image(baseImgRes);
+		baseImg.addStyleName("mainMenuButton_WidgetImage");
+		Anchor a = new Anchor();
+		a.addStyleName("mainMenuButton_WidgetAnchor");
+		a.addClickHandler(new MenuToggleSelector(a, baseImg, altImg, baseAction, altAction));
+		MenuButtonHover hover = new MenuButtonHover(panel);
+		a.addMouseOverHandler(hover);
+		a.addMouseOutHandler( hover);
+		panel.add(a);
+		return a;
 	}
 }
