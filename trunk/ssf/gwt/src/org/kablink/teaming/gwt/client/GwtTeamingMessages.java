66b255ae00d89ca91602338f1eefb1bd8fc112de
nbjensen <nbjensen@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-04-13 23:49:24 +0000

    Bug: PRD Reviewer: Nathan Comment: User Status Styling added with message changes

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.java
@@ -418,5 +418,6 @@ public interface GwtTeamingMessages extends Messages
 	String charactersTyped(int value);
 	String exceededMax(int value);
 	String exceededError();
+	String clearCurrentStatus();
 	
 }// end GwtTeamingMessages
