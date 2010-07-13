6a80b8c47b52dc4ef91ca0c2c0b8f54514083840
mikestoddard <mikestoddard@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-07-13 19:11:59 +0000

    Bug: 620944 Reviewer: mistoddard Fixed the click handler from the Profile and workspace page to bring up the conversation window.

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/presence/InstantMessageClickHandler.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/presence/InstantMessageClickHandler.java
@@ -32,20 +32,18 @@
  */
 package org.kablink.teaming.gwt.client.presence;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.Window;
-
 import org.kablink.teaming.gwt.client.GwtTeaming;
-import org.kablink.teaming.gwt.client.GwtTeamingException;
-import org.kablink.teaming.gwt.client.GwtTeamingMessages;
-import org.kablink.teaming.gwt.client.GwtTeamingException.ExceptionType;
 import org.kablink.teaming.gwt.client.service.GwtRpcServiceAsync;
 import org.kablink.teaming.gwt.client.util.GwtClientHelper;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
 public class InstantMessageClickHandler implements ClickHandler {
 	private String m_binderId;
+	private String m_url = null;
 
 	public InstantMessageClickHandler(String binderId){
 		m_binderId = binderId;
@@ -53,18 +51,27 @@ public class InstantMessageClickHandler implements ClickHandler {
 
 	/* Open IM URL */
 	public void onClick(ClickEvent event) {
-		GwtRpcServiceAsync rpcService = GwtTeaming.getRpcService();		
-		rpcService.getImUrl(m_binderId, 
-							new AsyncCallback<String>() {
-								public void onFailure(Throwable t) {
-									Window.alert("Error: " + t.toString());
-								}
-
-								public void onSuccess(String url) {
-									if (url != null && url.length() > 0) {
-										GwtClientHelper.loadUrlInContentFrame(url);
+		if (m_url != null && m_url.length() > 0) {
+			openUrl();
+		} else {
+			GwtRpcServiceAsync rpcService = GwtTeaming.getRpcService();		
+			rpcService.getImUrl(m_binderId, 
+								new AsyncCallback<String>() {
+									public void onFailure(Throwable t) {
+										Window.alert("Error: " + t.toString());
 									}
-								}
-							});
+	
+									public void onSuccess(String url) {
+										m_url = url;
+										openUrl();
+									}
+								});
+		}
+	}
+	
+	private void openUrl() {
+		if (m_url != null && m_url.length() > 0) {
+			GwtClientHelper.jsLoadUrlInTopWindow(m_url);
+		}
 	}
 }
