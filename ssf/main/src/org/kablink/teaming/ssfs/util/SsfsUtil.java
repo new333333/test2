930091cc74adf03a721e39d5f9a7c42de2120e74
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-07-21 13:05:40 +0000

    Bug:  475424 Reviewer:  Self Comments:  Applets now work with Safari.
    Files:
    M    web/docroot/WEB-INF/jsp/definition_elements/entry_attachment_options.jsp
    M    web/docroot/WEB-INF/jsp/definition_elements/folder_dropbox_add_attachments.jsp
    M    web/docroot/WEB-INF/classes/config/ssf.properties
    M    main/src/org/kablink/teaming/ssfs/util/SsfsUtil.java
    M    applets/droptarget/src/org/kablink/teaming/applets/droptarget/DataSink.java
    M    util/src/org/kablink/util/BrowserSniffer.java

--- ssf/main/src/org/kablink/teaming/ssfs/util/SsfsUtil.java
+++ ssf/main/src/org/kablink/teaming/ssfs/util/SsfsUtil.java
@@ -327,9 +327,11 @@ public class SsfsUtil {
 		if (platform.equals("mac")) {
 			String[] browsers = SPropsUtil.getStringArray("applet.support.in.mac.browsers", ",");
 			for (int i = 0; i < browsers.length; i++) {
-				if (browsers[i].equals("firefox") && BrowserSniffer.is_mozilla_5(req) &&
-						!BrowserSniffer.is_safari(req)) return true;
+				if (browsers[i].equals("firefox") && BrowserSniffer.is_mozilla_5(req)
+						&& !BrowserSniffer.is_safari(req)
+						&& !BrowserSniffer.is_chrome(req)) return true;
 				if (browsers[i].equals("safari") && BrowserSniffer.is_safari(req)) return true;
+				if (browsers[i].equals("chrome") && BrowserSniffer.is_chrome(req)) return true;
 			}
 			return false;
 		}
