535a1df3ce86475be2adde441cd8d04252f029f2
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-07-21 15:23:18 +0000

    Bug:  623685 Reviewer:  Self Comment:  Only treat a workspace as a team workspace if it has members assigned directly (ie., not inherited.)
    Files:
    M    main/src/org/kablink/teaming/web/util/BinderHelper.java

--- ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
+++ ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2009 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2010 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -1350,8 +1350,9 @@ public class BinderHelper {
 	 * @return true -> binder is a Team workspace.  false -> It isn't.
 	 */
 	static public boolean isBinderTeamWorkspace(Binder binder) {
-		// If binder is not a user Workspace...
-		boolean isTeamWs = (!(isBinderUserWorkspace(binder)));
+		// If binder is not a user Workspace and its team membership is
+		// not inherited...
+		boolean isTeamWs = ((!(isBinderUserWorkspace(binder))) && (!(binder.isTeamMembershipInherited())));
 		if (isTeamWs) {
 			// ...but it is a Workspace...
 			isTeamWs = ((null != binder) && (binder instanceof Workspace));
