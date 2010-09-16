1093cf80380f47bea7149651ccaa081715fe4012
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-09-16 21:53:14 +0000

    Bug:  PRD Reviewer:  Self Comments:  Continued work on the changes for Activity Streams.
    Continued work on the APIs to read the activity stream data from the
    server.
    
    Files:
    M    main/src/org/kablink/teaming/search/SearchUtils.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtActivityStreamHelper.java
    M    gwt/src/org/kablink/teaming/gwt/client/util/ActivityStreamEntry.java

--- ssf/main/src/org/kablink/teaming/search/SearchUtils.java
+++ ssf/main/src/org/kablink/teaming/search/SearchUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2009 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2010 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -87,6 +87,16 @@ public class SearchUtils {
 		crit.add(in(ENTRY_TYPE_FIELD,new String[] {Constants.ENTRY_TYPE_ENTRY, Constants.ENTRY_TYPE_REPLY}))
 			.add(in(DOC_TYPE_FIELD,new String[] {Constants.DOC_TYPE_ENTRY}))
 			.add(eq(CREATORID_FIELD,userId.toString()));
+		crit.addOrder(Order.desc(MODIFICATION_DATE_FIELD));
+		return crit;
+	}
+	
+	public static Criteria entryReplies(String entryId)
+	{
+		Criteria crit = new Criteria();
+		crit.add(in(ENTRY_TYPE_FIELD,new String[] {Constants.ENTRY_TYPE_REPLY}))
+			.add(in(DOC_TYPE_FIELD,new String[] {Constants.DOC_TYPE_ENTRY}))
+			.add(in(ENTRY_PARENT_ID_FIELD,new String[] {entryId}));
 		crit.addOrder(Order.desc(MODIFICATION_DATE_FIELD));
 		return crit;
 	}
