8852e2725a60daad9da1611816d495ed55ac7c57
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-09-20 17:42:00 +0000

    Bug: 640270 Reviewer: Self Comment: Changed indexing and email notification facility to reuse same builder instances rather than repeatedly recreating short-lived ones.

--- trunk/ssf/main/src/org/kablink/teaming/module/definition/index/FieldBuilderUtil.java
+++ trunk/ssf/main/src/org/kablink/teaming/module/definition/index/FieldBuilderUtil.java
@@ -34,20 +34,14 @@ package org.kablink.teaming.module.definition.index;
 
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.document.Field;
 import org.kablink.teaming.domain.DefinableEntity;
-import org.kablink.teaming.util.ReflectHelper;
+import org.kablink.teaming.util.cache.ClassInstanceCache;
 
 public class FieldBuilderUtil {
     
-	private static Log logger = LogFactory.getLog(FieldBuilderUtil.class);
-	
 	public static Field[] buildField(DefinableEntity entity, String dataElemName, String fieldBuilderClassName, Map args) {
-		if(logger.isDebugEnabled())
-			logger.debug("Instantiating index field builder [" + fieldBuilderClassName + "]");
-		FieldBuilder fieldBuilder = (FieldBuilder)ReflectHelper.getInstance(fieldBuilderClassName);
+		FieldBuilder fieldBuilder = (FieldBuilder) ClassInstanceCache.getInstance(fieldBuilderClassName);
 		return fieldBuilder.buildField(entity, dataElemName, args);
 	}    
     
