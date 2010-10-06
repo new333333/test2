721171b77ba00590b2c36eed51ed94c87e074684
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2010-10-06 20:46:04 +0000

    Bug: 643830 Reviewer: Self Comment: Fix another mistake

--- ssf/main/src/org/kablink/teaming/web/servlet/filter/DefaultCharacterEncodingFilter.java
+++ ssf/main/src/org/kablink/teaming/web/servlet/filter/DefaultCharacterEncodingFilter.java
@@ -56,8 +56,8 @@ public class DefaultCharacterEncodingFilter implements Filter {
 		if(characterEncoding == null || characterEncoding.equals("")) {
 			characterEncoding = SPropsUtil.getString("web.char.encoding", "UTF-8");
 			request.setCharacterEncoding(characterEncoding);
-			chain.doFilter(request, response);
 		}
+		chain.doFilter(request, response);
 	}
 
 	@Override
