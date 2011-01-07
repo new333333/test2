9309331de6c7c4f2444ce941090ce8a63d12bfa6
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-01-07 15:20:58 +0000

    Bug:  PRD Reviewer:  Self Comments:  Removed the static initialization of the zoneModule bean from MiscUtil.java.
    Files:
    M    main/src/org/kablink/teaming/web/util/MiscUtil.java

--- ssf/main/src/org/kablink/teaming/web/util/MiscUtil.java
+++ ssf/main/src/org/kablink/teaming/web/util/MiscUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -73,15 +73,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 /**
- * @author jwootton
- *
  * This class contains a collection of miscellaneous utility methods.
  * 
+ * @author jwootton
  */
 public final class MiscUtil
 {
 	protected static Log m_logger = LogFactory.getLog(MiscUtil.class);
-	private static ZoneModule m_zoneModule = ((ZoneModule) SpringContextUtil.getBean( "zoneModule" ));
 
 	
 	/**
@@ -426,7 +424,8 @@ public final class MiscUtil
 	 */
 	public static ZoneInfo getCurrentZone()
 	{
-		return m_zoneModule.getZoneInfo( RequestContextHolder.getRequestContext().getZoneId() );
+		ZoneModule zm = ((ZoneModule) SpringContextUtil.getBean( "zoneModule" ));
+		return zm.getZoneInfo( RequestContextHolder.getRequestContext().getZoneId() );
 	}//end getCurrentZone()
 	
 	/**
