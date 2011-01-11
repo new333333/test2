cf1c46b2a89c52200aeca75346c56f1014535362
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-01-11 02:17:23 +0000

    Bug: PRD Reviewer: Self Comment: Database schema and Hibernate mappings for email log - Needed for T-Mobile Email task

--- ssf/tools/sql/update-3.0.0-3.1.0/update-oracle.sql
+++ ssf/tools/sql/update-3.0.0-3.1.0/update-oracle.sql
@@ -4,3 +4,4 @@ ALTER TABLE SS_Events add dtCalcStart timestamp;
 ALTER TABLE SS_Events add dtCalcEnd timestamp;
 create table SS_EmailLog (id char(32) not null, zoneId number(19,0), sendDate timestamp not null, fromField varchar2(255 char), subj varchar2(255 char), comment varchar2(255 char), status varchar2(16 char) not null, type varchar2(32 char) not null, toEmailAddresses clob, fileAttachments clob, primary key (id));
 create index index_emaillog on SS_EmailLog (sendDate, status, type);
+INSERT INTO SS_SchemaInfo values (15);
