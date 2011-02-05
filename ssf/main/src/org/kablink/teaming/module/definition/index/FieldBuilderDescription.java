1df476f8e5687cf8233077781d8d60f77e0c5fc4
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-02-05 17:26:07 +0000

    Bug: PRD Reviewer: Self Comment: Replace TOKENIZED with ANALYZED, UN_TOKENIZED with NOT_ANALYZED

--- ssf/main/src/org/kablink/teaming/module/definition/index/FieldBuilderDescription.java
+++ ssf/main/src/org/kablink/teaming/module/definition/index/FieldBuilderDescription.java
@@ -85,7 +85,7 @@ public class FieldBuilderDescription extends AbstractFieldBuilder {
         	// later release.
         	Field descField = new Field(Constants.DESC_FIELD, text, Field.Store.YES, Field.Index.NO); 
         	Field descTextField = new Field(Constants.DESC_TEXT_FIELD, strippedText, Field.Store.NO, Field.Index.ANALYZED); 
-        	Field descFormatField = new Field(Constants.DESC_FORMAT_FIELD, String.valueOf(val.getFormat()), Field.Store.YES, Field.Index.TOKENIZED); 
+        	Field descFormatField = new Field(Constants.DESC_FORMAT_FIELD, String.valueOf(val.getFormat()), Field.Store.YES, Field.Index.ANALYZED); 
          	if (isFieldsOnly(args)) {
          		return new Field[] {descField, descTextField, descFormatField};
          	} else {
