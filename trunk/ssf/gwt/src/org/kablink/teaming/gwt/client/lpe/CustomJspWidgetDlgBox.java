969a1c1eda519a8c077b12d44e50170b517fe472
jwootton <jwootton@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-02-11 19:09:14 +0000

    Bug: PRD Reviewer: Jay Wootton Comment: In the landing page editor, reworked the way a user selects a folder or entry in the custom jsp dialog.

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/lpe/CustomJspWidgetDlgBox.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/lpe/CustomJspWidgetDlgBox.java
@@ -39,23 +39,26 @@ import org.kablink.teaming.gwt.client.GwtFolderEntry;
 import org.kablink.teaming.gwt.client.GwtSearchCriteria;
 import org.kablink.teaming.gwt.client.GwtTeaming;
 import org.kablink.teaming.gwt.client.util.ActionHandler;
+import org.kablink.teaming.gwt.client.util.GwtClientHelper;
+import org.kablink.teaming.gwt.client.util.HttpRequestInfo;
 import org.kablink.teaming.gwt.client.util.TeamingAction;
 import org.kablink.teaming.gwt.client.widgets.DlgBox;
 import org.kablink.teaming.gwt.client.widgets.FindCtrl;
 import org.kablink.teaming.gwt.client.widgets.PropertiesObj;
 
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.FocusWidget;
-import com.google.gwt.user.client.ui.HTMLTable;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
@@ -77,17 +80,21 @@ public class CustomJspWidgetDlgBox extends DlgBox
 	// The following data members are used if the user has checked the "Associate a folder with this custom jsp"
 	private String m_folderId = null;
 	private Panel m_selectFolderPanel = null;
+	private InlineLabel m_folderFindLabel;
 	private FindCtrl m_folderFindCtrl = null;
 	private CheckBox m_showFolderTitleCkBox = null;
 	private TextBox m_numEntriesToShowTxtBox = null;
 	private InlineLabel m_currentFolderNameLabel = null;
+	private Button m_folderEditBtn;
 	
 	// The following data members are used if the user has checked the "Associate an entry with this custom jsp"
 	private String m_entryId = null;
 	private Panel m_selectEntryPanel = null;
+	private InlineLabel m_entryFindLabel;
 	private FindCtrl m_entryFindCtrl = null;
 	private CheckBox m_showEntryTitleCkBox = null;
 	private InlineLabel m_currentEntryNameLabel = null;
+	private Button m_entryEditBtn;
 	
 	/**
 	 * 
@@ -169,37 +176,76 @@ public class CustomJspWidgetDlgBox extends DlgBox
 	{
 		VerticalPanel mainPanel;
 		FlexTable table;
-		FlowPanel flowPanel;
+		FlowPanel panel;
 		InlineLabel inlineLabel;
-		HTMLTable.CellFormatter cellFormatter;
-		Label label;
 		
 		mainPanel = new VerticalPanel();
 		mainPanel.setStyleName( "lpeMarginLeft1Point5" );
 		
-		// Add a label that will say Current entry: name of the currently selected entry
 		table = new FlexTable();
 		table.setCellSpacing( 8 );
-		flowPanel = new FlowPanel();
-		inlineLabel = new InlineLabel( GwtTeaming.getMessages().currentEntry() );
-		m_currentEntryNameLabel = new InlineLabel();
-		m_currentEntryNameLabel.addStyleName( "bold" );
-		m_currentEntryNameLabel.addStyleName( "marginLeftPoint25em" );
-		flowPanel.add( inlineLabel );
-		flowPanel.add( m_currentEntryNameLabel );
-		table.setWidget( 0, 0, flowPanel );
+
 		mainPanel.add( table );
 
-		// Add label and a "find" control.
-		table = new FlexTable();
-		table.setCellSpacing( 8 );
-		cellFormatter = table.getCellFormatter();
-		cellFormatter.setAlignment( 0, 0, HasHorizontalAlignment.ALIGN_LEFT, HasVerticalAlignment.ALIGN_TOP );
-		label = new Label( GwtTeaming.getMessages().findEntry() );
-		table.setWidget( 0, 0, label );
-		m_entryFindCtrl = new FindCtrl( this, GwtSearchCriteria.SearchType.ENTRIES );
-		table.setWidget( 0, 1, m_entryFindCtrl );
-		mainPanel.add( table );
+		// Add a label that will say Entry:
+		inlineLabel = new InlineLabel( GwtTeaming.getMessages().entryLabel() );
+		table.setWidget( 0, 0, inlineLabel );
+
+		// Add a label to hold the name of the selected entry.
+		m_currentEntryNameLabel = new InlineLabel( GwtTeaming.getMessages().noEntrySelected() );
+		m_currentEntryNameLabel.addStyleName( "noEntrySelected" );
+		m_currentEntryNameLabel.addStyleName( "marginLeftPoint25em" );
+		m_currentEntryNameLabel.addStyleName( "marginright10px" );
+		panel = new FlowPanel();
+		panel.add( m_currentEntryNameLabel );
+
+		// Add an "Edit" button
+		{
+			ClickHandler clickHandler;
+			
+			m_entryEditBtn = new Button( GwtTeaming.getMessages().edit() );
+			m_entryEditBtn.addStyleName( "teamingButton" );
+			panel.add( m_entryEditBtn );
+			
+			clickHandler = new ClickHandler()
+			{
+				/**
+				 * 
+				 */
+				public void onClick( ClickEvent event )
+				{
+					Scheduler.ScheduledCommand cmd;
+					
+					cmd = new Scheduler.ScheduledCommand()
+					{
+						/**
+						 * 
+						 */
+						public void execute()
+						{
+							// Make the find control visible.
+							showEntryFindControl();
+						}
+					};
+					Scheduler.get().scheduleDeferred( cmd );
+				}
+				
+			};
+			m_entryEditBtn.addClickHandler( clickHandler );
+		}
+
+		table.setWidget( 0, 1, panel );
+		
+		// Add a "find" control.
+		{
+			m_entryFindLabel = new InlineLabel( GwtTeaming.getMessages().find() );
+			m_entryFindLabel.setVisible( false );
+			table.setWidget( 1, 0, m_entryFindLabel );
+			
+			m_entryFindCtrl = new FindCtrl( this, GwtSearchCriteria.SearchType.ENTRIES );
+			m_entryFindCtrl.setVisible( false );
+			table.setWidget( 1, 1, m_entryFindCtrl );
+		}
 		
 		// Add a checkbox for "Show title"
 		table = new FlexTable();
@@ -219,41 +265,80 @@ public class CustomJspWidgetDlgBox extends DlgBox
 	{
 		VerticalPanel mainPanel;
 		FlexTable table;
-		FlowPanel flowPanel;
+		FlowPanel panel;
 		InlineLabel inlineLabel;
 		Label label;
-		HTMLTable.CellFormatter cellFormatter;
 		
 		mainPanel = new VerticalPanel();
 		mainPanel.setStyleName( "lpeMarginLeft1Point5" );
 		mainPanel.setVisible( false );
 
-		// Add a label that will say Current folder: name of the currently selected folder
 		table = new FlexTable();
 		table.setCellSpacing( 8 );
-		flowPanel = new FlowPanel();
-		inlineLabel = new InlineLabel( GwtTeaming.getMessages().currentFolder() );
-		m_currentFolderNameLabel = new InlineLabel();
-		m_currentFolderNameLabel.addStyleName( "bold" );
+
+		mainPanel.add( table );
+
+		// Add a label that will say Folder:
+		inlineLabel = new InlineLabel( GwtTeaming.getMessages().folderLabel() );
+		table.setWidget( 0, 0, inlineLabel );
+
+		// Add a label to hold the name of the selected folder.
+		m_currentFolderNameLabel = new InlineLabel( GwtTeaming.getMessages().noFolderSelected() );
+		m_currentFolderNameLabel.addStyleName( "noFolderSelected" );
 		m_currentFolderNameLabel.addStyleName( "marginLeftPoint25em" );
-		flowPanel.add( inlineLabel );
-		flowPanel.add( m_currentFolderNameLabel );
-		table.setWidget( 0, 0, flowPanel );
-		mainPanel.add( table );
+		m_currentFolderNameLabel.addStyleName( "marginright10px" );
+		panel = new FlowPanel();
+		panel.add( m_currentFolderNameLabel );
 
-		// Add label "New folder"
-		table = new FlexTable();
-		table.setCellSpacing( 8 );
+		// Add an "Edit" button
+		{
+			ClickHandler clickHandler;
+			
+			m_folderEditBtn = new Button( GwtTeaming.getMessages().edit() );
+			m_folderEditBtn.addStyleName( "teamingButton" );
+			panel.add( m_folderEditBtn );
+			
+			clickHandler = new ClickHandler()
+			{
+				/**
+				 * 
+				 */
+				public void onClick( ClickEvent event )
+				{
+					Scheduler.ScheduledCommand cmd;
+					
+					cmd = new Scheduler.ScheduledCommand()
+					{
+						/**
+						 * 
+						 */
+						public void execute()
+						{
+							// Make the find control visible.
+							showFolderFindControl();
+						}
+					};
+					Scheduler.get().scheduleDeferred( cmd );
+				}
+				
+			};
+			m_folderEditBtn.addClickHandler( clickHandler );
+		}
 
-		// Add controls for the folder.
-		label = new Label( GwtTeaming.getMessages().findFolderLabel() );
-		cellFormatter = table.getCellFormatter();
-		cellFormatter.setAlignment( 0, 0, HasHorizontalAlignment.ALIGN_LEFT, HasVerticalAlignment.ALIGN_TOP );
-		table.setWidget( 0, 0, label );
-		m_folderFindCtrl = new FindCtrl( this, GwtSearchCriteria.SearchType.PLACES );
-		m_folderFindCtrl.setSearchForFoldersOnly( true );
-		table.setWidget( 0, 1, m_folderFindCtrl );
-		mainPanel.add( table );
+		table.setWidget( 0, 1, panel );
+
+		// Add a "find" control
+		{
+			m_folderFindLabel = new InlineLabel( GwtTeaming.getMessages().find() );
+			m_folderFindLabel.setVisible( false );
+			table.setWidget( 1, 0, m_folderFindLabel );
+			
+			m_folderFindCtrl = new FindCtrl( this, GwtSearchCriteria.SearchType.PLACES );
+			m_folderFindCtrl.setSearchForFoldersOnly( true );
+			m_folderFindCtrl.setVisible( false );
+			
+			table.setWidget( 1, 1, m_folderFindCtrl );
+		}
 
 		// Add controls for "Number of entries to show"
 		table = new FlexTable();
@@ -344,6 +429,47 @@ public class CustomJspWidgetDlgBox extends DlgBox
 	
 	
 	/**
+	 * Issue an ajax request to get the entry for the given id.  After we get the entry
+	 * we will update the name of the selected entry.
+	 */
+	private void getEntry( final String entryId )
+	{
+		AsyncCallback<GwtFolderEntry> callback;
+		
+		callback = new AsyncCallback<GwtFolderEntry>()
+		{
+			/**
+			 * 
+			 */
+			public void onFailure( Throwable t )
+			{
+				GwtClientHelper.handleGwtRPCFailure(
+					t,
+					GwtTeaming.getMessages().rpcFailure_GetFolderEntry(),
+					entryId );
+			}
+	
+			/**
+			 * 
+			 * @param result
+			 */
+			public void onSuccess( GwtFolderEntry gwtFolderEntry )
+			{
+				if ( gwtFolderEntry != null )
+				{
+					// Update the name of the selected entry.
+					m_currentEntryNameLabel.setText( gwtFolderEntry.getEntryName() );
+					m_currentEntryNameLabel.removeStyleName( "noEntrySelected" );
+					m_currentEntryNameLabel.addStyleName( "bold" );
+				}
+			}
+		};
+
+		GwtTeaming.getRpcService().getEntry( HttpRequestInfo.createHttpRequestInfo(), null, entryId, callback );
+	}
+	
+
+	/**
 	 * Return the widget that should get the focus when the dialog is shown. 
 	 */
 	public FocusWidget getFocusWidget()
@@ -352,6 +478,47 @@ public class CustomJspWidgetDlgBox extends DlgBox
 	}// end getFocusWidget()
 	
 	
+	/**
+	 * Issue an ajax request to get the folder for the given id.  After we get the folder
+	 * we will update the name of the selected folder.
+	 */
+	private void getFolder( final String folderId )
+	{
+		AsyncCallback<GwtFolder> callback;
+		
+		callback = new AsyncCallback<GwtFolder>()
+		{
+			/**
+			 * 
+			 */
+			public void onFailure( Throwable t )
+			{
+				GwtClientHelper.handleGwtRPCFailure(
+					t,
+					GwtTeaming.getMessages().rpcFailure_GetFolder(),
+					folderId );
+			}
+	
+			/**
+			 * 
+			 * @param result
+			 */
+			public void onSuccess( GwtFolder gwtFolder )
+			{
+				if ( gwtFolder != null )
+				{
+					// Update the name of the selected folder.
+					m_currentFolderNameLabel.setText( gwtFolder.getFolderName() );
+					m_currentFolderNameLabel.removeStyleName( "noFolderSelected" );
+					m_currentFolderNameLabel.addStyleName( "bold" );
+				}
+			}
+		};
+
+		GwtTeaming.getRpcService().getFolder( HttpRequestInfo.createHttpRequestInfo(), null, folderId, callback );
+	}
+	
+
 	/**
 	 * Return the text found in the jsp name edit control.
 	 */
@@ -406,6 +573,72 @@ public class CustomJspWidgetDlgBox extends DlgBox
 	
 	
 	/**
+	 * This method gets called when the user selects an item from the search results in the "find" control.
+	 */
+	public void handleAction( TeamingAction ta, Object selectedObj )
+	{
+		if ( TeamingAction.SELECTION_CHANGED == ta )
+		{
+			// Are we dealing with a GwtFolder object?
+			if ( selectedObj instanceof GwtFolder )
+			{
+				GwtFolder gwtFolder;
+				
+				gwtFolder = (GwtFolder) selectedObj;
+				m_folderId = gwtFolder.getFolderId();
+
+				// Hide the find control.
+				hideFolderFindControl();
+				
+				// Issue an ajax request to get information about the selected folder.
+				getFolder( m_folderId );
+
+				// Show the edit button.
+				m_folderEditBtn.setVisible( true );
+			}
+			// Are we dealing with a GwtFolderEntry object?
+			else if ( selectedObj instanceof GwtFolderEntry )
+			{
+				GwtFolderEntry gwtFolderEntry;
+				
+				gwtFolderEntry = (GwtFolderEntry) selectedObj;
+				m_entryId = gwtFolderEntry.getEntryId();
+				
+				// Hide the find control.
+				hideEntryFindControl();
+				
+				// Issue an ajax request to get information about the selected entry.
+				getEntry( m_entryId );
+
+				// Show the edit button.
+				m_entryEditBtn.setVisible( true );
+			}
+		}
+	}// end handleAction()
+	
+	/**
+	 * 
+	 */
+	private void hideEntryFindControl()
+	{
+		m_entryFindLabel.setVisible( false );
+		m_entryFindCtrl.setVisible( false );
+		m_entryFindCtrl.hideSearchResults();
+	}
+	
+	
+	/**
+	 * 
+	 */
+	private void hideFolderFindControl()
+	{
+		m_folderFindLabel.setVisible( false );
+		m_folderFindCtrl.setVisible( false );
+		m_folderFindCtrl.hideSearchResults();
+	}
+	
+	
+	/**
 	 * Initialize the controls in the dialog with the values from the properties
 	 */
 	public void init( PropertiesObj props )
@@ -415,13 +648,9 @@ public class CustomJspWidgetDlgBox extends DlgBox
 		m_folderId = null;
 		m_entryId = null;
 		m_assocFolderCkBox.setValue( false );
-		m_currentFolderNameLabel.setText( "" ); 
 		m_numEntriesToShowTxtBox.setText( "" );
 		m_showFolderTitleCkBox.setValue( false );
-		m_folderFindCtrl.setInitialSearchString( "" );
-		m_currentEntryNameLabel.setText( "" ); 
 		m_showEntryTitleCkBox.setValue( false );
-		m_entryFindCtrl.setInitialSearchString( "" );
 		
 		properties = (CustomJspProperties) props;
 
@@ -431,41 +660,86 @@ public class CustomJspWidgetDlgBox extends DlgBox
 		m_assocEntryCkBox.setValue( false );
 
 		m_folderId = properties.getFolderId();
-		if ( m_folderId != null )
+		m_entryId = properties.getEntryId();
+
+		// Do we have a folder?
+		if ( m_folderId != null && m_folderId.length() > 0 )
 		{
 			int num;
 			
+			// Yes
 			// Check the "Associate a folder..." checkbox.
 			m_assocFolderCkBox.setValue( true );
 			
 			// Update the name of the currently selected folder.
 			m_currentFolderNameLabel.setText( properties.getFolderName() ); 
+			m_currentFolderNameLabel.removeStyleName( "noFolderSelected" );
+			m_currentFolderNameLabel.addStyleName( "bold" );
 
 			m_showFolderTitleCkBox.setValue( properties.getShowTitleValue() );
 			
-			// Hide the search-results widget.
-			m_folderFindCtrl.hideSearchResults();
-			
 			num = properties.getNumEntriesToBeShownValue();
 			m_numEntriesToShowTxtBox.setText( String.valueOf( num ) );
+
+			hideFolderFindControl();
+			
+			// Show the edit button.
+			m_folderEditBtn.setVisible( true );
 		}
 		else
 		{
-			m_entryId = properties.getEntryId();
-			if ( m_entryId != null )
-			{
-				// Check the "Associate an entry..." checkbox.
-				m_assocEntryCkBox.setValue( true );
-
-				// Update the name of the currently selected entry.
-				m_currentEntryNameLabel.setText( properties.getEntryName() ); 
-				 
-				m_showEntryTitleCkBox.setValue( properties.getShowTitleValue() );
-
-				// Hide the search-results widget.
-				m_entryFindCtrl.hideSearchResults();
-			}
+			// No
+			m_currentFolderNameLabel.setText( GwtTeaming.getMessages().noFolderSelected() );
+			m_currentFolderNameLabel.addStyleName( "noFolderSelected" );
+			m_currentFolderNameLabel.removeStyleName( "bold" );
+			
+			// Show the find control and give it the focus.
+			showFolderFindControl();
+			
+			// Hide the edit button.
+			m_folderEditBtn.setVisible( false );
 		}
+		
+		// Do we have an entry?
+		if ( m_entryId != null && m_entryId.length() > 0 )
+		{
+			// Check the "Associate an entry..." checkbox.
+			m_assocEntryCkBox.setValue( true );
+
+			// Update the name of the currently selected entry.
+			m_currentEntryNameLabel.setText( properties.getEntryName() ); 
+			m_currentEntryNameLabel.removeStyleName( "noEntrySelected" );
+			m_currentEntryNameLabel.addStyleName( "bold" );
+	 
+			m_showEntryTitleCkBox.setValue( properties.getShowTitleValue() );
+
+			// Hide the find control.
+			hideEntryFindControl();
+			
+			// Show the edit button.
+			m_entryEditBtn.setVisible( true );
+		}
+		else
+		{
+			// No
+			m_currentEntryNameLabel.setText( GwtTeaming.getMessages().noEntrySelected() );
+			m_currentEntryNameLabel.addStyleName( "noEntrySelected" );
+			m_currentEntryNameLabel.removeStyleName( "bold" );
+			
+			// Show the find control and give it the focus.
+			showEntryFindControl();
+			
+			// Hide the edit button.
+			m_entryEditBtn.setVisible( false );
+		}
+		
+		// Hide the search-results widget.
+		m_folderFindCtrl.hideSearchResults();
+		m_folderFindCtrl.setInitialSearchString( "" );
+
+		// Hide the search-results widget.
+		m_entryFindCtrl.hideSearchResults();
+		m_entryFindCtrl.setInitialSearchString( "" );
 		
 		// Hide/show the appropriate controls on the page based on which checkbox is checked,
 		// "Associate with folder" or "Associate with entry"
@@ -544,37 +818,33 @@ public class CustomJspWidgetDlgBox extends DlgBox
         }
 	}// end onKeyPress()
 
-	
 	/**
-	 * This method gets called when the user selects an item from the search results in the "find" control.
+	 * Show the find control and give it the focus.
 	 */
-	public void handleAction( TeamingAction ta, Object selectedObj )
+	private void showEntryFindControl()
 	{
-		if ( TeamingAction.SELECTION_CHANGED == ta )
-		{
-			// Are we dealing with a GwtFolder object?
-			if ( selectedObj instanceof GwtFolder )
-			{
-				GwtFolder gwtFolder;
-				
-				gwtFolder = (GwtFolder) selectedObj;
-				m_folderId = gwtFolder.getFolderId();
-				
-				// Hide the search-results widget.
-				m_folderFindCtrl.hideSearchResults();
-			}
-			// Are we dealing with a GwtFolderEntry object?
-			else if ( selectedObj instanceof GwtFolderEntry )
-			{
-				GwtFolderEntry gwtFolderEntry;
-				
-				gwtFolderEntry = (GwtFolderEntry) selectedObj;
-				m_entryId = gwtFolderEntry.getEntryId();
-				
-				// Hide the search-results widget.
-				m_entryFindCtrl.hideSearchResults();
-			}
-		}
-	}// end handleAction()
-	
+		FocusWidget focusWidget;
+
+		m_entryFindLabel.setVisible( true );
+		m_entryFindCtrl.setVisible( true );
+
+		focusWidget = m_entryFindCtrl.getFocusWidget();
+		if ( focusWidget != null )
+			focusWidget.setFocus( true );
+	}
+
+	/**
+	 * Show the folder find control and give it the focus.
+	 */
+	private void showFolderFindControl()
+	{
+		FocusWidget focusWidget;
+
+		m_folderFindLabel.setVisible( true );
+		m_folderFindCtrl.setVisible( true );
+
+		focusWidget = m_folderFindCtrl.getFocusWidget();
+		if ( focusWidget != null )
+			focusWidget.setFocus( true );
+	}
 }// end CustomJspWidgetDlgBox
