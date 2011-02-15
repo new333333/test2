88cbeb0fb1ebaf2bb4a067db5e402d0f8c1ee2a5
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-02-15 14:42:58 +0000

    Bug:  PRD Reviewer:  Self Comments:  Task Improvements (62nd.) 1. Added an Event.setDtEnd() method that takes a string as a parameter. 2. Fixed the exception that occurred when viewing the entry history of    task without a start date.
    Files:
    M    main/src/org/kablink/teaming/web/util/BinderHelper.java
    M    main/src/org/kablink/teaming/domain/Event.java

--- ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
+++ ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -2385,7 +2385,21 @@ public class BinderHelper {
 				String name = prop.attributeValue("name");
 				String value = prop.getText();
 				if (name.equals("start")) {
-					event.setDtStart(value);
+					if (MiscUtil.hasString(value)) {
+						event.setDtStart(value);
+					}
+				} else if (name.equals("calcStart")) {
+					if (MiscUtil.hasString(value)) {
+						event.setDtCalcStart(value);
+					}
+				} else if (name.equals("end")) {
+					if (MiscUtil.hasString(value)) {
+						event.setDtEnd(value);
+					}
+				} else if (name.equals("calcEnd")) {
+					if (MiscUtil.hasString(value)) {
+						event.setDtCalcEnd(value);
+					}
 				} else if (name.equals("duration")) {
 					event.setDuration(value);
 				} else if (name.equals("count")) {
