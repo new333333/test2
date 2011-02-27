c04cf57f98ef5be9a098337e8d9a3fa05272ffa9
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-02-27 21:52:49 +0000

    Bug: PRD Reviewer: Self Comment: Add binder_setDefinitionsInherited web services operation.

--- trunk/ssf/samples/wsclient/src/org/kablink/teaming/samples/wsclient/TeamingServiceClientWithCall.java
+++ trunk/ssf/samples/wsclient/src/org/kablink/teaming/samples/wsclient/TeamingServiceClientWithCall.java
@@ -85,6 +85,8 @@ public class TeamingServiceClientWithCall extends WSClientBase
 				wsClient.fetchAndPrintACK("TeamingServiceV1", "search_getUserTeams", new Object[] {null, Long.parseLong(args[1])});
 			} else if(args[0].equals("getBinder")) {
 				wsClient.fetchAndPrintDE("TeamingServiceV1", "binder_getBinder", new Object[] {null, Long.parseLong(args[1]), Boolean.parseBoolean(args[2])});
+			} else if(args[0].equals("setBinderDefinitionsInherited")) {
+				wsClient.fetchAndPrintACK("TeamingServiceV1", "binder_setDefinitionsInherited", new Object[] {null, Long.parseLong(args[1]), Boolean.parseBoolean(args[2])});
 			} else if(args[0].equals("getFolderEntry")) {
 				wsClient.fetchAndPrintDE("TeamingServiceV1", "folder_getEntry", new Object[] {null, Long.parseLong(args[1]), Boolean.parseBoolean(args[2]), Boolean.parseBoolean(args[3])});			
 			} else if(args[0].equals("getToken")) {
@@ -310,6 +312,7 @@ public class TeamingServiceClientWithCall extends WSClientBase
 		System.out.println("getTeams");
 		System.out.println("getUserTeams <user id>");
 		System.out.println("getBinder <binder id> <includeAttachments>");
+		System.out.println("setBinderDefinitionsInherited <binder id> <setBinderDefinitionsInherited>");
 		System.out.println("getFolderEntry <entry id> <includeAttachments> <eventAsIcalString>");
 		System.out.println("getDefinition <definition id>");
 		System.out.println("getDefinitions");
