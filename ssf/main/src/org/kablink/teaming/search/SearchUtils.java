9c7b5af4199a20e6e4b4d1fd5c70954a2d873570
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-03-04 22:27:38 +0000

    Bug:  554254 Reviewer:  Self Comments:  In the "Task and Calendars" relevance dashboard tab, don't show tasks that are completed or canceled.
    Files:
    M    main/src/org/kablink/teaming/search/SearchUtils.java

--- ssf/main/src/org/kablink/teaming/search/SearchUtils.java
+++ ssf/main/src/org/kablink/teaming/search/SearchUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -60,10 +60,12 @@ public class SearchUtils {
 		crit.add(eq(FAMILY_FIELD,FAMILY_FIELD_TASK))
 			.add(eq(DOC_TYPE_FIELD, DOC_TYPE_ENTRY))
 			.add(between(
-					TaskHelper.TIME_PERIOD_TASK_ENTRY_ATTRIBUTE_NAME + BasicIndexUtils.DELIMITER + Constants.EVENT_FIELD_END_DATE, 
+					TaskHelper.TIME_PERIOD_TASK_ENTRY_ATTRIBUTE_NAME + BasicIndexUtils.DELIMITER + Constants.EVENT_FIELD_LOGICAL_END_DATE, 
 					DateTools.dateToString(from, DateTools.Resolution.SECOND), 
-					DateTools.dateToString(to, DateTools.Resolution.SECOND)));
-		crit.addOrder(Order.desc(TaskHelper.TIME_PERIOD_TASK_ENTRY_ATTRIBUTE_NAME + BasicIndexUtils.DELIMITER + Constants.EVENT_FIELD_END_DATE));
+					DateTools.dateToString(to, DateTools.Resolution.SECOND)))
+			.add(not().add(eq("status","s3")))	// Not completed.
+			.add(not().add(eq("status","s4")));	// Not canceled.
+		crit.addOrder(Order.desc(TaskHelper.TIME_PERIOD_TASK_ENTRY_ATTRIBUTE_NAME + BasicIndexUtils.DELIMITER + Constants.EVENT_FIELD_LOGICAL_END_DATE));
 		
 		Disjunction disjunction = disjunction();
 		disjunction.add(eq(TaskHelper.ASSIGNMENT_TASK_ENTRY_ATTRIBUTE_NAME,userId.toString()));
@@ -75,7 +77,6 @@ public class SearchUtils {
 			disjunction.add(in(TaskHelper.ASSIGNMENT_TEAMS_TASK_ENTRY_ATTRIBUTE_NAME,teamIds));
 		}
 		crit.add(disjunction);
-		
 		return crit;
 	}
 	
