8dff8d88e53636874b55f05498e801fef904f26e
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-03-07 21:30:54 +0000

    Bug:  677505 Reviewer:  Self Comments:  Changed the masthead to say "xxx Vibe OnPrem Evergreen Beta" (note the addition of "OnPrem" from what was there.)
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/MastHead.java

--- ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/MastHead.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/MastHead.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -142,7 +142,11 @@ public class MastHead extends Composite
 		// Create a place for the beta text to go.
 		if ( beta )
 		{
-			m_betaLabel = new Label( GwtTeaming.getMessages().betaWithProduct( m_requestInfo.getProductName() ) );
+			String productName = m_requestInfo.getProductName();
+			if (productName.endsWith("Vibe")) {
+				productName += " OnPrem";
+			}
+			m_betaLabel = new Label( GwtTeaming.getMessages().betaWithProduct( productName ) );
 			m_betaLabel.addStyleName( "mastheadBeta" );
 			m_mainMastheadPanel.add( m_betaLabel );
 		}
