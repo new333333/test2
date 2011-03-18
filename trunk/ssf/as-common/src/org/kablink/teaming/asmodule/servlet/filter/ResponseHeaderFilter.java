a98422a1d265cf17867b3387d5794e21c826f859
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-03-18 16:12:44 +0000

    Bug:  PRD Reviewer:  Self Comments:  Turned on EmulateIE7 in the response header so it's no longer required for users to specify "Compatibility View" in the UI.
    Files:
    M    as-common/src/org/kablink/teaming/asmodule/servlet/filter/ResponseHeaderFilter.java
    M    web/docroot/WEB-INF/classes/config/ssf.properties

--- trunk/ssf/as-common/src/org/kablink/teaming/asmodule/servlet/filter/ResponseHeaderFilter.java
+++ trunk/ssf/as-common/src/org/kablink/teaming/asmodule/servlet/filter/ResponseHeaderFilter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2009 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -90,6 +90,10 @@ public class ResponseHeaderFilter implements Filter {
 					response.setHeader( "Cache-control", "public,max-age=31536000,no-check" );
 					response.setHeader( "Expires", "Wed, 01 Jan 2020 00:00:00 GMT" );
 				}
+
+				// Force IE7 emulation.  We need to do this until we
+				// fix the problems throughout Vibe that require this.
+				response.setHeader("X-UA-Compatible", "IE=EmulateIE7");
 			}
 			
 			// Set the provided HTTP response parameters
