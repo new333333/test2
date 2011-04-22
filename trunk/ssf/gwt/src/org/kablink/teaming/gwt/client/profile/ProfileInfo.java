f71a1b9378624f69af88d0d399ef7a32a33c796e
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-04-22 17:52:57 +0000

    Bug:  654674 Reviewer:  Self Comments:  Show a scaled version of the user's profile picture on their micro business card.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtProfileHelper.java
    M    gwt/src/org/kablink/teaming/gwt/client/profile/widgets/GwtQuickViewDlg.java
    M    gwt/src/org/kablink/teaming/gwt/client/profile/ProfileInfo.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/profile/ProfileInfo.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/profile/ProfileInfo.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -44,6 +44,7 @@ public class ProfileInfo implements IsSerializable  {
 	private String binderId;
 	private String userId;
 	private ArrayList<String> pictureUrls = new ArrayList<String>();
+	private ArrayList<String> pictureScaledUrls = new ArrayList<String>();
 	
 	private ArrayList<ProfileCategory> categories = new ArrayList<ProfileCategory>();
 	private boolean pictureEnabled = false;
@@ -107,12 +108,26 @@ public class ProfileInfo implements IsSerializable  {
 		return pictureUrl;
 	}
 
+	public String getPictureScaledUrl() {
+		String pictureScaledUrl = null;
+		if((!pictureScaledUrls.isEmpty())){ pictureScaledUrl = pictureScaledUrls.get(0); };
+		return pictureScaledUrl;
+	}
+
 	public List<String> getPicutres() {
 		return pictureUrls;
 	}
 	
+	public List<String> getPicutreScaleds() {
+		return pictureScaledUrls;
+	}
+	
 	public void addPictureUrl(String pictureUrl) {
 		this.pictureUrls.add(pictureUrl);
+	}
+	
+	public void addPictureScaledUrl(String pictureScaledUrl) {
+		this.pictureScaledUrls.add(pictureScaledUrl);
 	}
 	
 	public String getUserId() {
