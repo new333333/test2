0cfc0ef9bfe076d9ae199e83279458de50d8caa0
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-05-09 21:33:02 +0000

    Bug:  690381 Reviewer:  Self Comments:  Fixed column order dialog to restore defaults correctly.
    Files:
    M    main/src/org/kablink/teaming/portlet/forum/ListFolderController.java

--- trunk/ssf/main/src/org/kablink/teaming/portlet/forum/ListFolderController.java
+++ trunk/ssf/main/src/org/kablink/teaming/portlet/forum/ListFolderController.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -186,14 +186,12 @@ public class ListFolderController extends  SAbstractController {
 				values.put(ObjectKeys.SEARCH_SORT_DESCEND, "");
 				getProfileModule().setUserProperties(user.getId(), binderId, values);
 
-				//See if this request was to set the folder default
-				if (formData.containsKey("setFolderDefaultColumns")) {
-					Binder binder = getBinderModule().getBinder(binderId);
-					if (getBinderModule().testAccess(binder, BinderOperation.modifyBinder)) {
-						getBinderModule().setProperty(binder.getId(), ObjectKeys.BINDER_PROPERTY_FOLDER_COLUMNS, null);
-						getBinderModule().setProperty(binder.getId(), ObjectKeys.BINDER_PROPERTY_FOLDER_COLUMN_SORT_ORDER, null);
-						getBinderModule().setProperty(binder.getId(), ObjectKeys.BINDER_PROPERTY_FOLDER_COLUMN_TITLES, null);
-					}
+				//Set the folder default too
+				Binder binder = getBinderModule().getBinder(binderId);
+				if (getBinderModule().testAccess(binder, BinderOperation.modifyBinder)) {
+					getBinderModule().setProperty(binder.getId(), ObjectKeys.BINDER_PROPERTY_FOLDER_COLUMNS, null);
+					getBinderModule().setProperty(binder.getId(), ObjectKeys.BINDER_PROPERTY_FOLDER_COLUMN_SORT_ORDER, null);
+					getBinderModule().setProperty(binder.getId(), ObjectKeys.BINDER_PROPERTY_FOLDER_COLUMN_TITLES, null);
 				}
 				response.setRenderParameter(WebKeys.URL_NEW_TAB, "1");
 			}
