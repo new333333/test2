9434c4fd5f8f79165da1d65656b88374e4c7be8b
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-06-03 16:17:35 +0000

    Bug:  697792 Reviewer:  Self Comments:  Fixed handling of extended characters in email notifications.
    Files:
    M    main/src/org/kablink/teaming/module/mail/MimeNotifyPreparator.java
    M    main/src/org/kablink/teaming/module/mail/MimeMapPreparator.java
    M    main/src/org/kablink/teaming/module/mail/MailModule.java

--- ssf/main/src/org/kablink/teaming/module/mail/MimeMapPreparator.java
+++ ssf/main/src/org/kablink/teaming/module/mail/MimeMapPreparator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2010 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2010 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -75,7 +75,7 @@ public class MimeMapPreparator extends AbstractMailPreparator {
 			multipartMode = MimeMessageHelper.MULTIPART_MODE_MIXED;
 		}
 	
-		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, multipartMode);
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, multipartMode, MailModule.CONTENT_ENCODING);
 		mimeMessage.addHeader(MailModule.HEADER_CONTENT_TRANSFER_ENCODING, MailModule.HEADER_CONTENT_TRANSFER_ENCODING_8BIT);
 		helper.setSubject((String)details.get(MailModule.SUBJECT));
 		if (details.containsKey(MailModule.FROM)) 
