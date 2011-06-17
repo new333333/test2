671769a616c4ad4e80258bbbb14e220b7302ad68
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-06-17 20:19:50 +0000

    Bug: PRD Reviewer: Peter Comment: Hooks for version controls - add the database columns

---
+++ ssf/tools/sql/update-3.1.0-3.2.0/update-oracle.sql
@@ -0,0 +1,9 @@
+connect sitescape/sitescape;
+
+alter table SS_Forums add versionsEnabled number(1,0);
+alter table SS_Forums add versionsToKeep number(19,0);
+alter table SS_Forums add maxVersionAge number(19,0);
+alter table SS_Forums add maxFileSize number(19,0);
+alter table SS_Forums add fileEncryptionEnabled number(1,0);
+
+INSERT INTO SS_SchemaInfo values (21);
