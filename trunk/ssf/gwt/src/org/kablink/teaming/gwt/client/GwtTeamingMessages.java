78f8756fca6e314993f3a9b08abcf924cb582113
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-07-27 20:17:58 +0000

    Bug:  PRD Reviewer:  Self Comments:  Implemented inline task order number editing.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.properties
    M    gwt/src/org/kablink/teaming/gwt/client/tasklisting/TaskTable.java
    M    gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.java
    M    gwt/src/org/kablink/teaming/gwt/public/GwtTeaming.css

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.java
@@ -1013,6 +1013,7 @@ public interface GwtTeamingMessages extends Messages
 	String taskPriority_p4();
 	String taskPriority_p5();
 	String taskProcess_move();
+	String taskProcess_resize();
 	String taskProcess_selectAll();
 	String taskProcess_unSelectAll();
 	String taskProcess_updatingDates();
