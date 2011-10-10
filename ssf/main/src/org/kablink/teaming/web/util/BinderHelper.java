5c698847f718f2d928a99c835aec3799004797c3
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-10-10 18:45:19 +0000

    Bug: 721217 Reviewer: Peter Comment: Fix file version links to point to the right version (and no link if version was deleted)

--- ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
+++ ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
@@ -2367,6 +2367,7 @@ public class BinderHelper {
 			FileItem fi = new FileItem();
 			fa.setFileItem(fi);
 			fa.setId(fileAttId);
+			fa.setFileExists(false);	//Assume file does not exist until we actually find it.
 			
 			//Set the owner and modifier of the attachment
 			Element hs2 = (Element)fileAttachment.selectSingleNode("./historyStamp[@name='created']");
@@ -2459,6 +2460,7 @@ public class BinderHelper {
 					if (fv.getMajorVersion().equals(fa.getMajorVersion()) && 
 							fv.getMinorVersion().equals(fa.getMinorVersion())) {
 						fa.setId(fv.getId());
+						fa.setFileExists(true);
 						break;
 					}
 				}
