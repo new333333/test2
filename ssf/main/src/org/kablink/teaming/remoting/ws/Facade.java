f43d48a760d4f7d02f68404815e1b77fd959c015
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-10-24 23:09:34 +0000

    Bug: PRD Reviewer: Self Comment: Move Facade and RemotingException classes

--- ssf/main/src/org/kablink/teaming/remoting/ws/Facade.java
+++ ssf/main/src/org/kablink/teaming/remoting/ws/Facade.java
@@ -30,7 +30,7 @@
  * NOVELL and the Novell logo are registered trademarks and Kablink and the
  * Kablink logos are trademarks of Novell, Inc.
  */
-package org.kablink.teaming.remoting;
+package org.kablink.teaming.remoting.ws;
 import java.util.Calendar;
 /**
  * WS facade for business tier.
