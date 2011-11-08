4079255f4662f87ebc9b41aad18cc39bf95d72b3
jwootton <jwootton@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2011-11-08 22:56:08 +0000

    Bug: 729056 Reviewer: Jay Wootton Comment: Modified the ldap sync code that gets run when a user logs in to prevent the Vibe name from changing even if "name" is in the ldap mapping.

--- ssf/main/src/org/kablink/teaming/module/ldap/impl/LdapModuleImpl.java
+++ ssf/main/src/org/kablink/teaming/module/ldap/impl/LdapModuleImpl.java
@@ -882,6 +882,10 @@ public class LdapModuleImpl extends CommonDependencyInjection implements LdapMod
 						dn = bd.getNameInNamespace();
 					}
 					mods.put(ObjectKeys.FIELD_PRINCIPAL_FOREIGNNAME, dn);
+
+					// We never want to change a user's name in Teaming.  Remove the "name" attribute
+					// from the list of attributes to be written to the db.
+					mods.remove( ObjectKeys.FIELD_PRINCIPAL_NAME );
 				}
 				finally
 				{
