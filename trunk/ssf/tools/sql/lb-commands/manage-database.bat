1c54f4cd1161fd02814cbb4e4468474d6ddf9fc6
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-01-04 16:32:37 +0000

    Bug: 709080 Reviewer: Self Comment: More on liquibase command scripts

--- trunk/ssf/tools/sql/lb-commands/manage-database.bat
+++ trunk/ssf/tools/sql/lb-commands/manage-database.bat
@@ -1,8 +1,14 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+
 set "WD=%cd%"
 cd "%WD%"
+
 set CP=.\lib\mysql-connector.jar;.\lib\ojdbc6.jar;.\lib\jtds.jar
+
 rem Set default log level. Valid values are debug, info, warning, severe, off.
 set LL=info
+
 set JAVA_OPTS=
 
 if ""%1"" == """" goto displayUsage
@@ -16,12 +22,13 @@ echo   Invalid database type
 goto displayUsage
 
 :checkCommand:
-if ""%1"" == ""updateDatabase"" goto updateDatabase
-if ""%1"" == ""generateSqlToUpdateDatabase"" goto generateSqlToUpdateDatabase
-if ""%1"" == ""markDatabaseAsUpdated"" goto markDatabaseAsUpdated
-if ""%1"" == ""generateSqlToMarkDatabaseAsUpdated"" goto generateSqlToMarkDatabaseAsUpdated
-if ""%1"" == ""exportSchema"" goto exportSchema
-if ""%1"" == ""exportData"" goto exportData
+if ""%2"" == ""updateDatabase"" goto updateDatabase
+if ""%2"" == ""generateSqlToUpdateDatabase"" goto generateSqlToUpdateDatabase
+if ""%2"" == ""markDatabaseAsUpdated"" goto markDatabaseAsUpdated
+if ""%2"" == ""generateSqlToMarkDatabaseAsUpdated"" goto generateSqlToMarkDatabaseAsUpdated
+if ""%2"" == ""exportSchema"" goto exportSchema
+if ""%2"" == ""exportData"" goto exportData
+if ""%2"" == ""diffDatabases"" goto diffDatabases
 echo Errors:
 echo   Invalid command
 goto displayUsage
@@ -36,9 +43,10 @@ echo commands:
 echo   updateDatabase                      Update database schema to current version by applying un-run change sets to the database.
 echo   generateSqlToUpdateDatabase         Generate SQL in a file which can be later executed to update database schema to current version.
 echo   markDatabaseAsUpdated               Mark all change sets as ran against the database. Used only for transition from earlier legacy schema.
-echo   generateSqlToMarkDatabaseAsUpdated  Mark all change sets as ran against the database. Used only for transition from earlier legacy schema.
+echo   generateSqlToMarkDatabaseAsUpdated  Generate SQL in a file which can be later executed to mark all change sets as ran against the database.
 echo   exportSchema                        Export schema from existing database into a change log file. Useful for verification or troubleshooting.
 echo   exportData                          Export data from existing database into a change log file which can be later executed to import the data into another database.
+echo   diffDatabases                       Outputs schema difference between two databases into a change log file. Useful for verification or troubleshooting.
 go to end
 
 :updateDatabase
@@ -63,6 +71,10 @@ go to end
 
 :exportData
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --outputFile=".\%1-data-changelog.xml" --diffTypes="data" generateChangeLog
+go to end
+
+:diffDatabases
+java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --outputFile=".\%1-diff-changelog.xml" diff
 go to end
 
 :end
