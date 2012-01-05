1b0c79cff50cc17d73044a592d989165de09b735
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-01-05 17:26:55 +0000

    Bug: 709080 Reviewer: Self Comment: Fix bugs

--- ssf/tools/sql/lb-commands/manage-database.bat
+++ ssf/tools/sql/lb-commands/manage-database.bat
@@ -14,9 +14,9 @@ set JAVA_OPTS=
 if ""%1"" == """" goto displayUsage
 if ""%2"" == """" goto displayUsage
 
-if ""%1"" == "mysql" goto checkCommand
-if ""%1"" == "oracle" goto checkCommand
-if ""%1"" == "sqlserver" goto checkCommand
+if ""%1"" == ""mysql"" goto checkCommand
+if ""%1"" == ""oracle"" goto checkCommand
+if ""%1"" == ""sqlserver"" goto checkCommand
 echo Errors:
 echo    Invalid database type
 goto displayUsage
@@ -34,53 +34,53 @@ echo    Invalid command
 goto displayUsage
 
 :displayUsage
-echo Usage: manage-database <db type> <command>
-echo
+echo Usage: manage-database [db type] [command]
+echo.
 echo db types:
 echo    mysql                               MySQL
 echo    oracle                              Oracle
 echo    sqlserver                           MS SQL Server
-echo
+echo.
 echo primary commands:
 echo    updateDatabase                      Update database to current version by applying un-run change sets to the database.
 echo    generateSqlToUpdateDatabase         Apply un-run change sets to generate SQL in a file which can later be executed to update database to current version.
 echo    markDatabaseAsUpdated               Mark all change sets as ran against the database. Used only for transition from earlier legacy schema.
 echo    generateSqlToMarkDatabaseAsUpdated  Generate SQL in a file which can later be executed to mark all change sets as ran against the database.
-echo
+echo.
 echo secondary commands:
 echo    exportSchema                        Export schema from existing database into a change log file. Useful for verification, testing or troubleshooting.
 echo    exportData                          Export data from existing database into a change log file. Useful for verification, testing or troubleshooting.
 echo    diffDatabases                       Output schema differences between two databases into a change log file. Useful for verification, testing or troubleshooting.
-echo
+echo.
 echo Note: Additional parameters are read in from corresponding [db type]-liquibase.properties file.
-go to end
+goto end
 
 :updateDatabase
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --changeLogFile=".\scripts\changelog\%1-changelog-master.xml" update
-go to end
+goto end
 
 :generateSqlToUpdateDatabase
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --changeLogFile=".\scripts\changelog\%1-changelog-master.xml" --outputFile=".\%1-update.sql" updateSQL
-go to end
+goto end
 
 :markDatabaseAsUpdated
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --changeLogFile=".\scripts\changelog\%1-changelog-master.xml" changeLogSync
-go to end
+goto end
 
 :generateSqlToMarkDatabaseAsUpdated
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --changeLogFile=".\scripts\changelog\%1-changelog-master.xml" --outputFile=".\%1-markasupdated.sql" changeLogSyncSQL
-go to end
+goto end
 
 :exportSchema
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --outputFile=".\%1-schema-changelog.xml" generateChangeLog
-go to end
+goto end
 
 :exportData
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --outputFile=".\%1-data-changelog.xml" --diffTypes="data" generateChangeLog
-go to end
+goto end
 
 :diffDatabases
 java -jar ".\lib\liquibase.jar" --logLevel="%LL%" --defaultsFile=".\%1-liquibase.properties" --classpath="%CP%" --outputFile=".\%1-diff-changelog.xml" diff
-go to end
+goto end
 
 :end
