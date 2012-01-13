50c2ab8fa126794aba36d7cc606ed460b3c560ec
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-01-13 16:24:28 +0000

    Bug: 709080 Reviewer: Self Comment: Fix bugs in Linux script

--- ssf/tools/sql/lb-commands/manage-database.sh
+++ ssf/tools/sql/lb-commands/manage-database.sh
@@ -17,7 +17,7 @@ echo "                   later be executed to update database to current version
 echo "   mark32DatabaseAsUpdated"
 echo "                   Mark all change sets as ran against the 3.2 database."
 echo "                   Used for one-time transition from 3.2 schema to later version."
-echo "   generateSqlToMarkDatabaseAsUpdated"
+echo "   generateSqlToMark32DatabaseAsUpdated"
 echo "                   Generate SQL in a file which can later be executed to mark"
 echo "                   all change sets as ran against the 3.2 database."
 echo ""
@@ -33,11 +33,8 @@ echo "                   log file which can later be executed to upgrade the sch
 echo "                   the first database to that of the second (reference) database."
 echo "                   There are limitations with this function."
 echo ""
-echo "Note: Additional parameters are read in from [db type]-liquibase.properties file.
+echo "Note: Additional parameters are read in from [db type]-liquibase.properties file."
 }
-
-WORKING_DIR='cd'
-cd $WORKING_DIR
 
 CLASSPATH=./lib/mysql-connector.jar:./lib/ojdbc6.jar:./lib/jtds.jar
 
@@ -48,34 +45,36 @@ CONTEXTS=schema,data
 
 JAVA_OPTS=
 
-if [ "$1" != "mysql" ]; then
-  if [ "$1" != "oracle" ]; then
-    if [ "$1" != "sqlserver" ]; then
-      echo "Errors:"
-      echo "   Invalid database type"
-      displayUsage
-      exit 1
-    fi
-  fi
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+  displayUsage
+  exit 1
 fi
 
-if [ "$2 = "updateDatabase" ]; then
-  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --contexts="$CONTEXTS" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-master.xml" update
-elif [ "$2 = "generateSqlToUpdateDatabase" ]; then
-  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --contexts="$CONTEXTS" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-master.xml" updateSQL > "./$1-update.sql"
-elif [ "$2 = "mark32DatabaseAsUpdated" ]; then
-  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --contexts="$CONTEXTS" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-3.2.xml" changeLogSync
-elif [ "$2 = "generateSqlToMark32DatabaseAsUpdated" ]; then
-  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --contexts="$CONTEXTS" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-3.2.xml" changeLogSyncSQL > "./$1-markasupdated.sql"
-elif [ "$2 = "exportSchema" ]; then
-  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --contexts="$CONTEXTS" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" generateChangeLog > "./$1-schema-changelog.xml"
-elif [ "$2 = "exportData" ]; then
-  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --contexts="$CONTEXTS" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --diffTypes="data" generateChangeLog > "./$1-data-changelog.xml"
-elif [ "$2 = "diffDatabases" ]; then
-  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --contexts="$CONTEXTS" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" diffChangeLog > "./$1-diff-changelog.xml"
+if [ "$1" != "mysql" ] && [ "$1" != "oracle" ] && [ "$1" != "sqlserver" ]; then
+  echo "Errors:"
+  echo "   Invalid database type"
+  displayUsage
+  exit 1
+fi
+
+if [ "$2" = "updateDatabase" ]; then
+  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-master.xml" --contexts="$CONTEXTS" update
+elif [ "$2" = "generateSqlToUpdateDatabase" ]; then
+  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-master.xml" --contexts="$CONTEXTS" updateSQL > "./$1-update.sql"
+elif [ "$2" = "mark32DatabaseAsUpdated" ]; then
+  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-3.2.xml" --contexts="$CONTEXTS" changeLogSync
+elif [ "$2" = "generateSqlToMark32DatabaseAsUpdated" ]; then
+  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --changeLogFile="scripts/changelog/$1-changelog-3.2.xml" --contexts="$CONTEXTS" changeLogSyncSQL > "./$1-markasupdated.sql"
+elif [ "$2" = "exportSchema" ]; then
+  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" generateChangeLog > "./$1-schema-changelog.xml"
+elif [ "$2" = "exportData" ]; then
+  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" --diffTypes="data" generateChangeLog > "./$1-data-changelog.xml"
+elif [ "$2" = "diffDatabases" ]; then
+  java -jar "./lib/liquibase.jar" --logLevel="$LOG_LEVEL" --defaultsFile="./$1-liquibase.properties" --classpath="$CLASSPATH" diffChangeLog > "./$1-diff-changelog.xml"
 else
   echo "Errors:"
   echo "   Invalid command"
   displayUsage
   exit 1
 fi
+
