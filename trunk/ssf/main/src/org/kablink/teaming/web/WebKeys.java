4a20743776527b28c833266654714ccaf4aeb17b
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-01-13 19:03:52 +0000

    Bug:  740562 Reviewer:  Self Comments:  Changed the "vibeonprem_url" flag to "vibe_url".
    Files:
    M    main/src/org/kablink/teaming/portletadapter/PortletUrlToStringHelper.java
    M    main/src/org/kablink/teaming/web/servlet/filter/LoginFilter.java
    M    main/src/org/kablink/teaming/web/WebKeys.java
    M    main/src/org/kablink/teaming/web/util/GwtUIHelper.java

--- trunk/ssf/main/src/org/kablink/teaming/web/WebKeys.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/WebKeys.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2012 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -141,8 +141,10 @@ public class WebKeys {
 	public static final String URL_PRODUCT_GW="gw";
 	public static final String URL_PRODUCT_KABLINK="kablink";
 	public static final String URL_PRODUCT_NOVELL="novell";
-	public static final String URL_VIBEONPREM_ROOT_FLAG="vibeonprem_root";
-	public static final String URL_VIBEONPREM_URL_FLAG="vibeonprem_url";	
+	public static final String URL_VIBEONPREM_ROOT_FLAG_DEPRECATED="vibeonprem_root";
+	public static final String URL_VIBEONPREM_URL_FLAG_DEPRECATED="vibeonprem_url";	
+	public static final String URL_VIBE_ROOT_FLAG="vibe_root";
+	public static final String URL_VIBE_URL_FLAG="vibe_url";	
 	public static final String URL_CONTEXT="context";
 	public static final String URL_CSS_SHEET="sheet";
 	public static final String URL_CSS_THEME="theme";
--- trunk/ssf/main/src/org/kablink/teaming/web/util/GwtUIHelper.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/util/GwtUIHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2012 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -1135,12 +1135,19 @@ public class GwtUIHelper {
 	 */
 	private static boolean isVibeRootRequest(PortletRequest pRequest) {
 		// Is the base request a Vibe root URL?
-		String urlParam = PortletRequestUtils.getStringParameter(pRequest, WebKeys.URL_VIBEONPREM_ROOT_FLAG, "");
+		String urlParam = PortletRequestUtils.getStringParameter(pRequest, WebKeys.URL_VIBE_ROOT_FLAG, "");
 		boolean reply = MiscUtil.hasString(urlParam);
+		if (!(reply)) {
+			urlParam = PortletRequestUtils.getStringParameter(pRequest, WebKeys.URL_VIBEONPREM_ROOT_FLAG_DEPRECATED, "");
+			reply = MiscUtil.hasString(urlParam);
+		}
 		if (!(reply)) {
 			// No!  Does the request refer to a Vibe root URL?
 			urlParam = getRefererUrl(pRequest);
-			reply = (0 < urlParam.indexOf(WebKeys.URL_VIBEONPREM_ROOT_FLAG));
+			reply = (0 < urlParam.indexOf(WebKeys.URL_VIBE_ROOT_FLAG));
+			if (!reply) {
+				reply = (0 < urlParam.indexOf(WebKeys.URL_VIBEONPREM_ROOT_FLAG_DEPRECATED));
+			}
 		}
 		
 		// If we get here, reply is true if the request is a Vibe root
