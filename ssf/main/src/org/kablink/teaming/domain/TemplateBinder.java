f4f8b7f5b8a2a183b345812738ed0d45c4c922dc
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-01-14 15:10:36 +0000

    Bug: PRD Reviewer: Peter Comment: Add a column to the ss_forums table to hold the binder id of the template owning binder

--- ssf/main/src/org/kablink/teaming/domain/TemplateBinder.java
+++ ssf/main/src/org/kablink/teaming/domain/TemplateBinder.java
@@ -38,6 +38,8 @@ import java.util.List;
 public class TemplateBinder extends Binder {
 	protected Description tDescription;
 	protected String tTitle;
+	protected Long tOwningBinder;
+	
 	public TemplateBinder() {
 		super();
 	}
@@ -100,7 +102,15 @@ public class TemplateBinder extends Binder {
     public void setTemplateTitle(String tTitle) {
     	this.tTitle = tTitle;
     }
-    //we always can inherit
+    
+	public Long getTemplateOwningBinderId() {
+		return tOwningBinder;
+	}
+	public void setTemplateOwningBinderId(Long tOwningBinder) {
+		this.tOwningBinder = tOwningBinder;
+	}
+
+	//we always can inherit
     public boolean isFunctionMembershipInherited() {
        return functionMembershipInherited;
     }
