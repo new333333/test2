3738ba2103d2ca1072d2e7b1ac752a4b5525fb59
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-01-16 23:11:59 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for GWT based binder views.  {122nd} 1. Completed the implementation of the task graphs panel.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.properties
    M    gwt/src/org/kablink/teaming/gwt/client/GwtTeamingMessages.java
    M    gwt/src/org/kablink/teaming/gwt/client/binderviews/TaskGraphsPanel.java
    M    gwt/src/org/kablink/teaming/gwt/public/GwtBinderViews.css

--- ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/TaskGraphsPanel.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/TaskGraphsPanel.java
@@ -46,6 +46,7 @@ import org.kablink.teaming.gwt.client.util.BinderInfo;
 import org.kablink.teaming.gwt.client.util.GwtClientHelper;
 import org.kablink.teaming.gwt.client.util.TaskListItem;
 import org.kablink.teaming.gwt.client.widgets.VibeFlowPanel;
+import org.kablink.teaming.gwt.client.widgets.VibeVerticalPanel;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
@@ -57,6 +58,7 @@ import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -87,13 +89,12 @@ public class TaskGraphsPanel extends ToolPanelBase
 	private FlexTable					m_graphGrid;				// The FlexTable that holds the graphs.
 	private FlexCellFormatter			m_graphCellFormatter;		// A FlexCellFormatter for manipulating cells within the FlexTable.
 	private List<HandlerRegistration>	m_registeredEventHandlers;	// Event handlers that are currently registered.
-	@SuppressWarnings("unused")
-	private List<TaskListItem>			m_taskList;					// The current task list the graphs are displaying.
 	private TaskProvider				m_taskProvider;				// The interface that provides the task list.
+	private TaskStats					m_taskStats;				// The counts used to populate the graphs.
 	private VibeFlowPanel				m_fp;						// The panel holding the content.
-	private VibeFlowPanel				m_priorityPanel;			// The panel holding the priority graph.
 	private VibeFlowPanel				m_refreshPanel;				// The panel holding the refresh push button.
-	private VibeFlowPanel				m_statusPanel;				// The panel holding the status graph.
+	private VibeVerticalPanel			m_priorityPanel;			// The panel holding the priority graph.
+	private VibeVerticalPanel			m_statusPanel;				// The panel holding the status graph.
 	
 	// The following defines the TeamingEvents that are handled by
 	// this class.  See EventHelper.registerEventHandlers() for how
@@ -101,6 +102,109 @@ public class TaskGraphsPanel extends ToolPanelBase
 	private TeamingEvents[] m_registeredEvents = new TeamingEvents[] {
 		TeamingEvents.TASK_LIST_READY,
 	};
+
+	/*
+	 * Inner class used to track the task statistics.
+	 */
+	private static class TaskStats {
+		private int m_priorityCritical;		//
+		private int m_priorityHigh;			//
+		private int m_priorityLeast;		//
+		private int m_priorityLow;			//
+		private int m_priorityMedium;		//
+		private int m_priorityNone;			//
+		
+		private int m_statusCanceled;		//
+		private int m_statusCompleted;		//
+		private int m_statusInProcess;		//
+		private int m_statusNeedsAction;	//
+		
+		private int m_totalTasks;			//
+	
+		/**
+		 * Constructor method.
+		 * 
+		 * @param taskList
+		 */
+		public TaskStats(List<TaskListItem> taskList) {
+			// Gather the statistics from the task list.
+			gatherStats(taskList);
+		}
+		
+		/**
+		 * Get'er methods.
+		 * 
+		 * @return
+		 */
+		public int getPriorityCritical()  {return m_priorityCritical; }
+		public int getPriorityHigh()      {return m_priorityHigh;     }
+		public int getPriorityLeast()     {return m_priorityLeast;    }
+		public int getPriorityLow()       {return m_priorityLow;      }
+		public int getPriorityMedium()    {return m_priorityMedium;   }
+		public int getPriorityNone()      {return m_priorityNone;     }
+		
+		public int getStatusCanceled()    {return m_statusCanceled;   }
+		public int getStatusCompleted()   {return m_statusCompleted;  }
+		public int getStatusInProcess()   {return m_statusInProcess;  }
+		public int getStatusNeedsAction() {return m_statusNeedsAction;}
+		
+		public int getTotalTasks()        {return m_totalTasks;       }
+
+		/**
+		 * Returns the percentage a given count is of the total.
+		 * 
+		 * @param count
+		 * 
+		 * @return
+		 */
+		public int getPercent(int count) {
+			return ((int) Math.round((((double) count) / ((double) m_totalTasks)) * 100.0));
+		}
+		
+		/*
+		 * Reflects this task in the counts.
+		 */
+		private void countTask(TaskListItem task) {
+			// Count the priority...
+			String p = task.getTask().getPriority();
+			if      (p.equals(TaskListItem.TaskInfo.PRIORITY_NONE))     m_priorityNone     += 1;
+			else if (p.equals(TaskListItem.TaskInfo.PRIORITY_CRITICAL)) m_priorityCritical += 1;
+			else if (p.equals(TaskListItem.TaskInfo.PRIORITY_HIGH))     m_priorityHigh     += 1;
+			else if (p.equals(TaskListItem.TaskInfo.PRIORITY_MEDIUM))   m_priorityMedium   += 1;
+			else if (p.equals(TaskListItem.TaskInfo.PRIORITY_LOW))      m_priorityLow      += 1;
+			else if (p.equals(TaskListItem.TaskInfo.PRIORITY_LEAST))    m_priorityLeast    += 1;
+
+			// ...status...
+			String s = task.getTask().getStatus();
+			if      (s.equals(TaskListItem.TaskInfo.STATUS_NEEDS_ACTION)) m_statusNeedsAction += 1;
+			else if (s.equals(TaskListItem.TaskInfo.STATUS_IN_PROCESS))   m_statusInProcess   += 1;
+			else if (s.equals(TaskListItem.TaskInfo.STATUS_COMPLETED))    m_statusCompleted   += 1;
+			else if (s.equals(TaskListItem.TaskInfo.STATUS_CANCELED))     m_statusCanceled    += 1;
+			
+			// ...and total.
+			m_totalTasks += 1;
+		}
+		
+		/*
+		 * Gathers the counts from the given list.
+		 */
+		private void gatherStats(List<TaskListItem> taskList) {
+			// If we don't have a list...
+			if ((null == taskList) || taskList.isEmpty()) {
+				// ...there's nothing to count.  Bail.
+				return;
+			}
+
+			// Scan the tasks in the task list...
+			for (TaskListItem task:  taskList) {
+				// ...counting each one...
+				countTask(task);
+				
+				// ...and its subtasks.
+				gatherStats(task.getSubtasks());
+			}
+		}
+	}
 	
 	/*
 	 * Constructor method.
@@ -125,6 +229,49 @@ public class TaskGraphsPanel extends ToolPanelBase
 		m_fp.addStyleName("vibe-binderViewTools vibe-taskGraphsPanel");
 		initWidget(m_fp);
 		loadPart1Async();
+	}
+
+	/*
+	 * Adds a statistic bar to a graph.
+	 */
+	private void addBar(VibeVerticalPanel vp, String style, String message) {
+		// Create a panel for the bar...
+		VibeFlowPanel fp = new VibeFlowPanel();
+		fp.addStyleName("vibe-taskGraphsStatisticBar");
+		vp.add(fp);
+
+		// ...create the colored box...
+		VibeFlowPanel colorBox = new VibeFlowPanel();
+		colorBox.addStyleName("vibe-taskGraphsStatisticBox " + style);
+		fp.add(colorBox);
+
+		// ...and create its label.
+		InlineLabel il = new InlineLabel(message);
+		il.addStyleName("vibe-taskGraphsStatisticLabel");
+		fp.add(il);
+	}
+
+	/*
+	 * Adds a colored bar segment to a grid.
+	 */
+	private void addBarSegment(FlexTable grid, FlexCellFormatter gridCellFormatter, int count, int percent, String style, String message) {
+		// If this segment doesn't show anything...
+		if (0 == count) {
+			// ...skip it.
+			return;
+		}
+		
+		String width = (percent + "%");
+		InlineLabel il = new InlineLabel(width);
+		il.addStyleName("vibe-taskGraphsBarSegment");
+		il.setWordWrap(false);
+		il.setTitle(message);
+		int cell;
+		try                  {cell = grid.getCellCount(0);}
+		catch (Exception ex) {cell = 0;                   }
+		grid.setWidget(0, cell, il);
+		gridCellFormatter.setWidth(0, cell, width);
+		gridCellFormatter.addStyleName(0, cell, style);
 	}
 
 	/**
@@ -257,13 +404,17 @@ public class TaskGraphsPanel extends ToolPanelBase
 
 		// ...add a label to it...
 		InlineLabel il = new InlineLabel(m_messages.taskGraphs());
-		il.addStyleName("vibe-taskGraphsExpanderLabel");
+		il.addStyleName("vibe-taskGraphsLabel");
 		Element aE = a.getElement();
 		aE.appendChild(il.getElement());
 
 		// ...add an expand/collapse image to it...
-		final Image img = new Image(m_expandGraphs ? m_images.collapser() : m_images.expander());
+		ImageResource imgR = (m_expandGraphs ? m_images.collapser() : m_images.expander());
+		final Image img = new Image();
 		img.addStyleName("vibe-taskGraphsExpanderImg");
+		img.setUrl(imgR.getSafeUri());
+		img.setHeight("12px");
+		img.setWidth( "12px");
 		img.getElement().setAttribute("align", "absmiddle");
 		aE.appendChild(img.getElement());
 
@@ -287,7 +438,8 @@ public class TaskGraphsPanel extends ToolPanelBase
 				
 				// Toggle the state of the expander...
 				m_expandGraphs = (!m_expandGraphs);
-				img.setResource(m_expandGraphs ? m_images.collapser() : m_images.expander());
+				ImageResource imgR = (m_expandGraphs ? m_images.collapser() : m_images.expander());
+				img.setUrl(imgR.getSafeUri());
 				a.setTitle(m_expandGraphs ? m_messages.taskGraphsAltHide() : m_messages.taskGraphsAltShow());
 				panelResized();
 
@@ -329,15 +481,109 @@ public class TaskGraphsPanel extends ToolPanelBase
 	 * Cell:  1
 	 */
 	private void renderPriorityCell() {
-		m_priorityPanel = new VibeFlowPanel();
+		// Create a vertical panel for the priorities graph...
+		m_priorityPanel = new VibeVerticalPanel();
 		m_priorityPanel.addStyleName("vibe-taskGraphsPriorities");
 		if (!m_expandGraphs) {
 			m_priorityPanel.addStyleName("vibe-taskGraphsHidden");
 		}
 		m_graphGrid.setWidget(0, 1, m_priorityPanel);
+		m_graphCellFormatter.setAlignment(0, 1, HasHorizontalAlignment.ALIGN_LEFT, HasVerticalAlignment.ALIGN_TOP);
 		
-//!		...this needs to be implemented...
-		m_priorityPanel.add(new InlineLabel("...priorities graph..."));
+		// ...and add the header for the priorities graph.
+		InlineLabel il = new InlineLabel(m_messages.taskGraphsPriority());
+		il.addStyleName("vibe-taskGraphsLabel");
+		m_priorityPanel.add(il);
+
+		// Are we displaying any tasks? 
+		int    c;
+		int    p;
+		String m;
+		if (0 < m_taskStats.getTotalTasks()) {
+			// Yes!  Create a grid for the priorities bar chart...
+			FlexTable grid = new FlexTable();
+			grid.addStyleName("vibe-taskGraphsPrioritiesBar");
+			grid.setCellPadding(0);
+			grid.setCellSpacing(0);
+			m_priorityPanel.add(grid);
+			FlexCellFormatter gridCellFormatter = grid.getFlexCellFormatter();
+			
+			// ...and render the various priorities into that grid.
+			c = m_taskStats.getPriorityCritical();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsPriorityCritical(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsPriority0", m);
+			}
+			
+			c = m_taskStats.getPriorityHigh();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsPriorityHigh(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsPriority1", m);
+			}
+			
+			c = m_taskStats.getPriorityMedium();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsPriorityMedium(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsPriority2", m);
+			}
+			
+			c = m_taskStats.getPriorityLow();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsPriorityLow(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsPriority3", m);
+			}
+			
+			c = m_taskStats.getPriorityLeast();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsPriorityLeast(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsPriority4", m);
+			}
+			
+			c = m_taskStats.getPriorityNone();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsPriorityNone(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsPriority5", m);
+			}
+		}
+
+		// Finally, add the individual priority rows.
+		c = m_taskStats.getPriorityCritical();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsPriorityCritical(String.valueOf(p), String.valueOf(c));
+		addBar(m_priorityPanel, "vibe-taskGraphsPriority0", m);
+		
+		c = m_taskStats.getPriorityHigh();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsPriorityHigh(String.valueOf(p), String.valueOf(c));
+		addBar(m_priorityPanel, "vibe-taskGraphsPriority1", m);
+		
+		c = m_taskStats.getPriorityMedium();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsPriorityMedium(String.valueOf(p), String.valueOf(c));
+		addBar(m_priorityPanel, "vibe-taskGraphsPriority2", m);
+		
+		c = m_taskStats.getPriorityLow();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsPriorityLow(String.valueOf(p), String.valueOf(c));
+		addBar(m_priorityPanel, "vibe-taskGraphsPriority3", m);
+		
+		c = m_taskStats.getPriorityLeast();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsPriorityLeast(String.valueOf(p), String.valueOf(c));
+		addBar(m_priorityPanel, "vibe-taskGraphsPriority4", m);
+		
+		c = m_taskStats.getPriorityNone();
+		if (0 < c) {
+			p = m_taskStats.getPercent(c);
+			m = m_messages.taskGraphsPriorityNone(String.valueOf(p), String.valueOf(c));
+			addBar(m_priorityPanel, "vibe-taskGraphsPriority5", m);
+		}
 	}
 	
 	/*
@@ -379,15 +625,83 @@ public class TaskGraphsPanel extends ToolPanelBase
 	 * Cell:  2
 	 */
 	private void renderStatusCell() {
-		m_statusPanel = new VibeFlowPanel();
+		// Create a vertical panel for the status graph...
+		m_statusPanel = new VibeVerticalPanel();
 		m_statusPanel.addStyleName("vibe-taskGraphsStatus");
 		if (!m_expandGraphs) {
 			m_statusPanel.addStyleName("vibe-taskGraphsHidden");
 		}
 		m_graphGrid.setWidget(0, 2, m_statusPanel);
+		m_graphCellFormatter.setAlignment(0, 2, HasHorizontalAlignment.ALIGN_LEFT, HasVerticalAlignment.ALIGN_TOP);
+
+		// ...and add the header for the status graph.
+		InlineLabel il = new InlineLabel(m_messages.taskGraphsStatus());
+		il.addStyleName("vibe-taskGraphsLabel");
+		m_statusPanel.add(il);
 		
-//!		...this needs to be implemented...		
-		m_statusPanel.add(new InlineLabel("...status graph..."));
+		// Are we displaying any tasks? 
+		int    c;
+		int    p;
+		String m;
+		if (0 < m_taskStats.getTotalTasks()) {
+			// Yes!  Create a grid for the status bar chart...
+			FlexTable grid = new FlexTable();
+			grid.addStyleName("vibe-taskGraphsStatusBar");
+			grid.setCellPadding(0);
+			grid.setCellSpacing(0);
+			m_statusPanel.add(grid);
+			FlexCellFormatter gridCellFormatter = grid.getFlexCellFormatter();
+			
+			// ...and render the various status values into that grid.
+			c = m_taskStats.getStatusNeedsAction();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsStatusNeedsAction(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsStatus0", m);
+			}
+			
+			c = m_taskStats.getStatusInProcess();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsStatusInProcess(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsStatus1", m);
+			}
+			
+			c = m_taskStats.getStatusCompleted();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsStatusCompleted(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsStatus2", m);
+			}
+			
+			c = m_taskStats.getStatusCanceled();
+			if (0 < c) {
+				p = m_taskStats.getPercent(c);
+				m = m_messages.taskGraphsStatusCanceled(String.valueOf(p), String.valueOf(c));
+				addBarSegment(grid, gridCellFormatter, c, p, "vibe-taskGraphsStatus3", m);
+			}
+		}
+
+		// Finally, add the individual status value rows.
+		c = m_taskStats.getStatusNeedsAction();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsStatusNeedsAction(String.valueOf(p), String.valueOf(c));
+		addBar(m_statusPanel, "vibe-taskGraphsStatus0", m);
+		
+		c = m_taskStats.getStatusInProcess();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsStatusInProcess(String.valueOf(p), String.valueOf(c));
+		addBar(m_statusPanel, "vibe-taskGraphsStatus1", m);
+		
+		c = m_taskStats.getStatusCompleted();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsStatusCompleted(String.valueOf(p), String.valueOf(c));
+		addBar(m_statusPanel, "vibe-taskGraphsStatus2", m);
+		
+		c = m_taskStats.getStatusCanceled();
+		p = m_taskStats.getPercent(c);
+		m = m_messages.taskGraphsStatusCanceled(String.valueOf(p), String.valueOf(c));
+		addBar(m_statusPanel, "vibe-taskGraphsStatus3", m);
 	}
 	
 	/*
@@ -407,8 +721,9 @@ public class TaskGraphsPanel extends ToolPanelBase
 	 * Synchronously renders the tasks from the task list.
 	 */
 	private void renderTaskGraphsNow() {
-		// Pull the current task list from the task provider...
-		m_taskList = m_taskProvider.getTasks();
+		// Pull the current task list from the task provider and use
+		// them to calculate the counts...
+		m_taskStats = new TaskStats(m_taskProvider.getTasks());
 
 		// ...render the task graphs...
 		m_fp.clear();
