d68ac51fab4888e214e2ba7fb2df582f4dc70d44
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-01-28 23:39:16 +0000

    Bug: 728965 Reviewer: Self Comment: During modification of a binder, Lucene doc was generated from a state of the binder object that did not yet reflect the new value for library flag. Consequently, the new value for library flag was stored only in database but not in Lucene index. This fix cures that bug.

--- branches/granite/ssf/main/src/org/kablink/teaming/module/binder/impl/BinderModuleImpl.java
+++ branches/granite/ssf/main/src/org/kablink/teaming/module/binder/impl/BinderModuleImpl.java
@@ -647,7 +647,7 @@ public class BinderModuleImpl extends CommonDependencyInjection implements
 			}
 		}
 
-		// save library flag
+		// save library flag here since it will be changed to the new value during modifyBinder() call
 		boolean oldLibrary = binder.isLibrary();
 		boolean oldUnique = binder.isUniqueTitles();
 
@@ -707,7 +707,8 @@ public class BinderModuleImpl extends CommonDependencyInjection implements
 								query.close();
 							}
 						}
-						binder.setLibrary(newLibrary);
+						// Unnecessary since the new value was stored into the binder during modifyBinder() call
+						//binder.setLibrary(newLibrary);
 						return null;
 					}
 				});
