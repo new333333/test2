e32d7d3822ea2ecab8bf556575afb941da2034b3
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-03-21 14:26:07 +0000

    Bug: 751595 Reviewer: Peter Comment: Make it possible to add definition elements with the same caption

--- branches/granite/ssf/main/src/org/kablink/teaming/portlet/definitionBuilder/ViewController.java
+++ branches/granite/ssf/main/src/org/kablink/teaming/portlet/definitionBuilder/ViewController.java
@@ -352,12 +352,15 @@ public class ViewController extends SAbstractController {
 			String name = NLT.getDef(sourceEle.attributeValue("name"));
 			String caption = NLT.getDef(sourceEle.attributeValue("caption"));
 			if (properties != null) {
+				Element nameProp = (Element) properties.selectSingleNode("property[@name='name']");
 				Element captionProp = (Element) properties.selectSingleNode("property[@name='caption']");
 				if (captionProp != null) {
 					if (!captionProp.attributeValue("value", "").equals("")) {
+						if (name.equals("entryDataItem") && nameProp != null) {
+							caption += " [" +  nameProp.attributeValue("value", "") + "]";
+						}
 						caption += " - " + NLT.getDef(captionProp.attributeValue("value", ""));
 					} else {
-						Element nameProp = (Element) properties.selectSingleNode("property[@name='name']");
 						if (nameProp != null) {
 							if (!nameProp.attributeValue("value", "").equals("")) {
 								caption += " - " + nameProp.attributeValue("value", "");
