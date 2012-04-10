0d7d1bd189e046c1baa4e4bb71b862c06672beab
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-04-10 20:46:45 +0000

    Bug:  745692 Reviewer:  Self Comments:  Added "chunking" to the file upload applet in an attempt to fix the "out of memory" errors.  (Merge changes from Granite.)
    Files:
    M    applets/droptarget/src/org/kablink/teaming/applets/droptarget/PostFiles.java

--- ssf/applets/droptarget/src/org/kablink/teaming/applets/droptarget/PostFiles.java
+++ ssf/applets/droptarget/src/org/kablink/teaming/applets/droptarget/PostFiles.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2012 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -283,6 +283,7 @@ public class PostFiles extends Thread {
             conn.setRequestProperty("Accept", "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2");
             conn.setRequestProperty("Accept-Charset", "utf-8;q=0.9");
             conn.setRequestProperty("Connection", "keep-alive");
+            conn.setChunkedStreamingMode(BUFF_SIZE);
             try {
             	conn.connect();
             } catch(Exception e) {
