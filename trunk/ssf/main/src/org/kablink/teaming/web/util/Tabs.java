853625d55c12a7436bc12547a47b4415026b7a37
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-05-03 16:41:21 +0000

    Bug: 760303 Reviewer: Peter Comment: Never save user properties to disk or read in properties in from disk for Guest user

--- trunk/ssf/main/src/org/kablink/teaming/web/util/Tabs.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/util/Tabs.java
@@ -424,7 +424,13 @@ public class Tabs {
 	
 	private static void saveUserTabsImpl(PortletSession ps, HttpSession hs, Long userId) {
 		ProfileModule pm = ((ProfileModule) SpringContextUtil.getBean("profileModule"));
-		pm.setUserProperty(userId, ObjectKeys.USER_PROPERTY_TABS, getTabsImpl(ps, hs).getSerializationMap());
+		try {
+			User user = RequestContextHolder.getRequestContext().getUser();
+			if (!user.isShared()) {
+				//Only save the tabs to the disk if not the guest account
+				pm.setUserProperty(userId, ObjectKeys.USER_PROPERTY_TABS, getTabsImpl(ps, hs).getSerializationMap());
+			}
+		} catch(Exception e) {}
 	}
 	
 	/**
