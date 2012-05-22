33af2f02c1b89ce9febfeb21934ec756aebda456
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-05-22 21:44:51 +0000

    Bug: 763154 Reviewer: Self Comment: NO longer assume that NetBIOS domain name will match a prefix in DN obtained from AD for authenticated user

--- trunk/ssf/main/src/org/kablink/teaming/spring/security/ldap/StrictDomainMatcher.java
+++ trunk/ssf/main/src/org/kablink/teaming/spring/security/ldap/StrictDomainMatcher.java
@@ -34,7 +34,7 @@ package org.kablink.teaming.spring.security.ldap;
 
 import javax.naming.directory.SearchResult;
 
-public class DefaultDomainMatcher implements DomainMatcher {
+public class StrictDomainMatcher implements DomainMatcher {
 
 	public boolean matches(String domainName, String dn, SearchResult searchResult) {
 		return (dn.toLowerCase().contains("=" + domainName.toLowerCase()));
