01b289ce750551772c7b5348e9f595176fc10774
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-07-09 19:11:47 +0000

    Bug: BUILD Reviewer: Self Comment: Temporary check-in to fix compilation errors from missing methods

--- ssf/rest-shared/src/org/kablink/teaming/rest/v1/model/UserBrief.java
+++ ssf/rest-shared/src/org/kablink/teaming/rest/v1/model/UserBrief.java
@@ -41,4 +41,27 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement (name="user_brief")
 public class UserBrief extends PrincipalBrief {
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	
 }
