89f32232737f2d24e148ad00248722f07e8a53b7
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-08-14 22:02:49 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Filr integration.  {121st} 1. Removed the 'Messages' column from the 'Shared by/with Me' collection    views. 2. When in a collection view, made the 'Comments' field not sortable.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtViewHelper.java
    M    gwt/src/org/kablink/teaming/gwt/client/binderviews/CollectionView.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/CollectionView.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/CollectionView.java
@@ -74,14 +74,23 @@ public class CollectionView extends DataTableFolderViewBase {
 	 */
 	@Override
 	protected void adjustFixedColumnWidths(Map<String, ColumnWidth> columnWidths) {
-		columnWidths.put(FolderColumn.COLUMN_TITLE,             new ColumnWidth(20));	// = 20
-		columnWidths.put(FolderColumn.COLUMN_SHARE_SHARED_BY,   new ColumnWidth(13));	// = 33
-		columnWidths.put(FolderColumn.COLUMN_SHARE_SHARED_WITH, new ColumnWidth(13));	//     (By or with, never both.)
-		columnWidths.put(FolderColumn.COLUMN_SHARE_DATE,        new ColumnWidth( 7));	// = 40
-		columnWidths.put(FolderColumn.COLUMN_SHARE_EXPIRATION,  new ColumnWidth( 7));	// = 47
-		columnWidths.put(FolderColumn.COLUMN_SHARE_ACCESS,      new ColumnWidth( 7));	// = 54
-		columnWidths.put(FolderColumn.COLUMN_SHARE_MESSAGE,     new ColumnWidth(39));	// = 93
-		columnWidths.put(FolderColumn.COLUMN_COMMENTS,          new ColumnWidth( 7));	// = 100%
+		switch (getFolderInfo().getCollectionType()) {
+		case SHARED_BY_ME:
+		case SHARED_WITH_ME:
+			columnWidths.put(FolderColumn.COLUMN_TITLE,             new ColumnWidth(50));	// = 50
+			columnWidths.put(FolderColumn.COLUMN_SHARE_SHARED_BY,   new ColumnWidth(13));	// = 63
+			columnWidths.put(FolderColumn.COLUMN_SHARE_SHARED_WITH, new ColumnWidth(13));	//     (By or with, never both.)
+			columnWidths.put(FolderColumn.COLUMN_SHARE_DATE,        new ColumnWidth(10));	// = 73
+			columnWidths.put(FolderColumn.COLUMN_SHARE_EXPIRATION,  new ColumnWidth(10));	// = 83
+			columnWidths.put(FolderColumn.COLUMN_SHARE_ACCESS,      new ColumnWidth(10));	// = 93
+			columnWidths.put(FolderColumn.COLUMN_COMMENTS,          new ColumnWidth( 7));	// = 100%
+			
+			break;
+		
+		default:
+			// No changes for the other collection types.
+			break;
+		}
 	}
 
 	/**
