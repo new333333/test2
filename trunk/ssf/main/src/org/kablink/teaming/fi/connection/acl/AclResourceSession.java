b09f83062f105a33cf51851ca9a625fb1755b3b6
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-08-15 16:17:08 +0000

    Bug:PRD Reviewer:Jong Comment: Move connection.acl package from closed to open

---
+++ trunk/ssf/main/src/org/kablink/teaming/fi/connection/acl/AclResourceSession.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
+ * 
+ * This work is governed by the Common Public Attribution License Version 1.0 (the
+ * "CPAL"); you may not use this file except in compliance with the CPAL. You may
+ * obtain a copy of the CPAL at http://www.opensource.org/licenses/cpal_1.0. The
+ * CPAL is based on the Mozilla Public License Version 1.1 but Sections 14 and 15
+ * have been added to cover use of software over a computer network and provide
+ * for limited attribution for the Original Developer. In addition, Exhibit A has
+ * been modified to be consistent with Exhibit B.
+ * 
+ * Software distributed under the CPAL is distributed on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, either express or implied. See the CPAL for the specific
+ * language governing rights and limitations under the CPAL.
+ * 
+ * The Original Code is ICEcore, now called Kablink. The Original Developer is
+ * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
+ * (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * 
+ * Attribution Information:
+ * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
+ * Attribution URL: [www.kablink.org]
+ * Graphic Image as provided in the Covered Code
+ * [ssf/images/pics/powered_by_icecore.png].
+ * Display of Attribution Information is required in Larger Works which are
+ * defined in the CPAL as a work which combines Covered Code or portions thereof
+ * with code not governed by the terms of the CPAL.
+ * 
+ * NOVELL and the Novell logo are registered trademarks and Kablink and the
+ * Kablink logos are trademarks of Novell, Inc.
+ */
+
+package org.kablink.teaming.fi.connection.acl;
+
+import java.util.Set;
+
+import org.kablink.teaming.fi.connection.ResourceSession;
+
+/**
+ * A resource session interface that supports access control list (ACL).
+ * 
+ * @author jong
+ *
+ */
+public interface AclResourceSession extends ResourceSession {
+
+	/**
+	 * Reads the access control list. 
+	 * <p>
+	 * If there is no ACL item, it must return an empty set, not <code>null</code>.
+	 * 
+	 * @return a set of items representing the ACL. 
+	 */
+	public Set<AclItem> getAcl();
+
+	/**
+	 * Return the ACL resource driver that allocated this session. 
+	 * Convenience method.
+	 * 
+	 * @return
+	 */
+	public AclResourceDriver getAclResourceDriver();
+	
+}
