697ef968dcbdabaaa1607c25dff63d101b078780
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-10-12 22:44:33 +0000

    Bug:PRD Reviewer:Jong Comment:Undo previous change by removing reference to LocaleUtils class.

--- trunk/ssf/util/src/org/kablink/util/PropsUtil.java
+++ trunk/ssf/util/src/org/kablink/util/PropsUtil.java
@@ -38,8 +38,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 
-//!!!import org.kablink.teaming.util.LocaleUtils;
-
 public class PropsUtil {
 	// This is a singleton class. 
 	
@@ -267,8 +265,6 @@ public class PropsUtil {
 		if (null == teamingLocale) {
 			String language = getString("i18n.default.locale.language", "");
 			String country  = getString("i18n.default.locale.country",  "");
-//!!!			String language = LocaleUtils.getLocaleLanguage();
-//!!!			String country  = LocaleUtils.getLocaleCountry();
 			if ((null != language) && (0 < language.length())) {
 				if ((null != country) && (0 < country.length())) teamingLocale = new Locale(language, country);
 				else                                             teamingLocale = new Locale(language);
