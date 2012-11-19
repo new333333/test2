847214a39b24b7effdb20e41b407e40c4199c4bd
ranandalai <ranandalai@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-11-19 21:54:05 +0000

    Bug: None Reviewer: none Comment: Adding code to chkconfig teaming on script to be called.

--- ssf/ssf-install-config/src/org/kabling/teaming/install/server/InstallServiceImpl.java
+++ ssf/ssf-install-config/src/org/kabling/teaming/install/server/InstallServiceImpl.java
@@ -1898,13 +1898,16 @@ public class InstallServiceImpl extends RemoteServiceServlet implements InstallS
 					throw new ConfigurationSaveException();
 				}
 
-				// Setup
+				// Setup luceneindex.root.dir on ssf-ext.properties
 				info = executeCommand("sed -i  -e 's|data.luceneindex.root.dir=/vastorage/filr|data.luceneindex.root.dir=/vastorage/search|' /opt/novell/filr/apache-tomcat/webapps/ssf/WEB-INF/classes/config/ssf-ext.properties");
 				if (info.getExitValue() != 0)
 				{
 					logger.debug("Error setting up data.luceneindex.root.dir=/vastorage/search" + info.getExitValue());
 					throw new ConfigurationSaveException();
 				}
+				
+				// Setup luceneindex.root.dir on ssf-ext.properties
+				info = executeCommand("chkconfig teaming on");
 				
 				try
 				{
