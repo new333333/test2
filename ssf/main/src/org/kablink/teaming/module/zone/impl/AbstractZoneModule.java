05d23395bc6446e5503dce714b1c39ebb76549f0
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2012-11-19 15:30:24 +0000

    Bug: PRD Reviewer: Peter Comment: Add new right to access net folders

--- ssf/main/src/org/kablink/teaming/module/zone/impl/AbstractZoneModule.java
+++ ssf/main/src/org/kablink/teaming/module/zone/impl/AbstractZoneModule.java
@@ -1912,6 +1912,18 @@ public abstract class AbstractZoneModule extends CommonDependencyInjection imple
 			setGlobalWorkareaFunctionMembership(zoneConfig, function, new HashSet());
 		}
 		
+		if (!functionInternalIds.containsKey(ObjectKeys.FUNCTION_ALLOW_ACCESS_NET_FOLDER_INTERNALID)) {
+			function = new Function();
+			function.setZoneId(zoneConfig.getZoneId());
+			function.setName(ObjectKeys.ROLE_ALLOW_ACCESS_NET_FOLDER);
+			function.setScope(ObjectKeys.ROLE_TYPE_BINDER);
+			function.setInternalId(ObjectKeys.FUNCTION_ALLOW_ACCESS_NET_FOLDER_INTERNALID);
+			function.addOperation(WorkAreaOperation.ALLOW_ACCESS_NET_FOLDER);
+			//generate functionId
+			getFunctionManager().addFunction(function);
+			setGlobalWorkareaFunctionMembership(zoneConfig, function, new HashSet());
+		}
+		
 		if (!functionInternalIds.containsKey(ObjectKeys.FUNCTION_VIEW_BINDER_TITLE_INTERNALID)) {
 			addViewBinderTitleRole(zoneConfig.getZoneId());
 		}
