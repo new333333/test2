70e301733abaf29440f244014bf26ece42d2fd6f
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-01-14 22:27:19 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Neutron integration.  {372nd} 1. Added an ssf*.properties flag to allow easy non-LDAP 'Home' folder    tetsting.
    Files:
    M    main/src/org/kablink/teaming/search/SearchUtils.java

--- trunk/ssf/main/src/org/kablink/teaming/search/SearchUtils.java
+++ trunk/ssf/main/src/org/kablink/teaming/search/SearchUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -58,6 +58,7 @@ import org.kablink.teaming.security.runwith.RunWithTemplate;
 import org.kablink.teaming.task.TaskHelper;
 import org.kablink.teaming.util.AllModulesInjected;
 import org.kablink.teaming.util.NLT;
+import org.kablink.teaming.util.SPropsUtil;
 import org.kablink.teaming.util.SpringContextUtil;
 import org.kablink.teaming.util.Utils;
 import org.kablink.util.search.*;
@@ -1178,7 +1179,7 @@ public class SearchUtils {
 		if (Utils.checkIfFilr()) {
 			// ...and the user has been provisioned from ldap...
 			IdentityInfo idInfo = user.getIdentityInfo();
-			if (idInfo.isFromLdap()) {
+			if (idInfo.isFromLdap() || SPropsUtil.getBoolean("myfiles.allow.nonldap.homeOnly", false)) {
 				// ...check the user's and/or zone setting.
 				Boolean result = getEffectiveAdhocFolderSetting(bs, user);
 				if ((null != result) && (!(result))) {
