95b4d08e046027857db33549264c96d2a0ca933a
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-01-21 21:33:14 +0000

    Bug:PRD Reviewer:Jong Comment:Re-factorying related to performance optimization work

--- trunk/ssf/main/src/org/kablink/teaming/fi/connection/acl/ResourceItem.java
+++ trunk/ssf/main/src/org/kablink/teaming/fi/connection/acl/ResourceItem.java
@@ -30,7 +30,7 @@
  * NOVELL and the Novell logo are registered trademarks and Kablink and the
  * Kablink logos are trademarks of Novell, Inc.
  */
-package org.kablink.teaming.fi.connection;
+package org.kablink.teaming.fi.connection.acl;
 
 import org.kablink.teaming.util.Constants;
 
