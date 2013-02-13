e5e103357f2bb8d891447c5dd70d8c326c17ccb2
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-02-13 17:10:57 +0000

    Bug:  803187 Reviewer:  Self Comments:  Disable the Oracle Outside-in converters if the license is expired.  (Posted fix from Hudson.)
    Files:
    M    web/docroot/WEB-INF/messages/messages.properties
    M    main/src/org/kablink/teaming/module/license/LicenseChecker.java

--- ssf/main/src/org/kablink/teaming/module/license/LicenseChecker.java
+++ ssf/main/src/org/kablink/teaming/module/license/LicenseChecker.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2009 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -33,6 +33,8 @@
 package org.kablink.teaming.module.license;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.kablink.teaming.SingletonViolationException;
 import org.kablink.teaming.license.LicenseManager;
@@ -82,5 +84,18 @@ public class LicenseChecker {
 	public static boolean validLicense(Calendar when)
 	{
 		return getInstance().getLicenseManager().validLicense(when);
+	}
+
+	/**
+	 * Returns true if we're running with an expired license and false
+	 * otherwise.
+	 * 
+	 * @return
+	 */
+	public static boolean isLicenseExpired() {
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(new Date());
+		boolean reply = (!(validLicense(cal)));
+		return reply;
 	}
 }
