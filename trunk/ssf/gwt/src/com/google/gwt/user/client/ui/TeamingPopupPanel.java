0001a2af2e106a5bfacf130f501353d88ead09e6
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-03-18 20:32:50 +0000

    Bug:  810004 Reviewer:  Self Comments:  Fixed nested dialog handling.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/public/GwtTeaming.css
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/DlgBox.java
    M    gwt/src/com/google/gwt/user/client/ui/TeamingPopupPanel.java

--- trunk/ssf/gwt/src/com/google/gwt/user/client/ui/TeamingPopupPanel.java
+++ trunk/ssf/gwt/src/com/google/gwt/user/client/ui/TeamingPopupPanel.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -111,5 +111,17 @@ public class TeamingPopupPanel extends PopupPanel {
 		// If we get here, we need to pass the event on through the
 		// PopupPanel's handler.
 		super.onPreviewNativeEvent(nativeEvent);
+	}
+
+	/**
+	 * Provides access to the PopupPanel's glass Element.
+	 * 
+	 * Required since PopupPanel.getClassElement() is protected and
+	 * can't be called directly otherwise.
+	 * 
+	 * @return
+	 */
+	public Element getPopupGlassElement() {
+		return super.getGlassElement();
 	}
 }
