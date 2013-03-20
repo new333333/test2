0ff2bd0800d692e166212dbd811662673c7ebb9f
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-03-20 21:08:15 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Neutron integration.  {405th} 1. Fixed some potential stream memory leaks in GwtViewHelper and    GwtReportHelper. Coverity:  164258, 170007
    Files:
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtReportsHelper.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtViewHelper.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtReportsHelper.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtReportsHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -339,6 +339,7 @@ public class GwtReportsHelper {
 				tmpEffectiveDateStr = getLicenseValue(doc, "//Dates/@effective");
 				try {
 					if (tmpEffectiveDateStr.equalsIgnoreCase("trial")) {
+						@SuppressWarnings("unused")
 						String days = getLicenseValue(doc, "//Dates/@expiration");
 						int daysSinceInstallation = GwtServerHelper.getDaysSinceInstallation();
 						Calendar c = Calendar.getInstance();
@@ -694,12 +695,16 @@ public class GwtReportsHelper {
 	 * @throws GwtTeamingException
 	 */
 	public static String getSystemErrorLogUrl(AllModulesInjected bs, HttpServletRequest request) throws GwtTeamingException {
+		FileOutputStream	fo      = null;
+		ZipOutputStream		zipOut  = null;
+		FilterOutputStream	wrapper = null;
+		
 		try {
 			// Generate a ZIP file containing the system error logs...
-			File				tempFile = TempFileUtil.createTempFile("logfiles");
-			FileOutputStream	fo       = new FileOutputStream(tempFile);
-			ZipOutputStream		zipOut   = new ZipOutputStream(fo);
-			FilterOutputStream	wrapper  = new FilterOutputStream(zipOut) {
+			File tempFile = TempFileUtil.createTempFile("logfiles");
+			fo       = new FileOutputStream(tempFile);
+			zipOut   = new ZipOutputStream(fo);
+			wrapper  = new FilterOutputStream(zipOut) {
 				@Override
 				public void close() {}  // FileCopyUtils will try to close this too soon
 			};
@@ -731,6 +736,25 @@ public class GwtReportsHelper {
 			     m_logger.debug("GwtReportsHelper.getSystemErrorLogUrl( SOURCE EXCEPTION ):  ", ex);
 			}
 			throw GwtServerHelper.getGwtTeamingException(ex);
-		}		
+		}
+		
+		finally {
+			// Ensure we've closed the streams.
+			if (null != wrapper) {
+				try {wrapper.close();}
+				catch (Exception e) {}
+				wrapper = null;
+			}
+			if (null != zipOut) {
+				try {zipOut.close();}
+				catch (Exception e) {}
+				zipOut = null;
+			}
+			if (null != fo) {
+				try {fo.close();}
+				catch (Exception e) {}
+				fo = null;
+			}
+		}
 	}
 }
