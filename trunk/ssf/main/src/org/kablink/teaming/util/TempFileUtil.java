772a71a8a20972523782c7e04afd394150d2a910
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-03-29 15:40:32 +0000

    Bug:  803580 Reviewer:  Self Comments:  First step in handling Oracle Outside-in temporary files: 1. Moved OIT's temporary files into the Filr/Vibe temp directory area    under an 'oit' subdirectory.
    Files:
    M    main/src/org/kablink/teaming/util/TempFileUtil.java

--- trunk/ssf/main/src/org/kablink/teaming/util/TempFileUtil.java
+++ trunk/ssf/main/src/org/kablink/teaming/util/TempFileUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2009 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -43,8 +43,13 @@ import org.kablink.teaming.UncheckedIOException;
 import org.kablink.teaming.context.request.RequestContextHolder;
 import org.springframework.util.FileCopyUtils;
 
-
+/**
+ * ?
+ * 
+ * @author ?
+ */
 public class TempFileUtil {
+	private final static String OIT_TEMP_SUBDIR	= "oit";	// Must agree with the path injected by the installer.  See install.tcl for details.
 	
 	/**
 	 * Create a temporary file. The created temporary file is set to be deleted
@@ -244,4 +249,14 @@ public class TempFileUtil {
 		}
 	}
 	
+	/**
+	 * Validates that the OIT temporary directory has been created.
+	 */
+	public static void validateOITTempDir() {
+		String oitTempPath = (getTempFileDirRootPath() + File.separator + OIT_TEMP_SUBDIR);
+		File oitTempDir = new File(oitTempPath);
+		if (!(oitTempDir.exists())) {
+			oitTempDir.mkdir();
+		}
+	}
 }
