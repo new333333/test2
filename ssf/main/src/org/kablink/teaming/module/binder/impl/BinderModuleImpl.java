255242cb460e42ceafecfab29ff2ac201c57bd38
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-04-20 02:26:20 +0000

    Bug:815697 Reviewer:Jong Comment:Make Net Folder more robust

--- ssf/main/src/org/kablink/teaming/module/binder/impl/BinderModuleImpl.java
+++ ssf/main/src/org/kablink/teaming/module/binder/impl/BinderModuleImpl.java
@@ -3242,7 +3242,7 @@ public class BinderModuleImpl extends CommonDependencyInjection implements
 	}
 
 	protected void deleteBinderPhase2() {
-		if(SPropsUtil.getBoolean("binder.delete.immediate", true)) {
+		if(SPropsUtil.getBoolean("binder.delete.immediate", false)) {
 			// Initiate the phase 2 of the process needed for deleting a binder hierarchy. 
 			// This part is executed asynchronously and we do not check its outcome.
 			getRunAsyncManager().execute(new RunAsyncCallback() {
