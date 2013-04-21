997ef1b0386f297648e81b47cf657c14d6f89a25
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-04-21 21:10:44 +0000

    Bug:815697 Reviewer:Jong Comment:Fix issue with REST API test around sharing, which was introduced by earlier fix for this bug

--- trunk/ssf/main/src/org/kablink/teaming/dao/CoreDao.java
+++ trunk/ssf/main/src/org/kablink/teaming/dao/CoreDao.java
@@ -267,4 +267,6 @@ public interface CoreDao {
 	public boolean contains(Object obj);
 	
 	public BinderState loadBinderState(Long binderId);
+	
+	public void purgeShares(Binder binder, boolean includeEntryShares);
 }
