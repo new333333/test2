fd0559ebb77d5dacee0c6b2b32325106c7a19ac9
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-07-30 21:50:22 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Filr 1.1 development.  {23rd} 1. Reverted my changes dealing with approximate page size.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtViewHelper.java
    M    main/src/org/kablink/teaming/module/binder/impl/AbstractEntryProcessor.java
    M    main/src/org/kablink/teaming/ObjectKeys.java

--- trunk/ssf/main/src/org/kablink/teaming/module/binder/impl/AbstractEntryProcessor.java
+++ trunk/ssf/main/src/org/kablink/teaming/module/binder/impl/AbstractEntryProcessor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -1411,10 +1411,6 @@ public abstract class AbstractEntryProcessor extends AbstractBinderProcessor
         model.put(ObjectKeys.TOTAL_SEARCH_COUNT, new Integer(hits.getTotalHits()));
         //Total number of results returned
         model.put(ObjectKeys.TOTAL_SEARCH_RECORDS_RETURNED, new Integer(hits.length()));
-        //Are there more?
-        if (hits.getThereIsMore()) {
-            model.put(ObjectKeys.SEARCH_COUNT_TOTAL_APPROXIMATE, Boolean.TRUE);
-        }
         return model;
    }
     
