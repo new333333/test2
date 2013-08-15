adacfe0550d5e7dd1181416d493ce9327472c1ab
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-08-15 15:25:42 +0000

    Bug:825999 Reviewer:Jong Comment:Add hidden switches that can be used, if necessary, to throttle file synchronization

--- ssf/main/src/org/kablink/teaming/module/folder/impl/AbstractFolderModule.java
+++ ssf/main/src/org/kablink/teaming/module/folder/impl/AbstractFolderModule.java
@@ -1851,16 +1851,21 @@ public void modifyWorkflowState(Long folderId, Long entryId, Long stateId, Strin
 	}
 
     public Date getLastFullSyncCompletionTime(Long folderId) {
-        Folder folder = getTopMostMirroredFolder(getFolder(folderId));
-        if (folder!=null) {
-            BinderState binderState = (BinderState) getCoreDao().load(BinderState.class, folder.getId());
-            if (binderState!=null) {
-                return binderState.getLastFullSyncCompletionTime();
-            }
+        Folder topMostMirroredFolder = getTopMostMirroredFolder(getFolder(folderId));
+        if (topMostMirroredFolder!=null) {
+        	return getLastFullSyncCompletionTime(topMostMirroredFolder);
         }
         return null;
     }
 
+    protected Date getLastFullSyncCompletionTime(Folder topMostMirroredFolder) {
+        BinderState binderState = (BinderState) getCoreDao().load(BinderState.class, topMostMirroredFolder.getId());
+        if (binderState!=null) {
+            return binderState.getLastFullSyncCompletionTime();
+        }
+        return null;
+    }
+    
     protected Folder getTopMostMirroredFolder(Folder folder) {
         Folder top = folder;
         Binder parent;
