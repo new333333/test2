2c9d274e7534e434f2eec33e24a4ebe5d7d04202
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-09-13 19:58:25 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Filr 1.1 development.  {40th} 1. Began work on a Sharing blacklist/whitelist.
    Files:
    M    tools/sql/scripts/changelog/mysql-changelog-vibe4.0-filr1.1.xml
    M    tools/sql/scripts/changelog/oracle-changelog-vibe4.0-filr1.1.xml
    M    tools/sql/scripts/changelog/sqlserver-changelog-vibe4.0-filr1.1.xml
    M    main/src/org/kablink/teaming/module/sharing/SharingModule.java
    M    main/src/org/kablink/teaming/module/sharing/impl/SharingModuleImpl.java
    A    main/src/org/kablink/teaming/util/ShareLists.java
    M    main/src/org/kablink/teaming/domain/ZoneConfig.java
    M    main/hibernate/org/kablink/teaming/domain/ZoneConfig.hbm.xml

--- ssf/main/src/org/kablink/teaming/module/sharing/SharingModule.java
+++ ssf/main/src/org/kablink/teaming/module/sharing/SharingModule.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -54,6 +54,30 @@ public interface SharingModule {
 		addShareItem,
 		modifyShareItem,
 		deleteShareItem
+	}
+
+	/**
+	 * Enumeration value used to represent the status of sharing with a
+	 * specific external email address.
+	 */
+	public enum ExternalAddressStatus {
+		failsBlacklistDomain,
+		failsBlacklistEMA,
+		failsWhitelist,
+		
+		valid;
+
+		/**
+		 * Get'er methods.
+		 * 
+		 * @return
+		 */
+		public boolean failsBlackListDomain() {return failsBlacklistDomain.equals(this);                        }
+		public boolean failsBlackListEMA()    {return failsBlacklistEMA.equals(   this);                        }
+		public boolean failsBlackList()       {return (this.failsBlackListDomain() || this.failsBlackListEMA());}
+		public boolean failsWhitelist()       {return failsWhitelist.equals(      this);                        }
+		public boolean isInvalid()            {return (!(isValid()));                                           }
+		public boolean isValid()              {return valid.equals(               this);                        }
 	}
 	
 	public void checkAccess(ShareItem shareItem, SharingOperation operation)
@@ -166,4 +190,26 @@ public interface SharingModule {
      * @return
      */
     public Date getHiddenShareModTimeForCurrentUser(boolean recipient);
+
+
+    /**
+     * Returns true if the given email address is valid for sharing
+     * with based on the current sharing blacklist/whitelist.
+     * 
+     * @param ema
+     * 
+     * @return
+     */
+    public boolean isExternalAddressValid(String ema);
+    
+    /**
+     * Returns an ExternalAddressStatus value for the status of sharing
+     * with the given email address based on the current sharing
+     * blacklist/whitelist.
+     * 
+     * @param ema
+     * 
+     * @return
+     */
+    public ExternalAddressStatus getExternalAddressStatus(String ema);
 }
