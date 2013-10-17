ec31a9d6fdcf6120ff8c128abd182b67f8a8ef04
jwootton <jwootton@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-10-17 17:54:50 +0000

    Bug: PRD Reviewer: Jay Wootton Comment: Updated Edit Share dialog to use the ShareExpirationWidget.

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/EditShareDlg.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/EditShareDlg.java
@@ -34,7 +34,6 @@ package org.kablink.teaming.gwt.client.widgets;
 
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import org.kablink.teaming.gwt.client.EditSuccessfulHandler;
 import org.kablink.teaming.gwt.client.GwtTeaming;
@@ -43,38 +42,23 @@ import org.kablink.teaming.gwt.client.util.GwtClientHelper;
 import org.kablink.teaming.gwt.client.util.GwtShareItem;
 import org.kablink.teaming.gwt.client.util.ShareExpirationValue;
 import org.kablink.teaming.gwt.client.util.ShareRights;
-import org.kablink.teaming.gwt.client.util.ShareExpirationValue.ShareExpirationType;
 import org.kablink.teaming.gwt.client.util.ShareRights.AccessRights;
 import org.kablink.teaming.gwt.client.widgets.DlgBox;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyPressEvent;
-import com.google.gwt.event.dom.client.KeyPressHandler;
-import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
-import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.TextArea;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.datepicker.client.DateBox;
-import com.google.gwt.user.datepicker.client.DatePicker;
 
 /**
  * 
@@ -82,7 +66,7 @@ import com.google.gwt.user.datepicker.client.DatePicker;
  *
  */
 public class EditShareDlg extends DlgBox
-	implements EditSuccessfulHandler, KeyPressHandler
+	implements EditSuccessfulHandler
 {
 	private VibeFlowPanel m_rightsPanelForMultiEdit;
 	private VibeFlowPanel m_rightsPanelForSingleEdit;
@@ -109,17 +93,12 @@ public class EditShareDlg extends DlgBox
 	private ArrayList<GwtShareItem> m_listOfShareItems;
 	
 	// Data members used with share expiration
-	private ListBox m_expiresListbox;
-	private TextBox m_expiresAfterTextBox;
-	private TZDateBox m_dateBox;
-	private VibeFlowPanel m_expiresOnPanel;
-	private VibeFlowPanel m_expiresAfterPanel;
+	private ShareExpirationWidget m_expirationWidget;
 	
 	// Data members used with share note
 	private TextArea m_noteTextArea;
 
 	
-	private static long MILLISEC_IN_A_DAY = 86400000;
 	private static String VIEWER = "viewer";
 	private static String EDITOR = "editor";
 	private static String CONTRIBUTOR = "contributor";
@@ -183,128 +162,12 @@ public class EditShareDlg extends DlgBox
 	private void createExpirationContent( VibeFlowPanel panel )
 	{
 		VibeFlowPanel mainPanel;
-		VibeFlowPanel expiresPanel;
-		FlexTable mainTable;
-		int col;
 
 		mainPanel = new VibeFlowPanel();
 		mainPanel.addStyleName( "editShareDlg_expirationPanel" );
 
-		// Give this panel a fixed width so the dialog doesn't resize after the user
-		// selects an expiration type.
-		mainPanel.setWidth( "300px" );
-		mainPanel.setHeight( "40px" );
-
-		mainTable = new FlexTable();
-		mainTable.addStyleName( "shareExpirationDlg_table" );
-		mainPanel.add( mainTable );
-		
-		col = 0;
-		
-		mainTable.setText( 0, col, GwtTeaming.getMessages().shareExpirationDlg_expiresLabel() );
-		++col;
-		
-		// Create a select control for specifying when the share expires
-		{
-			// Add the listbox where the user can select how the share expires, never, after or on
-			m_expiresListbox = new ListBox( false );
-			m_expiresListbox.setVisibleItemCount( 1 );
-			
-			m_expiresListbox.addChangeHandler( new ChangeHandler()
-			{
-				@Override
-				public void onChange( ChangeEvent event )
-				{
-					Scheduler.ScheduledCommand cmd;
-					
-					cmd = new Scheduler.ScheduledCommand()
-					{
-						@Override
-						public void execute()
-						{
-							handleExpirationTypeSelected();
-						}
-					};
-					Scheduler.get().scheduleDeferred( cmd );
-				}
-			} );
-			mainTable.setWidget( 0, col, m_expiresListbox );
-			++col;
-		}
-		
-		expiresPanel = new VibeFlowPanel();
-		mainTable.setWidget( 0, col, expiresPanel );
-		++col;
-		
-		// Create the controls needed to specify "expires on"
-		{
-			FlexTable table;
-			DateTimeFormat dateFormat;
-			ImageResource imgResource;
-			Image img;
-			long offset;
-
-			m_expiresOnPanel = new VibeFlowPanel();
-			m_expiresOnPanel.addStyleName( "shareExpirationDlg_expiresOnPanel" );
-			m_expiresOnPanel.setVisible( false );
-			
-			table = new FlexTable();
-			table.addStyleName( "shareExpirationDlg_expiresOnTable" );
-			m_expiresOnPanel.add( table );
-			
-			dateFormat = DateTimeFormat.getFormat( PredefinedFormat.DATE_SHORT );
-			m_dateBox = new TZDateBox( new DatePicker(), (-1), new DateBox.DefaultFormat( dateFormat ) );
-			offset = GwtTeaming.m_requestInfo.getTimeZoneOffsetHour() * 60 * 60 * 1000;
-			m_dateBox.setTZOffset( offset );
-			m_dateBox.getDateBox().getTextBox().setVisibleLength( 8 );
-			table.setWidget( 0, 0, m_dateBox );
-			
-			imgResource = GwtTeaming.getImageBundle().calDatePicker();
-			img = new Image( imgResource );
-			img.addStyleName( "shareExpirationDlg_expiresOnImg" );
-			img.addClickHandler( new ClickHandler()
-			{
-				@Override
-				public void onClick( ClickEvent event )
-				{
-					Scheduler.ScheduledCommand cmd;
-					
-					cmd = new Scheduler.ScheduledCommand()
-					{
-						@Override
-						public void execute()
-						{
-							invokeDatePicker();
-						}
-					};
-					Scheduler.get().scheduleDeferred( cmd );
-				}
-			} );
-			table.setWidget( 0, 1, img );
-			
-			expiresPanel.add( m_expiresOnPanel );
-		}
-		
-		// Create the controls needed to specify "expires after"
-		{
-			FlexTable table;
-			
-			m_expiresAfterPanel = new VibeFlowPanel();
-			m_expiresAfterPanel.addStyleName( "shareExpirationDlg_expiresAfterPanel" );
-			m_expiresAfterPanel.setVisible( false );
-			
-			table = new FlexTable();
-			table.addStyleName( "shareExpirationDlg_expiresAfterTable" );
-			m_expiresAfterTextBox = new TextBox();
-			m_expiresAfterTextBox.setVisibleLength( 4 );
-			m_expiresAfterTextBox.addStyleName( "shareExpirationDlg_expiresAfterTextBox" );
-			m_expiresAfterTextBox.addKeyPressHandler( this );
-			table.setWidget( 0, 0, m_expiresAfterTextBox );
-			table.setText( 0, 1, GwtTeaming.getMessages().shareExpirationDlg_days() );
-			m_expiresAfterPanel.add( table );
-			
-			expiresPanel.add( m_expiresAfterPanel );
-		}
+		m_expirationWidget = new ShareExpirationWidget();
+		mainPanel.add( m_expirationWidget );
 
 		panel.add( mainPanel );
 	}
@@ -508,51 +371,6 @@ public class EditShareDlg extends DlgBox
 	}
 	
 	/**
-	 * Show/hide the appropriate controls based on the selected expiration type.
-	 */
-	private void danceDlg( boolean setFocus )
-	{
-		int selectedIndex;
-		
-		selectedIndex = m_expiresListbox.getSelectedIndex();
-		if ( selectedIndex >= 0 )
-		{
-			ShareExpirationType type;
-			
-			// Get the selected expiration type;
-			type = getSelectedExpirationType();
-
-			m_expiresAfterPanel.setVisible( false );
-			m_expiresOnPanel.setVisible( false );
-
-			if ( type != null )
-			{
-				switch ( type )
-				{
-				case AFTER_DAYS:
-					m_expiresAfterPanel.setVisible( true );
-					if ( setFocus )
-						m_expiresAfterTextBox.setFocus( true );
-					
-					break;
-					
-				case ON_DATE:
-					m_expiresOnPanel.setVisible( true );
-					if ( setFocus )
-						invokeDatePicker();
-					
-					break;
-					
-				case NEVER:
-				case UNKNOWN:
-				default:
-					break;
-				}
-			}
-		}
-	}
-	
-	/**
 	 * 
 	 */
 	@Override
@@ -592,47 +410,6 @@ public class EditShareDlg extends DlgBox
 	}
 	
 	/**
-	 * Return the number of days entered by the user.
-	 */
-	private Long getDaysFromDlg()
-	{
-		String value;
-		Long days = null;
-		
-		value = m_expiresAfterTextBox.getText();
-		if ( value != null && value.length() > 0 )
-		{
-			days = Long.parseLong( value );
-		}
-		
-		return days;
-	}
-	
-	
-	/**
-	 * Return the expiration date entered by the user.
-	 */
-	private Long getExpirationDateFromDlg()
-	{
-		Long value;
-		
-		value = m_dateBox.getValue();
-		if ( value == -1 )
-			value = null;
-		
-		if ( value != null )
-		{
-			// Have the share expire at 23:59:59 on the selected day
-			value += (MILLISEC_IN_A_DAY - 1000);
-
-			// Convert the time to GMT
-			value += (GwtTeaming.m_requestInfo.getTimeZoneOffsetHour() * 60 * 60 * 1000);
-		}
-		
-		return value;
-	}
-	
-	/**
 	 * Return the widget that should get the focus when the dialog is shown. 
 	 */
 	@Override
@@ -649,61 +426,6 @@ public class EditShareDlg extends DlgBox
 		return m_listOfShareItems;
 	}
 	
-	/**
-	 * Return the selected expiration type
-	 */
-	private ShareExpirationType getSelectedExpirationType()
-	{
-		int selectedIndex;
-		
-		selectedIndex = m_expiresListbox.getSelectedIndex();
-		if ( selectedIndex >= 0 )
-		{
-			String value;
-
-			value = m_expiresListbox.getValue( selectedIndex );
-			if ( value != null )
-			{
-				if ( value.equalsIgnoreCase( ShareExpirationType.AFTER_DAYS.toString() ) )
-					return ShareExpirationType.AFTER_DAYS;
-				
-				if ( value.equalsIgnoreCase( ShareExpirationType.NEVER.toString() ) )
-					return ShareExpirationType.NEVER;
-				
-				if ( value.equalsIgnoreCase( ShareExpirationType.ON_DATE.toString() ) )
-					return ShareExpirationType.ON_DATE;
-			}
-		}
-		
-		return null;
-	}
-
-	/**
-	 * 
-	 */
-	private Long getToday()
-	{
-		Date today;
-		Long value;
-		
-		today = new Date();
-		value = today.getTime();
-		
-		// Convert the time to GMT
-		value += (GwtTeaming.m_requestInfo.getTimeZoneOffsetHour() * 60 * 60 * 1000);
-		
-		return value;
-	}
-
-	/**
-	 * This method gets called when the user selects the expiration type, never, on, after.
-	 * Show/hide the appropriate controls based on the selected expiration type.
-	 */
-	private void handleExpirationTypeSelected()
-	{
-		danceDlg( true );
-	}
-
 	/**
 	 * Initialize the controls in the dialog with the values from the properties
 	 */
@@ -735,8 +457,6 @@ public class EditShareDlg extends DlgBox
 		initRightsControls( listOfShareItems, highestRightsPossible );
 		initExpirationControls( listOfShareItems );
 		initNoteControls( listOfShareItems );
-
-		danceDlg( false );
 	}
 	
 	/**
@@ -744,60 +464,22 @@ public class EditShareDlg extends DlgBox
 	 */
 	private void initExpirationControls( ArrayList<GwtShareItem> listOfShares )
 	{
-		GwtTeamingMessages messages;
-		
 		if ( listOfShares == null || listOfShares.size() == 0 )
 			return;
+		
+		m_expirationWidget.addStandardExpirationTypes();
 
-		messages = GwtTeaming.getMessages();
-		
-		m_expiresListbox.clear();
-		
-		m_expiresListbox.addItem( 
-							messages.shareExpirationDlg_expiresNever(),
-							ShareExpirationType.NEVER.toString() );
-		m_expiresListbox.addItem(
-							messages.shareExpirationDlg_expiresOn(),
-							ShareExpirationType.ON_DATE.toString() );
-		m_expiresListbox.addItem(
-							messages.shareExpirationDlg_expiresAfter(),
-							ShareExpirationType.AFTER_DAYS.toString() );
-	
 		if ( listOfShares.size() > 1 )
 		{
-			m_expiresListbox.addItem( messages.editShareDlg_leaveUnchanged(), LEAVE_UNCHANGED );
-			m_expiresListbox.setSelectedIndex( m_expiresListbox.getItemCount()-1 );
+			m_expirationWidget.addDoNotModify();
+			m_expirationWidget.init( null );
 		}
 		else
 		{
 			ShareExpirationValue expirationValue;
-			ShareExpirationType expirationType;
-			
+
 			expirationValue = listOfShares.get( 0 ).getShareExpirationValue();
-			expirationType = expirationValue.getExpirationType();
-			
-			// Select the appropriate expiration type.
-			GwtClientHelper.selectListboxItemByValue( m_expiresListbox, expirationType.toString() );
-			
-			switch (expirationType)
-			{
-			case AFTER_DAYS:
-				Long value;
-				
-				value = expirationValue.getValue();
-				if ( value != null )
-					m_expiresAfterTextBox.setText( value.toString() );
-				break;
-			
-			case ON_DATE:
-				m_dateBox.setValue( expirationValue.getValue() );
-				break;
-			
-			case NEVER:
-			case UNKNOWN:
-			default:
-				break;
-			}
+			m_expirationWidget.init( expirationValue );
 		}
 	}
 	
@@ -1031,84 +713,15 @@ public class EditShareDlg extends DlgBox
 	}
 	
 	/**
-	 * Invoke the date picker control.
-	 */
-	private void invokeDatePicker()
-	{
-		m_dateBox.getDateBox().showDatePicker();
-	}
-
-	/**
-	 * This method gets called when the user types in the "number of days after" text box.
-	 * We only allow the user to enter numbers.
-	 */
-	@Override
-	public void onKeyPress( KeyPressEvent event )
-	{
-    	final TextBox txtBox;
-        int keyCode;
-    	Object source;
-
-    	// Make sure we are dealing with a text box.
-    	source = event.getSource();
-
-    	if ( source instanceof TextBox )
-    		txtBox = (TextBox) source;
-    	else
-    		txtBox = null;
-
-        // Get the key the user pressed
-        keyCode = event.getNativeEvent().getKeyCode();
-        
-        // Only let the user enter a valid digit.
-        if ( GwtClientHelper.isKeyValidForNumericField( event.getCharCode(), keyCode ) == false )
-        {
-        	// Make sure we are dealing with a text box.
-        	if ( txtBox != null )
-        	{
-        		// Suppress the current keyboard event.
-        		txtBox.cancelKey();
-        	}
-        }
-	}
-
-	/**
 	 * Update the GwtShareItem with the expiration value from the dialog
 	 */
 	private void saveExpirationValue( GwtShareItem shareItem )
 	{
-		ShareExpirationType expirationType;
-
-		expirationType = getSelectedExpirationType();
+		ShareExpirationValue expirationValue;
 		
-		if ( expirationType != null )
-		{
-			ShareExpirationValue value;
-
-			value = new ShareExpirationValue();
-
-			if ( expirationType == ShareExpirationType.AFTER_DAYS )
-			{
-				Long days;
-				
-				// The user selected "after days".
-				// Did they enter the number of days?
-				days = getDaysFromDlg();
-				value.setValue( days );
-			}
-			else if ( expirationType == ShareExpirationType.ON_DATE )
-			{
-				Long date;
-				
-				// The user selected "on date".
-				// Did they enter a date?
-				date = getExpirationDateFromDlg();
-				value.setValue( date );
-			}
-			
-			value.setType( expirationType );
-			shareItem.setShareExpirationValue( value );
-		}
+		expirationValue = m_expirationWidget.getExpirationValue();
+		if ( expirationValue != null )
+			shareItem.setShareExpirationValue( expirationValue );
 	}
 		
 	
@@ -1320,52 +933,7 @@ public class EditShareDlg extends DlgBox
 	 */
 	private boolean validateExpirationValue()
 	{
-		ShareExpirationType expirationType;
-
-		expirationType = getSelectedExpirationType();
-		
-		if ( expirationType == ShareExpirationType.AFTER_DAYS )
-		{
-			Long days;
-			
-			// The user selected "after days".
-			// Did they enter the number of days?
-			days = getDaysFromDlg();
-			if ( days == null )
-			{
-				// No, tell the user they need to enter a number of days.
-				Window.alert( GwtTeaming.getMessages().shareExpirationDlg_noDaysEntered() );
-				m_expiresAfterTextBox.setFocus( true );
-				return false;
-			}
-		}
-		else if ( expirationType == ShareExpirationType.ON_DATE )
-		{
-			Long date;
-			Long today;
-			
-			// The user selected "on date".
-			// Did they enter a date?
-			date = getExpirationDateFromDlg();
-			if ( date == null )
-			{
-				// No, tell the user they need to enter the expiration date.
-				Window.alert( GwtTeaming.getMessages().shareExpirationDlg_noDateEntered() );
-				return false;
-			}
-			
-			// Did the user enter a date from the past?
-			today = getToday();
-			if ( date < today )
-			{
-				// Yes, tell them not to do that.
-				Window.alert( GwtTeaming.getMessages().shareExpirationDlg_cantEnterPriorDate() );
-				return false;
-			}
-		}
-		
-		// If we get here everything is valid.
-		return true;
+		return m_expirationWidget.validateExpirationValue();
 	}
 	
 	/**
