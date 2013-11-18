942c602449e1f6aa402a513d3e7a922754016e91
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2013-11-18 20:38:42 +0000

    Bug:849642 Reviewer:Jong Comment:Make group membership information available to FAMT side when calling getPermissionName() method

--- ssf/main/src/org/kablink/teaming/fi/connection/acl/AclItemPrincipalMapper.java
+++ ssf/main/src/org/kablink/teaming/fi/connection/acl/AclItemPrincipalMapper.java
@@ -32,6 +32,7 @@
  */
 package org.kablink.teaming.fi.connection.acl;
 
+import java.util.List;
 import java.util.Map;
 
 import org.kablink.teaming.domain.ResourceDriverConfig;
@@ -69,4 +70,13 @@ public interface AclItemPrincipalMapper {
 	 * @throws AclItemPrincipalMappingException
 	 */
 	public Map<String,String> toFileSystemPrincipalId(User vibeUser) throws AclItemPrincipalMappingException;
+	
+	/**
+	 * Returns the file system IDs of the groups that the specified Vibe user is a member of.
+	 * 
+	 * @param vibeUser
+	 * @return
+	 * @throws AclItemPrincipalMappingException
+	 */
+	public Map<String, List<String>> toFileSystemGroupIds(User vibeUser) throws AclItemPrincipalMappingException;
 }
