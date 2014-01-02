e9bb9f1619e8f8561c705f3be5933103965f4229
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-01-02 22:16:14 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Filr 1.1 development.  {180th} 1. Got refresh working when an activity stream is selected.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/GwtMainPage.java
    M    gwt/src/org/kablink/teaming/gwt/client/GwtTeaming.java
    M    gwt/src/org/kablink/teaming/gwt/client/RequestInfo.java
    M    gwt/src/org/kablink/teaming/gwt/client/util/HistoryHelper.java
    M    gwt/src/org/kablink/teaming/gwt/client/util/HistoryInfoCallback.java
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/ContentControl.java
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/WorkspaceTreeControl.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtHistoryHelper.java
    M    web/docroot/WEB-INF/jsp/common/GwtRequestInfo.jsp

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/util/HistoryHelper.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/util/HistoryHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -65,7 +65,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  * @author drfoster@novell.com
  */
 public class HistoryHelper {
-	private final static boolean ENABLE_BROWSER_HISTORY	= false;	//! DRF (20131231):  Leave false on checkin until it's all working.
+	private final static boolean ENABLE_BROWSER_HISTORY	= false;	//! DRF (20141231):  Leave false on checkin until it's all working.
 	
 	private final static String	HISTORY_MARKER			= "history";				// Marker appended to a URL with a history token so that we can relocate the URL during browser navigations.
 	private final static int	HISTORY_MARKER_LENGTH	= HISTORY_MARKER.length();	// Length of HISTORY_MARKER.
