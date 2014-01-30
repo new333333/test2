f370d02e11e09104ec0f00e5b7397d911a3693c2
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-01-30 22:44:42 +0000

    Bug:  861412 Reviewer:  Self Comments:  Fixed an issue with sorting Shared by/with Me.  The sorting needs to use the locale of the current user.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtReportsHelper.java
    M    main/src/org/kablink/teaming/web/util/MiscUtil.java

--- trunk/ssf/main/src/org/kablink/teaming/web/util/MiscUtil.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/util/MiscUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -58,6 +58,7 @@ import org.kablink.teaming.ObjectKeys;
 import org.kablink.teaming.comparator.StringComparator;
 import org.kablink.teaming.context.request.RequestContextHolder;
 import org.kablink.teaming.domain.*;
+import org.kablink.teaming.gwt.server.util.GwtServerHelper;
 import org.kablink.teaming.module.folder.FolderModule;
 import org.kablink.teaming.module.profile.ProfileModule;
 import org.kablink.teaming.module.zone.ZoneModule;
@@ -511,7 +512,7 @@ public final class MiscUtil
 	 * @return
 	 */
 	public static int safeSColatedCompare(String s1, String s2) {
-		Collator collator = Collator.getInstance();
+		Collator collator = Collator.getInstance(GwtServerHelper.getCurrentUser().getLocale());
 		collator.setStrength(Collator.IDENTICAL);
 		return
 			collator.compare(
