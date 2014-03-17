d7bc82a8ca5bdf210b118413e27272fba04cd52d
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-03-17 18:19:45 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Filr 1.1 development.  {235th} 1. Changed "Task Entry" terminology to "Task" throughout. 2. Fixed handling of ss_dialogClosed in ss_onSubmit().
    Files:
    M    main/src/org/kablink/teaming/web/util/ServerTaskLinkage.java
    M    samples/wsclient/src/org/kablink/teaming/samples/wsclient/TeamingServiceClientWithStub.java
    M    web/docroot/WEB-INF/messages/messages.properties
    M    web/docroot/js/common/ss_common.js
    M    web/docroot/js/common/ss_mobile.js

--- trunk/ssf/main/src/org/kablink/teaming/web/util/ServerTaskLinkage.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/util/ServerTaskLinkage.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2011 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2011 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -40,9 +40,9 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.kablink.teaming.ObjectKeys;
 import org.kablink.teaming.module.binder.BinderModule;
-
 
 /**
  * Class used to represent order and hierarchy for the tasks within
@@ -346,7 +346,7 @@ public class ServerTaskLinkage {
 			Long newEntryId = entryIdMap.get(entryId);
 			if (null == newEntryId) {
 				// No!  Log an error and remove it from the linkage.
-				m_logger.error("Task entry " + entryId + " missing from entry ID map, dropped from folder's linkage.");
+				m_logger.error("Task " + entryId + " missing from entry ID map, dropped from folder's linkage.");
 				tlList.remove(i);
 			}
 			else {
@@ -447,7 +447,7 @@ public class ServerTaskLinkage {
 	}
 	
 	/**
-	 * Validates the task entry IDs referenced by a ServerTaskLinkage.
+	 * Validates the task IDs referenced by a ServerTaskLinkage.
 	 * 
 	 * @param entryIds
 	 */
