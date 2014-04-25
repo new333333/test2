668eeedcb3e54ed7305b00c45f24010556985687
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-04-25 16:37:36 +0000

    Bug:  857032,875005,874875 Reviewer:  Self Comments:  Pending checkins.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/HoverHintPopup.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtServerHelper.java
    M    main/src/org/kablink/teaming/dao/impl/CoreDaoImpl.java
    M    main/src/org/kablink/teaming/domain/FileItem.java
    M    main/src/org/kablink/teaming/module/file/impl/FileModuleImpl.java
    M    main/src/org/kablink/teaming/module/shared/XmlUtils.java
    M    main/src/org/kablink/teaming/servlet/forum/AbstractReadFileController.java
    M    util/src/org/kablink/util/Validator.java

--- trunk/ssf/main/src/org/kablink/teaming/module/file/impl/FileModuleImpl.java
+++ trunk/ssf/main/src/org/kablink/teaming/module/file/impl/FileModuleImpl.java
@@ -2054,7 +2054,7 @@ public class FileModuleImpl extends CommonDependencyInjection implements FileMod
    		   	if ((entry.getParentBinder() != null) && entry.getParentBinder().isUniqueTitles()) getCoreDao().updateTitle(entry.getParentBinder(), entry, oldTitle, entry.getNormalTitle());
 		} 
    		//add file name so not null
-    	if (Validator.isNull(entry.getTitle())) entry.setTitle(fAtt.getFileItem().getName());
+    	if (Validator.isEmptyString(entry.getTitle())) entry.setTitle(fAtt.getFileItem().getName());
     	
     	if(versionCreated) {
     		// The content was committed creating a new version. Increment disk usage for the user.
@@ -3170,7 +3170,8 @@ public class FileModuleImpl extends CommonDependencyInjection implements FileMod
 		}
     }
     
-    public boolean isLockExpired(FileLock lock) {
+    @Override
+	public boolean isLockExpired(FileLock lock) {
     	// Note that we take additional 
 		// "allowance" value into consideration when computing the
 		// expiration date used for the comparison. This is to 
@@ -3178,11 +3179,11 @@ public class FileModuleImpl extends CommonDependencyInjection implements FileMod
 		// or data update request following initial lock request is
 		// delayed significantly that it actually arrives at the server
 		// after the initial lock has expired. Although expected to be
-		// rare, this situation can occur by environmental flunctuations
+		// rare, this situation can occur by environmental fluctuations
 		// such as unusual network latency or extremely high system 
 		// load, etc. To prevent undesired version proliferation from
-		// occuring as result, we give each lock some reasonable 
-		// allowance (ie, extended life).
+		// occurring as result, we give each lock some reasonable 
+		// allowance (i.e., extended life).
     	
     	return (lock.getExpirationDate().getTime() + 
     			this.getLockExpirationAllowanceMilliseconds() <= System.currentTimeMillis());
