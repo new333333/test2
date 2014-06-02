4064b9745b321232a2010b2bc21b955475e6bc4a
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-06-02 21:05:25 +0000

    Bug:  800816 Reviewer:  Self Comments:  Added a 'Rating' column to File and Mirrored File folders when configuring them in Vibe.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/mainmenu/FolderColumnsConfigDlg.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtViewHelper.java

--- ssf/gwt/src/org/kablink/teaming/gwt/client/mainmenu/FolderColumnsConfigDlg.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/mainmenu/FolderColumnsConfigDlg.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -59,13 +59,13 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.InputElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
@@ -100,7 +100,7 @@ public class FolderColumnsConfigDlg extends DlgBox implements EditSuccessfulHand
 	private CheckBox						m_folderDefaultCheckBox;	// Set the folder default columns.
 	private FlexCellFormatter				m_folderColumnsGridCF;		//
 	private FlexTable						m_folderColumnsGrid;		// Once displayed, the table of folder columns.
-	private GwtTeamingMainMenuImageBundle	m_images;	//
+	private GwtTeamingMainMenuImageBundle	m_images;					//
 	private GwtTeamingMessages				m_messages;					// Access to the GWT UI messages.
 	private int								m_folderColumnsListCount;	// Count of items in m_folderColumnsList.
 	private List<FolderColumn>				m_folderColumnsList;		// List of selected folder column items.
