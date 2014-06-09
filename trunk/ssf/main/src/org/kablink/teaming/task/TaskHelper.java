1c890c012139fc4ab40ce92e6a02f4942cc18021
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-06-09 19:52:25 +0000

    Bug:  795135 Reviewer:  Self Comments:  Don't change completed date on a task when uploading a file attachment to it.
    Files:
    M    task/TaskHelper.java

--- trunk/ssf/main/src/org/kablink/teaming/task/TaskHelper.java
+++ trunk/ssf/main/src/org/kablink/teaming/task/TaskHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -47,10 +47,13 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.dom4j.Document;
 import org.dom4j.Element;
+
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+
 import org.kablink.teaming.ObjectKeys;
 import org.kablink.teaming.context.request.RequestContextHolder;
 import org.kablink.teaming.domain.Binder;
@@ -81,6 +84,7 @@ import org.kablink.teaming.web.util.ServerTaskLinkage.ServerTaskLink;
 import org.kablink.teaming.web.util.WebHelper;
 import org.kablink.teaming.web.util.ListFolderHelper.ModeType;
 import org.kablink.util.search.Constants;
+
 import org.springframework.web.portlet.bind.PortletRequestBindingException;
 
 /**
@@ -249,7 +253,7 @@ public class TaskHelper {
 	 * 
 	 * @return
 	 */
-	@SuppressWarnings({"unchecked", "incomplete-switch"})
+	@SuppressWarnings({"unchecked", "incomplete-switch", "deprecation"})
 	public static SearchFilter buildSearchFilter(FilterType filterType, ListFolderHelper.ModeType modeType, Map model, Binder binder) {
 		SearchFilter searchFilter = new SearchFilter(true);
 		
@@ -736,7 +740,7 @@ public class TaskHelper {
 					c = COMPLETED_100;
 				}
 			}
-			String cV = validateCompleted(c);
+			String cV = ((null == c) ? null : validateCompleted(c));
 			if (null != cV) {
 				c = cV;
 			}
