367121a8f4169593d901498c9de4d17717f56cb2
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-06-17 16:13:48 +0000

    Bug:  882897 Reviewer:  Self Comments:  Fixed handling of: 1) Author being disabled; and 2) Author being File Sync Agent.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/datatable/PresenceCell.java
    M    gwt/src/org/kablink/teaming/gwt/server/GwtRpcServiceImpl.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtViewHelper.java

--- ssf/gwt/src/org/kablink/teaming/gwt/client/datatable/PresenceCell.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/datatable/PresenceCell.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -300,7 +300,9 @@ public class PresenceCell extends AbstractCell<PrincipalInfo> {
 		Label presenceLabel = new Label(pi.getTitle());
 		presenceLabel.addStyleName("vibe-dataTablePresence-label");
 		presenceLabel.addStyleName(pi.isUserDisabled() ? "vibe-dataTablePresence-disabled" : "vibe-dataTablePresence-enabled");
-		if (!(pi.isUserWSInTrash())) {
+		if ((!(pi.isUserWSInTrash())) && (!(pi.isUserDisabled()))) {
+			// ...unless the user's workspace is in the trash or the
+			// ...user is disabled...
 			presenceLabel.getElement().setAttribute(VibeDataTableConstants.CELL_WIDGET_ATTRIBUTE, VibeDataTableConstants.CELL_WIDGET_PRESENCE_LABEL);
 		}
 		String hover = pi.getEmailAddress();
