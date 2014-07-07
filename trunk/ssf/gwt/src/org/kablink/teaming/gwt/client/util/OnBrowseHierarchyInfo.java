a579870cd318681c6e140150530cf603a4067cb0
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-07-07 18:57:34 +0000

    Bug:  721022 Reviewer:  Self Comments:  When clicking on the bread crumb browser button while the bread crumb tree is open, simply close it.  Don't close and then reopen it.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/GwtMainPage.java
    M    gwt/src/org/kablink/teaming/gwt/client/event/BrowseHierarchyExitEvent.java
    M    gwt/src/org/kablink/teaming/gwt/client/util/OnBrowseHierarchyInfo.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/util/OnBrowseHierarchyInfo.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/util/OnBrowseHierarchyInfo.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -32,6 +32,7 @@
  */
 package org.kablink.teaming.gwt.client.util;
 
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -95,4 +96,17 @@ public class OnBrowseHierarchyInfo {
 	 */
 	public int getLeft() {return ((null != m_browseWidget) ? m_browseWidget.getAbsoluteLeft()                : m_browseItem.getAbsoluteLeft());                               }
 	public int getTop()  {return ((null != m_browseWidget) ? m_browseWidget.getElement().getAbsoluteBottom() : m_browseItem.getParentMenu().getElement().getAbsoluteBottom());}
+	
+	/**
+	 * Returns the Element that running the hierarchy browser.
+	 * 
+	 * @return
+	 */
+	public Element getBrowseHierarchyElement() {
+		Element reply;
+		if      (null != m_browseWidget) reply = m_browseWidget.getElement();
+		else if (null != m_browseItem)   reply = m_browseItem.getElement();
+		else                             reply = null;
+		return reply;
+	}
 }
