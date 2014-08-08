464ecf6ef022507ed8589054f29736042ead3e71
jwootton <jwootton@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-08-08 19:02:19 +0000

    Bug: 891024 Reviewer: Jay Wootton Comment: Set the format of the DateBox so it knows how to display the selected date.

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/TZDateBox.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/TZDateBox.java
@@ -56,6 +56,7 @@ import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.datepicker.client.DateBox;
@@ -106,6 +107,16 @@ public class TZDateBox extends Composite implements HasValue<Long>, HasValueChan
                 fireValueChangeEvent(date2tz(event.getValue()));
             }
         });
+        
+        {
+        	DateTimeFormat dateFormat;
+        	DateBox.Format format;
+
+        	dateFormat = GwtClientHelper.getShortDateFormat();
+
+        	format = new DateBox.DefaultFormat( dateFormat );
+        	m_datebox.setFormat( format );        	
+        }
        
         initWidget(m_datebox);
     }
