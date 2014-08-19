5d34e7f7b616370c8873e00dbc82d184e7c4d272
petermhurley <petermhurley@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-08-19 14:07:12 +0000

    Bug: 886982 Reviewer: Peter Comment: Remove "Filr" specific options from the "Workspace and Folder Templates" dialog

--- trunk/ssf/main/src/org/kablink/teaming/security/function/WorkAreaOperation.java
+++ trunk/ssf/main/src/org/kablink/teaming/security/function/WorkAreaOperation.java
@@ -45,6 +45,7 @@ import org.kablink.teaming.InternalException;
 import org.kablink.teaming.module.binder.BinderModule.BinderOperation;
 import org.kablink.teaming.module.folder.FolderModule.FolderOperation;
 import org.kablink.teaming.util.SPropsUtil;
+import org.kablink.teaming.util.Utils;
 
 /**
  * <code>Operation</code> class defines an operation that can be performed
@@ -142,8 +143,7 @@ public class WorkAreaOperation {
     private boolean zoneWide=false;
     private WorkAreaOperation(String name) {
 	    this.name = name;
-    	if (!"viewBinderTitle".equals(name) || 
-    			SPropsUtil.getBoolean("accessControl.viewBinderTitle.enabled", false)) {
+    	if (checkIfOperationValid(name)) {
 		    Instances.put(name, this);
     	}
     }
@@ -151,8 +151,25 @@ public class WorkAreaOperation {
     private WorkAreaOperation(String name, boolean zoneWide) {
         this.name = name;
         this.zoneWide = zoneWide;
-    	Instances.put(name, this);
+        if (checkIfOperationValid(name)) {
+        	Instances.put(name, this);
+        }
     }
+    
+    private boolean checkIfOperationValid(String name) {
+    	if ("viewBinderTitle".equals(name) && 
+    			!SPropsUtil.getBoolean("accessControl.viewBinderTitle.enabled", false)) {
+    		return false;
+    	}
+    	if (!Utils.checkIfFilr() && !SPropsUtil.getBoolean("keepFilrRolesAndRightsInVibe", false)) {
+    		//Remove the Filr specific rights if not Filr (unless a developer wants them preserved)
+        	if ("allowAccessNetFolder".equals(name)) {
+        		return false;
+        	}
+    	}
+    	return true;
+    }
+    
     public String getName() {
         return name;
     }
