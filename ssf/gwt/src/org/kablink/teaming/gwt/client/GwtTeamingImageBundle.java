ee7724eced8d0999d083f9a46ff803bed29b6a50
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-09-09 12:12:18 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Vibe Hudson development.  {25th} 1. New Shared All and Share Files button images for the entry menu in    the Shared with/by Me views from Lynn.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/GwtTeamingImageBundle.java
    M    gwt/src/org/kablink/teaming/gwt/client/binderviews/EntryMenuPanel.java
    D    gwt/src/org/kablink/teaming/gwt/public/images/sharedAll.gif
    A  + gwt/src/org/kablink/teaming/gwt/public/images/sharedAll.png
    D    gwt/src/org/kablink/teaming/gwt/public/images/sharedFiles.gif
    A  + gwt/src/org/kablink/teaming/gwt/public/images/sharedFiles.png

--- ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeamingImageBundle.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/GwtTeamingImageBundle.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -585,11 +585,11 @@ public interface GwtTeamingImageBundle extends ClientBundle
 	public ImageResource resumeActivityStream();
 
 	@ImageOptions(repeatStyle = RepeatStyle.Both)
-	@Source("org/kablink/teaming/gwt/public/images/sharedAll.gif")
+	@Source("org/kablink/teaming/gwt/public/images/sharedAll.png")
 	public ImageResource sharedAll();
 	
 	@ImageOptions(repeatStyle = RepeatStyle.Both)
-	@Source("org/kablink/teaming/gwt/public/images/sharedFiles.gif")
+	@Source("org/kablink/teaming/gwt/public/images/sharedFiles.png")
 	public ImageResource sharedFiles();
 	
 	@ImageOptions(repeatStyle = RepeatStyle.Both)
@@ -819,6 +819,4 @@ public interface GwtTeamingImageBundle extends ClientBundle
 	@ImageOptions(repeatStyle = RepeatStyle.Both)
 	@Source("org/kablink/teaming/gwt/public/images/list-item-arrow.gif")
 	public ImageResource listItemArrow();
-	
-
 }// end GwtTeamingImageBundle
