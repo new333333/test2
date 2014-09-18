9f9d0050e08d77f941be136355fac4a0dfc76439
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-09-18 20:57:17 +0000

    Bug:  829244 Reviewer:  Self Comments:  Changed the JSP based user find widget to not return disabled users.
    Files:
    M    main/src/org/kablink/teaming/portlet/binder/TypeToFindAjaxController.java

--- trunk/ssf/main/src/org/kablink/teaming/portlet/binder/TypeToFindAjaxController.java
+++ trunk/ssf/main/src/org/kablink/teaming/portlet/binder/TypeToFindAjaxController.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2012 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2012 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -298,6 +298,7 @@ public class TypeToFindAjaxController extends SAbstractController {
 				model.put(WebKeys.ENTRIES, searchEntries);
 				model.put(WebKeys.SEARCH_TOTAL_HITS, searchHits);
 			} else if (findType.equals(WebKeys.FIND_TYPE_USER)) {
+	    	    options.put(ObjectKeys.SEARCH_IS_ENABLED_USERS, Boolean.TRUE);	// Type to find should only return enabled users.
 	    	    if (!user.isSuper()) {
 	     		   options.put(ObjectKeys.SEARCH_FILTER_AND, SearchUtils.buildExcludeFilter(org.kablink.util.search.Constants.HIDDEN_FROM_FIND_USER_FIELD, "true"));
 	     	    }
