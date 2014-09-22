8df6f4cd37693aa0fb24dd5093fd3e98d2689f76
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-09-22 12:50:11 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Vibe Hudson development.  {44th} 1. Tweaked the GWT 'type to find' search widget to not return disabled    users.  This now matches the JSP based version.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtSearchHelper.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtSearchHelper.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtSearchHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -37,7 +37,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -45,6 +44,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.kablink.teaming.ObjectKeys;
 import org.kablink.teaming.domain.Binder;
 import org.kablink.teaming.domain.Description;
@@ -337,6 +337,10 @@ public class GwtSearchHelper
 			// Should we search for ldap containers?
 			if ( searchCriteria.getSearchForLdapContainers() == false )
 				searchTermFilter.addAndLdapContainerFilter( false );
+			
+			// Type to find should only return enabled users.
+    	    options.put(ObjectKeys.SEARCH_IS_ENABLED_USERS, Boolean.TRUE);
+    	    
 			break;
 
 		case GROUP:
