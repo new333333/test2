37d76d3a16aff8d0fa91347c148f72803a59afb1
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2014-10-29 12:29:00 +0000

    Bug:  898311 Reviewer:  Self Comments:  Ported the change to disable the OK push button during a long copy/move operation from the Filr 1.1.1 code based into the Filr 1.1.0 FTF code base.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/binderviews/CopyMoveEntriesDlg.java
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/DlgBox.java

--- ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/CopyMoveEntriesDlg.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/CopyMoveEntriesDlg.java
@@ -220,7 +220,8 @@ public class CopyMoveEntriesDlg extends DlgBox
 		// do if we've already been sending chunks or the source list
 		// contains more items then our threshold.)
 		boolean cmdIsChunkList = (cmd.getEntityIds() != sourceEntityIds);
-		if (cmdIsChunkList || ProgressDlg.needsChunking(sourceEntityIds.size())) {
+		boolean showProgress   = (cmdIsChunkList || ProgressDlg.needsChunking(sourceEntityIds.size()));
+		if (showProgress) {
 			// Yes!  If we're not showing the progress bar or panel
 			// yet...
 			if ((!(m_progressPanel.isVisible())) || (!(m_progressBar.isVisible()))) {
@@ -277,6 +278,9 @@ public class CopyMoveEntriesDlg extends DlgBox
 		// Do we have any entities to be copied/moved?
 		if (!(cmd.getEntityIds().isEmpty())) {
 			// Yes!  Perform the final move/copy.
+			if (!showProgress) {
+				showDlgBusySpinner();
+			}
 			copyMoveEntriesImpl(
 				cmd,
 				targetFolder,
@@ -305,6 +309,7 @@ public class CopyMoveEntriesDlg extends DlgBox
 				GwtClientHelper.handleGwtRPCFailure(
 					caught,
 					m_strMap.get(StringIds.RPC_FAILURE));
+				hideDlgBusySpinner();
 			}
 
 			@Override
@@ -389,6 +394,7 @@ public class CopyMoveEntriesDlg extends DlgBox
 							// Finally, save the target folder we just
 							// copied/moved to and close the dialog,
 							// we're done!
+							hideDlgBusySpinner();	// Innocuous if it's not showing.
 							m_recentDest = targetFolder;
 							hide();
 						}
@@ -619,6 +625,30 @@ public class CopyMoveEntriesDlg extends DlgBox
 		});
 	}
 	
+    /**
+     * Called after the EditSuccessfulHandler has been called by
+     * DlgBox.
+     * 
+     * Overrides the DlgBox.okBtnProcessingEnded() method.
+     */
+	@Override
+    protected void okBtnProcessingEnded() {
+		// Ignored!  The copy/move dialog is handling enabling and
+		// disabling of the OK button itself.
+    }
+    
+    /**
+     * Called before the EditSuccessfulHandler has been called by
+     * DlgBox.
+     * 
+     * Overrides the DlgBox.okBtnProcessingEnded() method.
+     */
+	@Override
+    protected void okBtnProcessingStarted() {
+		// Ignored!  The copy/move dialog is handling enabling and
+		// disabling of the OK button itself.
+    }
+    
 	/**
 	 * Called when the dialog is attached.
 	 * 
