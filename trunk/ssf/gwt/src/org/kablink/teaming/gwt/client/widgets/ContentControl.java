6453896dd0c6e839b70786c34f331f9bef0538c6
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-05 17:27:46 +0000

    Bug:  911797 Reviewer:  Self Comments:  Fixed handling of navigating to a user that's never logged in from the masthead search widget.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/ContentControl.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtViewHelper.java
    M    main/src/org/kablink/teaming/portlet/profile/ViewEntryController.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/ContentControl.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/ContentControl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -792,12 +792,19 @@ public class ContentControl extends Composite
 				{				
 					// Show the context asynchronously so that we can
 					// release the AJAX request ASAP.
-					ViewInfo vi = ((ViewInfo) response.getResponseData());
-					String overrideUrl = vi.getOverrideUrl();
 					String targetUrl;
-					if ( GwtClientHelper.hasString( overrideUrl ) )
-					     targetUrl = overrideUrl;
-					else targetUrl = url;
+					ViewInfo vi = ((ViewInfo) response.getResponseData());
+					if ( null == vi )
+					{
+						targetUrl = url;
+					}
+					else
+					{
+						String overrideUrl = vi.getOverrideUrl();
+						if ( GwtClientHelper.hasString( overrideUrl ) )
+						     targetUrl = overrideUrl;
+						else targetUrl = url;
+					}
 					setViewAsync( vi, targetUrl, instigator, historyAction, historySelectedMastheadCollection );
 				}//end onSuccess()
 			});
