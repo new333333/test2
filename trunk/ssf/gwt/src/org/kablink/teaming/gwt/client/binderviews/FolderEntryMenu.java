75ee2e31125c3bf6738e731379e8a3c139bec182
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-06 17:30:51 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Filr 1.1.1 development.  {9th} 1. Fixed a bogus debug alert for the More -> Permalink menu item in the    view details view.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/binderviews/FolderEntryMenu.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtMenuHelper.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/FolderEntryMenu.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/binderviews/FolderEntryMenu.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -92,7 +92,13 @@ public class FolderEntryMenu extends VibeFlowPanel {
 	private GwtTeamingMessages				m_messages;		// Access to Vibe's messages.
 	private List<ToolbarItem>				m_toolbarItems;	// Information about the entry being viewed's menus.
 	private VibeMenuBar						m_entryMenu;	// The menu bar that contains all the menu items.
-	
+
+	/**
+	 * Constructor method.
+	 * 
+	 * @param fec
+	 * @param toolbarItems
+	 */
 	public FolderEntryMenu(FolderEntryCallback fec, List<ToolbarItem> toolbarItems) {
 		// Initialize the super class...
 		super();
@@ -224,7 +230,7 @@ public class FolderEntryMenu extends VibeFlowPanel {
 						if (GwtClientHelper.hasString(binderIdS) && GwtClientHelper.hasString(entryIdS))
 						     eid = new EntityId(Long.parseLong(binderIdS), Long.parseLong(entryIdS), EntityId.FOLDER_ENTRY);
 						else eid = null;
-						GwtClientHelper.debugAlert("FolderEntryMenu.renderSimpleTBI( Manually created EntityID ):  Should this event pass an EntityId directly?");
+						GwtClientHelper.debugAlert("FolderEntryMenu.renderSimpleTBI( Manually created EntityID for event '" + simpleEvent.name() + "' ):  Should this event pass an EntityId directly?");
 					}
 
 					// Generate the specific event for this menu item...
