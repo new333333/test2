f727d478fbd8538aca8d1f1af5ce3cf441ecb155
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-06 17:30:51 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Filr 1.1.1 development.  {9th} 1. Fixed a bogus debug alert for the More -> Permalink menu item in the    view details view.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/binderviews/FolderEntryMenu.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtMenuHelper.java

--- ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtMenuHelper.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtMenuHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -4607,6 +4607,7 @@ public class GwtMenuHelper {
 			actionTBI = new ToolbarItem(PERMALINK);
 			markTBITitle(   actionTBI, "toolbar.menu.showPermalinks"     );
 			markTBIEvent(   actionTBI, TeamingEvents.SHOW_VIEW_PERMALINKS);
+			markTBIEntityId(actionTBI, fe                                );
 			dropdownTBI.addNestedItem(actionTBI);
 			
 			// If we added anything to the more toolbar...
