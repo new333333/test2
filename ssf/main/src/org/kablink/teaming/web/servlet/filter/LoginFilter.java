81fff1f1202899b9a39ac3701fdd15b9bfec5cf3
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-09 18:50:14 +0000

    Bug:  912103 Reviewer:  Self Comments:  Fixed handling of invitation confirmation URLs in LoginFilter when running in a clustered environment.
    Files:
    M    main/src/org/kablink/teaming/web/servlet/filter/LoginFilter.java
    M    main/src/org/kablink/teaming/web/util/WebUrlUtil.java

--- ssf/main/src/org/kablink/teaming/web/servlet/filter/LoginFilter.java
+++ ssf/main/src/org/kablink/teaming/web/servlet/filter/LoginFilter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -49,6 +49,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.kablink.teaming.ObjectKeys;
 import org.kablink.teaming.asmodule.zonecontext.ZoneContextHolder;
 import org.kablink.teaming.context.request.RequestContextHolder;
+import org.kablink.teaming.context.request.RequestContextUtil;
 import org.kablink.teaming.domain.AuthenticationConfig;
 import org.kablink.teaming.domain.Binder;
 import org.kablink.teaming.domain.Folder;
@@ -134,11 +135,35 @@ public class LoginFilter  implements Filter {
 				}
 
 				if(req.getQueryString() != null && req.getQueryString().contains(ExternalUserUtil.QUERY_FIELD_NAME_EXTERNAL_USER_ENCODED_TOKEN + "=")) {
-					// This might be a response from external user to an invitation. Should check and deal with it if so.
-					ExternalUserUtil.handleResponseToInvitationOrConfirmation(WebHelper.getRequiredSession(req), Http.getCompleteURL(req));
+					// If we don't have a request context for this
+					// thread...
+					boolean hasRC = (null != RequestContextHolder.getRequestContext());
+					if (!hasRC) {
+						// ...create one so the URL construction below
+						// ...works correctly.
+						RequestContextUtil.setThreadContext(req);
+					}
+					try {
+						// This might be a response from external user
+						// to an invitation.  Should check and deal
+						// with it if so.
+						ExternalUserUtil.handleResponseToInvitationOrConfirmation(
+							WebHelper.getRequiredSession(                req),
+							WebUrlUtil.getCompleteURLFromRequestForEmail(req));
+					}
+					finally {
+						// If we created a request context specifically
+						// for this thread...
+						if (!hasRC) {
+							// ...clear it out so things revert to
+							// ...where they stood when we started.
+							RequestContextHolder.clear();
+						}
+					}
 					
-					// This might be a response from external user to reset their password or verify that
-					// they reset their password.
+					// This might be a response from external user to
+					// reset their password or verify that they reset
+					// their password.
 					ExternalUserUtil.handleResponseToPwdReset(
 															WebHelper.getRequiredSession( req ),
 															Http.getCompleteURL( req ) );
