abd1b250208d9e4e9c984d70a7fe30bb14a27c62
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-15 14:11:46 +0000

    Bug:  912378 Reviewer:  Self Comments:  Don't show a comment bubble on entries that can't have comments.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/datatable/CommentsCell.java
    M    gwt/src/org/kablink/teaming/gwt/client/util/CommentsInfo.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtServerHelper.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtViewHelper.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/datatable/CommentsCell.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/datatable/CommentsCell.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -170,8 +170,9 @@ public class CommentsCell extends AbstractCell<CommentsInfo> implements CommentA
 	 */
 	@Override
 	public void render(Context context, CommentsInfo commentsInfo, SafeHtmlBuilder sb) {
-		// If we weren't given a CommentsInfo...
-		if ((null == commentsInfo) || commentsInfo.getEntityId().isBinder()) {
+		// If we weren't given a CommentsInfo or it's for binder or
+		// comments are disabled on the entity...
+		if ((null == commentsInfo) || commentsInfo.getEntityId().isBinder() || commentsInfo.isCommentsDisabled()) {
 			// ...bail.  Cell widgets can pass null to cells if the
 			// ...underlying data contains a null, or if the data
 			// ...arrives out of order.
