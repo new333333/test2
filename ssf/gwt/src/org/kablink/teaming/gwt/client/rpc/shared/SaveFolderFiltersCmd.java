f05bb79ed4d05d6f5670e7e3548bed1b83e91e74
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-16 20:54:09 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Vibe Hudson development.  {118th} 1. Fixed handling of copy filters so that the copied filter references    the target folder instead of the source.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/rpc/shared/SaveFolderFiltersCmd.java
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/CopyFiltersDlg.java
    M    gwt/src/org/kablink/teaming/gwt/server/GwtRpcServiceImpl.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtServerHelper.java

--- ssf/gwt/src/org/kablink/teaming/gwt/client/rpc/shared/SaveFolderFiltersCmd.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/rpc/shared/SaveFolderFiltersCmd.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -35,6 +35,7 @@ package org.kablink.teaming.gwt.client.rpc.shared;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.kablink.teaming.gwt.client.GwtFolder;
 import org.kablink.teaming.gwt.client.util.BinderInfo;
 
 /**
@@ -45,6 +46,7 @@ import org.kablink.teaming.gwt.client.util.BinderInfo;
  */
 public class SaveFolderFiltersCmd extends VibeRpcCmd {
 	private BinderInfo 			m_folderInfo;		//
+	private GwtFolder			m_sourceFolder;		//
 	private List<FolderFilter>	m_globalFilters;	//
 	private List<FolderFilter>	m_personalFilters;	//
 	
@@ -67,12 +69,13 @@ public class SaveFolderFiltersCmd extends VibeRpcCmd {
 	 * 
 	 * @param folderInfo
 	 */
-	public SaveFolderFiltersCmd(BinderInfo folderInfo) {
+	public SaveFolderFiltersCmd(BinderInfo folderInfo, GwtFolder sourceFolder) {
 		// Initialize this object...
 		this();
 
 		// ...and store the parameter.
-		setFolderInfo(folderInfo);
+		setFolderInfo(  folderInfo  );
+		setSourceFolder(sourceFolder);
 	}
 	
 	/**
@@ -81,6 +84,7 @@ public class SaveFolderFiltersCmd extends VibeRpcCmd {
 	 * @return
 	 */
 	public BinderInfo         getFolderInfo()      {return m_folderInfo;     }
+	public GwtFolder          getSourceFolder()    {return m_sourceFolder;   }
 	public List<FolderFilter> getGlobalFilters()   {return m_globalFilters;  }
 	public List<FolderFilter> getPersonalFilters() {return m_personalFilters;}
 
@@ -89,7 +93,8 @@ public class SaveFolderFiltersCmd extends VibeRpcCmd {
 	 * 
 	 * @param
 	 */
-	public void setFolderInfo(    BinderInfo   folderInfo)     {m_folderInfo = folderInfo;               }
+	public void setFolderInfo(    BinderInfo   folderInfo)     {m_folderInfo   = folderInfo;             }
+	public void setSourceFolder(  GwtFolder    sourceFolder)   {m_sourceFolder = sourceFolder;           }
 	public void addGlobalFilter(  FolderFilter globalFilter)   {getGlobalFilters().add(  globalFilter  );}
 	public void addPersonalFilter(FolderFilter personalFilter) {getPersonalFilters().add(personalFilter);}
 	
