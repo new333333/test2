19ce34613584dcf26d2413f224b6daf66e2965e9
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-20 13:58:18 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Vibe Hudson development.  {119th} 1. Changed the path to the help page launched by the administration console's License page.
    Files:
    M    main/src/org/kablink/teaming/web/util/MiscUtil.java
    M    web/docroot/WEB-INF/jsp/administration/manage_license.jsp

--- trunk/ssf/main/src/org/kablink/teaming/web/util/MiscUtil.java
+++ trunk/ssf/main/src/org/kablink/teaming/web/util/MiscUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -878,6 +878,7 @@ public final class MiscUtil
 	private static final String USER_GUIDE = "user";
 	private static final String ADV_USER_GUIDE = "adv_user";
 	private static final String ADMIN_GUIDE = "admin";
+	private static final String INSTALLATION_GUIDE = "install";
 
 	/**
 	 * Return the url that repairs to the appropriate help documentation.
@@ -932,6 +933,11 @@ public final class MiscUtil
 			{
 				// Get the url to the administration guide.
 				guideComponent = product + "_admin/data/";
+			}
+			else if ( guideName.equalsIgnoreCase( INSTALLATION_GUIDE ) )
+			{
+				// Get the url to the installation guide.
+				guideComponent = product + "_inst/data/";
 			}
 			else
 				guideComponent = null;
