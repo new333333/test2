963a52bd745128131f3e46b2a25b52b31d90aad3
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-01-20 12:55:32 +0000

    Bug:  911567 Reviewer:  Self Comments:  Made the file extensions that can be viewed as HTML extensibile in the installer.
    Files (ssf):
    M    main/src/org/kablink/teaming/ssfs/util/SsfsUtil.java
    M    web/docroot/WEB-INF/classes/config/ssf.properties

--- ssf/main/src/org/kablink/teaming/ssfs/util/SsfsUtil.java
+++ ssf/main/src/org/kablink/teaming/ssfs/util/SsfsUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -31,6 +31,7 @@
  * Kablink logos are trademarks of Novell, Inc.
  */
 package org.kablink.teaming.ssfs.util;
+
 import java.util.Iterator;
 
 import javax.portlet.PortletRequest;
@@ -332,6 +333,15 @@ public class SsfsUtil {
 		for (int i = 0; i < s.length; i += 1) {
 			if (extension.endsWith(s[i].toLowerCase())) {
 				return true;
+			}
+		}
+		
+		if (!(cfm.isOOHtmlConverter())) {
+			s = SPropsUtil.getStringArray((viewAsHtmlKey + ".installer.added"), ",");
+			for (int i = 0; i < s.length; i += 1) {
+				if (extension.endsWith(s[i].toLowerCase())) {
+					return true;
+				}
 			}
 		}
 		
