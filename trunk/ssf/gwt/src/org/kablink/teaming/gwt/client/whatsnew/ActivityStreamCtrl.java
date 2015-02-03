ba696d53b75413b5757f515a0af8ede81e8e5eb5
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-02-03 20:09:26 +0000

    Bug:  914839 Reviewer:  Self Comments:  Fixed the Tag action from an activity stream control to only bring up one instance of the tag dialog.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/whatsnew/ActivityStreamCtrl.java

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/whatsnew/ActivityStreamCtrl.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/whatsnew/ActivityStreamCtrl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -2395,11 +2395,17 @@ public class ActivityStreamCtrl extends ResizeComposite
 	@Override
 	public void onInvokeTag( InvokeTagEvent event )
 	{
+		// Do we have the entry to invoke the tag dialog on?
 		ActivityStreamUIEntry uiEntry = event.getUIEntry();
 		if ( null != uiEntry )
 		{
-			// Invoke the Tag this Entry dialog.
-			invokeTagThisDlg( uiEntry );
+			// Yes!  Is it from this ActivityStreamCtrl?
+			ActivityStreamCtrl asCtrl = uiEntry.getActivityStreamCtrl();
+			if ( ( null != asCtrl ) && ( asCtrl.equals( this ) ))
+			{
+				// Yes!  Invoke the Tag this Entry dialog.
+				invokeTagThisDlg( uiEntry );
+			}
 		}
 	}// end onInvokeTag()
 	
