80ac0d3a98cd0e54a77ad5b9523e2024a20b3a9b
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-02-04 17:31:00 +0000

    Bug:  916168 Reviewer:  Self Comments:  Fixed handling of stack trace in KeyShield SSO configuration error on Safari.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/util/GwtClientHelper.java
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/EditKeyShieldConfigDlg.java
    M    gwt/src/org/kablink/teaming/gwt/public/GwtTeaming.css

--- ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/EditKeyShieldConfigDlg.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/EditKeyShieldConfigDlg.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2009 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2009 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -31,7 +31,6 @@
  * Kablink logos are trademarks of Novell, Inc.
  */
 package org.kablink.teaming.gwt.client.widgets;
-
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -80,11 +79,10 @@ import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.web.bindery.event.shared.HandlerRegistration;
 
-
 /**
- * 
+ * ?
+ *  
  * @author jwootton
- *
  */
 public class EditKeyShieldConfigDlg extends DlgBox
 	implements
@@ -901,6 +899,10 @@ public class EditKeyShieldConfigDlg extends DlgBox
 							m_stackTracePanel = new FlowPanel();
 							m_stackTracePanel.setVisible( false );
 							m_stackTracePanel.addStyleName( "editKeyShieldDlg_StackTracePanel" );
+							if ( GwtClientHelper.jsIsSafari() )
+							{
+								m_stackTracePanel.addStyleName( "maxWidth500" );
+							}
 							shBuilder = new SafeHtmlBuilder().appendEscapedLines( stackTrace );
 							safeHtml = shBuilder.toSafeHtml();
 							label = new Label( safeHtml.asString() );
