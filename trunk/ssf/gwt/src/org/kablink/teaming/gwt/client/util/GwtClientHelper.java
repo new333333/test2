4de66108c464f96a04e10a6e5f81296a1de5cb1c
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-02-04 17:31:00 +0000

    Bug:  916168 Reviewer:  Self Comments:  Fixed handling of stack trace in KeyShield SSO configuration error on Safari.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/util/GwtClientHelper.java
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/EditKeyShieldConfigDlg.java
    M    gwt/src/org/kablink/teaming/gwt/public/GwtTeaming.css

--- trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/util/GwtClientHelper.java
+++ trunk/ssf/gwt/src/org/kablink/teaming/gwt/client/util/GwtClientHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -1794,6 +1794,20 @@ public class GwtClientHelper {
 		var agent = navigator.userAgent.toLowerCase();
 		if (agent.indexOf("like gecko") != (-1) && (agent.indexOf("rv:11.0") != (-1))) {
 			return true;
+		}
+		return false;
+	}-*/;
+	
+	/**
+	 * Returns true if we're running in any flavor of Safari and false
+	 * otherwise.
+	 * 
+	 * @return
+	 */
+	public static native boolean jsIsSafari() /*-{
+		var agent = navigator.userAgent.toLowerCase();
+		if (agent.indexOf("safari") != (-1)) {
+			return (agent.indexOf("chrome") == (-1));
 		}
 		return false;
 	}-*/;
