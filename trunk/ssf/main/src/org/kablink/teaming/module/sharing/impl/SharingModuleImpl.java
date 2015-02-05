3ccdf3958b27a09371e830941627905e10513a12
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-02-05 21:29:40 +0000

    Bug:  916029 Reviewer:  Self Comments:  Allow sharing whitelist/blacklist modifications for anybody that's got zone administration rights.
    Files:
    M    main/src/org/kablink/teaming/module/sharing/impl/SharingModuleImpl.java

--- trunk/ssf/main/src/org/kablink/teaming/module/sharing/impl/SharingModuleImpl.java
+++ trunk/ssf/main/src/org/kablink/teaming/module/sharing/impl/SharingModuleImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -45,6 +45,7 @@ import org.kablink.teaming.domain.ShareItem.RecipientType;
 import org.kablink.teaming.jobs.ExpiredShareHandler;
 import org.kablink.teaming.jobs.ZoneSchedule;
 import org.kablink.teaming.module.admin.AdminModule;
+import org.kablink.teaming.module.admin.AdminModule.AdminOperation;
 import org.kablink.teaming.module.binder.BinderModule;
 import org.kablink.teaming.module.binder.BinderModule.BinderOperation;
 import org.kablink.teaming.module.binder.processor.BinderProcessor;
@@ -1472,8 +1473,12 @@ public class SharingModuleImpl extends CommonDependencyInjection implements Shar
      */
     @Override
 	public void setShareLists(ShareLists shareLists) {
-   		Binder top = RequestContextHolder.getRequestContext().getZone();
-   		getBinderModule().checkAccess(top, BinderOperation.manageConfiguration);
+    	// If the user doesn't have zone admin rights...
+   		if (!(getAdminModule().testAccess(AdminOperation.manageFunction))) {
+   			// ...do they have management rights on the home workspace?
+   	   		Binder top = RequestContextHolder.getRequestContext().getZone();
+   			getBinderModule().checkAccess(top, BinderOperation.manageConfiguration);
+   		}
 		
   		ZoneConfig zoneConfig = getCoreDao().loadZoneConfig(RequestContextHolder.getRequestContext().getZoneId());
   		zoneConfig.setShareLists(shareLists);
