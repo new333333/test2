23e411fda4d454790d7170a4ada5d46e76dfc40e
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-02-09 16:28:22 +0000

    Bug:916204 Reviewer:Jong Comment:Fix up sqlserver sql script so that it can be loaded and executed through JDBC interface

--- ssf/tools/sql/scripts/sql/sqlserver-create-empty-database.sql
+++ ssf/tools/sql/scripts/sql/sqlserver-create-empty-database.sql
@@ -1,8 +1,4 @@
 -- drop database sitescape;
 CREATE DATABASE sitescape COLLATE Latin1_General_CI_AS_KS_WS
 
-ALTER DATABASE sitescape
-
-SET READ_COMMITTED_SNAPSHOT ON
-
-GO
+ALTER DATABASE sitescape SET READ_COMMITTED_SNAPSHOT ON
