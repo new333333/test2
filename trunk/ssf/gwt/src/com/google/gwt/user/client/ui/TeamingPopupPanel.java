4f78817b2e53f28f12813bf28c8b55bd26c1b66b
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-04-08 16:17:10 +0000

    Bug:  PRD Reviewer:  Self Comments:  Changes for Vibe Inverness (4.1) development.  {11th} 1. Code cleanups based on move to GWT v2.7.0.
    Files:
    D    gwt/src/com/google/gwt/user/client/ui/MenuBar.java
    M    gwt/src/com/google/gwt/user/client/ui/TeamingPopupPanel.java
    M    gwt/src/org/kablink/teaming/gwt/client/util/GwtClientHelper.java

--- trunk/ssf/gwt/src/com/google/gwt/user/client/ui/TeamingPopupPanel.java
+++ trunk/ssf/gwt/src/com/google/gwt/user/client/ui/TeamingPopupPanel.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2013 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2013 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -40,7 +40,7 @@ import com.google.gwt.user.client.ui.PopupPanel;
 
 /**
  * Implements an extension of GWT's PopupPanel so that Vibe can
- * expose the roll down animation feature from it.
+ * access features in PopupPanel that aren't otherwise exposed.
  *  
  * @author drfoster@novell.com
  */
@@ -63,20 +63,6 @@ public class TeamingPopupPanel extends PopupPanel {
 		this(autoHide, false);
 	}
 	
-	/**
-	 * Sets the popup panel's animation type to roll down.
-	 */
-	public void setAnimationTypeToRollDown() {
-		setAnimationType(PopupPanel.AnimationType.ROLL_DOWN);
-	}
-
-	/**
-	 * Sets the popup panel's animation type to one way corner.
-	 */
-	public void setAnimationTypeToOneWayCorner() {
-		setAnimationType(PopupPanel.AnimationType.ONE_WAY_CORNER);
-	}
-
 	/*
 	 * Overrides PopupPanel.onPreviewNativeEvent() to address an issue
 	 * with PopupPanel's in FF closing with auto hide if the user
