692e9cfdcc0fb417207b0db43e4ca6be9aa89e27
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-04-30 16:51:36 +0000

    Bug:  929249 Reviewer:  Self Comments:  Don't delete the mirrored source of a top level Vibe mirrored folder.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtDeleteHelper.java
    M    main/src/org/kablink/teaming/web/util/BinderHelper.java

--- branches/4.0.1/ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
+++ branches/4.0.1/ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -74,10 +74,12 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.document.DateTools;
+
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+
 import org.kablink.teaming.NoObjectByTheIdException;
 import org.kablink.teaming.NotSupportedException;
 import org.kablink.teaming.ObjectKeys;
@@ -167,6 +169,7 @@ import org.kablink.util.Validator;
 import org.kablink.util.search.Constants;
 import org.kablink.util.search.Criteria;
 import org.kablink.util.search.Restrictions;
+
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.web.multipart.MultipartFile;
@@ -5878,5 +5881,21 @@ public class BinderHelper {
      */
     public static String getDefaultViewDisplayStyle() {
     	return DEFAULT_VDS;
+    }
+    
+    /**
+     * Returns true if the given binder is a Vibe mirrored folder and
+     * false otherwise.
+     * 
+     * @param binder
+     * 
+     * @return
+     */
+    public static boolean isVibeMirroredFolder(Binder binder) {
+    	return (
+    		(null != binder)           &&
+    		(binder instanceof Folder) &&
+    		binder.isMirrored()        &&
+    		(!(binder.isAclExternallyControlled())));
     }
 }
