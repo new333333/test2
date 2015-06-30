470562e6dbca71d97a5f9d183e62c89377357577
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-06-30 15:29:50 +0000

    Bug:  934273,935485 Reviewer:  Self Comments:  Ported fixes for these bugs from Vibe 4.0.1 to the Vibe Hudson FTF.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/RequestInfo.java
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/MainMenuControl.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtProfileHelper.java
    M    main/src/org/kablink/teaming/module/workflow/impl/WorkflowModuleImpl.java
    M    main/src/org/kablink/teaming/web/util/GwtUIHelper.java
    M    web/docroot/WEB-INF/jsp/common/GwtRequestInfo.jsp

--- branches/hudson/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/MainMenuControl.java
+++ branches/hudson/ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/MainMenuControl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -502,16 +502,20 @@ public class MainMenuControl extends Composite
 	 * bar.
 	 */
 	private void addMyWorkspaceToCommon(MenuBar menuPanel) {
-		m_myWorkspaceBox = new MenuBarBox(MenuIds.MAIN_MY_WORKSPACE, m_images.home16(), m_messages.mainMenuBarMyWorkspace());
-		m_myWorkspaceBox.setScheduledCommand(
-			new ScheduledCommand() {
-				@Override
-				public void execute() {
-					GotoMyWorkspaceEvent.fireOneAsync();
-				}
-			});
-		menuPanel.addItem(m_myWorkspaceBox);
-		MenuLoadedEvent.fireOneAsync(MenuItem.MY_WORKSPACE);
+		// Can the logged in user access their own personal workspace?
+		if (GwtClientHelper.getRequestInfo().canAccessOwnWorkspace()) {
+			// Yes!  Add the 'My Workspace' menu item.
+			m_myWorkspaceBox = new MenuBarBox(MenuIds.MAIN_MY_WORKSPACE, m_images.home16(), m_messages.mainMenuBarMyWorkspace());
+			m_myWorkspaceBox.setScheduledCommand(
+				new ScheduledCommand() {
+					@Override
+					public void execute() {
+						GotoMyWorkspaceEvent.fireOneAsync();
+					}
+				});
+			menuPanel.addItem(m_myWorkspaceBox);
+			MenuLoadedEvent.fireOneAsync(MenuItem.MY_WORKSPACE);
+		}
 	}
 
 	/*
--- branches/hudson/ssf/web/docroot/WEB-INF/jsp/common/GwtRequestInfo.jsp
+++ branches/hudson/ssf/web/docroot/WEB-INF/jsp/common/GwtRequestInfo.jsp
@@ -1,6 +1,6 @@
 <%
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -16,10 +16,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -61,6 +61,7 @@
 		allowSharePoint2013AsAServerType:	'${allowSharePoint2013AsAServerType}',
 		allowSharePoint2010AsAServerType:	'${allowSharePoint2010AsAServerType}',
 		allowShowPeople:					'${allowShowPeople}',
+		canAccessOwnWorkspace:				'${canAccessOwnWorkspace}',
 		showSyncOnlyDirStructureUI:			'${showSyncOnlyDirStructureUI}',
 		baseVibeUrl:						'<ssf:escapeJavaScript><ssf:url></ssf:url></ssf:escapeJavaScript>',
 		canSeeOtherUsers:					'<ssf:escapeJavaScript>${canSeeOtherUsers}</ssf:escapeJavaScript>',
