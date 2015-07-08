72ea9b01c4705f4e42f8b529d7761c1aeb6218ef
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-07-08 16:43:34 +0000

    Bug:  929716 Reviewer:  Self Comments:  Ported fix from 4.0.1 to the Vibe Hudson FTF code base.
    Files:
    M    main/src/org/kablink/teaming/web/util/BinderHelper.java
    M    main/src/org/kablink/teaming/web/util/ListFolderHelper.java
    M    web/docroot/js/common/ss_common.js

--- branches/hudson/ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
+++ branches/hudson/ssf/main/src/org/kablink/teaming/web/util/BinderHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -74,10 +74,12 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.document.DateTools;
+
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+
 import org.kablink.teaming.NoObjectByTheIdException;
 import org.kablink.teaming.NotSupportedException;
 import org.kablink.teaming.ObjectKeys;
@@ -167,6 +169,7 @@ import org.kablink.util.Validator;
 import org.kablink.util.search.Constants;
 import org.kablink.util.search.Criteria;
 import org.kablink.util.search.Restrictions;
+
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.web.multipart.MultipartFile;
@@ -2825,7 +2828,7 @@ public class BinderHelper {
     }
 
 	public static Tabs.TabEntry initTabs(Tabs tabs, Binder binder, boolean clearData) throws Exception {
-		return tabs.findTab(binder, true);
+		return tabs.findTab(binder, clearData);
 	}
 
 	public static Tabs.TabEntry initTabs(PortletRequest request, Binder binder) throws Exception {
--- branches/hudson/ssf/web/docroot/js/common/ss_common.js
+++ branches/hudson/ssf/web/docroot/js/common/ss_common.js
@@ -9957,6 +9957,16 @@ function ss_ensureAnchorsTargetTopFrame() {
 				continue;
 			}
 			
+			// If it's a page navigation URL...
+			if (0 < href.indexOf("operation=save_folder_page_info")) {
+				// ...skip it.
+				//
+				// DRF (20150706):  This is a special case to address
+				//    bug#929716.  Without this fix, paging through a
+				//    photo album did not work.
+				continue;
+			}
+			
 			// Add a 'target="_top"' to it.
 			patched += 1;
 			anchor.setAttribute("target", "_top");
