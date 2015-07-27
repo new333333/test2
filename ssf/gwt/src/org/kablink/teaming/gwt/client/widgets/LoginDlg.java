f3653c175a34e3ca7d9e89735ef951fd9d384e62
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-07-27 14:59:32 +0000

    Bug:  939116, 939111, 939010 Reviewer:  Self Comments:  Ported fixed from Vibe 4.0.1 into the Vibe Hudson FTF code base.
    Files:
    M    gwt/src/org/kablink/teaming/gwt/client/widgets/LoginDlg.java
    M    gwt/src/org/kablink/teaming/gwt/server/util/GwtServerHelper.java
    M    main/src/org/kablink/teaming/module/definition/impl/DefinitionModuleImpl.java
    M    main/src/org/kablink/teaming/smtp/impl/SMTPManagerImpl.java
    M    web/docroot/WEB-INF/context/applicationContext.xml

--- ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/LoginDlg.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/client/widgets/LoginDlg.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -121,6 +121,7 @@ import com.google.web.bindery.event.shared.HandlerRegistration;
 @SuppressWarnings("unused")
 public class LoginDlg extends DlgBox
 	implements
+		// Event handlers implemented by this class.
 		DialogClosedEvent.Handler,
 		WindowTitleSetEvent.Handler
 {
@@ -1228,6 +1229,8 @@ public class LoginDlg extends DlgBox
 							@Override
 							public void execute()
 							{
+								clearErrorPanel();
+								hideErrorPanel();
 								showExternalUserPasswordResetRequestedUI();
 							}
 						} );
