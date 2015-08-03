b33b84eb6d74fdde24dfd229605ea435eba31368
drfoster <drfoster@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2015-08-03 18:33:32 +0000

    Bug:  940292 Reviewer:  Self Comments:  Fixed spurious navigation to a calendar folder when loading a complex landing page with an enhanced calendar view.
    Files:
    M    web/docroot/js/common/ss_calendar.js

--- trunk/ssf/web/docroot/js/common/ss_calendar.js
+++ trunk/ssf/web/docroot/js/common/ss_calendar.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1998-2014 Novell, Inc. and its licensors. All rights reserved.
+ * Copyright (c) 1998-2015 Novell, Inc. and its licensors. All rights reserved.
  * 
  * This work is governed by the Common Public Attribution License Version 1.0 (the
  * "CPAL"); you may not use this file except in compliance with the CPAL. You may
@@ -15,10 +15,10 @@
  * 
  * The Original Code is ICEcore, now called Kablink. The Original Developer is
  * Novell, Inc. All portions of the code written by Novell, Inc. are Copyright
- * (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * 
  * Attribution Information:
- * Attribution Copyright Notice: Copyright (c) 1998-2014 Novell, Inc. All Rights Reserved.
+ * Attribution Copyright Notice: Copyright (c) 1998-2015 Novell, Inc. All Rights Reserved.
  * Attribution Phrase (not exceeding 10 words): [Powered by Kablink]
  * Attribution URL: [www.kablink.org]
  * Graphic Image as provided in the Covered Code
@@ -30,7 +30,6 @@
  * NOVELL and the Novell logo are registered trademarks and Kablink and the
  * Kablink logos are trademarks of Novell, Inc.
  */
-
 
 dojo.require("dojo.date");
 dojo.require("dojo.date.locale");
@@ -312,9 +311,15 @@ function ss_calendarEngine(
 	}
 		
 	this.createTemplate = function() {
-		var container = dojo.byId(containerId) || document.body;
-		container.innerHTML = that.getTemplateHTML();
-		templateInitialized = true;
+		// Bugzilla 940292:
+		//    If we can't find the container, DON'T default to the
+		//    document body as that ends up replacing the entire
+		//    window with the content of the calendar.
+		var container = dojo.byId(containerId);	// ...was || document.body;
+		if (container) {
+			container.innerHTML = that.getTemplateHTML();
+			templateInitialized = true;
+		}
 	}
 	
 	this.removeEntryEvents = function(entryId) {
@@ -449,6 +454,9 @@ function ss_calendarEngine(
 	this.addEvents = function(eventsData, date) {
 		if (!templateInitialized) {
 			that.createTemplate();
+			if (!templateInitialized) {
+				return;
+			}
 		}
 		var today = new Date();
 		if (eventsData.today) {
