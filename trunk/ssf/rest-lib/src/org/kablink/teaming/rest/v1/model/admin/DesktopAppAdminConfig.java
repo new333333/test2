9c326f1fa783a87195ad2a81354b5565e7cd6a58
davidlewis77 <davidlewis77@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2016-01-27 21:34:38 +0000

    Bug: PRD Reviewer: David Comment: REST documentation changes.

---
+++ trunk/ssf/rest-lib/src/org/kablink/teaming/rest/v1/model/admin/DesktopAppAdminConfig.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright © 2009-2010 Novell, Inc.  All Rights Reserved.
+ *
+ * THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND TREATIES.  IT MAY NOT BE USED, COPIED,
+ * DISTRIBUTED, DISCLOSED, ADAPTED, PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT NOVELL'S
+ * PRIOR WRITTEN CONSENT.  USE OR EXPLOITATION OF THIS WORK WITHOUT AUTHORIZATION COULD SUBJECT THE
+ * PERPETRATOR TO CRIMINAL AND CIVIL LIABILITY.
+ *
+ * NOVELL PROVIDES THE WORK "AS IS," WITHOUT ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING WITHOUT THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. NOVELL, THE
+ * AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT LIABLE FOR ANY CLAIM, DAMAGES,
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN
+ * CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+ */
+package org.kablink.teaming.rest.v1.model.admin;
+
+import org.kablink.teaming.rest.v1.annotations.Undocumented;
+import org.kablink.teaming.rest.v1.model.BaseRestObject;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Data type representing the desktop application system settings.
+ */
+@XmlRootElement(name="admin_desktop_app_config")
+public class DesktopAppAdminConfig extends BaseRestObject {
+    private Boolean enabled;
+    private Boolean allowCachedPassword;
+    private Integer syncFrequencyInMinutes;
+    private Integer maxSyncSizeInMBs;
+    private DesktopProcessConfig processConfig;
+
+    public DesktopAppAdminConfig() {
+    }
+
+    /**
+     * Controls whether users can use the desktop application to browse the system.
+     */
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /**
+     * Controls whether the desktop application should store the users password.
+     * <p>If true, the desktop application will give the user the option to remember his or her password</p>
+     * <p>If false, the desktop application will require the user to log in each time the application starts.</p>
+     */
+    @XmlElement(name="allow_cached_password")
+    public Boolean getAllowCachedPassword() {
+        return allowCachedPassword;
+    }
+
+    public void setAllowCachedPassword(Boolean allowCachedPassword) {
+        this.allowCachedPassword = allowCachedPassword;
+    }
+
+    /**
+     * Maximum size of file that the desktop application will sync to the desktop.
+     */
+    @XmlElement(name="max_file_size_mbs")
+    public Integer getMaxSyncSizeInMBs() {
+        return maxSyncSizeInMBs;
+    }
+
+    public void setMaxSyncSizeInMBs(Integer maxSyncSizeInMBs) {
+        this.maxSyncSizeInMBs = maxSyncSizeInMBs;
+    }
+
+    /**
+     * Controls how often the desktop application will check with the server for updated configuration and new files.
+     */
+    @XmlElement(name="sync_interval_mins")
+    public Integer getSyncFrequencyInMinutes() {
+        return syncFrequencyInMinutes;
+    }
+
+    public void setSyncFrequencyInMinutes(Integer syncFrequencyInMinutes) {
+        this.syncFrequencyInMinutes = syncFrequencyInMinutes;
+    }
+
+    @XmlElement(name="process_config")
+    @Undocumented
+    public DesktopProcessConfig getProcessConfig() {
+        return processConfig;
+    }
+
+    public void setProcessConfig(DesktopProcessConfig processConfig) {
+        this.processConfig = processConfig;
+    }
+}
---
+++ trunk/ssf/rest-lib/src/org/kablink/teaming/rest/v1/model/admin/LdapHomeDirConfig.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright © 2009-2010 Novell, Inc.  All Rights Reserved.
+ *
+ * THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND TREATIES.  IT MAY NOT BE USED, COPIED,
+ * DISTRIBUTED, DISCLOSED, ADAPTED, PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT NOVELL'S
+ * PRIOR WRITTEN CONSENT.  USE OR EXPLOITATION OF THIS WORK WITHOUT AUTHORIZATION COULD SUBJECT THE
+ * PERPETRATOR TO CRIMINAL AND CIVIL LIABILITY.
+ *
+ * NOVELL PROVIDES THE WORK "AS IS," WITHOUT ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING WITHOUT THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. NOVELL, THE
+ * AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT LIABLE FOR ANY CLAIM, DAMAGES,
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN
+ * CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+ */
+
+package org.kablink.teaming.rest.v1.model.admin;
+
+import com.webcohesion.enunciate.metadata.DocumentationExample;
+import com.webcohesion.enunciate.metadata.rs.TypeHint;
+import org.kablink.teaming.rest.v1.model.LongIdLinkPair;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Data type that configures how home directory net folders are created for users that are imported from LDAP.
+ */
+@XmlRootElement(name="home_dir_config")
+public class LdapHomeDirConfig {
+    public static final String TYPE_HOME_DIR_ATTRIBUTE = "home_dir_attribute";
+    public static final String TYPE_CUSTOM_NET_FOLDER = "custom_net_folder";
+    public static final String TYPE_CUSTOM_ATTRIBUTE = "custom_attribute";
+    public static final String TYPE_NONE = "none";
+
+    private String option;
+    // The following data members are relevant when the creation option is "formula"
+    private LongIdLinkPair netFolderServer;
+    private String path;
+    // The following data members are relevant when the creation option is "custom attribute"
+    private String ldapAttribute;
+
+    /**
+     * One of <code>home_dir_attribute</code>,<code>custom_attribute</code>,<code>custom_net_folder</code>,<code>none</code>
+     * <p>
+     *     <ul>
+     *         <li><code>home_dir_attribute</code>: The LDAP home directory attribute contains the location of the user's home directory.  No other fields are necessary.</li>
+     *         <li><code>custom_attribute</code>: A custom LDAP attribute contains the location of the user's home directory.  The attribute is defined in the <code>ldap_attribute</code> field.</li>
+     *         <li><code>custom_net_folder</code>: User home directories can be found in a custom location defined by <code>net_folder_server</code> and <code>path</code>.</li>
+     *         <li><code>none</code>: Home directory net folders are not created.</li>
+     *     </ul>
+     * </p>
+     */
+    @XmlElement(name="type")
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    /**
+     * When <code>type</code> is <code>custom_net_folder</code>, this defines the Net Folder Server that hosts the user home directory net folders.
+     */
+    @XmlElement(name="net_folder_server")
+    public LongIdLinkPair getNetFolderServer() {
+        return netFolderServer;
+    }
+
+    public void setNetFolderServer(LongIdLinkPair netFolderServer) {
+        this.netFolderServer = netFolderServer;
+    }
+
+    /**
+     * When <code>type</code> is <code>custom_net_folder</code>, this defines the path on the Net Folder Server where the user home directory net folders reside.
+     */
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * When <code>type</code> is <code>custom_attribute</code>, this defines the LDAP attribute that contains the location of the user home directory..
+     */
+    @XmlElement(name="ldap_attribute")
+    public String getLdapAttribute() {
+        return ldapAttribute;
+    }
+
+    public void setLdapAttribute(String ldapAttribute) {
+        this.ldapAttribute = ldapAttribute;
+    }
+}
