ab754f76ab3e4b74fc790777337f345175231517
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2016-03-17 12:30:30 +0000

    Bug:PRD Reviewer:Jong Comment:Make database schema changes to add another column for enabling/disabling the use of terms and conditions for external user registration.

--- trunk/ssf/main/src/org/kablink/teaming/domain/ZoneConfig.java
+++ trunk/ssf/main/src/org/kablink/teaming/domain/ZoneConfig.java
@@ -110,6 +110,7 @@ public class ZoneConfig extends ZonedObject implements WorkArea {
 	private Boolean telemetryEnabled;
 	private Boolean telemetryTier2Enabled;
 	private String extUserTermsAndConditions;
+	private Boolean extUserTermsAndConditionsOn;
 
 	public ZoneConfig()
 	{
@@ -441,13 +442,6 @@ public void setExtFunctionMembershipInherited(boolean extFunctionMembershipInher
 			(null == desktopApplicationsLists) ?
 				null                           :
 				desktopApplicationsLists.getFsaApplicationsBlob());
-	}
-	
-	public String getExtUserTermsAndConditions() {
-		return extUserTermsAndConditions;
-	}
-	public void setExtUserTermsAndConditions(String extUserTermsAndConditions) {
-		this.extUserTermsAndConditions = extUserTermsAndConditions;
 	}
 	
 	/**
@@ -864,5 +858,23 @@ public void setExtFunctionMembershipInherited(boolean extFunctionMembershipInher
 	}
 	public void setTelemetryTier2Enabled(boolean telemetryOptinEnabled) {
 		this.telemetryTier2Enabled = Boolean.valueOf(telemetryOptinEnabled);
+	}
+	
+	public String getExtUserTermsAndConditions() {
+		return extUserTermsAndConditions;
+	}
+	public void setExtUserTermsAndConditions(String extUserTermsAndConditions) {
+		this.extUserTermsAndConditions = extUserTermsAndConditions;
+	}
+	
+	public boolean isExtUserTermsAndConditionsEnabled() {
+		if(extUserTermsAndConditionsOn == null)
+			return false; // off by default
+		else
+			return extUserTermsAndConditionsOn.booleanValue();
+	}
+	
+	public void setExtUserTermsAndConditionsEnabled(boolean on) {
+		this.extUserTermsAndConditionsOn = Boolean.valueOf(on);
 	}
 }
