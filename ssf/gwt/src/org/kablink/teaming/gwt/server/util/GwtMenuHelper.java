6341f4f30778df6c0ffd38a5b5e420dde8c6e463
loreddy <loreddy@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2016-06-13 16:30:07 +0000

    Edit In Place Applet functionality.

--- ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtMenuHelper.java
+++ ssf/gwt/src/org/kablink/teaming/gwt/server/util/GwtMenuHelper.java
@@ -273,10 +273,10 @@ public class GwtMenuHelper {
 	 */
 	private static ToolbarItem buildEditInPlaceToolbarItem(AllModulesInjected bs, HttpServletRequest request, FolderEntry fe, boolean isFilr) {
 		// If NPAPI isn't supported on the browser...
-		if (!(MiscUtil.isNPAPISupported(request))) {
+		/*if (!(MiscUtil.isNPAPISupported(request))) {
 			// ...there can be no applets and hence no edit-in-place.
 			return null;
-		}
+		}*/
 		
 		// Is the entry unlock and does the user have rights to
 		// modify it?
