b54db06d4ec4422aed33180b8e27c9c91b8e3650
jongkim <jongkim@56c73aa1-8ba3-204a-a85c-99fbb776543b>
2017-12-20 22:11:05 +0000

    Bug:1002792 Reviewer:Jong Comment:Address performance problem raised when user is member of large number of groups; Avoid computing expensive group membership during XSS check when it's not actually needed; Change group membership computation login; Avoid adding root acl when unnecessary.

--- branches/Vibe-4.0.3/ssf/main/src/org/kablink/teaming/domain/IPrincipal.java
+++ branches/Vibe-4.0.3/ssf/main/src/org/kablink/teaming/domain/IPrincipal.java
@@ -91,8 +91,6 @@ public interface IPrincipal extends PersistentLongId {
     public String getForeignName();
 
     public void setForeignName(String foreignName);
-     
-    public Set computePrincipalIds(GroupPrincipal reservedGroup);
     
     public List getMemberOf();
 
